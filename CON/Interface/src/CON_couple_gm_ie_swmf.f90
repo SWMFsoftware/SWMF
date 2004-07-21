5b42f94bd2840770837938ea8f2258ecdd267084
gtoth <>
2004-07-21 15:37:24 +0000

    i_proc --> i_proc()
    Former-commit-id: 7b4c4acee663423b431e99612321a7e259ff5fbf

--- CON/Interface/src/CON_couple_gm_ie_swmf.f90
+++ CON/Interface/src/CON_couple_gm_ie_swmf.f90
@@ -201,7 +201,7 @@ contains
             mapping=sort_hemispheres)
 !\end{verbatim}
 !EOP
-       if(DoTest)write(*,*)NameSub//' Router is done at PE=',i_proc
+       if(DoTest)write(*,*)NameSub//' Router is done at PE=',i_proc()
        !Allocate arrays in IE_ to get data
        nPoint_B=0
        do iPE=0,RouterGmIe%nProc-1
