5b81a1b8886c1729cfdd4c1a63e791f502cede23
darrens <>
2004-07-28 16:10:41 +0000

    Added empty subroutines so that linking with Empty does not give errors.
    Former-commit-id: 968c59b543513f046fcf82972671ee262887f6fd

--- SP/Empty/src/SP_wrapper.f90
+++ SP/Empty/src/SP_wrapper.f90
@@ -1,3 +1,23 @@
 subroutine SP_run
   implicit none
 end subroutine SP_run
+subroutine write_ihdata
+  implicit none
+end subroutine write_ihdata
+subroutine sp_smooth_ihdata
+  implicit none
+end subroutine sp_smooth_ihdata
+subroutine sp_set_ihdata(nPointIn,XyzIn_DI)
+  implicit none
+  integer,intent(in)::nPointIn
+  real,intent(in)::XyzIn_DI
+end subroutine sp_set_ihdata
+subroutine SP_put_from_ih(nPartial,iPutStart,Put,W,DoAdd,Buff_I,nVar)
+  use CON_coupler, ONLY: IndexPtrType, WeightPtrType
+  implicit none
+  integer,intent(in)::nPartial,iPutStart,nVar
+  type(IndexPtrType),intent(in)::Put
+  type(WeightPtrType),intent(in)::W
+  logical,intent(in)::DoAdd
+  real,dimension(nVar),intent(in)::Buff_I
+end subroutine SP_put_from_ih
