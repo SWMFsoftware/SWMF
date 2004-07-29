2a5afce37e8714873d6aec830d24635f63d15e17
volov <>
2004-07-29 22:18:08 +0000

    couple_mpi was eliminated. CON_couple_gm_rb.f90 was added to Makefile.
    Former-commit-id: c250dd4688987a7516b196a9807cb79545ad4d91

--- CON/Interface/src/CON_couple_gm_rb.f90
+++ CON/Interface/src/CON_couple_gm_rb.f90
@@ -58,9 +58,9 @@ contains
 
   !BOP =======================================================================
   !IROUTINE: couple_gm_rb - couple GM to RB component
-  !INTERFACE:
+  !INTERFACE: couple_gm_rb(tSimulation)
   subroutine couple_gm_rb(tSimulation)
-    
+
     !INPUT ARGUMENTS:
     real, intent(in) :: tSimulation     ! simulation time at coupling
 
@@ -74,111 +74,102 @@ contains
     !EOP
 
     !\
-    ! General coupling variables
+    ! Coupling variables
     !/
 
-    ! Name of this interface
+    ! Number of variables to pass
+    integer, parameter :: nVarGmRb=6
+
+    ! Names of variables to pass
+    character (len=*), parameter :: NameVar='vol:z0x:z0y:bmin:rho:p'
+
+    ! Buffer for the variables on the 2D RB grid
+    real, dimension(:,:,:), allocatable :: Buffer_IIV
+
+    ! MPI related variables
+
+    ! MPI status variable
+    integer :: iStatus_I(MPI_STATUS_SIZE)
+
+    ! General error code
+    integer :: iError
+
+    ! Message size
+    integer :: nSize
+
+    ! Name of this method
     character (len=*), parameter :: NameSub='couple_gm_rb'
 
     logical :: DoTest, DoTestMe
-    integer :: iProcWorld,iError
+    integer :: iProcWorld
+
     !-------------------------------------------------------------------------
     call CON_set_do_test(NameSub,DoTest,DoTestMe)
 
     iProcWorld = i_proc()
-    call couple_mpi
+
+    ! After everything is initialized exclude PEs which are not involved
+    if(.not.UseMe) RETURN
+
+    if(DoTest)write(*,*)NameSub,' starting, iProc=',iProcWorld
+    if(DoTest)write(*,*)NameSub,', iProc, GMi_iProc0, i_proc0(RB_)=', &
+         iProcWorld,i_proc0(GM_),i_proc0(RB_)
+
+    !\
+    ! Allocate buffers both in GM and RB
+    !/
+    allocate(Buffer_IIV(iSize,jSize,nVarGmRb), stat=iError)
+    call check_allocate(iError,NameSub//": Buffer_IIV")
+
+    if(DoTest)write(*,*)NameSub,', variables allocated',&
+         ', iProc:',iProcWorld
+
+    !\
+    ! Get field line integrals from GM
+    !/
+    if(is_proc(GM_)) &
+         call GM_get_for_rb(Buffer_IIV,iSize,jSize,nVarGmRb,NameVar)
+
+    !\
+    ! Transfer variables from GM to RB
+    !/
+    if(i_proc0(RB_) /= i_proc0(GM_))then
+       nSize = iSize*jSize*nVarGmRb
+       if(is_proc0(GM_)) &
+            call MPI_send(Buffer_IIV,nSize,MPI_REAL,i_proc0(RB_),&
+            1,i_comm(),iError)
+       if(is_proc0(RB_)) &
+            call MPI_recv(Buffer_IIV,nSize,MPI_REAL,i_proc0(GM_),&
+            1,i_comm(),iStatus_I,iError)
+    end if
+
+    if(DoTest)write(*,*)NameSub,', variables transferred',&
+         ', iProc:',iProcWorld
+
+    !\
+    ! Put variables into RB
+    !/
+    if(is_proc0(RB_))then
+       call RB_put_from_gm(Buffer_IIV,iSize,jSize,nVarGmRb,NameVar)
+       if(DoTest) &
+            write(*,*)'RB got from GM: RB iProc, Buffer(1,1)=',&
+            iProcWorld,Buffer_IIV(1,1,:)
+    end if
+
+    !\
+    ! Deallocate buffer to save memory
+    !/
+    deallocate(Buffer_IIV)
+
+    if(DoTest)write(*,*)NameSub,', variables deallocated',&
+         ', iProc:',iProcWorld
+
+    if(DoTest)write(*,*)NameSub,' finished, iProc=',iProcWorld
+
 
     if(DoTest)write(*,*)NameSub,': finished iProc=',iProcWorld
 
     ! if(DoTest.and.is_proc0(RB_)) call RB_print_variables('GM',iError)
-
-  contains
-
-    !==========================================================================
-    subroutine couple_mpi
-
-      character (len=*), parameter :: NameSubSub=NameSub//'.couple_mpi'
-
-      ! Number of variables to pass
-      integer, parameter :: nVarGmRb=6
-
-      character (len=*), parameter :: NameVar='vol:z0x:z0y:bmin:rho:p'
-
-      ! Buffer for the variables on the 2D RB grid
-      real, dimension(:,:,:), allocatable :: Buffer_IIV
-
-      ! MPI related variables
-
-      ! MPI status variable
-      integer :: iStatus_I(MPI_STATUS_SIZE)
-
-      ! General error code
-      integer :: iError
-
-      ! Message size
-      integer :: nSize
-
-      !------------------------------------------------------------------------
-
-      ! After everything is initialized exclude PEs which are not involved
-      if(.not.UseMe) RETURN
-
-      if(DoTest)write(*,*)NameSubSub,' starting, iProc=',iProcWorld
-      if(DoTest)write(*,*)NameSubSub,', iProc, GMi_iProc0, i_proc0(RB_)=', &
-           iProcWorld,i_proc0(GM_),i_proc0(RB_)
-
-      !\
-      ! Allocate buffers both in GM and RB
-      !/
-      allocate(Buffer_IIV(iSize,jSize,nVarGmRb), stat=iError)
-      call check_allocate(iError,NameSubSub//": Buffer_IIV")
-
-      if(DoTest)write(*,*)NameSubSub,', variables allocated',&
-           ', iProc:',iProcWorld
-
-      !\
-      ! Get field line integrals from GM
-      !/
-      if(is_proc(GM_)) &
-           call GM_get_for_rb(Buffer_IIV,iSize,jSize,nVarGmRb,NameVar)
-
-      !\
-      ! Transfer variables from GM to RB
-      !/
-      if(i_proc0(RB_) /= i_proc0(GM_))then
-         nSize = iSize*jSize*nVarGmRb
-         if(is_proc0(GM_)) &
-              call MPI_send(Buffer_IIV,nSize,MPI_REAL,i_proc0(RB_),&
-              1,i_comm(),iError)
-         if(is_proc0(RB_)) &
-              call MPI_recv(Buffer_IIV,nSize,MPI_REAL,i_proc0(GM_),&
-              1,i_comm(),iStatus_I,iError)
-      end if
-
-      if(DoTest)write(*,*)NameSubSub,', variables transferred',&
-           ', iProc:',iProcWorld
-
-      !\
-      ! Put variables into RB
-      !/
-      if(is_proc0(RB_))then
-         call RB_put_from_gm(Buffer_IIV,iSize,jSize,nVarGmRb,NameVar)
-         if(DoTest) &
-              write(*,*)'RB got from GM: RB iProc, Buffer(1,1)=',&
-              iProcWorld,Buffer_IIV(1,1,:)
-      end if
-
-      !\
-      ! Deallocate buffer to save memory
-      !/
-      deallocate(Buffer_IIV)
-
-      if(DoTest)write(*,*)NameSubSub,', variables deallocated',&
-           ', iProc:',iProcWorld
-
-      if(DoTest)write(*,*)NameSubSub,' finished, iProc=',iProcWorld
-
-    end subroutine couple_mpi
 
   end subroutine couple_gm_rb
 
