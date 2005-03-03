0cf8b36dd357a822f383db9aa21c497302a390d2
darrens <>
2005-03-03 18:32:08 +0000

    Changed to only modify Root and Repository files.
    Former-commit-id: feaa19e107d11d5fe8a3463eccf238a3027e1ead

--- share/Scripts/FixCVSRoot.sh
+++ share/Scripts/FixCVSRoot.sh
@@ -3,22 +3,29 @@
 # FixCVSRoot.sh: This script will replace all occurrences of /p/volov/DEVTREE
 #   with /FRAMEWORK in all files found below the current directory.
 
-echo 'Replacing /p/volov/DEVTREE with /FRAMEWORK in '$PWD
+echo Replacing /p/volov/DEVTREE with /FRAMEWORK in $PWD
 
-FILES=`find . -type f`
+FILES=`find . -type f -name Root`
 
 for file in $FILES
 do
   if grep -q '/p/volov/DEVTREE' $file
   then
-    if grep -iq FixCVSRoot $file
-    then
-      echo Skipping $file
-    else
-      echo Fixing $file
-      cat $file | sed s/\\/p\\/volov\\/DEVTREE/\\/FRAMEWORK/ > fixedfile
-      mv -f fixedfile $file
-    fi
+    echo Fixing $file
+    cat $file | sed s/\\/p\\/volov\\/DEVTREE/\\/FRAMEWORK/ > fixedfile
+    mv -f fixedfile $file
+  fi
+done
+
+FILES=`find . -type f -name Repository`
+
+for file in $FILES
+do
+  if grep -q '/p/volov/DEVTREE' $file
+  then
+    echo Fixing $file
+    cat $file | sed s/\\/p\\/volov\\/DEVTREE/\\/FRAMEWORK/ > fixedfile
+    mv -f fixedfile $file
   fi
 done
 
