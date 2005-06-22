06dcbbf9afb2d9717fc81ba6d21507f005913912
gtoth <>
2005-06-22 15:51:04 +0000

    Allow for the relative motion of the IH and GM grids. Redo mapping transformations and router as necessary similar to IH->SC coupler. Added missing 'save' statement. Improved variables names and comments.
    Former-commit-id: e11cce70cf63bd1f0bfec0a0c6d0a598b1e415be

--- CON/Interface/src/CON_couple_ih_gm.f90
+++ CON/Interface/src/CON_couple_ih_gm.f90
@@ -6,33 +6,33 @@
 !INTERFACE:
 module CON_couple_ih_gm
 !DESCRIPTION:
-!This coupler is  based on the global message  pass.
-!The toolkit for coupling the different data sets within a
-!single component code, or within a framework as it is now 
-!
-!The features of the present coupler: the domain decompositions
-!both for a target and for a source are with AMR.
-!IH is a source, GM is a target
+! This coupler uses the SWMF coupling toolkit.
+! Both the IH and GM grids use AMR.
+! IH is a source, GM is a target.
+! It is assumed that IH uses the solar radius rSun as distance unit, while
+! GM uses the Earth's radius as distance unit.
 
-!Typically the mapping is charcterized by some real matrices
-!and/or vectors. In the particular case of the cartesian
-!generalized coordinates this is the position of the origin of
-!the GM frame of reference in the IH frame of refernce  and the
-!matrix which characterizes the turn of the IH frame of
-!reference with respect to GM :
+! Typically the mapping is charcterized by some real matrices
+! and/or vectors. In the particular case of the cartesian
+! generalized coordinates this is the position of the origin of
+! the GM frame of reference in the IH frame of refernce  and the
+! matrix which characterizes the turn of the IH frame of
+! reference with respect to GM :
 !
-!$$
-!{\bf V}_{IH}={\bf A}_{IH,GM}\cdot{\bf V}_{GM},\\
-!{\bf V}_{GM}={\bf A}_{GM,IH}\cdot{\bf V}_{IH}
-!$$
+! $$
+! {\bf V}_{IH}={\bf A}_{IH,GM}\cdot{\bf V}_{GM},\\
+! {\bf V}_{GM}={\bf A}_{GM,IH}\cdot{\bf V}_{IH}
+! $$
   !USES:
   use CON_coupler
   use CON_time,ONLY:TimeStart
   use ModConst
-  use CON_geopack
-  use CON_axes, ONLY: transform_matrix, vPlanetHgi_D
+  use CON_axes, ONLY: transform_matrix, vPlanetHgi_D, XyzPlanetHgi_D
 
   implicit none
+
+  save
+
   private !except
 
   !
@@ -40,32 +40,33 @@ module CON_couple_ih_gm
   public:: couple_ih_gm_init
   public:: couple_ih_gm
 
-  real,public::IH_GM_CouplingTime
+  real, public:: IH_GM_CouplingTime
 
   !REVISION HISTORY:
   ! 7/23/03 Sokolov I.V. <igorsok@umich.edu> - initial prototype
   ! 9/02/03 G.Toth <gtoth@umich.edu> - minor changes
+  ! 6/18/05 Sokolov - generalized to arbitrary coordinate systems
+  ! 6/22/05 G.Toth  - redo mapping and router to allow for relative motion
   !EOP
 
   !To trace the possible changes in the grids and/or mapping
   integer :: IH_iGridRealization=-2
   integer :: GM_iGridRealization=-2
-  integer :: IH_GM_iMapping=-1
 
-  real,dimension(3)   :: IH_EarthPosition_D,vPlanetIh_D
-  real,dimension(3,3) :: IH_a_GM_DD,GM_a_IH_DD
+  real, dimension(3)   :: XyzPlanetIh_D, vPlanetIh_D
+  real, dimension(3,3) :: GmToIh_DD, IhToGm_DD, HgiToIh_DD
 
-  type(RouterType),save         :: Router
-  type(GridDescriptorType),save :: GM_Grid
-  type(GridDescriptorType),save :: IH_Grid
+  ! Maximum time difference [s] without remap
+  ! The 600 s corresponds to about 0.1 degree rotation between IH and GM
+  real :: dTimeMappingMax = 600.0
+
+  type(RouterType)         :: Router
+  type(GridDescriptorType) :: GM_Grid
+  type(GridDescriptorType) :: IH_Grid
 
   logical :: DoInitialize=.true., DoTest, DoTestMe
 
   character(len=*), parameter :: NameMod='CON_couple_ih_gm'
-
-  ! The integer time array for GeoPack, 
-  ! only first 6 elements are used by GeoPack
-  integer :: TimeGeoPack(7)
 
 contains
 
@@ -77,27 +78,6 @@ contains
 
     call CON_set_do_test(NameMod,DoTest,DoTestMe)
     if(DoTest)write(*,*)'couple_ih_gm_init iProc=',i_proc()
-
-    !Initialize the transformation matrices, compute the position 
-    !of the Earth and its orbital velocity in IH, at the time zero  
-
-    IH_a_GM_DD         = transform_matrix(cZero, &
-         Grid_C(GM_) % TypeCoord, Grid_C(IH_) % TypeCoord)
-
-    GM_a_IH_DD         = transpose(IH_a_GM_DD)
-
-    !Compute the Earth position in Hgi and transform 
-    !to IH coordinates
- 
-    IH_EarthPosition_D = -cAU/rSun*SunEMBDistance*matmul(&
-         transform_matrix(cZero, 'HGI', &
-         Grid_C(IH_) % TypeCoord),HgiGse_DD(:,1))
-
-    !Compute the Earth velocity in Hgi and transform 
-    !to IH coordinates 
-
-    vPlanetIh_D = matmul(transform_matrix(cZero,'HGI',&
-            Grid_C(IH_) % TypeCoord),vPlanetHgi_D)
 
     call init_coupler(              &    
        iCompSource=IH_,             & ! component index for source
@@ -114,9 +94,9 @@ contains
 
   end subroutine couple_ih_gm_init
   !===============================================================!
-!BOP
-!IROUTINE: couple_ih_gm - interpolate and get  MHD state at GM west ghostpoints
-!INTERFACE:
+  !BOP
+  !IROUTINE: couple_ih_gm - get IH solution in the GM inflow boundary ghost cells
+  !INTERFACE:
   subroutine couple_ih_gm(TimeCoupling)
     !INPUT ARGUMENTS:
     interface
@@ -137,9 +117,11 @@ contains
        end subroutine GM_put_from_ih
     end interface
     real,intent(in)::TimeCoupling
-!EOP
-    integer::iMappingNew
+    !EOP
 
+    ! Last coupling time
+    real :: TimeCouplingLast = -1.0
+    !-------------------------------------------------------------------------
     call CON_set_do_test(NameMod,DoTest,DoTestMe)
     if(DoTest)write(*,*)'couple_ih_gm iProc=',i_proc()
 
@@ -149,16 +131,34 @@ contains
 
     IH_GM_CouplingTime=TimeCoupling
 
-    call update_mapping_parameters(iMappingNew)
-
     if(IH_iGridRealization/=i_realization(IH_).or.&     
          GM_iGridRealization/=i_realization(GM_).or.&
-         IH_GM_iMapping/=iMappingNew)then  
+         TimeCoupling - TimeCouplingLast > dTimeMappingMax)then  
+
+       ! Set the transformation matrices, compute the position and
+       ! orbital veloctiy of the Earth in IH at the coupling time.
+
+       GmToIh_DD  = transform_matrix(TimeCoupling , &
+            Grid_C(GM_) % TypeCoord, Grid_C(IH_) % TypeCoord)
+       IhToGm_DD  = transpose(GmToIh_DD)
+
+       HgiToIh_DD = transform_matrix(TimeCoupling, &
+            'HGI', Grid_C(IH_) % TypeCoord)
+
+       ! Transform the Earth position in HGI to IH coordinates 
+       ! and change distance units to rSun (used in IH/BATSRUS)
+ 
+       XyzPlanetIh_D = matmul(HgiToIh_DD, XyzPlanetHgi_D) / rSun
+
+       ! Transform the Earth velocity from Hgi to IH coordinates 
+
+       vPlanetIh_D = matmul(HgiToIh_DD, vPlanetHgi_D)
 
        if(DoTest)write(*,*)'couple_ih_gm call set_router iProc=',i_proc()
        
-       if(DoTestMe)write(*,*)'couple_ih_gm_init IH_EarthPosition_D=',&
-            IH_EarthPosition_D
+       if(DoTestMe)write(*,*)'couple_ih_gm_init XyzPlanetIh_D=',&
+            XyzPlanetIh_D
+
        call set_router(&
             GridDescriptorSource=IH_Grid,&
             GridDescriptorTarget=GM_Grid,&
@@ -167,9 +167,10 @@ contains
             interface_point_coords=GM_west_cells, &
             mapping=GM_IH_mapping,&
             interpolate=interpolation_fix_reschange)
-       IH_iGridRealization=i_realization(IH_)
-       GM_iGridRealization=i_realization(GM_)
-       IH_GM_iMapping=iMappingNew      
+
+       IH_iGridRealization = i_realization(IH_)
+       GM_iGridRealization = i_realization(GM_)
+       TimeCouplingLast    = TimeCoupling
     end if
 
     call couple_comp(&
@@ -229,54 +230,10 @@ contains
     real,dimension(IH_nDim),intent(out)::IH_Xyz_D
     logical,intent(out)::IsInterfacePoint
 
-    IH_Xyz_D=IH_EarthPosition_D+&
-         matmul(IH_a_GM_DD,GM_Xyz_D)*REarth/RSun
+    IH_Xyz_D = XyzPlanetIh_D + matmul(GmToIh_DD, GM_Xyz_D)*REarth/RSun
     IsInterfacePoint=.true.
 
   end subroutine GM_IH_Mapping
-
-  !===============================================================!
-
-  subroutine update_mapping_parameters(iMappingNew)
-
-    use ModKind
-    use CON_physics, ONLY: get_time, time_real_to_int
-    use CON_geopack
-    ! The time at the beginning of the simulation
-    real(Real8_), save :: tStart
-    integer,intent(out)::iMappingNew
-
-    ! For sake of simplicity the initial conversion matrix is used.
-    iMappingNew=1
-    RETURN
-
-    !Initialize the transformation matrices, compute the position 
-    !of the Earth and its orbital velocity in IH, at the coupling time  
-
-    !This matrix is needed to map the points of the target (GM)
-    !to the source grid (IH), for interpolation
-    IH_a_GM_DD         = transform_matrix(IH_GM_CouplingTime, &
-            Grid_C(GM_) % TypeCoord, Grid_C(IH_) % TypeCoord)
-
-    !The transposed matrix is needed to convert vectors to be sent
-    !from the source (IH) to target (GM)
-
-    GM_a_IH_DD         = transpose(IH_a_GM_DD)
-
-    !Compute the Earth position in Hgi and transform 
-    !to IH coordinates
-
-    IH_EarthPosition_D = -cAU/rSun*SunEMBDistance*&
-         matmul(transform_matrix(IH_GM_CouplingTime,'HGI',&
-         Grid_C(IH_) % TypeCoord),HgiGse_DD(:,1))
-
-    !Compute the Earth velocity in Hgi and transform 
-    !to IH coordinates
-
-    vPlanetIh_D = matmul(transform_matrix(IH_GM_CouplingTime,'HGI',&
-         Grid_C(IH_) % TypeCoord),vPlanetHgi_D)
-
-  end subroutine update_mapping_parameters
 
   !===============================================================!
 
@@ -294,9 +251,10 @@ contains
        nPartial,iGetStart,Get,W,State_V,nVar,IH_GM_CouplingTime)
 
     State_V(rhoUx_:rhoUz_)=&
-         matmul(GM_a_IH_DD,State_V(rhoUx_:rhoUz_) &
+         matmul(IhToGm_DD,State_V(rhoUx_:rhoUz_) &
          - State_V(rho_)*vPlanetIh_D )
-    State_V(Bx_:Bz_)=matmul(GM_a_IH_DD,State_V(Bx_:Bz_))
+    State_V(Bx_:Bz_)=matmul(IhToGm_DD,State_V(Bx_:Bz_))
+
   end subroutine IH_get_for_gm_and_transform
 
 end module CON_couple_ih_gm
