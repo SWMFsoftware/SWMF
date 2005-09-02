04a2c3f1c76094cd2342f127e3e4cf9b2378b668
gtoth <>
2005-09-02 21:37:03 +0000

    Index names for integer time arrays.
    Former-commit-id: 8dec82b718ad622bec2e7480907cc9560c5b3ee7

---
+++ ESMF/ESMF_SWMF/src/ModTimeArray.f90
@@ -0,0 +1,10 @@
+module ModTimeArray
+
+  ! This module contains the named integer indexes for integer time arrays
+
+  implicit none
+
+  integer, parameter :: &
+       Year_=1, Month_=2, Day_=3, Hour_=4, Minute_=5, Second_=6, MilliSec_=7
+
+end module ModTimeArray
