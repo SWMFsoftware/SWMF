457020daa71d8f8a5430fcc0120585d5bfb21677
gtoth <>
2005-09-02 17:06:23 +0000

    Added protex documentation.
    Former-commit-id: 0b9d9edf902eedd1579c5fb2e002a2ca34fc6397

--- CON/Control/src/swmf.f90
+++ CON/Control/src/swmf.f90
@@ -3,29 +3,38 @@
 !           SWMF: Space Weather Modeling Framework          |
 !                   University of Michigan                  |
 !============================================================
+!BOP 
+!
+!QUOTE: \chapter{Control Module}
+!QUOTE: \section{CON/Control: Main Executable and Control}
+!MODULE: swmf - the main program for the stand-alone SWMF executable
 !INTERFACE:
 program SWMF
 
   !USES:
-  use CON_main
+  use CON_main,      ONLY: initialize, run, finalize
   use CON_variables, ONLY: iErrorSwmf
-  use ModMpi
-
-  !REVISION HISTORY:
-  ! This main program is for the stand alone SWMF.
-  ! It uses methods from CON_main which can also be used when 
-  ! the SWMF is a library, e.g. when it is run by an ESMF application.
-  !EOP
+  use ModMpi,        ONLY: MPI_SUCCESS
 
   implicit none
-  integer :: iError
+
+  !LOCAL VARIABLES:
+  integer :: iErrorMpi ! MPI error code
+
+  !DESCRIPTION:
+  ! This main program is for the stand alone SWMF.
+  ! It uses methods from CON\_main which can also be used when 
+  ! the SWMF is a library, e.g. when it is run by an ESMF application.
+
+  !REVISION HISTORY:
+  ! 09/01/05 G.Toth - initial version
+  !EOP
   !---------------------------------------------------------------------------
-
-  call MPI_init(iError)
-  if(iError==MPI_SUCCESS) call initialize
-  if(iErrorSwmf==0)       call run
-  if(iErrorSwmf==0)       call finalize
-  call MPI_finalize(iError)
-
+  !BOC
+  call MPI_init(iErrorMpi)
+  if(iErrorMpi  == MPI_SUCCESS) call initialize
+  if(iErrorSwmf == 0          ) call run
+  if(iErrorSwmf == 0          ) call finalize
+  call MPI_finalize(iErrorMpi)
+  !EOC
 end program SWMF
-
