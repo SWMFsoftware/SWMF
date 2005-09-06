00451e47cc1016c65134b0faf7431ff1921193e1
gtoth <>
2005-09-06 21:10:24 +0000

    Added reading the ESMF/SWMF layout. Gridded components are created according to the layout. The two codes can run in parallel (nonblocking) before they are coupled. Added ESMF_wait statements, but not clear why they are needed.
    Former-commit-id: 9b5cf9bc90caec917b01156ad0d6661cabeb27f8

--- ESMF/ESMF_SWMF/src/ESMF_SWMF_GridComp.f90
+++ ESMF/ESMF_SWMF/src/ESMF_SWMF_GridComp.f90
@@ -56,6 +56,15 @@ contains
     type(ESMF_VM)   :: parentvm
     type(ESMF_Grid) :: parentgrid
 
+    ! Variables related to the layout information
+    ! For sake of simplicity SWMF runs on the first nProcSwmf CPU-s
+    ! while ESMF runs on CPU-s starting from iProc0Esmf
+    character (len=*), parameter :: NameParamFile = "ESMF_SWMF.input"
+    type(ESMF_Config) :: config
+    integer :: i, iProc, nProc
+    integer :: iProcRootSwmf, iProcLastSwmf
+    integer :: iProcRootEsmf, iProcLastEsmf
+
     !-------------------------------------------------------------------------
 
     call ESMF_LogWrite("ESMF_SWMF Initialize routine called", ESMF_LOG_INFO)
@@ -63,14 +72,108 @@ contains
     ! Get the layout and grid associated with this component
     call ESMF_GridCompGet(gcomp, vm=parentvm, grid=parentgrid, rc=rc)
 
+    ! Get the number of processors
+    call ESMF_VMGet(parentvm, petCount=nProc, localPET=iProc, rc=rc)
+
+    ! Read in layout information
+    config = ESMF_ConfigCreate(rc)
+    call ESMF_ConfigLoadFile(config, NameParamFile, rc = rc)
+    if(rc /= ESMF_SUCCESS) then
+       if(iProc == 0)write(*,*) &
+            'ESMF_ConfigLoadFile FAILED for file '//NameParamFile
+       RETURN
+    endif
+
+    ! Read root PE for the SWMF
+    call ESMF_ConfigGetAttribute(config, iProcRootSwmf, 'SWMF Root PE:', rc=rc)
+    if(rc /= ESMF_SUCCESS) then
+       if(iProc == 0)write(*,*) &
+            'Did not read SWMF Root PE: setting default value = 0'
+       iProcRootSwmf = 0
+    end if
+    if(iProcRootSwmf < 0) then
+       if(iProc == 0)write(*,*) &
+            'SWMF Root PE: negative rank, setting it to 0'
+       iProcRootSwmf = 0
+    end if
+    if(iProcRootSwmf >= nProc) then
+       if(iProc == 0)write(*,*) &
+            'SWMF Root PE: rank must be less than nProc=',nProc,&
+            ' setting it to', nProc-1
+       iProcRootSwmf = nProc-1
+    end if
+
+    ! Read last PE for the SWMF
+    call ESMF_ConfigGetAttribute(config, iProcLastSwmf, 'SWMF Last PE:', rc=rc)
+    if(rc /= ESMF_SUCCESS) then
+       if(iProc == 0)write(*,*) &
+            'Did not read SWMF Last PE: setting default value = ',nProc-1
+       iProcLastSwmf = nProc-1
+    end if
+    if(iProcLastSwmf < iProcRootSwmf) then
+       if(iProc == 0)write(*,*) &
+            'SWMF Last PE: rank cannot be less than root PE rank=',&
+            iProcRootSwmf,', setting last=', iProcRootSwmf
+       iProcLastSwmf = iProcRootSwmf
+    end if
+    if(iProcLastSwmf >= nProc) then
+       if(iProc == 0)write(*,*) &
+            'SWMF Last PE: rank must be less than nProc=',nProc,&
+            ' setting last=',nProc-1
+       iProcLastSwmf = nProc-1
+    end if
+
+    ! Read root PE for the ESMF
+    call ESMF_ConfigGetAttribute(config, iProcRootEsmf, 'ESMF Root PE:', rc=rc)
+    if(rc /= ESMF_SUCCESS) then
+       if(iProc == 0)write(*,*) &
+            'Did not read ESMF Root PE: setting default value = 0'
+       iProcRootEsmf = 0
+    end if
+    if(iProcRootEsmf < 0) then
+       if(iProc == 0)write(*,*) &
+            'ESMF Root PE: negative rank, setting root=0'
+       iProcRootEsmf = 0
+    end if
+    if(iProcRootEsmf >= nProc) then
+       if(iProc == 0)write(*,*) &
+            'ESMF Root PE: rank must be less than nProc=',nProc,&
+            ' setting root=', nProc-1
+       iProcRootEsmf = nProc-1
+    end if
+
+    ! Read last PE for the ESMF
+    call ESMF_ConfigGetAttribute(config, iProcLastEsmf, 'ESMF Last PE:', rc=rc)
+    if(rc /= ESMF_SUCCESS) then
+       if(iProc == 0)write(*,*) &
+            'Did not read ESMF Last PE: setting default value = ',nProc-1
+       iProcLastEsmf = nProc-1
+    end if
+    if(iProcLastEsmf < iProcRootEsmf) then
+       if(iProc == 0)write(*,*) &
+            'ESMF Last PE: rank cannot be less than root PE rank=',&
+            iProcRootEsmf,', setting last=',iProcRootEsmf
+       iProcLastEsmf = iProcRootEsmf
+    end if
+    if(iProcLastEsmf >= nProc) then
+       if(iProc == 0)write(*,*) &
+            'ESMF Last PE: rank must be less than nProc=',nProc,&
+            ' setting last=', nProc-1
+       iProcLastEsmf = nProc-1
+    end if
+
+    rc = ESMF_SUCCESS
+
     ! Create the SWMF Gridded component
     gname1 = "SWMF Gridded Component"
     comp1Grid = ESMF_GridCompCreate(parentvm, name=gname1, & 
+         petlist = (/ (i, i=iProcRootSwmf, iProcLastSwmf) /), &
          grid=parentgrid, rc=rc)
 
-    ! Create thw ESMF Gridded component(s, there could be more than one here)
+    ! Create the ESMF Gridded component(s, there could be more than one here)
     gname2 = "ESMF Gridded Component"
     comp2Grid = ESMF_GridCompCreate(parentvm, name=gname2, &
+         petlist = (/ (i, i=iProcRootEsmf, iProcLastEsmf) /), &
          grid=parentgrid, rc=rc)
 
     ! Create the Coupler component
@@ -84,7 +187,6 @@ contains
     call ESMF_GridCompSetServices(comp1Grid,  SWMF_SetServices, rc)
     call ESMF_GridCompSetServices(comp2Grid,  ESMF_SetServices, rc)
     call ESMF_CplCompSetServices(compCoupler, Coupler_SetServices, rc)
-
 
     ! Now create Import and Export State objects in order to pass data
     ! between the Coupler and the Gridded Components
@@ -130,14 +232,21 @@ contains
 
     call ESMF_LogWrite("ESMF-SWMF Run routine called", ESMF_LOG_INFO)
 
-    ! Now run and couple the subcomponents (assumes an SWMF->ESMF coupling)
-    call ESMF_GridCompRun(comp1Grid, G1imp, G1exp, parentclock, rc=rc)
+    ! Run the subcomponents concurrently if possible
+    call ESMF_GridCompRun(comp1Grid, G1imp, G1exp, parentclock, &
+         blockingflag=ESMF_NONBLOCKING, rc=rc)
     if(rc /= ESMF_SUCCESS) RETURN
 
+    call ESMF_GridCompRun(comp2Grid, G2imp, G2exp, parentclock, &
+         blockingflag=ESMF_NONBLOCKING, rc=rc)
+    if(rc /= ESMF_SUCCESS) RETURN
+
+    ! Wait until both of them finish ???
+    call ESMF_GridCompWait(comp1Grid, rc)
+    call ESMF_GridCompWait(comp2Grid, rc)
+
+    ! Couple the subcomponents
     call ESMF_CplCompRun(compCoupler, G1exp, G2imp, parentclock, rc=rc)
-    if(rc /= ESMF_SUCCESS) RETURN
-
-    call ESMF_GridCompRun(comp2Grid, G2imp, G2exp, parentclock, rc=rc)
     if(rc /= ESMF_SUCCESS) RETURN
 
     call ESMF_LogWrite("ESMF-SWMF Run finished", ESMF_LOG_INFO)
