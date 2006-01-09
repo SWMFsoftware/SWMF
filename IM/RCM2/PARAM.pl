9dbe9a00750a734f6b753389b07351705cc56604
gtoth <>
2006-01-09 15:33:25 +0000

    Better description for the #SAVEPLOT command.
    Former-commit-id: cc0d3412dd40b96701b47f5b6236f194c7602b98

--- IM/RCM2/PARAM.pl
+++ IM/RCM2/PARAM.pl
@@ -1,16 +1,16 @@
 #^CFG FILE _FALSE_
-$tree = [{'name' => 'commandList','content' => [{'name' => 'commandgroup','content' => [{'name' => 'command','content' => [{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'UseStrict','type' => 'logical','default' => 'T'}},{'content' => '
+$tree = [{'type' => 'e','attrib' => {'name' => 'Rice Convection Model 2: IM Component'},'name' => 'commandList','content' => [{'type' => 'e','attrib' => {'name' => 'Testing'},'name' => 'commandgroup','content' => [{'type' => 'e','attrib' => {'name' => 'STRICT'},'name' => 'command','content' => [{'type' => 'e','attrib' => {'type' => 'logical','name' => 'UseStrict','default' => 'T'},'name' => 'parameter','content' => []},{'type' => 't','content' => '
 
 #STRICT
 T                       UseStrict
 
 If true then stop when parameters are incompatible. If false, try to
 correct parameters and continue. Default is true, ie. strict mode.
-','type' => 't'}],'type' => 'e','attrib' => {'name' => 'STRICT'}}],'type' => 'e','attrib' => {'name' => 'Testing'}},{'name' => 'commandgroup','content' => [{'content' => '
+'}]}]},{'type' => 'e','attrib' => {'name' => 'Output'},'name' => 'commandgroup','content' => [{'type' => 't','content' => '
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! OUTPUT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-','type' => 't'},{'name' => 'command','content' => [{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'IsAscii','type' => 'logical','default' => 'T'}},{'content' => '
+'},{'type' => 'e','attrib' => {'name' => 'ASCII'},'name' => 'command','content' => [{'type' => 'e','attrib' => {'type' => 'logical','name' => 'IsAscii','default' => 'T'},'name' => 'parameter','content' => []},{'type' => 't','content' => '
 
 #ASCII
 T			IsAscii
@@ -18,7 +18,7 @@ T			IsAscii
 The input and output files for RCM can be either ascii or binary.
 Default value is true for ascii.
 
-','type' => 't'}],'type' => 'e','attrib' => {'name' => 'ASCII'}},{'name' => 'command','content' => [{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'length' => '100','name' => 'NameRcmDir','type' => 'string','default' => 'IM'}},{'content' => '
+'}]},{'type' => 'e','attrib' => {'name' => 'RCMDIR'},'name' => 'command','content' => [{'type' => 'e','attrib' => {'type' => 'string','length' => '100','name' => 'NameRcmDir','default' => 'IM'},'name' => 'parameter','content' => []},{'type' => 't','content' => '
 
 #RCMDIR
 IM/Plots		NameRcmDir
@@ -26,45 +26,58 @@ IM/Plots		NameRcmDir
 The NameRcmDir variable contains the name of the directory to store
 output files. Default value is "IM".
 
-','type' => 't'}],'type' => 'e','attrib' => {'name' => 'RCMDIR'}},{'name' => 'command','content' => [{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'nFilesPlot','type' => 'integer','default' => '0','min' => '0','max' => '9'}},{'name' => 'for','content' => [{'name' => 'parameter','content' => [{'name' => 'part','content' => [{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => '2D vars.','value' => '2d'}},{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => '2D vars.','value' => '2D'}},{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => '3D vars.','value' => '3d'}},{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => '3D vars.','value' => '3D'}}],'type' => 'e','attrib' => {'input' => 'select','name' => 'plotarea','type' => 'string','required' => 'T'}},{'name' => 'part','content' => [{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => 'minimum variables','value' => 'min'}},{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => 'minimum variables','value' => 'MIN'}},{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => 'maximum variables','value' => 'max'}},{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => 'maximum variables','value' => 'MAX'}},{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => 'normal variables','value' => 'rcm'}},{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => 'normal variables','value' => 'RCM'}}],'type' => 'e','attrib' => {'input' => 'select','name' => 'plotvar','type' => 'string','required' => 'T'}},{'name' => 'part','content' => [{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => 'TECPLOT','value' => 'tec'}},{'name' => 'option','content' => [],'type' => 'e','attrib' => {'name' => 'IDL','value' => 'idl'}}],'type' => 'e','attrib' => {'input' => 'select','name' => 'plotformat','type' => 'string','required' => 'T'}}],'type' => 'e','attrib' => {'name' => 'StringPlot','type' => 'strings','min' => '3','max' => '3'}},{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'DnSavePlot','type' => 'integer','min' => '-1'}},{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'DtSavePlot','type' => 'integer','min' => '-1'}}],'type' => 'e','attrib' => {'from' => '1','to' => '$nFilesPlot'}},{'content' => '
+'}]},{'type' => 'e','attrib' => {'name' => 'SAVEPLOT'},'name' => 'command','content' => [{'type' => 'e','attrib' => {'type' => 'integer','max' => '9','min' => '0','name' => 'nFilesPlot','default' => '0'},'name' => 'parameter','content' => []},{'type' => 'e','attrib' => {'from' => '1','to' => '$nFilesPlot'},'name' => 'for','content' => [{'type' => 'e','attrib' => {'type' => 'strings','max' => '3','min' => '3','name' => 'StringPlot'},'name' => 'parameter','content' => [{'type' => 'e','attrib' => {'input' => 'select','type' => 'string','required' => 'T','name' => 'plotarea'},'name' => 'part','content' => [{'type' => 'e','attrib' => {'value' => '2d/2D','name' => '2D vars.','default' => 'T'},'name' => 'option','content' => []},{'type' => 'e','attrib' => {'value' => '3d/2D','name' => '3D vars.'},'name' => 'option','content' => []}]},{'type' => 'e','attrib' => {'input' => 'select','type' => 'string','required' => 'T','name' => 'plotvar'},'name' => 'part','content' => [{'type' => 'e','attrib' => {'value' => 'min/MIN','name' => 'minimum set','default' => 'T'},'name' => 'option','content' => []},{'type' => 'e','attrib' => {'value' => 'max/MAX/rcm/RCM','name' => 'maximum set'},'name' => 'option','content' => []}]},{'type' => 'e','attrib' => {'input' => 'select','type' => 'string','required' => 'T','name' => 'plotformat'},'name' => 'part','content' => [{'type' => 'e','attrib' => {'value' => 'tec/TEC','name' => 'TECPLOT','default' => 'T'},'name' => 'option','content' => []},{'type' => 'e','attrib' => {'value' => 'idl/IDL','name' => 'IDL','if' => '$plotarea =~ /2d/i'},'name' => 'option','content' => []}]}]},{'type' => 'e','attrib' => {'type' => 'integer','min' => '-1','name' => 'DnSavePlot'},'name' => 'parameter','content' => []},{'type' => 'e','attrib' => {'type' => 'integer','min' => '-1','name' => 'DtSavePlot'},'name' => 'parameter','content' => []}]},{'type' => 't','content' => '
 #SAVEPLOT
 2			nFilesPlot
-RCM 2d tec		StringPlot
+2d min idl		StringPlot
 100			DnSavePlot
--1			DtSavePlot
-RCM 3d idl		StringPlot
+-1			DtSavePlot [sec]
+3d max tec		StringPlot
 -1			DnSavePlot
-60			DtSavePlot
+60			DtSavePlot [sec]
 
-! Default is nFilesPlot=0. \\\\
-!
-! \\noindent
-! StringPlot must contain the following 3 parts in arbitrary order
-!\\begin{verbatim}
-! plotvar plotarea plotformat
-!
-! plotarea   = \'2d\', \'3d\'
-! plotvar    = \'min\', \'max\', \'rcm\'
-! plotformat = \'tec\', \'idl\'
-!\\end{verbatim}
-!
-!\\noindent
-! The plotarea string defines the region for the plots, 2d or 3d.
-! The plotvar string specifies which variables to write, either a minimum,
-! maximum, or typical values.
-! The plotformat specifies whether to output files for processing in either
-! Tecplot or Idl.
-! The plot_string is always followed by the plotting frequency
-! DnSavePlot and DtSavePlot, whether the run it time accurate or not.\\\\
-!
+Default is nFilesPlot=0. \\\\
 
-','type' => 't'}],'type' => 'e','attrib' => {'name' => 'SAVEPLOT'}}],'type' => 'e','attrib' => {'name' => 'Output'}},{'name' => 'commandgroup','content' => [{'content' => '
+\\noindent
+StringPlot must contain the following 3 parts (separated with space)
+in arbitrary order:
+\\begin{verbatim}
+plotarea   = \'2d\', \'3d\'
+plotvar    = \'min\', \'max\', \'rcm\'
+plotformat = \'tec\', \'idl\'
+\\end{verbatim}
+
+\\noindent
+The plotformat specifies whether to output files for processing in either
+Tecplot or Idl.
+
+The plotarea string defines the region for the plots, 2d or 3d.
+Currently the 3D output is implemented for Tecplot format only.
+
+The plotvar string specifies which variables to write, either a minimum set,
+or a maximum set (the \'rcm\' string is preserved for backwards
+compatibility only, and it is the same as the \'max\' string).
+The \'min\' value corresponds to the variables 
+\'rho T P rho(MHD) T(MHD) P(MHD\' in 2D and to \'eeta veff\' in 3D.
+
+The \'max\' value corresponds to a lot of variables in 2D. For Tecplot format
+they include the \'min\' variables, but for IDL format the \'max\' and
+\'min\' variables are distinct, so you need to specify two IDL plot files
+to get all the variables. In 3D only the Tecplot format is available,
+and the \'max\' variable set contains \'bndloc Vm |B| V birk alam eeta veff w\'.
+
+The DnSavePlot and DtSavePlot integers define the plotting frequency
+in number of time steps and number of seconds, respectively.
+A negative value -1 means that the frequency parameter is ignored.
+Note that DtSavePlot must be a multiple of the time step iDtRcm
+(see the #TIMESTEP command).
+'}]}]},{'type' => 'e','attrib' => {'name' => 'TIME INTEGRATION'},'name' => 'commandgroup','content' => [{'type' => 't','content' => '
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!  TIME INTEGRATION PARAMETERS  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-','type' => 't'},{'name' => 'command','content' => [{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'DoRestart','type' => 'logical','default' => 'F'}},{'name' => 'rule','content' => [{'content' => '
+'},{'type' => 'e','attrib' => {'name' => 'RESTART'},'name' => 'command','content' => [{'type' => 'e','attrib' => {'type' => 'logical','name' => 'DoRestart','default' => 'F'},'name' => 'parameter','content' => []},{'type' => 'e','attrib' => {'expr' => '-f \'IM/restartIN/RCMrestart.rst\' or not $DoRestart'},'name' => 'rule','content' => [{'type' => 't','content' => '
 		File IM/restartIN/RCMrestart.rst should exist!
-	','type' => 't'}],'type' => 'e','attrib' => {'expr' => '-f \'IM/restartIN/RCMrestart.rst\' or not $DoRestart'}},{'content' => '
+	'}]},{'type' => 't','content' => '
 
 #RESTART
 T			DoRestart
@@ -72,25 +85,25 @@ T			DoRestart
 The DoRestart parameter determines if the RCM starts from the restart
 files saved from a previous run, or from scratch via the standard input files.
 The default is DoRestart = .false.
-','type' => 't'}],'type' => 'e','attrib' => {'name' => 'RESTART'}},{'name' => 'command','content' => [{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'iDtRcm','type' => 'integer','default' => '5','min' => '1'}},{'content' => '
+'}]},{'type' => 'e','attrib' => {'name' => 'TIMESTEP'},'name' => 'command','content' => [{'type' => 'e','attrib' => {'type' => 'integer','min' => '1','name' => 'iDtRcm','default' => '5'},'name' => 'parameter','content' => []},{'type' => 't','content' => '
 
 #TIMESTEP
 5			iDtRcm
 
 The iDtRcm parameter defines the time step in seconds. The default
 value is 5 seconds.
-','type' => 't'}],'type' => 'e','attrib' => {'name' => 'TIMESTEP'}}],'type' => 'e','attrib' => {'name' => 'TIME INTEGRATION'}},{'name' => 'commandgroup','content' => [{'content' => '
+'}]}]},{'type' => 'e','attrib' => {'name' => 'PHYSICS PARAMETERS'},'name' => 'commandgroup','content' => [{'type' => 't','content' => '
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!! PHYSICS PARAMETERS  !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-','type' => 't'},{'name' => 'command','content' => [{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'FractionH','type' => 'real','default' => '0.7'}},{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'FractionO','type' => 'real','default' => '0.3'}},{'content' => '
+'},{'type' => 'e','attrib' => {'name' => 'COMPOSITION'},'name' => 'command','content' => [{'type' => 'e','attrib' => {'type' => 'real','name' => 'FractionH','default' => '0.7'},'name' => 'parameter','content' => []},{'type' => 'e','attrib' => {'type' => 'real','name' => 'FractionO','default' => '0.3'},'name' => 'parameter','content' => []},{'type' => 't','content' => '
 
 #COMPOSITION
 0.7      		FractionH
 0.3      		FractionO
 
 Fractional composition in RCM of H+ and O+.  The two need to add up to 1.0
-','type' => 't'}],'type' => 'e','attrib' => {'name' => 'COMPOSITION'}},{'name' => 'command','content' => [{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'UseChargeExchange','type' => 'logical','default' => 'T'}},{'name' => 'if','content' => [{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'SunspotNumber','type' => 'real','default' => '125.','min' => '0'}},{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'F107MonthlyMean','type' => 'real','default' => '169.','min' => '0'}},{'name' => 'parameter','content' => [],'type' => 'e','attrib' => {'name' => 'DayOfYear','type' => 'real','default' => '90.','min' => '0','max' => '366'}}],'type' => 'e','attrib' => {'expr' => '$UseChargeExchange'}},{'content' => '
+'}]},{'type' => 'e','attrib' => {'name' => 'CHARGEEXCHANGE'},'name' => 'command','content' => [{'type' => 'e','attrib' => {'type' => 'logical','name' => 'UseChargeExchange','default' => 'T'},'name' => 'parameter','content' => []},{'type' => 'e','attrib' => {'expr' => '$UseChargeExchange'},'name' => 'if','content' => [{'type' => 'e','attrib' => {'type' => 'real','min' => '0','name' => 'SunspotNumber','default' => '125.'},'name' => 'parameter','content' => []},{'type' => 'e','attrib' => {'type' => 'real','min' => '0','name' => 'F107MonthlyMean','default' => '169.'},'name' => 'parameter','content' => []},{'type' => 'e','attrib' => {'type' => 'real','max' => '366','min' => '0','name' => 'DayOfYear','default' => '90.'},'name' => 'parameter','content' => []}]},{'type' => 't','content' => '
 
 #CHARGEEXCHANGE
 T			UseChargeExchange
@@ -99,6 +112,6 @@ T			UseChargeExchange
 90.			DayOfYear
 
 Activate charge exchange in RCM and specify solar conditions.
-','type' => 't'}],'type' => 'e','attrib' => {'name' => 'CHARGEEXCHANGE'}}],'type' => 'e','attrib' => {'name' => 'PHYSICS PARAMETERS'}},{'name' => 'rule','content' => [{'content' => '
+'}]}]},{'type' => 'e','attrib' => {'expr' => '-d \'IM/restartOUT\' or not $_IsFirstSession'},'name' => 'rule','content' => [{'type' => 't','content' => '
 	Directory IM/restartOUT should exist!
-','type' => 't'}],'type' => 'e','attrib' => {'expr' => '-d \'IM/restartOUT\' or not $_IsFirstSession'}}],'type' => 'e','attrib' => {'name' => 'Rice Convection Model 2: IM Component'}}];
+'}]}]}];
