01acea142c1587cd5e8bfba037387ed260e7a881
ridley <>
2006-11-03 22:20:15 +0000

    1. Cleaned up the code substantially. 2. Fixed a MAJOR bug in the ion density boundary condition.
    Former-commit-id: d7d729f2ba01e7260f0629662f50db3e924cbc58

--- UA/GITM2/src/set_vertical_bcs.f90
+++ UA/GITM2/src/set_vertical_bcs.f90
@@ -36,6 +36,10 @@ subroutine set_vertical_bcs(LogRho,LogNS,Vel_GD,Temp, LogINS, iVel, VertVel)
 
   integer, dimension(25) :: sw
 
+  !-----------------------------------------------------------
+  ! Bottom
+  !-----------------------------------------------------------
+
   if (IsEarth) UseMsisBCs = UseMsis
 
   if (IsFirstTime .and. UseMsisBCs) then
@@ -45,10 +49,8 @@ subroutine set_vertical_bcs(LogRho,LogNS,Vel_GD,Temp, LogINS, iVel, VertVel)
   endif
 
   if (UseMsisBCs) then
-
-
-    call get_HPI(CurrentTime, HP, iError)  
-!    Ap = log(HP)/log(4.2) *30.
+     call get_HPI(CurrentTime, HP, iError)  
+     if (iError > 0) hp = 40.0
      Ap = min(200.,max(-40.72 + 1.3 * HP, 10.))
      do iAlt = -1, 0
         Alt = Altitude(iAlt)/1000.0
@@ -57,58 +59,29 @@ subroutine set_vertical_bcs(LogRho,LogNS,Vel_GD,Temp, LogINS, iVel, VertVel)
         call msis_bcs(iJulianDay,UTime,Alt,Lat,Lon,Lst, &
              F107A,F107,AP,LogNS(iAlt,:), Temp(iAlt), &
              LogRho(iAlt))
-!       logNS(ialt,:)=logNS(ialt+1,:) - &                                    
-!            dalt(ialt+1) * Boltzmanns_Constant * temp(ialt+1)/ &                                            
-!            mass(iN2_)/gravity(ialt+1)                                                                      
-!       logRho(ialt)=logRho(ialt+1) - &                                                                      
-!            dalt(ialt+1) * Boltzmanns_Constant * temp(ialt+1)/ &                                            
-!            mass(iN2_)/gravity(ialt+1)              
-!
+
      enddo
   else
 
-     !  ! Bottom
-     !
-     !  do iSpecies=1,nIonsAdvect
-     !     LogINS(0,iSpecies) = LogINS(1,iSpecies) - &
-     !          dAlt(1)* Gravity(1)/Temp(1)
-     !     LogINS(-1,iSpecies) = LogINS(1,iSpecies) - &
-     !          2*dAlt(1)* Gravity(1)/Temp(1)
-     !  enddo
-     !
-     !  ! Bottom
-     !  ! Density and temperature are fixed
-     !
-     !  Temp(0)  = max(Temp(1),TempMin/TempUnit)
-     !  Temp(-1) = max(Temp(1),TempMin/TempUnit)
+     ! do nothing - which means don't change the initial condition.
 
   endif
 
-  ! Slipping wall condition
   Vel_GD(-1:0,iEast_)  = 0.0
   Vel_GD(-1:0,iNorth_) = 0.0
-!  Vel_GD(-1:0,iEast_)  = Vel_GD(1,iEast_)
-!  Vel_GD(-1:0,iNorth_) = Vel_GD(1,iNorth_)
- ! Vel_GD( 0,iUp_)      = - Vel_GD(1,iUp_)
- ! Vel_GD(-1,iUp_)      = - Vel_GD(2,iUp_)
+  Vel_GD(-1:0,iUp_)    = 0.0
+  VertVel(-1:0,:)      = 0.0
+  IVel(-1:0,iUp_)      = 0.0
 
- Vel_GD( 0,iUp_)      = 0.0
- Vel_GD(-1,iUp_)      = 0.0
+  ! If you want to slip:
+  !  Vel_GD(-1:0,iEast_)  = Vel_GD(1,iEast_)
+  !  Vel_GD(-1:0,iNorth_) = Vel_GD(1,iNorth_)
 
-!!$ Vel_GD( 0,iUp_)      = Vel_GD( 1,iUp_)
-!!$ Vel_GD(-1,iUp_)      = Vel_GD( 1,iUp_)
-!!$
-!!$ VertVel(0,:)  = VertVel(1,:)
-!!$ VertVel(-1,:) = VertVel(1,:)
-
- VertVel(0,:)  = 0
- VertVel(-1,:) = 0
-
-
- IVel( 0,iUp_)      = 0.0
- IVel(-1,iUp_)      = 0.0
-
+  !-----------------------------------------------------------
   ! Top
+  !-----------------------------------------------------------
+
+  ! Slip flow at the top
 
   Vel_GD(nAlts+1:nAlts+2,iEast_)  = Vel_GD(nAlts,iEast_)
   Vel_GD(nAlts+1:nAlts+2,iNorth_) = Vel_GD(nAlts,iNorth_)
@@ -116,37 +89,27 @@ subroutine set_vertical_bcs(LogRho,LogNS,Vel_GD,Temp, LogINS, iVel, VertVel)
   IVel(nAlts+1:nAlts+2,iEast_)  = IVel(nAlts,iEast_)
   IVel(nAlts+1:nAlts+2,iNorth_) = IVel(nAlts,iNorth_)
 
-!  Vel_GD(nAlts+1,iUp_) = -Vel_GD(nAlts  ,iUp_)
-!  Vel_GD(nAlts+2,iUp_) = -Vel_GD(nAlts-1,iUp_)
+  ! Things can go up or down in the ions
+
+  IVel(nAlts+1,iUp_)   =  IVel(nAlts,iUp_)
+  IVel(nAlts+2,iUp_)   =  IVel(nAlts-1,iUp_)
+
+  ! We only let stuff flow out in the neutrals
 
   if(Vel_GD(nAlts,iUp_)>0.)then
-
      Vel_GD(nAlts+1:nAlts+2,iUp_) = Vel_GD(nAlts,iUp_)
-     IVel(nAlts+1:nAlts+2,iUp_)   = IVel(nAlts,iUp_)
-
      VertVel(nAlts+1,:) = VertVel(nAlts,:)
      VertVel(nAlts+2,:) = VertVel(nAlts,:)
-
   else
      ! Vel_GD(nAlts+1:nAlts+2,iUp_) = 0.0 ! -Vel(nAlts)
      Vel_GD(nAlts+1,iUp_) = -Vel_GD(nAlts,iUp_)
      Vel_GD(nAlts+2,iUp_) = -Vel_GD(nAlts-1,iUp_)
-
      VertVel(nAlts+1,:) = -VertVel(nAlts,:)
      VertVel(nAlts+2,:) = -VertVel(nAlts-1,:)
-
   endif
 
-!  if (UseConduction) then
-!     Temp(nAlts+1) = max(Temp(nAlts), TempMin/TempUnit(1,1,1))
-!     Temp(nAlts+2) = max(Temp(nAlts), TempMin/TempUnit(1,1,1))
-!  else
-!     Temp(nAlts+1) = TempMax/TempUnit(1,1,nalts+1)
-!     Temp(nAlts+2) = TempMax/TempUnit(1,1,nalts+2)
-!  endif
+  ! Float temp
 
-
-!!!! CHANGE !!!!
   Temp(nAlts+1) = Temp(nAlts)
   Temp(nAlts+2) = Temp(nAlts-1)
 
@@ -157,24 +120,22 @@ subroutine set_vertical_bcs(LogRho,LogNS,Vel_GD,Temp, LogINS, iVel, VertVel)
           LogRho(iAlt-1)-(Altitude(iAlt)-Altitude(iAlt-1))*InvScaleHgt
   enddo
 
+  ! Limit the slope of the ion density
+
   do iSpecies=1,nIonsAdvect
      dn = (LogINS(nAlts,iSpecies) - LogINS(nAlts-1,iSpecies))
-!     if (dn > 0.0) dn = -dn*1.0
-     if (dn > -0.1*LogINS(nAlts,iSpecies)) dn = -0.1*LogINS(nAlts,iSpecies)
-!     LogINS(nAlts+1,iSpecies) = max( &
-!          LogINS(nAlts,iSpecies) + dn, LogINS(nAlts,iSpecies) / 2.0)
-!     LogINS(nAlts+2,iSpecies) = max( &
-!          LogINS(nAlts+1,iSpecies) + dn, LogINS(nAlts+1,iSpecies) / 2.0)
-     LogINS(nAlts+1,iSpecies) = LogINS(nAlts,iSpecies) + dn
-     LogINS(nAlts+2,iSpecies) = LogINS(nAlts+1,iSpecies) + dn
+     if (dn < 0.75*LogINS(nAlts,iSpecies) .and. dn > 0) &
+          dn = 0.75*LogINS(nAlts,iSpecies)
+     LogINS(nAlts+1,iSpecies) = LogINS(nAlts,iSpecies) - dn
+     LogINS(nAlts+2,iSpecies) = LogINS(nAlts+1,iSpecies) - dn
   enddo
+
+  ! Hydrostatic pressure for the neutrals
 
   do iSpecies=1,nSpecies
      do iAlt = nAlts+1, nAlts+2
         InvScaleHeightS = -Gravity(iAlt) * &
              Mass(iSpecies) / (Temp(iAlt)*Boltzmanns_Constant)
-!!!! CHANGE !!!!
-!             Mass(iSpecies) / (Temp(iAlt)*TempUnit(1,1,iAlt)*Boltzmanns_Constant)
         LogNS(iAlt,iSpecies) = &
              LogNS(iAlt-1,iSpecies) &
              -(Altitude(iAlt)-Altitude(iAlt-1))*InvScaleHeightS
