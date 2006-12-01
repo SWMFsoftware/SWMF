77d7aeaa81af311300c7dc19491c9ac256ef9d5b
ridley <>
2006-12-01 14:55:45 +0000

    Works now.
    Former-commit-id: a9318e4b8ad127ee31e2dc128a213e369179d36b

--- UA/GITM2/src/output.Mars.f90
+++ UA/GITM2/src/output.Mars.f90
@@ -105,7 +105,7 @@ subroutine output_1d(dir, cName, iBlock, Position)
   call calc_physics(iBlock)
   call chapman_integrals(iBlock)
   call calc_rates(iBlock)
-  call calc_efield(iBlock)
+!  call calc_efield(iBlock)
 
   !! construct naming strings
 
@@ -136,7 +136,6 @@ end subroutine output_1d
 subroutine output(dir, iBlock, iOutputType)
 
   use ModSphereInterface, only:iStartBlk
-  use ModPlanet
   use ModGITM
   use ModEUV
   use ModTime
@@ -149,11 +148,11 @@ subroutine output(dir, iBlock, iOutputType)
   integer, intent(in) :: iBlock
   integer, intent(in) :: iOutputType
 
-  character (len=5) :: blk_str
+  character (len=5) :: proc_str,blk_str
   character (len=14) :: cTime
   character (len=100) :: output_format
   integer :: iLon,iLat,iAlt, nvars_to_write, nlines, iBLK
-  integer :: iiLon, iiLat, iiAlt, nGCs, i
+  integer :: iiLon, iiLat, iiAlt, nGCs
   logical :: done
 
   real :: LatFind = 2.0*pi/180.0, LonFind = 10.0*pi/180.0
@@ -198,18 +197,18 @@ subroutine output(dir, iBlock, iOutputType)
   endif
 
   if (iProc == 0 .and. iBlock == 1) &
-       write(*,*) "Writing Output files at iStep : ",iStep, tSimulation
+       write(*,'(a,i7,i5,5i3)') &
+       "Writing Output files at iStep : ",iStep, iTimeArray(1:6)
 
   call calc_physics(iBlock)
   call chapman_integrals(iBlock)
   call calc_rates(iBlock)
-  call calc_efield(iBlock)
+  if (.not. Is1D) call calc_efield(iBlock)
 
   iBLK = iStartBLK + iBlock
 
-  blk_str = "b0000"
-
   write(blk_str,'(a1,i4.4)') "b",iBLK
+
   call i2s(mod(iTimeArray(1),100), cYear, 2)
   call i2s(iTimeArray(2), cMonth, 2)
   call i2s(iTimeArray(3), cDay, 2)
@@ -219,32 +218,116 @@ subroutine output(dir, iBlock, iOutputType)
 
   cTime = "t"//cYear//cMonth//cDay//"_"//cHour//cMinute//cSecond
 
-  !! open file
-  open(unit=iOutputUnit_, &
-       file=dir//"/"//blk_str//"_"//cTime//"."//OutputType(iOutputType),&
-       status="unknown")
-
-  write(iOutputUnit_,*) ""
-  write(iOutputUnit_,*) "TIME"
-  do i=1,7
-     write(iOutputUnit_,*) iTimeArray(i)
-  enddo
-  write(iOutputUnit_,*) ""
-
   select case (OutputType(iOutputType))
 
   case ('3DALL')
 
+     !! open file
+     open(unit=iOutputUnit_, &
+          file=dir//"/"//blk_str//"_"//cTime//"."//OutputType(iOutputType),&
+          status="unknown")
+
+     call write_head_blocks
+     call write_head_time
+     call write_head_version
      call output_3dall
+
+     !! close file
+     close(unit=iOutputUnit_)
+
+!!  case ('3DSRC')
+!!
+!!     !! open file
+!!     open(unit=iOutputUnit_, &
+!!          file=dir//"/"//blk_str//"_"//cTime//"."//OutputType(iOutputType),&
+!!          status="unknown")
+!!
+!!     call write_head_blocks
+!!     call write_head_time
+!!     call write_head_version
+!!     call output_3dsrc
+!!
+!!     !! close file
+!!     close(unit=iOutputUnit_)
+!!
+!!  case ('3DAUR')
+!!
+!!     !! open file
+!!     open(unit=iOutputUnit_, &
+!!          file=dir//"/"//blk_str//"_"//cTime//"."//OutputType(iOutputType),&
+!!          status="unknown")
+!!
+!!     call write_head_blocks
+!!     call write_head_time
+!!     call write_head_version
+!!     call output_3daur
+!!
+!!     !! close file
+!!     close(unit=iOutputUnit_)
+!!
+!!  case ('2DGEO')
+!!
+!!     !! open file
+!!     open(unit=iOutputUnit_, &
+!!          file=dir//"/"//blk_str//"_"//cTime//"."//OutputType(iOutputType),&
+!!          status="unknown")
+!!
+!!     call write_head_blocks
+!!     call write_head_time
+!!     call write_head_version
+!!     call output_2d
+!!
+!!     !! close file
+!!     close(unit=iOutputUnit_)
+!!
+!!  case ('2DMAG')
+!!
+!!     if (iBLK == 1) then
+!!
+!!        !! open file
+!!        open(unit=iOutputUnit_, &
+!!             file=dir//"/"//blk_str//"_"//cTime//"."//OutputType(iOutputType),&
+!!             status="unknown")
+!!
+!!        call write_head_time
+!!        call write_head_version
+!!        call output_2delectro
+!!
+!!        !! close file
+!!        close(unit=iOutputUnit_)
+!!
+!!     endif
 
   case ('1DALL')
 
+     !! open file
+     open(unit=iOutputUnit_, &
+          file=dir//"/"//blk_str//"_"//cTime//"."//OutputType(iOutputType),&
+          status="unknown")
+
+     call write_head_time
+     call write_head_version
      call output_1dall(iiLon, iiLat, iBlock, rLon, rLat, iOutputUnit_)
 
-  end select
+     !! close file
+     close(unit=iOutputUnit_)
 
-  !! close file
-  close(unit=iOutputUnit_)
+!!  case ('3DION')
+!!
+!!     !! open file
+!!     open(unit=iOutputUnit_, &
+!!          file=dir//"/"//blk_str//"_"//cTime//"."//OutputType(iOutputType),&
+!!          status="unknown")
+!!
+!!     call write_head_blocks
+!!     call write_head_time
+!!     call write_head_version
+!!     call output_3dion
+!!
+!!     !! close file
+!!     close(unit=iOutputUnit_)
+
+  end select
 
 contains
 
@@ -252,9 +335,177 @@ contains
   !
   !----------------------------------------------------------------
 
+  subroutine write_head_blocks
+
+    write(iOutputUnit_,*) "BLOCKS"
+    write(iOutputUnit_,"(I7,A)") 1, " nBlocksAlt"
+    write(iOutputUnit_,"(I7,A)") nBlocksLat, " nBlocksLat"
+    write(iOutputUnit_,"(I7,A)") nBlocksLon, " nBlocksLon"
+    write(iOutputUnit_,*) ""
+
+  end subroutine write_head_blocks
+
+
+  !----------------------------------------------------------------
+  !
+  !----------------------------------------------------------------
+
+  subroutine write_head_time
+
+    write(iOutputUnit_,*) "TIME"
+    write(iOutputUnit_,"(I7,A)") iTimeArray(1), " Year"
+    write(iOutputUnit_,"(I7,A)") iTimeArray(2), " Month"
+    write(iOutputUnit_,"(I7,A)") iTimeArray(3), " Day"
+    write(iOutputUnit_,"(I7,A)") iTimeArray(4), " Hour"
+    write(iOutputUnit_,"(I7,A)") iTimeArray(5), " Minute"
+    write(iOutputUnit_,"(I7,A)") iTimeArray(6), " Second"
+    write(iOutputUnit_,*) ""
+
+  end subroutine write_head_time
+
+  !----------------------------------------------------------------
+  !
+  !----------------------------------------------------------------
+
+  subroutine write_head_version
+
+    write(iOutputUnit_,*) "VERSION"
+    write(iOutputUnit_,*) 2.4
+    write(iOutputUnit_,*) ""
+
+  end subroutine write_head_version
+
+  !----------------------------------------------------------------
+  !
+  !----------------------------------------------------------------
+
+  subroutine output_2d
+
+    use ModElectrodynamics
+
+    nvars_to_write = 12
+    write(output_format,"('(1p,',I2,'E11.3)')") nvars_to_write
+
+    write(iOutputUnit_,*) "NUMERICAL VALUES"
+    write(iOutputUnit_,"(I7,A)") nvars_to_write, " nvars"
+    write(iOutputUnit_,"(I7,A)") 1, " nAltitudes"
+    write(iOutputUnit_,"(I7,A)") nLats, " nLatitude"
+    write(iOutputUnit_,"(I7,A)") nLons+1, " nLongitudes"
+
+    write(iOutputUnit_,*) ""
+
+    write(iOutputUnit_,*) "VARIABLE LIST"
+    write(iOutputUnit_,"(I7,A1,a)")  1, " ", "Longitude"
+    write(iOutputUnit_,"(I7,A1,a)")  2, " ", "Latitude"
+    write(iOutputUnit_,"(I7,A1,a)")  3, " ", "Altitude"
+    write(iOutputUnit_,"(I7,A1,a)")  4, " ", "Potential"
+    write(iOutputUnit_,"(I7,A1,a)")  5, " ", "Pedersen Conductance"
+    write(iOutputUnit_,"(I7,A1,a)")  6, " ", "Hall Conductance"
+    write(iOutputUnit_,"(I7,A1,a)")  7, " ", "Electron_Average_Energy"
+    write(iOutputUnit_,"(I7,A1,a)")  8, " ", "Electron_Energy_Flux"
+    write(iOutputUnit_,"(I7,A1,a)")  9, " ", "DivJuAlt"
+    write(iOutputUnit_,"(I7,A1,a)") 10, " ", "Pedersen FL Conductance"
+    write(iOutputUnit_,"(I7,A1,a)") 11, " ", "Hall FL Conductance"
+    write(iOutputUnit_,"(I7,A1,a)") 12, " ", "FL Length"
+    write(iOutputUnit_,*) ""
+    write(iOutputUnit_,*) "BEGIN"
+
+    iAlt = 1
+    do iLat=1,nLats
+       do iLon=1,nLons+1
+          write(iOutputUnit_,output_format)       &
+               Longitude(iLon,iBlock), &
+               Latitude(iLat,iBlock),&
+               Altitude(iAlt),&
+               Potential(iLon,iLat,iAlt,iBlock), &
+               PedersenConductance(iLon,iLat,iBlock), &
+               HallConductance(iLon,iLat,iBlock), &
+               ElectronAverageEnergy(iLon,iLat), &
+               ElectronEnergyFlux(iLon,iLat), &
+               DivJuAlt(iLon,iLat), &
+               PedersenFieldLine(iLon, iLat), &
+               HallFieldLine(iLon, iLat), &
+               LengthFieldLine(iLon, iLat)
+       enddo
+    enddo
+
+  end subroutine output_2d
+
+  !----------------------------------------------------------------
+  !
+  !----------------------------------------------------------------
+
+  subroutine output_2delectro
+
+    use ModElectrodynamics
+    use ModConstants, only:Pi
+
+    nvars_to_write = 15
+    write(output_format,"('(1p,',I2,'E11.3)')") nvars_to_write
+
+    write(iOutputUnit_,*) "BLOCKS"
+    write(iOutputUnit_,"(I7,A)") 1, " nBlocksAlt"
+    write(iOutputUnit_,"(I7,A)") 1, " nBlocksLat"
+    write(iOutputUnit_,"(I7,A)") 1, " nBlocksLon"
+    write(iOutputUnit_,*) ""
+
+    write(iOutputUnit_,*) "NUMERICAL VALUES"
+    write(iOutputUnit_,"(I7,A)") nvars_to_write, " nvars"
+    write(iOutputUnit_,"(I7,A)") 1, " nAltitudes"
+    write(iOutputUnit_,"(I7,A)") nMagLats, " nLatitude"
+    write(iOutputUnit_,"(I7,A)") nMagLons+1, " nLongitudes"
+
+    write(iOutputUnit_,*) ""
+
+    write(iOutputUnit_,*) "VARIABLE LIST"
+    write(iOutputUnit_,"(I7,A1,a)")  1, " ", "Longitude"
+    write(iOutputUnit_,"(I7,A1,a)")  2, " ", "Latitude"
+    write(iOutputUnit_,"(I7,A1,a)")  3, " ", "Altitude"
+    write(iOutputUnit_,"(I7,A1,a)")  4, " ", "Pedersen Conductance"
+    write(iOutputUnit_,"(I7,A1,a)")  5, " ", "Hall Conductance"
+    write(iOutputUnit_,"(I7,A1,a)")  6, " ", "DivJuAlt"
+    write(iOutputUnit_,"(I7,A1,a)")  7, " ", "Field Line Length"
+    write(iOutputUnit_,"(I7,A1,a)")  8, " ", "Sigma PP"
+    write(iOutputUnit_,"(I7,A1,a)")  9, " ", "Sigma LL"
+    write(iOutputUnit_,"(I7,A1,a)") 10, " ", "Sigma H"
+    write(iOutputUnit_,"(I7,A1,a)") 11, " ", "Sigma C"
+    write(iOutputUnit_,"(I7,A1,a)") 12, " ", "Sigma PL"
+    write(iOutputUnit_,"(I7,A1,a)") 13, " ", "Sigma LP"
+    write(iOutputUnit_,"(I7,A1,a)") 14, " ", "K^D_{m\phi}"
+    write(iOutputUnit_,"(I7,A1,a)") 15, " ", "K^D_{m\lamda}"
+    write(iOutputUnit_,*) ""
+    write(iOutputUnit_,*) "BEGIN"
+
+    iAlt = 1
+    do iLat=1,nMagLats
+       do iLon=1,nMagLons+1
+          write(iOutputUnit_,output_format)       &
+               MagLocTimeMC(iLon,iLat)*Pi/12.0, &
+               MagLatMC(iLon,iLat)*Pi/180.0,Altitude(iAlt),&
+               SigmaPedersenMC(iLon,iLat), &
+               SigmaHallMC(iLon,iLat), &
+               DivJuAltMC(iLon,iLat), &
+               LengthMC(iLon,iLat), &
+               SigmaHHMC(iLon,iLat), &
+               SigmaLLMC(iLon,iLat), &
+               SigmaHHMC(iLon,iLat), &
+               SigmaCCMC(iLon,iLat), &
+               SigmaLPMC(iLon,iLat), &
+               SigmaPLMC(iLon,iLat), &
+               KDmpMC(iLon,iLat), &
+               KdmlMC(iLon,iLat)
+       enddo
+    enddo
+
+  end subroutine output_2delectro
+
+  !----------------------------------------------------------------
+  !
+  !----------------------------------------------------------------
+
   subroutine output_3dall
 
-    nvars_to_write = 13
+    nvars_to_write = 18
     write(output_format,"('(1p,',I2,'E11.3)')") nvars_to_write
 
     if (Is1D) then
@@ -284,6 +535,11 @@ contains
     write(iOutputUnit_,"(I7,A1,a)") 11, " ", "Vn (east)"
     write(iOutputUnit_,"(I7,A1,a)") 12, " ", "Vn (north)"
     write(iOutputUnit_,"(I7,A1,a)") 13, " ", "Vn (up)"
+    write(iOutputUnit_,"(I7,A1,a)") 14, " ", "[O2+]"
+    write(iOutputUnit_,"(I7,A1,a)") 15, " ", "[CO2+]"
+    write(iOutputUnit_,"(I7,A1,a)") 16, " ", "[NO+]"
+    write(iOutputUnit_,"(I7,A1,a)") 17, " ", "[O+]"
+    write(iOutputUnit_,"(I7,A1,a)") 18, " ", "[e-]"
     write(iOutputUnit_,*) ""
 
     write(iOutputUnit_,*) "BEGIN"
@@ -300,14 +556,19 @@ contains
                   altitude(iAlt),&
                   LocalTime(iiLon), &
                   Rho(iLon,iLat,iAlt,iBlock),&
-                  Temperature(iLon,iLat,iAlt,iBlock)*TempUnit,&
+                  Temperature(iLon,iLat,iAlt,iBlock)*TempUnit(iLon,iLat,iAlt),&
                   NDensityS(iLon,iLat,iAlt,iCO_,iBlock), &
                   NDensityS(iLon,iLat,iAlt,iCO2_,iBlock), &
                   NDensityS(iLon,iLat,iAlt,iO_,iBlock), &
                   NDensityS(iLon,iLat,iAlt,iN2_,iBlock), &
                   velocity(iLon,iLat,iAlt,iEast_,iBlock), &
                   velocity(iLon,iLat,iAlt,iNorth_,iBlock), &
-                  velocity(iLon,iLat,iAlt,iUp_,iBlock)
+                  velocity(iLon,iLat,iAlt,iUp_,iBlock), &
+                  IDensityS(iLon,iLat,iAlt,iO2_,iBlock), &
+                  IDensityS(iLon,iLat,iAlt,iCO2P_,iBlock), &
+                  IDensityS(iLon,iLat,iAlt,iNOP_,iBlock), &
+                  IDensityS(iLon,iLat,iAlt,iOP_,iBlock), &
+                  IDensityS(iLon,iLat,iAlt,ie_,iBlock)
           enddo
        enddo
     enddo
@@ -373,7 +634,7 @@ subroutine output_1dall(iiLon, iiLat, iBlock, rLon, rLat, iUnit)
           (1-rLon)*(  rLat)*Rho(iiLon+1,iiLat,iAlt,iBlock) + &
           (  rLon)*(1-rLat)*Rho(iiLon,iiLat+1,iAlt,iBlock) + &
           (1-rLon)*(1-rLat)*Rho(iiLon+1,iiLat+1,iAlt,iBlock)
-     Vars(5) = TempUnit * ( &
+     Vars(5) = TempUnit(iiLon,iiLat,iAlt) * ( &
           (  rLon)*(  rLat)*Temperature(iiLon,iiLat,iAlt,iBlock) + &
           (1-rLon)*(  rLat)*Temperature(iiLon+1,iiLat,iAlt,iBlock) + &
           (  rLon)*(1-rLat)*Temperature(iiLon,iiLat+1,iAlt,iBlock) + &
