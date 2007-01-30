94b3e556a6511ac492ac07df19014e3964c02ca4
gtoth <>
2007-01-30 16:18:52 +0000

    Added PW to valid components
    Former-commit-id: ebe517598e3010fe1001f8b48218b61be84c3bc3

--- share/Scripts/CheckParam.pl
+++ share/Scripts/CheckParam.pl
@@ -27,7 +27,7 @@ my $StandAlone  = $S; undef $S;
 use strict;
 
 # Pattern to match component ID-s
-my $ValidComp = 'SC|IH|SP|GM|IM|RB|IE|UA';
+my $ValidComp = 'SC|IH|SP|GM|IM|PW|RB|IE|UA';
 
 # Error string
 my $ERROR = 'CheckParam_ERROR:';
