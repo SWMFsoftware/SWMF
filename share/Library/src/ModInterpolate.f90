de021165893c76862ad4ca53e8db1f3f4ce498e8
gtoth <>
2007-03-09 21:29:02 +0000

    Pass array index limits to bilinear and trilinear to make the use more intuitive.
    Former-commit-id: 8d55c76347a00f73d5321ad59088e133209bfb77

--- share/Library/src/ModInterpolate.f90
+++ share/Library/src/ModInterpolate.f90
@@ -1,15 +1,20 @@
 module ModInterpolate
 
   ! Use second order interpolation in a uniform grid with normalized
-  ! coordinates. The grid indexes are assumed to go from 0 to nI-1,
-  ! 0 to nJ-1 etc. The zero index can be regarded as a ghost cell
-  ! or as the boundary nodal value.
-  ! The coordinates are normalized such that the
+  ! coordinates. The coordinates are normalized such that the
   ! the coordinates coincide with the indexes at the grid points.
-  ! Typical usage:
   !
-  ! InterpolatedValue = bilinear(Value_II, nI+2, nJ+2, &
-  !                     (/ (x - x(0))/DeltaX, (y - y(0))/DeltaY) /) )
+  ! Examples of usage:
+  !
+  ! Cell based grid ghost cells:
+  !
+  !     InterpolatedValue = bilinear(Value_II, 0, nI+1, 0, nJ+1, &
+  !                         (/ (x - x(0))/DeltaX, (y - y(0))/DeltaY) /) )
+  !
+  ! Node based grid with x(1)=y(1)=0.0:
+  !
+  !     InterpolatedValue = bilinear(Value_II, 1, nI, 1, nJ, &
+  !                        (/ x/DeltaX, y/DeltaY /) )
 
   implicit none
 
@@ -24,27 +29,23 @@ module ModInterpolate
 contains
 
   !=========================================================================
-  real function bilinear(A_II, nI, nJ, Xy_D)
+  real function bilinear(A_II, iMin, iMax, jMin, jMax, Xy_D)
 
     ! Calculate bilinear interpolation of A_II at position Xy_D
 
     implicit none
-    integer, intent(in) :: nI, nJ
-    real, intent(in)    :: A_II(0:nI-1,0:nJ-1)
+    integer, intent(in) :: iMin, iMax, jMin, jMax
+    real, intent(in)    :: A_II(iMin:iMax,jMin:jMax)
     real, intent(in)    :: Xy_D(2)
 
     integer :: i1, i2, j1, j2
     real :: Dx1, Dx2, Dy1, Dy2
     character (len=*), parameter :: NameSub=NameMod//'::trilinear'
     !--------------------------------------------------------------------------
-    if(any(Xy_D < 0.0)) then
+    if(any( Xy_D < (/iMin, jMin/)) .or. any(Xy_D > (/ iMax, jMax /))) then
+       write(*,*)'iMin, iMax, jMin, jMax=',iMin, iMax, jMin, jMax
        write(*,*)'Xy_D =',Xy_D
-       call CON_stop(NameSub//': negative coordinate is not valid')
-    end if
-    if(any(Xy_D > (/ nI-1, nJ-1 /))) then
-       write(*,*)'nI-1, nJ-1=',nI-1, nJ-1
-       write(*,*)'Xy_D =',Xy_D
-       call CON_stop(NameSub//': normalized coordinates exceed array size')
+       call CON_stop(NameSub//': normalized coordinates are out of range')
     end if
 
     i1 = floor(Xy_D(1))
@@ -68,27 +69,28 @@ contains
          +             Dx1*A_II(i2,j2))
 
   end function bilinear
+
   !=========================================================================
-  real function trilinear(A_III, nI, nJ, nK, Xyz_D)
+  real function trilinear(A_III, iMin, iMax, jMin, jMax, kMin, kMax, Xyz_D)
 
     ! Calculate trilinear interpolation of A_III at position Xyz_D
 
-    integer, intent(in) :: nI, nJ, nK
-    real, intent(in) :: A_III(0:nI-1,0:nJ-1,0:nK-1)
-    real, intent(in) :: Xyz_D(3)
+    implicit none
+    integer, intent(in) :: iMin, iMax, jMin, jMax, kMin, kMax
+    real, intent(in)    :: A_III(iMin:iMax,jMin:jMax,kMin:kMax)
+    real, intent(in)    :: Xyz_D(3)
 
     integer :: i1, i2, j1, j2, k1, k2
     real    :: Dx1, Dx2, Dy1, Dy2, Dz1, Dz2
     character (len=*), parameter :: NameSub=NameMod//'::trilinear'
     !--------------------------------------------------------------------------
-    if(any(Xyz_D < 0.0)) then
+
+    if(  any( Xyz_D < (/iMin, jMin, kMin/)) .or. &
+         any( Xyz_D > (/iMax, jMax, kMax/))) then
+       write(*,*)'iMin, iMax, jMin, jMax, kMin, kMax=',&
+            iMin, iMax, jMin, jMax, kMin, kMax
        write(*,*)'Xyz_D =',Xyz_D
-       call CON_stop(NameSub//': negative coordinate is not valid')
-    end if
-    if(any(Xyz_D > (/ nI-1, nJ-1, nK-1 /))) then
-       write(*,*)'nI-1, nJ-1, nK-1=',nI-1, nJ-1, nK-1
-       write(*,*)'Xyz_D =',Xyz_D
-       call CON_stop(NameSub//': normalized coordinates exceed array size')
+       call CON_stop(NameSub//': normalized coordinates are out of range')
     end if
        
     i1 = floor(Xyz_D(1))
@@ -116,7 +118,7 @@ contains
 
     real :: A_II(2,3) = reshape((/ 1., 20., 3., 40., 5., 60. /), (/2, 3/))
     
-    real :: A_III(2,2,3) = reshape((/ &
+    real :: A_III(2,2,0:2) = reshape((/ &
          1., 20., 3., 40., &
          100., 2000., 300., 4000., &
          10000., 200000., 30000., 400000. /), (/2, 2, 3/))
@@ -124,13 +126,13 @@ contains
     real :: Result, GoodResult
 
     write(*,'(a)')'Testing function bilinear'
-    Result = bilinear(A_II, 2, 3, (/0.1, 1.2/))
+    Result = bilinear(A_II, 1, 2, 1, 3, (/1.1, 2.2/))
     GoodResult = 7.46
     if(abs(Result - GoodResult) > 1.e-5) &
          write(*,*) 'Test failed: Result=',Result,' differs from ',GoodResult
 
     write(*,'(a)')'Testing function trilinear'
-    Result = trilinear(A_III, 2, 2, 3, (/0.1, 0.2, 1.3/))
+    Result = trilinear(A_III, 1, 2, 1, 2, 0, 2, (/1.1, 1.2, 1.3/))
     GoodResult = 11236.2
     if(abs(Result - GoodResult) > 1.e-2) &
          write(*,*) 'Test failed: Result=',Result,' differs from ',GoodResult
