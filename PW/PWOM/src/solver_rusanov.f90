f3634494d9d7fcbe328b1b2c94727af092932a95
aglocer <>
2007-04-16 20:42:24 +0000

    Planet specific cases are taken out, general collision terms used instead of planet specific variable names. Old comments taken out.
    Former-commit-id: 964ab6766ebc99d960b4dee3edef77b7f82b7830

--- PW/PWOM/src/solver_rusanov.f90
+++ PW/PWOM/src/solver_rusanov.f90
@@ -4,7 +4,7 @@
 ! ----------------------------------------------------------------------------
 !/
 
-subroutine rusanov_solver(NameVariable, &
+subroutine rusanov_solver(iIon, &
      Rgas, &
      OldRho_C, OldU_C, OldP_C, OldT_C, &
      RhoBottom, UBottom, PBottom, &
@@ -16,7 +16,7 @@ subroutine rusanov_solver(NameVariable, &
   use ModCommonVariables
   implicit none
 
-  character(len=*), intent(in)          :: NameVariable
+  integer, intent(in)          :: iIon
   real, intent(in)                      :: Rgas
   real, dimension(MaxGrid), intent(in)  :: OldRho_C, OldU_C, OldP_C, OldT_C
   real, intent(in)                      :: RhoBottom, UBottom, PBottom
@@ -30,19 +30,12 @@ subroutine rusanov_solver(NameVariable, &
   real, dimension(MaxGrid+1)    :: RightRho_F, RightU_F, RightP_F
   real, dimension(MaxGrid+1)    :: RhoFlux_F, RhoUFlux_F, eFlux_F
 
-  logical:: DoTest,DoTest2,DoTest3
+
 
   !---------------------------------------------------------------------------
 
 
-  DoTest  = index(NameVariable,'H') > 0
 
-
-  if (NamePlanet == 'Earth ') then
-     DoTest  = index(NameVariable,'H') > 0
-     DoTest2 = index(NameVariable,'O') > 0
-     DoTest3 = index(NameVariable,'e') > 0
-  endif
 
 
 
@@ -83,7 +76,7 @@ subroutine rusanov_solver(NameVariable, &
 
 
   ! update the cells one timestep 
-  call update_state( DoTest, DoTest2, DoTest3,&
+  call update_state( iIon,&
        Rgas, &
        OldRho_C, OldU_C, OldP_C, OldT_C, &
        LeftRho_F, RightRho_F,LeftU_F, RightU_F,LeftP_F, RightP_F, &
@@ -91,50 +84,6 @@ subroutine rusanov_solver(NameVariable, &
        RhoSource_C, RhoUSource_C, eSource_C, &
        NewRho_C, NewU_C, NewP_C, NewT_C)
 
-!  if(DoTest)then
-!!     write(*,*)'OldRho=',OldRho_C(nDim-1:nDim)
-!!     write(*,*)'DHYD  =',DHYD(nDim-1:nDim)
-!!     write(*,*)'OldP  =',OldP_C(nDim-1:nDim)
-!!     write(*,*)'PHYD  =',PHYD(nDim-1:nDim)
-!!     write(*,*)'THYD  =',THYD(nDim-1:nDim)
-!!     write(*,*)'LeftP =',LeftP_F(nDim-1:nDim+1)
-!!     write(*,*)'RightP=',RightP_F(nDim-1:nDim+1)
-!!     write(*,*)'RhoFlux =',RhoFlux_F(nDim-1:nDim+1)
-!!     write(*,*)'RhoUFlux=',RhoUFlux_F(nDim-1:nDim+1)
-!!     write(*,*)'eFlux   =',eFlux_F(nDim-1:nDim+1)
-!!     write(*,*)'LeftRho_F  =',LeftRho_F(nDim-1:nDim+1)
-!!     write(*,*)'RightRho_F =',RightRho_F(nDim-1:nDim+1)
-!!     write(*,*)'LeftU_F    =',LeftU_F(nDim-1:nDim+1)
-!!     write(*,*)'RightU_F   =',RightU_F(nDim-1:nDim+1)
-!!     write(*,*)'OldU_G     =',OldU_G(nDim-1:nDim+2)
-!!     write(*,*)'LeftP_F    =',LeftP_F(nDim-1:nDim+1)
-!!     write(*,*)'RightP_F   =',RightP_F(nDim-1:nDim+1)
-!!     write(*,*)'NewRho=',NewRho_C(nDim-1:nDim)
-!!     write(*,*)'NewP  =',NewP_C(nDim-1:nDim)
-!!     write(*,*)'NewT  =',NewT_C(nDim-1:nDim)
-!
-!     write(*,*)'OldRho=',OldRho_C(1:3)
-!     write(*,*)'DHYD  =',DHYD(1:3)
-!     write(*,*)'OldP  =',OldP_C(1:3)
-!     write(*,*)'PHYD  =',PHYD(1:3)
-!     write(*,*)'THYD  =',THYD(1:3)
-!     write(*,*)'LeftP =',LeftP_F(1:3)
-!     write(*,*)'RightP=',RightP_F(1:3)
-!     write(*,*)'RhoFlux =',RhoFlux_F(1:3)
-!     write(*,*)'RhoUFlux=',RhoUFlux_F(1:3)
-!     write(*,*)'eFlux   =',eFlux_F(1:3)
-!     write(*,*)'LeftRho_F  =',LeftRho_F(1:3)
-!     write(*,*)'RightRho_F =',RightRho_F(1:3)
-!     write(*,*)'LeftU_F    =',LeftU_F(1:3)
-!     write(*,*)'RightU_F   =',RightU_F(1:3)
-!     write(*,*)'OldU_G     =',OldU_G(1:3)
-!     write(*,*)'LeftP_F    =',LeftP_F(1:3)
-!     write(*,*)'RightP_F   =',RightP_F(1:3)
-!     write(*,*)'NewRho=',NewRho_C(1:3)
-!     write(*,*)'NewP  =',NewP_C(1:3)
-!     write(*,*)'NewT  =',NewT_C(1:3)
-!
-!  end if
 
 end subroutine rusanov_solver
 
@@ -186,7 +135,7 @@ end subroutine rusanov_flux
 
 !==============================================================================
 
-subroutine update_state( DoTest, DoTest2, DoTest3,&
+subroutine update_state( iIon,&
      Rgas,&
      OldRho_C, OldU_C, OldP_C, OldT_C, &
      LeftRho_F, RightRho_F,LeftU_F, RightU_F,LeftP_F, RightP_F, &
@@ -196,7 +145,7 @@ subroutine update_state( DoTest, DoTest2, DoTest3,&
 
   use ModCommonVariables
   implicit none
-  logical, intent(in)                   :: DoTest,DoTest2,DoTest3  
+  integer, intent(in)                   :: iIon  
   real, intent(in)                      :: Rgas 
   real, dimension(MaxGrid), intent(in)  :: OldRho_C, OldU_C, OldP_C, OldT_C
   real, dimension(MaxGrid+1), intent(in):: LeftRho_F, LeftU_F, LeftP_F
@@ -205,14 +154,14 @@ subroutine update_state( DoTest, DoTest2, DoTest3,&
   real, dimension(MaxGrid), intent(in)  :: RhoSource_C, RhoUSource_C, eSource_C
   real, dimension(MaxGrid), intent(out) :: NewRho_C, NewU_C, NewP_C,NewT_C
   real, dimension(MaxGrid) :: NewRhoU, NewE
-  real :: InvGammaMinus1
+  real :: InvGammaMinus1,SumCollisionFreq,SumHTxCol,SumFHxCol
   real, dimension(MaxGrid) :: NewRhoUSource_C, ExplicitRhoU_C
   real :: CollisionNeutral1, CollisionNeutral2, &
           CollisionNeutral3,Collisionneutral4
   real :: CollisionCoefT1, CollisionCoefM1, CollisionCoefT2, CollisionCoefM2,&
           CollisionCoefT3, CollisionCoefM3, CollisionCoefT4, CollisionCoefM4
   real :: ModifyESource
-  integer :: i
+  integer :: i,iSpecies,MinSpecies
   !----------------------------------------------------------------------------
   InvGammaMinus1 = 1.0/(Gamma - 1.0)
 
@@ -237,172 +186,51 @@ subroutine update_state( DoTest, DoTest2, DoTest3,&
 
      NewP_C(i) = gMin1*(NewE(i) - 0.5 * NewU_C(i)**2 * NewRho_C(i))
 
-     If (IsImplicit .and. (NamePlanet .eq. 'Saturn')) then
-!        !get rid of the contribution to the terms that are implicit
-        If (DoTest) then
-           CollisionNeutral1=CFHpH(i)
-           CollisionNeutral2=CFHPH2(i)
-           CollisionCoefT1  =CTHpH
-           CollisionCoefM1  =XMSH/(XMSH+XMSH)
-           CollisionCoefT2  =CTHpH2
-           CollisionCoefM2  =XMSH/(XMSH+2.*XMSH)
-
-           CollisionNeutral3=CFHpH3p(i)
-           CollisionCoefT3  =CTHpH3p
-           CollisionCoefM3  =XMSH/(XMSH+XMSO)
-
-           CollisionNeutral4=CFHpEL(i)
-           CollisionCoefT4  =CTHpEL
-           CollisionCoefM4  =XMSH/(XMSH+XMSE)
-
+     If (IsImplicit) then
+        !If is ImplictAll = T then treat ions and neutral contributions
+        !with point implicit. Else Just treat neutrals with point implicit
+        If (IsImplicitAll) then
+           MinSpecies = 1
         else
-           CollisionNeutral1=CFH3pH(i)
-           CollisionNeutral2=CFH3pH2(i)
-           CollisionCoefT1  =CTH3pH
-           CollisionCoefM1  =XMSO/(XMSO+XMSH)
-           CollisionCoefT2  =CTH3pH2
-           CollisionCoefM2  =XMSO/(XMSO+2.*XMSH)
-
-           CollisionNeutral3=CFH3pHp(i)
-           CollisionCoefT3  =CTH3pHp
-           CollisionCoefM3  =XMSO/(XMSO+XMSH)
-
-           CollisionNeutral4=CFH3pEL(i)
-           CollisionCoefT4  =CTH3pEL
-           CollisionCoefM4  =XMSO/(XMSO+XMSE)
-
+           MinSpecies = 5
         endif
-        !eliminate contribution of implicit sources in RhoU
-        ExplicitRhoU_C(i) = NewRhoU(i)+Dt*OldU_C(i)*OldRho_C(i)* &
-             ( CollisionNeutral1 + CollisionNeutral2 &
-             + CollisionNeutral3 + CollisionNeutral4)
+        SumCollisionFreq = 0.0
+        SumHTxCol        = 0.0
+        SumFHxCol        = 0.0
+        do iSpecies=MinSpecies,nSpecies
+           if (iSpecies /= iIon) then
+              SumCollisionFreq = &
+                   SumCollisionFreq+CollisionFreq_IIC(iIon,iSpecies,i)
+              SumHTxCol        = SumHTxCol &
+                   + HeatFlowCoef_II(Ion2_,iSpecies)*CollisionFreq_IIC(iIon,iSpecies,i)
+              SumFHxCol        = SumFHxCol &
+                   + FricHeatCoef_II(Ion2_,iSpecies)*CollisionFreq_IIC(iIon,iSpecies,i)
+           endif
+        enddo
         
+        !eliminate contribution of implicit sources in RhoU
+        ExplicitRhoU_C(i) = &
+             NewRhoU(i)+Dt*OldU_C(i)*OldRho_C(i)*SumCollisionFreq
+        
+
         NewRhoU(i)= ExplicitRhoU_C(i) / &
-             (1+Dt*(CollisionNeutral1 + CollisionNeutral2 &
-             +      CollisionNeutral3 + CollisionNeutral4))
+             (1+Dt*SumCollisionFreq)
         
         NewU_C(i) = NewRhoU(i)/NewRho_C(i)
+
         !When making p implicit we have to take the cf*cm*Ti term out of 
         !esource. Do this by adding ModifyEsource*dt to esource.
         ModifyESource = OldRho_C(i)* &
-             (CollisionNeutral1*CollisionCoefT1  &
-             +CollisionNeutral2*CollisionCoefT2  &
-             +CollisionNeutral3*CollisionCoefT3  &
-             +CollisionNeutral4*CollisionCoefT4)*OldT_C(i)
-!
-!        if (i .lt. 3 .and. dotest)write(*,*) ModifyESource, &
-!             -tHyd(i)*dHyd(i)*(CTHpH*CFHpH(I)+CTHpH2*CFHpH2(I))
-!        
-!        if (i .lt. 4)write(*,*) i,newe(i)
-        NewE(i)=NewE(i) + Dt*ModifyESource
-!        if (i .lt. 4)write(*,*) i,newe(i)
+             SumHTxCol*OldT_C(i)
+
+        NewE(i)   = NewE(i) + Dt*ModifyESource
 
         NewP_C(i) = gMin1*(NewE(i) - 0.5 * NewU_C(i)**2 * NewRho_C(i))
-!        if (i .lt. 3) write(*,*) i,(1+Dt*3.* &
-!             (CollisionNeutral1*CollisionCoefM1 + &
-!             CollisionNeutral1*CollisionCoefM1)),NewP_C(i)
         
-        NewP_C(i) = NewP_C(i) / (1+Dt*2.* &
-             (CollisionNeutral1*CollisionCoefM1  &
-             +CollisionNeutral2*CollisionCoefM2  & 
-             +CollisionNeutral3*CollisionCoefM3  &
-             +CollisionNeutral4*CollisionCoefM4))
+        NewP_C(i) = NewP_C(i) / (1+Dt*2.*SumFHxCol)
 
-!        if (i .lt. 4)write(*,*) NewP_C(i)/gMin1 +.5*NewU_C(i)**2*NewRho_C(i)
      endif
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-     If (IsImplicit .and. (NamePlanet .eq. 'Earth ')) then
-!        !get rid of the contribution to the terms that are implicit
-        If (DoTest .and. .not. DoTest3) then
-           CollisionNeutral1=CFHN2(i)
-           CollisionNeutral2=CFHO(i)
-           CollisionCoefT1  =CTHN2
-           CollisionCoefM1  = XMSH/(XMSH+28.0*XAMU)
-           CollisionCoefT2  =CTHO
-           CollisionCoefM2  =XMSH/(XMSH+XMSO)
-
-           CollisionNeutral3=CFHO2(i)
-           CollisionCoefT3  =CTHO2
-           CollisionCoefM3  =XMSH/(XMSH+2.0*XMSO)
-                             
-           CollisionNeutral4=CFHHL(i)
-           CollisionCoefT4  =CTHHL
-           CollisionCoefM4  =XMSH/(XMSH+XMSHE)
-
-        else if (DoTest2) then
-           CollisionNeutral1=CFOXN2(i)
-           CollisionNeutral2=CFOXO(i)
-           CollisionCoefT1  =CTOXN2
-           CollisionCoefM1  =XMSO/(XMSO+28.0*XAMU)
-           CollisionCoefT2  =CTOXO
-           CollisionCoefM2  =XMSO/(XMSO+XMSO)
-
-           CollisionNeutral3=CFOXO2(i)
-           CollisionCoefT3  =CTOXO2
-           CollisionCoefM3  =xmso/(xmso+2.0*xmso)
-                             
-           CollisionNeutral4=CFOXHL(i)
-           CollisionCoefT4  =CTOXHL
-           CollisionCoefM4  =xmso/(xmso+xmshe)
-
-        else
-           CollisionNeutral1=CFHeN2(i)
-           CollisionNeutral2=CFHeO(i)
-           CollisionCoefT1  =CTHeN2
-           CollisionCoefM1  =XMSHe/(XMSHe+28.0*XAMU)
-           CollisionCoefT2  =CTHeO
-           CollisionCoefM2  =XMSHe/(XMSHe+XMSO)
-
-           CollisionNeutral3=CFHeO2(i)
-           CollisionCoefT3  =CTHeO2
-           CollisionCoefM3  =xmsHe/(xmsHe+2.0*xmso)
-                             
-           CollisionNeutral4=CFHeHE(i)
-           CollisionCoefT4  =CTHeHE
-           CollisionCoefM4  =xmsHe/(xmsHe+xmshe)
-
-        endif
-        !eliminate contribution of implicit sources in RhoU
-        ExplicitRhoU_C(i) = NewRhoU(i)+Dt*OldU_C(i)*OldRho_C(i)* &
-             ( CollisionNeutral1 + CollisionNeutral2 &
-             + CollisionNeutral3 + CollisionNeutral4)
-        
-        NewRhoU(i)= ExplicitRhoU_C(i) / &
-             (1+Dt*(CollisionNeutral1 + CollisionNeutral2 &
-             +      CollisionNeutral3 + CollisionNeutral4))
-        
-        NewU_C(i) = NewRhoU(i)/NewRho_C(i)
-        !When making p implicit we have to take the cf*cm*Ti term out of 
-        !esource. Do this by adding ModifyEsource*dt to esource.
-        ModifyESource = OldRho_C(i)* &
-             (CollisionNeutral1*CollisionCoefT1  &
-             +CollisionNeutral2*CollisionCoefT2  &
-             +CollisionNeutral3*CollisionCoefT3  &
-             +CollisionNeutral4*CollisionCoefT4)*OldT_C(i)
-!
-!        if (i .lt. 3 .and. dotest)write(*,*) ModifyESource, &
-!             -tHyd(i)*dHyd(i)*(CTHpH*CFHpH(I)+CTHpH2*CFHpH2(I))
-!        
-!        if (i .lt. 4)write(*,*) i,newe(i)
-        NewE(i)=NewE(i) + Dt*ModifyESource
-!        if (i .lt. 4)write(*,*) i,newe(i)
-
-        NewP_C(i) = gMin1*(NewE(i) - 0.5 * NewU_C(i)**2 * NewRho_C(i))
-!        if (i .lt. 3) write(*,*) i,(1+Dt*3.* &
-!             (CollisionNeutral1*CollisionCoefM1 + &
-!             CollisionNeutral1*CollisionCoefM1)),NewP_C(i)
-        
-        NewP_C(i) = NewP_C(i) / (1+Dt*2.* &
-             (CollisionNeutral1*CollisionCoefM1  &
-             +CollisionNeutral2*CollisionCoefM2  & 
-             +CollisionNeutral3*CollisionCoefM3  &
-             +CollisionNeutral4*CollisionCoefM4))
-
-!        if (i .lt. 4)write(*,*) NewP_C(i)/gMin1 +.5*NewU_C(i)**2*NewRho_C(i)
-     endif
-
- 
+     
      
      NewT_C(i)=NewP_C(i)/Rgas/NewRho_C(i)
   end do
