8a8d24ef2a3992a6725633542cf29be0e00aaf03
aglocer <>
2007-04-17 20:46:55 +0000

    These changes reflect the new chemical scheme used in the saturn polar wind paper
    Former-commit-id: 2990f4db02d09292fb7cc6b4f449ed9daa2cc6ed

--- PW/PWOM/src/calc_chemical_equilibrium.f90
+++ PW/PWOM/src/calc_chemical_equilibrium.f90
@@ -3,6 +3,7 @@
 !******************************************************************************
 
 subroutine calc_chemical_equilibrium(DensityHp, DensityH3p)
+  use ModCommonVariables
   implicit none
   real, intent(out) :: DensityHp, DensityH3p
   real :: OldDensityHp, OldDensityH3p, NewDensityHp, NewDensityH3p
@@ -10,38 +11,28 @@ subroutine calc_chemical_equilibrium(DensityHp, DensityH3p)
   real :: alpha1, alpha2, beta1, beta2, beta3
   real :: eta0, eta1, eta2, eta3
   real :: zeta0,zeta1,zeta2
-  real :: jp1, jp2, jp3, jp4, kc1, kc2, kc3, kc6, kc7, kc8, kr1, kr2
-  real :: InitialGuess
+  real :: InitialGuess,kr1T,kr2T
   integer :: i
   
   call MODATM(1400.0e5,DensityH2, DensityH, DensityH2O, DensityCH4,Temperature)
   
 
-  jp1=9.5e-11
-  jp2=5.4e-10
-  jp3=7.3e-10
-  jp4=1.3e-10
-  kc1=2.0e-9
-  kc2=3.2e-29
-  kc3=4.15e-9
-  kc6=2.4e-9
-  kc7=5.3e-9
-  kc8=8.2e-9
-  kr1=2.0e-12
-  kr2=4.6e-6*Temperature**(-0.65)
-  
+  kr1T=kr1*(Temperature**(-0.7))
+  kr2T=kr2*(Temperature**(-0.5))
+
 
   alpha1= jp1*DensityH2 + jp3*DensityH + jp4*DensityH2O
-  alpha2= -kc2*DensityH2**2. - kc3*DensityCH4 - kc8*DensityH2O
+  alpha2= -kc2*DensityH2**2. - kc3*DensityCH4 - kc8*DensityH2O &
+          - kc9(1)*DensityH2
   
   beta1 = kc1*(jp2/kc1)*DensityH2
-  beta2 =kc2*DensityH2**2.
+  beta2 = kc2*DensityH2**2.+ kc9(1)*DensityH2
   beta3 =-kc6*DensityCH4-kc7*DensityH2O
   
-  eta0  =-kr2*alpha1**2.
-  eta1  =beta3*kr1*alpha1 - 2.*kr2*alpha1*alpha2
-  eta2  =beta1*kr1**2. + beta3*kr1*alpha2 + kr2*kr1*alpha1 - kr2*alpha2**2.
-  eta3  =beta2*kr1**2.-beta3*kr1**2.+kr2*kr1*alpha2 
+  eta0  =-kr2T*alpha1**2.
+  eta1  =beta3*kr1T*alpha1 - 2.*kr2T*alpha1*alpha2
+  eta2  =beta1*kr1T**2. + beta3*kr1T*alpha2 + kr2T*kr1T*alpha1 - kr2T*alpha2**2.
+  eta3  =beta2*kr1T**2.-beta3*kr1T**2.+kr2T*kr1T*alpha2 
 
   
   InitialGuess=1500.
@@ -60,11 +51,11 @@ subroutine calc_chemical_equilibrium(DensityHp, DensityH3p)
   
   DensityHp=NewDensityHp
 
-!  DensityH3p=(kr1*DensityHp**2.- alpha1 - alpha2*DensityHp)/(-kr1*DensityHp)
+!  DensityH3p=(kr1T*DensityHp**2.- alpha1 - alpha2*DensityHp)/(-kr1T*DensityHp)
 
   zeta0=beta1+beta2*DensityHp
-  zeta1=beta3-kr2*DensityHp
-  zeta2=-kr2
+  zeta1=beta3-kr2T*DensityHp
+  zeta2=-kr2T
 
   DensityH3p=(-zeta1-(zeta1**2.0-4.0*zeta2*zeta0)**0.5)/(2.0*zeta2)
 
