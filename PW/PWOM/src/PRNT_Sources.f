b77eab6beba4e9b0217236cfe87df14b9a0f838b
aglocer <>
2007-05-14 20:40:59 +0000

    replace admso fclnso with Source_GV
    Former-commit-id: 2d21db047932a36e7d6892740c89ee5c2b1e3172

--- PW/PWOM/src/PRNT_Sources.f
+++ PW/PWOM/src/PRNT_Sources.f
@@ -15,8 +15,8 @@ C ============================================================================
 
 
          WRITE (iUnitSourceGraphics,"(100(1pe18.10))") 
-     &        ALTD(K),ADMSO(K),ADMSH(K),ADMSHE(K),ADMSE(K),FCLSNO(K),FCLSNH(K),
-     &        FCLSHE(K),FCLSNE(K),ECLSNO(K),ECLSNE(K),ECLSHE(K),ECLSNE(K),
+     &        ALTD(K),Source_GV(K,RhoO_),Source_GV(K,RhoH_),Source_GV(K,RhoHe_),Source_GV(K,RhoE_),Source_GV(K,uO_),Source_GV(K,uH_),
+     &        Source_GV(K,uHe_),Source_GV(K,uE_),Source_GV(K,pO_),Source_GV(K,pE_),Source_GV(K,pHe_),Source_GV(K,pE_),
      &        EFIELD(K), GRAVTY(K)
       enddo
 
