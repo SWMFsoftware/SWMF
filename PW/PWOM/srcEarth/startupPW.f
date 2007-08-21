fccf481c5c68fb74a3971d916bcef1509bd8f9c2
aglocer <>
2007-08-21 20:42:20 +0000

    fixed date update
    Former-commit-id: 72aad6ef97c344500316ae8836ae84a7cdadc669

--- PW/PWOM/srcEarth/startupPW.f
+++ PW/PWOM/srcEarth/startupPW.f
@@ -213,8 +213,9 @@ CALEX IYD=year_day of year
 !      GMLAT=80.
 C END
       CALL GGM(IART,GLONG,GLAT,GMLONG,GMLAT)
+      
       iDay  = mod(IYD,1000)+floor(Time/24.0/3600.0)
-      iYear = mod(IYD,1000)
+      iYear = IYD/1000
       if(iDay > 364) then
          iDay = mod(iDay,364)
          iYear= iYear+1
@@ -222,6 +223,7 @@ C END
       else
          IYD = iYear*1000+iDay
       endif
+      
       SEC=mod(Time,24.0*3600.0)
       !mod((STL-GLONG/15.)*3600.,24.*3600.)
       STL=SEC/3600+GLONG/15
