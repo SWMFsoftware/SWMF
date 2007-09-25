be7ad8d5741c5ed1e72559f0536b94e97fa46774
aglocer <>
2007-09-25 17:57:52 +0000

    Add Te state to implicitly solved variables
    Former-commit-id: 7e13906cabc98f041014260f825619d6875f1192

--- PW/PWOM/src/ModPwImplicit.f90
+++ PW/PWOM/src/ModPwImplicit.f90
@@ -1,16 +1,16 @@
 module ModPwImplicit
-
+  use ModCommonPlanet, ONLY: iRho,iU,iP,iTe_
   implicit none
 
-  real :: Te
-  real, allocatable :: StateOrig_GV(:,:)
+
+  real, allocatable :: StateOrig_GV(:,:),StateFull_GV(:,:)
   contains
   !============================================================================
 
   subroutine PW_calc_residual(nOrder, DtIn, nCell, nVar, StateIn_GV, Resid_CV)
 
     use ModCommonVariables, ONLY: iRho_I,iU_I,iP_I,iT_I,Source_CV,RGAS_I, &
-         MassElecIon_I,nIon,RhoE_,uE_,Te_,pE_,Curr,HeatCon_GI
+         MassElecIon_I,nIon,RhoE_,uE_,Te_,pE_,Curr,CURRMX,HeatCon_GI
     use ModPWOM, ONLY: beta
 
     implicit none
@@ -32,53 +32,50 @@ module ModPwImplicit
     ! Put the updated ReducedState back into the State array,
     ! and get temperature
     do iIon = 1,nIon-1
-       StateOrig_GV(:,iRho_I(iIon))= StateIn_GV(:,iRho(iIon))
-       StateOrig_GV(:,iU_I  (iIon))= StateIn_GV(:,iU  (iIon))
-       StateOrig_GV(:,iP_I  (iIon))= StateIn_GV(:,iP  (iIon))
-       StateOrig_GV(:,iT_I  (iIon))= &
-            StateOrig_GV(:,iP_I(iIon))/Rgas_I(iIon)/StateOrig_GV(:,iRho_I(iIon))
+       StateFull_GV(:,iRho_I(iIon))= StateIn_GV(:,iRho(iIon))
+       StateFull_GV(:,iU_I  (iIon))= StateIn_GV(:,iU  (iIon))
+       StateFull_GV(:,iP_I  (iIon))= StateIn_GV(:,iP  (iIon))
+       StateFull_GV(:,iT_I  (iIon))= &
+            StateFull_GV(:,iP_I(iIon))/Rgas_I(iIon)/StateFull_GV(:,iRho_I(iIon))
     enddo
-        
-    ! Set electron density and velocity
-    StateOrig_GV(1:nCell,RhoE_)=0.0
-    StateOrig_GV(1:nCell,uE_)  =0.0
-    do k=1,nCell
+    StateFull_GV(:,iT_I(nIon))= StateIn_GV(:,iTe_)
+    
+    ! Set electron density and velocity and pressure
+    StateFull_GV(1:nCell+2,RhoE_)=0.0
+    StateFull_GV(1:nCell+2,uE_)  =0.0
+    StateFull_GV(-1:0,RhoE_)=StateOrig_GV(-1:0,RhoE_)
+    StateFull_GV(-1:0,uE_)  =StateOrig_GV(-1:0,uE_)
+    do k=1,nCell+2
        do iIon=1,nIon-1
-          StateOrig_GV(k,RhoE_) = &
-               StateOrig_GV(k,RhoE_)+MassElecIon_I(iIon)*StateOrig_GV(k,iRho_I(iIon))
-          StateOrig_GV(k,uE_)= &
-               StateOrig_GV(k,uE_)+ &
-               (MassElecIon_I(iIon)*StateOrig_GV(k,iRho_I(iIon))&
-               *StateOrig_GV(k,iU_I(iIon)))
+          StateFull_GV(k,RhoE_) = &
+               StateFull_GV(k,RhoE_)+MassElecIon_I(iIon)*StateFull_GV(k,iRho_I(iIon))
+          StateFull_GV(k,uE_)= &
+               StateFull_GV(k,uE_)+ &
+               (MassElecIon_I(iIon)*StateFull_GV(k,iRho_I(iIon))&
+               *StateFull_GV(k,iU_I(iIon)))
        enddo
-
-       StateOrig_GV(k,uE_)=(StateOrig_GV(k,uE_) -1.8965E-18*CURR(k))&
-                               /StateOrig_GV(k,RhoE_)
+       if (k <= nCell) then
+          StateFull_GV(k,uE_)=(StateFull_GV(k,uE_) -1.8965E-18*CURR(k))&
+               /StateFull_GV(k,RhoE_)
+       elseif (k > nCell) then
+          StateFull_GV(k,uE_)=(StateFull_GV(k,uE_) -1.8965E-18*CURRMX)&
+               /StateFull_GV(k,RhoE_)
+       endif
     enddo
+
+    StateFull_GV(:,iP_I(nIon))= &
+         Rgas_I(nIon)*StateFull_GV(:,iRho_I(nIon))*StateIn_GV(:,iTe_)
     
-    call COLLIS(nCell,StateOrig_GV)
-
-    call PW_calc_efield(nCell,StateOrig_GV)         
-
-
-!    Source_CV(:,:) = 0.0
+    call COLLIS(nCell,StateFull_GV)
     
-!    if (nOrder == 1) then
-!       do iIon=1,nIon-1
-!          call solver(iIon,DtIn, StateIn_GV(:,iRho_I(iIon):iT_I(iIon)),&
-!               Source_CV(:,iRho_I(iIon)),Source_CV(:,iP_I(iIon)),&
-!               Source_CV(:,iU_I(iIon)),&
-!               RGAS_I(iIon),NewState_GV(:,iRho_I(iIon):iT_I(iIon)))
-!       enddo
-!
-!    else if (nOrder == 2) then
+    call PW_calc_efield(nCell,StateFull_GV)         
 
     if(nOrder == 1)then
        beta = 0.0
     else
        beta = 1.5
     end if
-
+    
     do iIon=1,nIon-1
        call rusanov_solver(iIon,nCell,RGAS_I(iIon),DtIn,   &
             StateIn_GV(-1:nCell+2,iRho(iIon):iP(iIon)),&
@@ -88,6 +85,13 @@ module ModPwImplicit
             NewState_GV(-1:nCell+2,iRho(iIon):iP(iIon)))
     enddo
 
+    call PW_eheat_flux_explicit(nCell,RGAS_I(nIon),DtIn,      &
+         StateFull_GV(-1:nCell+2,iRho_I(nIon):iP_I(nIon)),       &
+         Source_CV(1:nCell,iRho_I(nIon)), Source_CV(1:nCell,iU_I(nIon)),&
+         Source_CV(1:nCell,iP_I(nIon)),                           &
+         HeatCon_GI(0:nCell+1,nIon),                        &
+         NewState_GV(-1:nCell+2,iTe_))
+    
     Resid_CV(1:nCell,:) = NewState_GV(1:nCell,:)
 
     deallocate(NewState_GV)
@@ -119,6 +123,8 @@ module ModPwImplicit
        ! Lower boundary is fixed, copy it from StateOrig_GV
        State_GV(-1:0,iRho(iIon):iP(iIon)) = &
             StateOrig_GV(-1:0,iRho_I(iIon):iP_I(iIon))
+       State_GV(-1:0,iTe_) = &
+            StateOrig_GV(-1:0,iT_I(nIon))
 
        ! Upper boundary requires temperature and scaleheight
        T_C(iIon) = &
@@ -128,7 +134,7 @@ module ModPwImplicit
               
        ! cBoltzmann [cgs] = 1.0e7 * cBoltzmann [SI] 
        ScaleHeight_I(iIon) =&
-            1.0e7*cBoltzmann*( T_C(iIon)+Te )&
+            1.0e7*cBoltzmann*( T_C(iIon)+State_GV(nCell,iTe_) )&
             /(abs(Gravty(nCell))*Mass_I(iIon))
 
        !State_GV(nCell+1:nCell+2,iP(iIon)) = &
@@ -159,7 +165,13 @@ module ModPwImplicit
             /State_GV(nCell+2,iRho(iIon))
        
     enddo
-
+    
+    ! Set Te boundary
+      
+      State_GV(nCell+1,iTe_)=&
+           State_GV(nCell,iTe_)+ETOP/State_GV(nCell,iTe_)**2.5
+      State_GV(nCell+2,iTe_)=&
+           State_GV(nCell+1,iTe_)+ETOP/State_GV(nCell,iTe_)**2.5
 
     ! If using GM --> PW coupling, overwrite ghostcell pressure
     ! with fraction of MHD pressure using 
@@ -168,7 +180,7 @@ module ModPwImplicit
     if (UseGmToPw) then
        State_GV(nCell+1,iP(Ion1_)) = &
             (  T_C(Ion1_)&
-            / ( T_C(Ion1_) + Te )&
+            / ( T_C(Ion1_) + State_GV(nCell,iTe_) )&
             )*p_I(iLine)
        State_GV(nCell+1,iRho(Ion1_)) = &
             State_GV(nCell+1,iP(Ion1_)) / Rgas_I(Ion1_) / T_C(Ion1_)
@@ -185,23 +197,27 @@ module ModPwImplicit
     use ModCommonVariables, ONLY: Dt,DrBnd,nDim,nVar,State_GV,nIon,&
                                   RhoE_,uE_,iRho_I,iU_I,iP_I,iT_I,&
                                   MassElecIon_I,&
-                                  Curr,nCell => nDim,Rgas_I
+                                  Curr,nCell => nDim,Rgas_I,HeatCon_GI,Dt
     
-    real                :: ImplPar = 0.005, DtExpl, DtImpl
+    real                :: ImplPar = 1.0, DtExpl, DtImpl
     real,allocatable    :: ReducedState_GV(:,:)
     integer :: k,iIon, nVarReduced
     !--------------------------------------------------------------------------
-    
     DtImpl = Dt
-    DtExpl = 0.001*(DrBnd/2.0e6)
+    !DtExpl = 0.01*(DrBnd/2.0e6)
+    ! DtExpl = dx^2/2/max(kappa) 
+    
+    DtExpl = &
+         0.5*DrBnd**2.0 &
+         *minval(State_GV(1:nCell,iRho_I(nIon))/HeatCon_GI(1:nCell,nIon)) * 0.01
 
-    write(*,*)'DtImpl, DtExpl=',DtImpl, DtExpl
+    !write(*,*)'DtImpl, DtExpl=',DtImpl, DtExpl
 
-    nVarReduced = 3*(nIon-1)
+    nVarReduced = 3*(nIon-1)+1
 
     if (.not.allocated(ReducedState_GV)) then
        allocate(ReducedState_GV(-1:nCell+2,nVarReduced),&
-            StateOrig_GV(-1:nCell+2,nVar))
+            StateOrig_GV(-1:nCell+2,nVar),StateFull_GV(-1:nCell+2,nVar))
     endif
 
     StateOrig_GV(-1:nCell+2,:) =  State_GV(-1:nCell+2,:)
@@ -211,9 +227,8 @@ module ModPwImplicit
        ReducedState_GV(-1:nCell+2,iU  (iIon)) = State_GV(-1:nCell+2,iU_I  (iIon))
        ReducedState_GV(-1:nCell+2,iP  (iIon)) = State_GV(-1:nCell+2,iP_I  (iIon))
     enddo
-    
-    Te = State_GV(nCell,iT_I(nIon))
-    
+    ReducedState_GV(-1:nCell+2,iTe_) = State_GV(-1:nCell+2,iT_I(nIon))
+
     ! Get the implicit update for the independent variables 
     call implicit_solver(ImplPar, DtImpl, DtExpl, nDim, nVarReduced, &
          ReducedState_GV(-1:nDim+2,:), &
@@ -232,8 +247,9 @@ module ModPwImplicit
             State_GV(-1:nCell+2,iP_I  (iIon))&
             /Rgas_I(iIon)/State_GV(-1:nCell+2,iRho_I(iIon))
     enddo
-        
-    ! Set electron density and velocity
+    State_GV(-1:nCell+2,iT_I(nIon)) = ReducedState_GV(-1:nCell+2,iTe_)
+    
+    ! Set electron density and velocity and pressure
     State_GV(1:nCell,RhoE_)=0.0
     State_GV(1:nCell,uE_)  =0.0
     do k=1,nCell
@@ -247,42 +263,46 @@ module ModPwImplicit
        enddo
 
        State_GV(k,uE_)=(State_GV(k,uE_) -1.8965E-18*CURR(k))/State_GV(k,RhoE_)
+       
     enddo
-    
-    deallocate(ReducedState_GV,StateOrig_GV)
+    State_GV(-1:nCell+2,iP_I  (nIon)) = &
+            State_GV(-1:nCell+2,iT_I(nIon))&
+            *Rgas_I(iIon)*State_GV(-1:nCell+2,iRho_I(nIon))
+
+    deallocate(ReducedState_GV,StateOrig_GV,StateFull_GV)
   end subroutine PW_implicit_update
-  !============================================================================
-
-  integer function iRho(iIon)
-    integer, intent(in) :: iIon
-    
-    !--------------------------------------------------------------------------
-    
-    iRho = 3*(iIon-1) + 1
-    return
-  end function iRho
-  
-  !============================================================================
-
-  integer function iU(iIon)
-    integer, intent(in) :: iIon
-    
-    !--------------------------------------------------------------------------
-    
-    iU = 3*(iIon-1) + 2
-    return
-  end function iU
-  
-  !============================================================================
-
-  integer function iP(iIon)
-    integer, intent(in) :: iIon
-    
-    !--------------------------------------------------------------------------
-    
-    iP = 3*(iIon-1) + 3
-    return
-  end function iP
+!  !============================================================================
+!
+!  integer function iRho(iIon)
+!    integer, intent(in) :: iIon
+!    
+!    !--------------------------------------------------------------------------
+!    
+!    iRho = 3*(iIon-1) + 1
+!    return
+!  end function iRho
+!  
+!  !============================================================================
+!
+!  integer function iU(iIon)
+!    integer, intent(in) :: iIon
+!    
+!    !--------------------------------------------------------------------------
+!    
+!    iU = 3*(iIon-1) + 2
+!    return
+!  end function iU
+!  
+!  !============================================================================
+!
+!  integer function iP(iIon)
+!    integer, intent(in) :: iIon
+!    
+!    !--------------------------------------------------------------------------
+!    
+!    iP = 3*(iIon-1) + 3
+!    return
+!  end function iP
 
 
 end module ModPwImplicit
