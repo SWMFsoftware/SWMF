e1bad1bbdc4de444d0ea90a6b07f3abb6919ff97
gtoth <>
2007-09-28 20:05:39 +0000

    cd into local directory if filename contains a / write out script name for errors save lines after the final #END command if any
    Former-commit-id: d3b83ea459d65c8bc52aea0a266d82454ae7a367

--- share/Scripts/ExpandParam.pl
+++ share/Scripts/ExpandParam.pl
@@ -15,14 +15,24 @@
 # 01/20/2002 G. Toth - initial version
 #EOP
 
+$ERROR = "ExpandParam.pl_ERROR";
+
 # Default filename
-$input='PARAM.in';
+$basefile='PARAM.in';
 
 # Overwrite with argument if it exists
-$input=$ARGV[0] if $ARGV[0];
+$basefile=$ARGV[0] if $ARGV[0];
+
+# Extract directoryname if any
+
+if($basefile =~ /\/([^\/]+)$/){
+    $dir = $`;
+    $basefile = $1;
+    chdir $dir or die "$ERROR: could not cd $dir\n";
+}
 
 # Expand the file recursively
-&process_file($input, 'fh00');
+&process_file($basefile, 'fh00');
 
 exit;
 ##############################################################################
@@ -30,7 +40,7 @@ exit;
 sub process_file {
     local($filename, $input) = @_;
     $input++;
-    open($input, $filename) || die"Can't open $filename: $!\n";
+    open($input, $filename) or die"$ERROR: can't open $filename: $!\n";
     while (<$input>) {
 	# Stop reading if #END command is read
         last if /^#END\b/;
@@ -45,6 +55,9 @@ sub process_file {
 	    # Print line as it is otherwise
 	    print;
 	}
+    }
+    if($basefile eq $filename){
+	print "\#END\n",<$input>;
     }
     close $input;
 }
