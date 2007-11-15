47d0a6d4fba5a573b6e0b057f4d6f0b55c09e131
plasmint <>
2007-11-15 22:37:17 +0000

    Fixed typo
    Former-commit-id: 4f4595d7c77cdbf905a165bcc1182e07cc935c72

--- util/EMPIRICAL/srcGM/ModTsyganenko.f
+++ util/EMPIRICAL/srcGM/ModTsyganenko.f
@@ -1,4 +1,4 @@
-      BLOCK DATA EGM_ModTsyanenkoData
+      BLOCK DATA EGM_ModTsyganenkoData
 
       IMPLICIT REAL*8 (A-H,O-Z)
       COMMON /EGM_COORD11/ XX1(12),YY1(12)
@@ -24,7 +24,7 @@ C     CURVATURE  OF THE WARPING (SEE P.89, NB #2)
       DATA TILT,XCENTRE,RADIUS,DIPX,DIPY /1.00891,2.28397,-5.60831,
      *     1.86106,7.83281,1.12541,0.945719/
 
-      End Block Data EGM_ModTsyganenkoData
+      end BLOCK DATA EGM_ModTsyganenkoData
 
 
       module EGM_ModTsyganenko
