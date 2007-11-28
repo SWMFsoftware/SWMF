6d972289ab24fec1315e2779c1bd248fe16c9ed9
gtoth <>
2007-11-28 20:56:05 +0000

    Added ssflux.f and change -O0 to -O1 for all files. Passed the tests.
    Former-commit-id: 6c36aea938aba2c90fd51fa7f24e9f8cf6bdb017

--- PW/PWOM/src/Makefile.RULES.Darwin.f95
+++ PW/PWOM/src/Makefile.RULES.Darwin.f95
@@ -1,14 +1,16 @@
 PhotoElectronPW_planet.o: PhotoElectronPW_planet.f
-	${COMPILE.f77} ${Cflag0} -132 PhotoElectronPW_planet.f
+	${COMPILE.f77} ${Cflag1} -132 PhotoElectronPW_planet.f
 IonHeatFluxPW_planet.o: IonHeatFluxPW_planet.f
-	${COMPILE.f77} ${Cflag0} -132 IonHeatFluxPW_planet.f
+	${COMPILE.f77} ${Cflag1} -132 IonHeatFluxPW_planet.f
 collisionPW_planet.o: collisionPW_planet.f
-	${COMPILE.f77} ${Cflag0} -132 collisionPW_planet.f
+	${COMPILE.f77} ${Cflag1} -132 collisionPW_planet.f
 precipitationPW.o: precipitation_planet.f
-	${COMPILE.f77} ${Cflag0} -132 precipitationPW_planet.f
+	${COMPILE.f77} ${Cflag1} -132 precipitationPW_planet.f
 glowex_planet.o: glowex_planet.f
-	${COMPILE.f77} ${Cflag0} -132 glowex_planet.f
+	${COMPILE.f77} ${Cflag1} -132 glowex_planet.f
 eHeatFluxPW.o: eHeatFluxPW.f
-	${COMPILE.f77} ${Cflag0} -132 eHeatFluxPW.f
+	${COMPILE.f77} ${Cflag1} -132 eHeatFluxPW.f
 startupPW_planet.o: startupPW_planet.f
-	${COMPILE.f77} ${Cflag0} -132 startupPW_planet.f
+	${COMPILE.f77} ${Cflag1} -132 startupPW_planet.f
+ssflux_planet.o: ssflux_planet.f
+	${COMPILE.f77} ${Cflag1} -132 ssflux_planet.f
