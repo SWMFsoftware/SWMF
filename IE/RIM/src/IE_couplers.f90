b0dd5259b09fd45d872b3cae906ffe3bddbf7d47
ridley <>
2008-01-16 19:42:24 +0000

    more
    Former-commit-id: 2030136f5c863e1d63d945dde0740b5cfb4bd63c

--- IE/RIM/src/IE_couplers.f90
+++ IE/RIM/src/IE_couplers.f90
@@ -50,3 +50,34 @@ subroutine IE_setnLats(iComponent, nLatsIn, iError)
   endif
 
 end subroutine IE_setnLats
+
+!--------------------------------------------------------------
+
+subroutine IE_setgrid(iComponent, MLTsIn, LatsIn, iError)
+
+  use CON_comp_param
+  use ModIEInterface
+
+  integer, intent(in) :: iComponent
+  real, dimension(iecouplerinfo(iComponent)%nLons,&
+                  iecouplerinfo(iComponent)%nLats), intent(in) :: MLTsIn,LatsIn
+  integer, intent(out) :: iError
+
+  integer :: nLatsIE, nLonsIE
+  real, allocatable :: LatitudeIE(:,:), LongitudeIE(:,:)
+
+  logical :: IsIEGridRefined = .true.
+
+  iError = 0
+
+  if (.not.IsIEGridRefined) then
+     ! Check to see if we need to redo grid - should be NO!
+  endif
+
+  if (IsIEGridRefined) then
+     ! get IE grid (i.e. message pass from iProc = 0 on IE to here)
+     ! make new interpolation indices (stored in allocable arrays above)
+     IsIEGridRefined = .false.
+  else
+
+end subroutine IE_setgrid
