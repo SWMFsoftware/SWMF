e404fae0c225d38baf6ddd2d89f55c91be89cb52
aglocer <>
2008-01-24 18:07:33 +0000

    set_boundary modified to take pressure and temp depending on ghost cell velocity
    Also DtExpl reduced
    
    
    Former-commit-id: 45b6e2f9ec4e34d7d17c1d9ca031ec21c1eac64e

--- PW/PWOM/src/ModPwImplicit.f90
+++ PW/PWOM/src/ModPwImplicit.f90
@@ -143,8 +143,16 @@ module ModPwImplicit
             StateOrig_GV(-1:0,iRho_I(iIon):iP_I(iIon))
        State_GV(-1:0,iU(iIon)) = &
             State_GV(1,iU(iIon))*State_GV(1,iRho(iIon))/State_GV(-1:0,iRho(iIon))
-       State_GV(-1:0,iTe_) = &
-            StateOrig_GV(-1:0,iT_I(nIon))
+
+      if (State_GV(0,iU(iIon)) > 0.0) then
+         State_GV(-1:0,iP(iIon)) = StateOrig_GV(-1:0,iP_I(iIon))
+      else
+         State_GV(-1:0,iP(iIon)) = &
+              State_GV(1,iP(iIon))/State_GV(1,iRho(iIon)) * State_GV(-1:0,iRho(iIon))
+      endif
+      
+      State_GV(-1:0,iTe_) = &
+           StateOrig_GV(-1:0,iT_I(nIon))
 
        ! Upper boundary requires temperature and scaleheight
        T_C(iIon) = &
@@ -239,7 +247,7 @@ module ModPwImplicit
     integer :: k,iIon, nVarReduced
     !--------------------------------------------------------------------------
     DtImpl = Dt
-    DtExpl = 1.0e-3*(DrBnd/2.0e6)
+    DtExpl = 1.0e-4*(DrBnd/2.0e6)
 !godunov    DtExpl = 1.0e-2
     !DtExpl = dx^2/2/max(kappa) 
     
