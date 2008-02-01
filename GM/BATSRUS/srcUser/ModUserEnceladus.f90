58d2ba45932fcaae43140a9347c7423407b27912
dnajib <>
2008-02-01 21:06:42 +0000

    *** empty log message ***
    Former-commit-id: 649d59263f9f4322314c398a43d66d62a1b028ad

--- GM/BATSRUS/srcUser/ModUserEnceladus.f90
+++ GM/BATSRUS/srcUser/ModUserEnceladus.f90
@@ -5,11 +5,14 @@
 module ModUser
   ! This is the user module for Enceladus
   use ModSize
-  use ModUserEmpty,               &
-        IMPLEMENTED1 => user_calc_sources
-
+  use ModUserEmpty,                      &
+       IMPLEMENTED1 => user_read_inputs, &
+       IMPLEMENTED2 => user_calc_sources
 
   include 'user_module.h' !list of public methods
+
+  ! This is a temporary solution
+  real, public:: BoundaryCoeffB=2.0, BoundaryCoeffU=2.0
 
   !\
   ! Here you must define a user routine Version number and a 
@@ -19,11 +22,38 @@ module ModUser
   integer,parameter :: MaxNuSpecies=1
   integer::iBlock
   real::NumDenNeutral_VC(nI,nJ,nK)
+
   real,dimension(0:26)::Impact,Te
   character (len=*), parameter :: NameUserModule = &
        'Enceladus 1 species MHD code, Dalal Najib'
 
 contains
+
+  subroutine user_read_inputs
+
+    use ModReadParam
+    character (len=100) :: NameCommand
+
+    do
+       if(.not.read_line() ) EXIT
+       if(.not.read_command(NameCommand)) CYCLE
+       select case(NameCommand)
+       
+       case('#BOUNDARYCOEFF')
+          call read_var('BoundaryCoeffB',BoundaryCoeffB)
+          call read_var('BoundaryCoeffU',BoundaryCoeffU)
+          !write(*,*)'BoundaryCoeffB=',BoundaryCoeffB
+          !write(*,*)'BoundaryCoeffU=',BoundaryCoeffU
+       case('#USERINPUTEND')
+          EXIT
+       case default
+          call stop_mpi('Unknown user command:'//trim(NameCommand))
+       end select
+    end do
+
+  end subroutine user_read_inputs
+
+  !============================================================
 
   subroutine user_calc_sources
 
@@ -32,7 +62,7 @@ contains
     use ModProcMH,   ONLY: iProc
     use ModMain, ONLY: PROCTEST,GLOBALBLK,BLKTEST, nI, nJ,nK,iTest,jTest,kTest 
     use ModPhysics, ONLY: Rbody, inv_gm1, gm1, No2Si_V, Si2No_V, No2Io_V, Io2No_V,UnitN_, UnitT_,UnitTemperature_,&
-UnitX_, UnitRhoU_, UnitU_
+         UnitX_, UnitRhoU_, UnitU_
     use ModProcMH,   ONLY: iProc
     use ModGeometry, ONLY: x_BLK, y_BLK, z_BLK, R_BLK, vInv_CB 
     use ModBlockData,ONLY: use_block_data, put_block_data, get_block_data
@@ -57,48 +87,48 @@ UnitX_, UnitRhoU_, UnitU_
     real:: ReactionRate_H2O_dim, ReactionRate_H2O,ImpactRate_dim, RecombRate_dim, RecombRate 
     real:: Nu_C,Nu_C_dim, Nu_exchange!Collision frequency
     real,parameter::nu0=5.4745e-10!this is in cm^3/s
-   
+
 
     !------------------------------------------------------------------
     iBlock = globalBLK
     !write(*,*)'iBlock',iBlock
-    
+
 !!$
 !!$    write(*,*)'before update'
 !!$    write(*,*)' Source_VC(Rho)=',Source_VC(rho_,iTest,jTest,kTest)
 !!$    write(*,*)' Source_VC(Ux,Uy,Uz)=',Source_VC(RhoUx_:RhoUz_,iTest,jTest,kTest)
 !!$    write(*,*)' Source_VC(Energy)=',Source_VC(Energy_,iTest,jTest,kTest)
 !!$    write(*,*)' Source_VC(P)=',Source_VC(p_,iTest,jTest,kTest)
-    
-   
- !put the interpolated data in ModBlockData     
-       if(iBlock /= iBlockLast)then       
+
+
+    !put the interpolated data in ModBlockData     
+    if(iBlock /= iBlockLast)then       
        iBlockLast = iBlock       
        if(use_block_data(iBlock))then          
           !call get_block_data(iBlock, MaxNuSpecies, nI, nJ, nK, NumDenNeutral_VC)         
           call get_block_data(iBlock, nI, nJ, nK, NumDenNeutral_VC)
-         
+
        else         
           call enceladus_input(iBlock)
           !write(*,*) 'enceladus_input has been read'
           !write(*,*)'Te(22)=',Te(22)
-         
+
           !call put_block_data(iBlock, MaxNuSpecies, nI, nJ, nK, NumDenNeutral_VC)         
           call put_block_data(iBlock, nI, nJ, nK, NumDenNeutral_VC)
-         
+
        end if
     end if
 
- !End of interpolation of Data
+    !End of interpolation of Data
 
-   do k = 1, nK ;   do j = 1, nJ ;  do i = 1, nI  
-      R0=R_BLK(i,j,k,iBlock)
-      R0_Si=R_BLK(i,j,k,iBlock)*No2Si_V(UnitX_)
+    do k = 1, nK ;   do j = 1, nJ ;  do i = 1, nI  
+       R0=R_BLK(i,j,k,iBlock)
+       R0_Si=R_BLK(i,j,k,iBlock)*No2Si_V(UnitX_)
        ! write(*,*)'R_block_Si=',R0_Si
        !write(*,*)'Density_Valiery=',NumDenNeutral_VC(i,j,k)
        !write(*,*)'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
        totalNumRho=State_VGB(rho_,i,j,k,iBlock)/(MassFluid_I(1))
-      
+
        ReactionRate_H2O_dim=PhotoIonRate_H2O*NumDenNeutral_VC(i,j,k)!neutral_density(R0)
 
        ! Now normalize the Reaction Rate
@@ -106,9 +136,9 @@ UnitX_, UnitRhoU_, UnitU_
 
        ! Calculating the source term due to photoionization
        RhoDotPhoto= ReactionRate_H2O*(MassFluid_I(1)) !in this case the mass of the fluid is 18 amu      
-        !the ion and electron temperature
-        kTi = State_VGB(p_,i,j,k,iBlock)/ totalNumRho/2.0
-        kTi_dim=kTi*No2Io_V(UnitTemperature_)
+       !the ion and electron temperature
+       kTi = State_VGB(p_,i,j,k,iBlock)/ totalNumRho/2.0
+       kTi_dim=kTi*No2Io_V(UnitTemperature_)
 
        !Calculating recombination rates, it is actually dissociative recombination rates are in Nagy's book p.228/229
 
@@ -119,19 +149,19 @@ UnitX_, UnitRhoU_, UnitU_
        else
           RecombRate_dim=1.03e-3*(kTi_dim)**(-1.111)
        end if
-          RecombRate=RecombRate_dim*No2Io_V(UnitN_)*No2Io_V(UnitT_)
-                       
+       RecombRate=RecombRate_dim*No2Io_V(UnitN_)*No2Io_V(UnitT_)
+
        !Calculating the loss term due to recombination
-          RhoDotRecomb=RecombRate*totalNumRho*State_VGB(rho_,i,j,k,iBlock)              
-       
+       RhoDotRecomb=RecombRate*totalNumRho*State_VGB(rho_,i,j,k,iBlock)              
+
        !Calculating the RhoDot due to charge exchange   
-         !sigma_exchange=sigma_exchange_dim*Si2No_V(UnitX_)*Si2No_V(UnitX_)
-         
-        ! nion=State_VGB(rho_,i,j,k,iBlock)*No2Io_V(UnitN_)/(MassFluid_I(1))    
-         rhotimeU=sqrt(State_VGB(Ux_,i,j,k,iBlock)*State_VGB(Ux_,i,j,k,iBlock)  &
+       !sigma_exchange=sigma_exchange_dim*Si2No_V(UnitX_)*Si2No_V(UnitX_)
+
+       ! nion=State_VGB(rho_,i,j,k,iBlock)*No2Io_V(UnitN_)/(MassFluid_I(1))    
+       rhotimeU=sqrt(State_VGB(Ux_,i,j,k,iBlock)*State_VGB(Ux_,i,j,k,iBlock)  &
             +State_VGB(Uy_,i,j,k,iBlock)*State_VGB(Uy_,i,j,k,iBlock)  &
             +State_VGB(Uz_,i,j,k,iBlock)*State_VGB(Uz_,i,j,k,iBlock))
-         !rhotimeU_Si=rhotimeU*No2Si_V(UnitRhoU_)
+       !rhotimeU_Si=rhotimeU*No2Si_V(UnitRhoU_)
 
        inv_rho = 1.00/State_VGB(rho_,i,j,k,iBlock)
        inv_rho2 = inv_rho**2
@@ -140,46 +170,48 @@ UnitX_, UnitRhoU_, UnitU_
             +State_VGB(Uz_,i,j,k,iBlock)*State_VGB(Uz_,i,j,k,iBlock)) &
             *inv_rho2
 
-      ! U_Si=sqrt(uu2)*No2Si_V(UnitU_)
-      !U_No=sqrt(uu2)
+       ! U_Si=sqrt(uu2)*No2Si_V(UnitU_)
+       !U_No=sqrt(uu2)
        !Rho_No=State_VGB(rho_,i,j,k,iBlock)
 
-         Nu_exchange=sigma_exchange*rhotimeU*No2Si_V(UnitU_)*100 
-         !Nu_exchange=sigma_exchange*sqrt(uu2)*No2Si_V(UnitU_)*100*State_VGB(rho_,i,j,k,iBlock)
+       Nu_exchange=sigma_exchange*rhotimeU*No2Si_V(UnitU_)*100 
+       !Nu_exchange=sigma_exchange*sqrt(uu2)*No2Si_V(UnitU_)*100*State_VGB(rho_,i,j,k,iBlock)
 
-         RhoDotchargeX=Nu_exchange*NumDenNeutral_VC(i,j,k)
-         RhoDotchargeX=RhoDotchargeX*Io2No_V(UnitN_)
-         
+       RhoDotchargeX=Nu_exchange*NumDenNeutral_VC(i,j,k)
+       RhoDotchargeX=RhoDotchargeX*Io2No_V(UnitN_)
+
        !Calculating impact ionization
-        !write(*,*)'Ti_dim=',kTi_dim
-	if (kTi_dim.ge.1.0e4.and.kTi_dim.le.1.0e7)then
-		if(kTi_dim.le.1.0e5)then
-		h=int(kTi_dim/1.04 -1.0)
-	else if(kTi_dim.le.1.0e6)then
-		h=int(kTi_dim/1.0e5+9.0)
-	else if(kTi_dim.le.5.0e6)then
-		h=int(kTi_dim/1.0e6+19.0)
-        else if(kTi_dim.le.7.0e6)then
-                h=int(kTi_dim/1.0e6+18.0)
-        else 
-                h=25
-	end if
-        !write(*,*)'before calculating kappa'
-        !write(*,*)'h',h
-        !write(*,*)'Te(22)=',Te(22)
-        !write(*,*)'Te(23)=',Te(23)
-        !write(*,*)'(Te(h+1)-Te(h)',(Te(h+1)-Te(h))
-	kappa = Impact(h)+&
-                        (Impact(h+1)-Impact(h))*(kTi_dim-Te(h))/(Te(h+1)-Te(h))
-        !write(*,*)'after calculating kappa'
-	else
-		kappa=0.0
-	end if	 
+       !write(*,*)'Ti_dim=',kTi_dim
+       if (kTi_dim.ge.1.0e4.and.kTi_dim.le.1.0e7)then
+          if(kTi_dim.le.1.0e5)then
+             h=int(kTi_dim/1.04 -1.0)
+          else if(kTi_dim.le.1.0e6)then
+             h=int(kTi_dim/1.0e5+9.0)
+          else if(kTi_dim.le.5.0e6)then
+             h=int(kTi_dim/1.0e6+19.0)
+          else if(kTi_dim.le.7.0e6)then
+             h=int(kTi_dim/1.0e6+18.0)
+          else 
+             h=25
+          end if
+          !write(*,*)'before calculating kappa'
+          !write(*,*)'h',h
+          !write(*,*)'Te(22)=',Te(22)
+          !write(*,*)'Te(23)=',Te(23)
+          !write(*,*)'(Te(h+1)-Te(h)',(Te(h+1)-Te(h))
+          kappa = Impact(h)+&
+               (Impact(h+1)-Impact(h))*(kTi_dim-Te(h))/(Te(h+1)-Te(h))
+          !write(*,*)'after calculating kappa'
+       else
+          kappa=0.0
+       end if
 
-         ImpactRate_dim=kappa*NumDenNeutral_VC(i,j,k)*ne_hot
-         RhoDotImpact=ImpactRate_dim*Io2No_V(UnitN_)*No2Io_V(UnitT_)*(MassFluid_I(1))
-   
-    !Calculating the general source and Loss term
+       ImpactRate_dim=kappa*NumDenNeutral_VC(i,j,k)*ne_hot
+       RhoDotImpact=ImpactRate_dim*Io2No_V(UnitN_)*No2Io_V(UnitT_)*(MassFluid_I(1))
+
+       !Calculating the general source and Loss term
+       !RhoDotImpact=0.0
+       !RhoDotchargeX=0.0
        RhoDot=RhoDotPhoto+RhoDotchargeX+RhoDotImpact
        RhoDotL=RhoDotRecomb+RhoDotchargeX
        RhoNumDot=RhoDot/(MassFluid_I(1))
@@ -202,14 +234,14 @@ UnitX_, UnitRhoU_, UnitU_
 !!$            +State_VGB(Uz_,i,j,k,iBlock)*State_VGB(Uz_,i,j,k,iBlock)) &
 !!$            *inv_rho2
 !!$       U_Si=sqrt(uu2)*No2Si_V(UnitU_)
-       
+
        Source_VC(rho_,i,j,k) = Source_VC(rho_,i,j,k) + RhoDot-RhoDotL
        Source_VC(RhoUx_:RhoUz_,i,j,k) = Source_VC(RhoUx_:RhoUz_,i,j,k) &
             -Nu_C*State_VGB(RhoUx_:RhoUz_,i,j,k,iBlock)&
             -RhoDotLx*State_VGB(RhoUx_:RhoUz_,i,j,k,iBlock)
-      
+
        kTn = kTn_dim*Io2No_V(UnitTemperature_)
-            
+
 
        Source_VC(Energy_,i,j,k) = Source_VC(Energy_,i,j,k) &
             -0.5*State_VGB(rho_,i,j,k,iBlock)*uu2*Nu_C     &
@@ -217,20 +249,20 @@ UnitX_, UnitRhoU_, UnitU_
             +1.5*totalNumRho*(kTn-kTi)*Nu_C&
             -0.50*uu2*RhoDotL
 
-      
+
        Source_VC(p_,i,j,k) = Source_VC(p_,i,j,k) &
             +0.5*gm1*State_VGB(rho_,i,j,k,iBlock)*uu2*Nu_C  &
             +(RhoNumDot*kTn-RhoNumDotL*kTi) &
             +0.50*(gm1)*uu2*(RhoDot) &
-            +totalNumRho*(kTn-KTi)*Nu_C 
+            +1.5*gm1*totalNumRho*(kTn-KTi)*Nu_C 
 
-      
-    !write(*,*)'RhoDotPhoto=',RhoDotPhoto   
+
+       !write(*,*)'RhoDotPhoto=',RhoDotPhoto   
 !!$    write(*,*)'RhoDotChargeX=',RhoDotChargeX
 !!$    !write(*,*)'charge exchangecrosssection=',sigma_exchange
 !!$    write(*,*)'Rho*U=',rhotimeU      
-    !write(*,*)'RhoDotImpact=',RhoDotImpact
-    !write(*,*)'RhoDotRecomb=',RhoDotRecomb
+       !write(*,*)'RhoDotImpact=',RhoDotImpact
+       !write(*,*)'RhoDotRecomb=',RhoDotRecomb
 !!$    write(*,*)'RhoDottotal=',RhoDot   
     end do; end do; end do
 
@@ -264,83 +296,84 @@ UnitX_, UnitRhoU_, UnitU_
   end subroutine user_calc_sources
 
   !==========================================================================
-subroutine enceladus_input(iBlock)
-use ModMain, ONLY: GLOBALBLK  
-use ModInterpolate, ONLY: trilinear
-use ModPhysics
-use ModGeometry, ONLY: x_BLK, y_BLK, z_BLK, R_BLK
-!-------------------------------------------------------------------------
-integer,parameter::imax=49
-integer,parameter::jmax=49
-integer,parameter::kmax=49
-character(2000)::line
-integer::iBlock
-real,dimension(0:imax,0:jmax,0:kmax)::density
-!real,dimension(0:26)::Impact,Te
-real::Xyz_D(3)
-real::x,y,z,dx,x_BLK_Si,y_BLK_Si,z_BLK_Si, NumDenNeutral_VC_Si
-real,parameter::dx_dim=120.0e3!the step in m
-integer::m,n,p,i,j,k,h
-!===================================================================
-iBlock=globalBLK
-open(130, file='DensityH2O.dat ', status='old')
-do m=0,1
-read(130,'(a)')line
-end do
+  subroutine enceladus_input(iBlock)
+    use ModMain, ONLY: GLOBALBLK  
+    use ModInterpolate, ONLY: trilinear
+    use ModPhysics
+    use ModGeometry, ONLY: x_BLK, y_BLK, z_BLK, R_BLK
+    !-------------------------------------------------------------------------
+    integer,parameter::imax=49
+    integer,parameter::jmax=49
+    integer,parameter::kmax=49
+    character(2000)::line
+    integer::iBlock
+    real,dimension(0:imax,0:jmax,0:kmax)::density
+    !real,dimension(0:26)::Impact,Te
+    real::Xyz_D(3)
+    real::x,y,z,dx,x_BLK_Si,y_BLK_Si,z_BLK_Si, NumDenNeutral_VC_Si
+    real,parameter::dx_dim=120.0e3!the step in m
 
-do m=0,imax
-   do n=0,jmax
-      do p=0,kmax
-         read(130,*)x,y,z,density(m,n,p)
-      end do
-   end do
-end do
-dx=dx_dim*Si2No_V(UnitX_)
-do k=1,nK; do j=1,nJ;do i=1,nI
-Xyz_D(1)=x_BLK(i,j,k,iBlock)/dx+25.0
-Xyz_D(2)=y_BLK(i,j,k,iBlock)/dx+25.0
-Xyz_D(3)=z_BLK(i,j,k,iBlock)/dx+25.0
-!x_BLK_Si=x_BLK(i,j,k,iBlock)*No2Si_V(UnitX_)
-!y_BLK_Si=y_BLK(i,j,k,iBlock)*No2Si_V(UnitX_)
-!z_BLK_Si=z_BLK(i,j,k,iBlock)*No2Si_V(UnitX_)
-if (any(Xyz_D<(/0, 0, 0/)) .or. &
-     any(Xyz_D>(/iMax,jMax,kMax/)))then
-   NumDenNeutral_VC(i,j,k)=0.0
-else
-   NumDenNeutral_VC(i,j,k)=trilinear(density,0,imax,0,jmax,0,kmax,Xyz_D)
-   NumDenNeutral_VC_Si=NumDenNeutral_VC(i,j,k)
-   !write(*,*)'Si2No_V(UnitN_)',Si2No_V(UnitN_)
-   !write(*,*)'x_BLK_Si=',x_BLK_Si
-   !write(*,*)'Xyz_D(1)=',Xyz_D(1)
-   !write(*,*)'y_BLK_Si=',y_BLK_Si
-   !write(*,*)'Xyz_D(2)=',Xyz_D(2)
-   !write(*,*)'z_BLK_Si=',z_BLK_Si
-   !write(*,*)'Xyz_D(3)=',Xyz_D(3)
-   !write(*,*)'NumDenValieryinterpolated',NumDenNeutral_VC_Si!But I want it in Si to be able to read it
-end if   
-end do; end do; end do
-NumDenNeutral_VC=NumDenNeutral_VC*Si2No_V(UnitN_)
-close (130)
+    integer::m,n,p,i,j,k,h
+    !===================================================================
+    iBlock=globalBLK
+    open(130, file='DensityH2O.dat ', status='old')
+    do m=0,1
+       read(130,'(a)')line
+    end do
+
+    do m=0,imax
+       do n=0,jmax
+          do p=0,kmax
+             read(130,*)x,y,z,density(m,n,p)
+          end do
+       end do
+    end do
+    dx=dx_dim*Si2No_V(UnitX_)
+    do k=1,nK; do j=1,nJ;do i=1,nI
+       Xyz_D(1)=x_BLK(i,j,k,iBlock)/dx+25.0
+       Xyz_D(2)=y_BLK(i,j,k,iBlock)/dx+25.0
+       Xyz_D(3)=z_BLK(i,j,k,iBlock)/dx+25.0
+       !x_BLK_Si=x_BLK(i,j,k,iBlock)*No2Si_V(UnitX_)
+       !y_BLK_Si=y_BLK(i,j,k,iBlock)*No2Si_V(UnitX_)
+       !z_BLK_Si=z_BLK(i,j,k,iBlock)*No2Si_V(UnitX_)
+       if (any(Xyz_D<(/0, 0, 0/)) .or. &
+            any(Xyz_D>(/iMax,jMax,kMax/)))then
+          NumDenNeutral_VC(i,j,k)=0.0
+       else
+          NumDenNeutral_VC(i,j,k)=trilinear(density,0,imax,0,jmax,0,kmax,Xyz_D)
+          NumDenNeutral_VC_Si=NumDenNeutral_VC(i,j,k)
+          !write(*,*)'Si2No_V(UnitN_)',Si2No_V(UnitN_)
+          !write(*,*)'x_BLK_Si=',x_BLK_Si
+          !write(*,*)'Xyz_D(1)=',Xyz_D(1)
+          !write(*,*)'y_BLK_Si=',y_BLK_Si
+          !write(*,*)'Xyz_D(2)=',Xyz_D(2)
+          !write(*,*)'z_BLK_Si=',z_BLK_Si
+          !write(*,*)'Xyz_D(3)=',Xyz_D(3)
+          !write(*,*)'NumDenValieryinterpolated',NumDenNeutral_VC_Si!But I want it in Si to be able to read it
+       end if
+    end do; end do; end do
+    NumDenNeutral_VC=NumDenNeutral_VC*Si2No_V(UnitN_)
+    close (130)
 
 !!$write(*,*)'density(-3000,-3000, -3000)',density(0,0,0)
 !!$write(*,*)'density(0,0,0)',density(25,25,25)
 !!$write(*,*)'density(2880,2880,2880)', density(49,49,49)
 
-open(150,file='Impact_rates.dat')
-!write(*,*)'Impact_rates has been opened'
-read(150,'(a)')line
-do h=0,26
-	read(150,*)Te(h),Impact(h)
-end do
-!write(*,*)'Te(22)=',Te(22)
-!write(*,*)'Te(23)=',Te(23)
-close(150)
+    open(150,file='Impact_rates.dat')
+    !write(*,*)'Impact_rates has been opened'
+    read(150,'(a)')line
+    do h=0,26
+       read(150,*)Te(h),Impact(h)
+    end do
+    !write(*,*)'Te(22)=',Te(22)
+    !write(*,*)'Te(23)=',Te(23)
+    close(150)
 
-!write(*,*)'Impact_Rates has been read'
-end subroutine enceladus_input
-!========================================================================
- 
-!=========================================================================
+    !write(*,*)'Impact_Rates has been read'
+  end subroutine enceladus_input
+  !========================================================================
+
+  !=========================================================================
   real function neutral_density(R0)
     use ModPhysics, ONLY :Rbody,cZero
 
