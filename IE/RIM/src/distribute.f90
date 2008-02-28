87c69853729ace875373b7b746d5518fdb813bdb
ridley <>
2008-02-28 16:36:27 +0000

    Needed by RIM
    Former-commit-id: 8750ce07ec67d34845224309f95602bad6a06302

---
+++ IE/RIM/src/distribute.f90
@@ -0,0 +1,69 @@
+
+subroutine distribute
+
+  use ModRIM
+  use ModMpi
+  use ModProcIE
+  use ModParamRIM, only: iDebugLevel
+
+  implicit none
+
+  integer :: iError, iSize
+
+  if (maxval(OuterMagJrAll) > -1.0e31) then
+     call rearrange(OuterMagJrAll, OuterMagJr)
+  else
+     OuterMagJr = 0.0
+  endif
+
+  if (maxval(InnerMagJrAll) > -1.0e31) then
+     call rearrange(InnerMagJrAll, InnerMagJr)
+  else
+     InnerMagJr = 0.0
+  endif
+
+  if (maxval(IonoJrAll) > -1.0e31) then
+     call rearrange(IonoJrAll, IonoJr)
+  else
+     IonoJr = 0.0
+  endif
+
+  if (maxval(OuterMagInvBAll) > -1.0e31) then
+     call rearrange(OuterMagInvBAll, OuterMagInvB)
+     call rearrange(OuterMagRhoAll, OuterMagRho)
+     call rearrange(OuterMagPAll, OuterMagP)
+  endif
+
+contains
+
+  subroutine rearrange(ValueAll, ValueLocal)
+
+    real, intent(out) :: ValueLocal(0:nLons+1, nLats)
+    real, intent(in)  :: ValueAll(nLats+2, nLons*nProc+1)
+
+    integer :: iLat, iLon, iLatFrom, iLonFrom, iError, iLatTo, iLonTo
+
+    !\
+    ! We need to reverse the latitude, shift the longitudes, and swap the
+    ! index order....
+    !/
+
+    ! Skip the North and South pole, since RIM doesn't actually use this
+    ! information
+
+    do iLat = 1, nLats
+       ! Need to reverst latitudes
+       iLatFrom = iLat+1
+       iLatTo   = nLats-(iLat-1)
+       do iLon = 0, nLons+1
+          ! Need to shift longitudes
+          iLonTo   = iLon
+          iLonFrom = mod(iLon + nLons*nProc/2, nLons*nProc)
+          if (iLonFrom == 0) iLonFrom = nLons*nProc
+          ValueLocal(iLonTo, iLatTo) = ValueAll(iLatFrom, iLonFrom)
+       enddo
+    enddo
+
+  end subroutine rearrange
+
+end subroutine distribute
