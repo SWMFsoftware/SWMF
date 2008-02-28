dc71b069171a36e3dc2cc7cb96245c3a023baf1e
gtoth <>
2008-02-28 03:05:05 +0000

    Added more init_mod_indices calls.
    Former-commit-id: 337afc26426f509f48f6403d1f4b34f7145b1d44

--- util/DATAREAD/srcIndices/IO_library.f90
+++ util/DATAREAD/srcIndices/IO_library.f90
@@ -7,6 +7,8 @@ subroutine IO_set_IMF_Bz_single(BzIn)
   implicit none
   real, intent(in) :: BzIn
 
+  call init_mod_indices
+
   nIndices_V(imf_bz_) = 1
   Indices_TV(1,imf_bz_) = BzIn
 
@@ -19,6 +21,8 @@ subroutine IO_set_IMF_By_single(ByIn)
   use ModIndices
   implicit none
   real, intent(in) :: ByIn
+
+  call init_mod_indices
 
   nIndices_V(imf_by_) = 1
   Indices_TV(1,imf_by_) = ByIn
@@ -33,6 +37,8 @@ subroutine IO_set_IMF_Bx_single(BxIn)
   implicit none
   real, intent(in) :: BxIn
 
+  call init_mod_indices
+
   nIndices_V(imf_bx_) = 1
   Indices_TV(1,imf_bx_) = BxIn
 
@@ -45,6 +51,8 @@ subroutine IO_set_SW_v_single(VIn)
   use ModIndices
   implicit none
   real, intent(in) :: VIn
+
+  call init_mod_indices
 
   nIndices_V(sw_v_) = 1
   Indices_TV(1,sw_v_) = VIn
@@ -59,6 +67,8 @@ subroutine IO_set_hpi_single(HpiIn)
   implicit none
   real, intent(in) :: HpiIn
 
+  call init_mod_indices
+
   nIndices_V(Hpi_) = 1
   Indices_TV(1,Hpi_) = HpiIn
 
@@ -71,6 +81,8 @@ subroutine IO_set_f107_single(F107In)
   use ModIndices
   implicit none
   real, intent(in) :: F107In
+
+  call init_mod_indices
 
   nIndices_V(f107_) = 1
   Indices_TV(1,f107_) = F107In
@@ -85,6 +97,8 @@ subroutine IO_set_f107a_single(F107aIn)
   implicit none
   real, intent(in) :: F107aIn
 
+  call init_mod_indices
+
   nIndices_V(f107a_) = 1
   Indices_TV(1,f107a_) = F107aIn
 
@@ -97,6 +111,8 @@ subroutine IO_set_kp_single(kpIn)
   use ModIndices
   implicit none
   real, intent(in) :: kpIn
+
+  call init_mod_indices
 
   nIndices_V(kp_) = 1
   Indices_TV(1,kp_) = kpIn
@@ -111,6 +127,8 @@ subroutine IO_set_ap_single(apIn)
   use ModIndices
   implicit none
   real, intent(in) :: apIn
+
+  call init_mod_indices
 
   nIndices_V(ap_) = 1
   Indices_TV(1,ap_) = apIn
