31c3e6f32faad4c1c1833f12ebeaf09f8ae16444
aglocer <>
2008-03-02 19:40:55 +0000

    remove code that sets grid and replace it with call to set_vertical_grid
    Former-commit-id: 0e0bb646a278e7bdb0da6a652346c0ed6253ffdb

--- PW/PWOM/srcEarth/startupPW.f
+++ PW/PWOM/srcEarth/startupPW.f
@@ -78,74 +78,6 @@ C kB/m_e
       CVH=RGAS_I(Ion2_)/GMIN1
       CVHE=RGAS_I(Ion3_)/GMIN1
       CVE=RGAS_I(Ion4_)/GMIN1
-C                                                                      C
-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-C                                                                      C
-C     DEFINE THE RADIAL GRID STRUCTURE                                 C
-C                                                                      C
-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-C                                                                      C
-C      READ (5,2) DRBND
-CALEX DRBND=altitude step? I think the units here are in cm not meters
-CALEX like most of the code.
-!      DRBND=2.E6
-
-CALEX RN=lower boundary of the simulation? 
-CALEX RAD=radial distance of cell centers?      
-CALEX RBOUND=radial distance of lower boundary of cell     
-CALEX ALTD = same as RAD but distance is from surface, not center of planet
-      RN=6.55677E8+0.5*DRBND
-      DO 20 KSTEP=1,NDIM1
-         RBOUND(KSTEP)=RN+(KSTEP-1)*DRBND
-20    CONTINUE
-      DO 30 KSTEP=1,NDIM
-         KSTEP1=KSTEP+1
-         RAD(KSTEP)=(RBOUND(KSTEP)+RBOUND(KSTEP1))/2.
-         ALTD(KSTEP)=RAD(KSTEP)-6356.77E5
- 30   CONTINUE
-      ALTMIN=ALTD(1)-DRBND
-      ALTMAX=ALTD(NDIM)+DRBND
-C                                                                      C
-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-C                                                                      C
-C     READ THE EXPONENT OF THE  A(R)=R**NEXP  AREA FUNCTION            C
-C                                                                      C
-CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
-C                                                                      C
-C      READ (5,1) NEXP
-      NEXP=3
-
-CAlex      write(*,*) NEXP
-
-CALEX AR stands for area function. 12 is the lower boundary of the cell
-CALEX and 23 is the upper boundary
-
-      DO 40 K=1,NDIM
-      DAREA(K)=NEXP/RAD(K)
-      AR12(K)=(RBOUND(K)/RAD(K))**NEXP
-      AR23(K)=(RBOUND(K+1)/RAD(K))**NEXP
-
-!     For the cell volume in the Rusanov Solver we use a cell volume 
-!     calculated by assuming an area function in the form A=alpha r^3
-!     and then assuming each cell is a truncated cone.
-!     So CellVolume_C is the volume of cell j divided by the crossesction
-!     of cell j, A(j). 
-
-      CellVolume_C(K)=1.0/3.0 * DrBnd *
-     &     ( Ar12(K) + Ar23(K) + ( Ar12(K)*Ar23(K) )**0.5 ) 
-
-40    CONTINUE
-! area and volume for ghost cell
-      AR12top(1)=((RN+nDim*drbnd)/(RN+nDim*drbnd+drbnd*0.5))**NEXP
-      AR23top(1)=((RN+(nDim+1.0)*drbnd)/(RN+(nDim+1.0)*drbnd+drbnd*0.5))**NEXP
-      CellVolumeTop(1)=1.0/3.0 * DrBnd *
-     &     ( Ar12top(1) + Ar23top(1) + ( Ar12top(1)*Ar23top(1) )**0.5 ) 
-
-
-      AR12top(2)=((RN+(nDim+1.0)*drbnd)/(RN+(nDim+1.0)*drbnd+drbnd*0.5))**NEXP
-      AR23top(2)=((RN+(nDim+2.0)*drbnd)/(RN+(nDim+2.0)*drbnd+drbnd*0.5))**NEXP
-      CellVolumeTop(2)=1.0/3.0 * DrBnd *
-     &     ( Ar12top(2) + Ar23top(2) + ( Ar12top(2)*Ar23top(2) )**0.5 ) 
 
 C                                                                      C
 CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
@@ -157,6 +89,8 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 C                                                                      C
 C      READ (5,2) CURR(1)
 C      CURR(1)=0.E-6
+
+      call set_vertical_grid
 
       CURR(1)=2.998E2*CURR(1)
 !      CURTIM=150.
@@ -1492,7 +1426,7 @@ CALEX similar to nagy p95 but I cant find the connection???
          Centrifugal(I)=wHorizontal**2.0*RAD(I)
  20   CONTINUE
 cAlex
-      NEXP=3
+
 cendAlex      
 !      CRRX=CURR(1)*RAD(1)**NEXP
 !      DO 25 I=1,NDIM
