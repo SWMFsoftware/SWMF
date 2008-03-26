c46864e41c6ed21aa850236a0c0360dce756f13c
aglocer <>
2008-03-26 15:45:53 +0000

    set lower boundary velocity to zero instead of to conserve flux
    Former-commit-id: 052e43fde32b657708e2f83d6d46ba1134cf0af1

--- PW/PWOM/srcEarth/startupPW.f
+++ PW/PWOM/srcEarth/startupPW.f
@@ -1310,10 +1310,10 @@ C     State_GV(0,RhoO_)=PHIOX*Mass_I(Ion1_)*XNO/(1.53E-12*XNN2*TMP1+2.82E-11*XNO
      &        State_GV(-1:0,RhoE_)+MassElecIon_I(iIon)*State_GV(-1:0,iRho_I(iIon))
       enddo
       
-      State_GV(-1:0,uO_)=State_GV(1,RhoO_)*State_GV(1,uO_)/State_GV(-1:0,RhoO_)
-      State_GV(-1:0,uH_)=State_GV(1,RhoH_)*State_GV(1,uH_)/State_GV(-1:0,RhoH_)
-      State_GV(-1:0,uHe_)=State_GV(1,RhoHe_)*State_GV(1,uHe_)/State_GV(-1:0,RhoHe_)
-      State_GV(-1:0,ue_) = 0.0     
+!      State_GV(-1:0,uO_)=State_GV(1,RhoO_)*State_GV(1,uO_)/State_GV(-1:0,RhoO_)
+!      State_GV(-1:0,uH_)=State_GV(1,RhoH_)*State_GV(1,uH_)/State_GV(-1:0,RhoH_)
+!      State_GV(-1:0,uHe_)=State_GV(1,RhoHe_)*State_GV(1,uHe_)/State_GV(-1:0,RhoHe_)
+!      State_GV(-1:0,ue_) = 0.0     
 
       State_GV(-1:0,pO_) =RGAS_I(Ion1_)*State_GV(-1:0,To_) *State_GV(-1:0,RhoO_)
       State_GV(-1:0,pH_) =RGAS_I(Ion2_)*State_GV(-1:0,Th_) *State_GV(-1:0,RhoH_)
