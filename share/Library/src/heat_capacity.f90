699b1926256e49014781c8243ba5498081f48d97
igorsok <>
2008-04-24 17:27:18 +0000

    included CRASH_definitions.h Modified Files:  	share/Library/src/heat_capacity.f90
    Former-commit-id: ec0ee5e1565879660c8a10c04c2d449385b31c24

--- share/Library/src/heat_capacity.f90
+++ share/Library/src/heat_capacity.f90
@@ -1,5 +1,4 @@
 !^CFG COPYRIGHT UM
-
 !=====================Heat capacities, at constant volume===============!!!!
 !
 ! Thermodynamical variables and other notations
@@ -10,25 +9,25 @@
 !        \left(\frac{\partial...}{\partial...}\right)_V - thermodynamical
 !             derivative at constant volume
 !        T_{e,i}, Te,Ti - electron and ion temperature
-!        iLevel - integer variable, a signature of the material:
-!        iLevel=0 - xenon
-!        ilevel=1 - beryllium         
+!        iMaterial - integer variable, a signature of the material:
+!        iMaterial=0 - xenon
+!        iMaterial=1 - beryllium         
 !===================== Electron part ===================================!!!!
 !       
 !=====================  Heat capacity per volume:=======================!!!!
 !               Rho\left(\frac{\partial E}{\partial T_e}\right)_{V,T_i} !!!!
-subroutine get_e_heat_capacity_per_vol(HeatCapacity,Te,Rho,iLevel)
+subroutine get_e_heat_capacity_per_vol(HeatCapacity,Te,Rho,iMaterial)
   implicit none
   include 'CRASH_definitions.h'
   real,intent(out)::HeatCapacity  !In SI: J/(K m^3)
   real,intent(in):: Te            !Electron temperature in SI: K
   real,intent(in):: Rho           !Mass density in SI: kg/m^3
-  integer,intent(in)::iLevel      !iLevel=0 - xenon, ilevel=1 - berillium
+  integer,intent(in)::iMaterial   !iMaterial=0 - xenon,iMaterial=1 - beryllium
   !-----------------------------------------------------------------------!
   real::HeatCapacityPerMass
   !-------------------------!
 
-  call get_e_heat_capacity_per_mass(HeatCapacityPerMass,Te,Rho,iLevel)
+  call get_e_heat_capacity_per_mass(HeatCapacityPerMass,Te,Rho,iMaterial)
   HeatCapacity = HeatCapacityPerMass * Rho
 
 end subroutine get_e_heat_capacity_per_vol
@@ -36,14 +35,14 @@ end subroutine get_e_heat_capacity_per_vol
 !
 !=====================  Heat capacity per mass:  ========================!!!!
 !                \left(\frac{\partial E}{\partial T_e}\right)_{V,T_i}    !!!!
-subroutine get_e_heat_capacity_per_mass(HeatCapacity,Te,Rho,iLevel)      !!!!
+subroutine get_e_heat_capacity_per_mass(HeatCapacity,Te,Rho,iMaterial)      !!!!
   use ModConst
   implicit none
   include 'CRASH_definitions.h'
   real,intent(out)::HeatCapacity  !In SI: J/(K m^3)
   real,intent(in):: Te            !Electron temperature in SI: K
   real,intent(in):: Rho           !Mass density in SI: kg/m^3
-  integer,intent(in)::iLevel      !iLevel=0 - xenon, ilevel=1 - berillium
+  integer,intent(in)::iMaterial   !iMaterial=0 - xenon, iMaterial=1 - beryllium
   !----------------------------------------------------------------------!
   real,parameter :: cAtomicToMass = cBoltzmann / cAtomicMass 
   real,parameter,dimension(0:1) :: AtomicMass_I=(/131.29, &
@@ -54,9 +53,9 @@ subroutine get_e_heat_capacity_per_mass(HeatCapacity,Te,Rho,iLevel)      !!!!
   !----------------------------------------------------------------------!
   !        Version for fully ionized plasma                              !
   !         Only a stab, should be 1.5*Z(n,Te)+(1.5*Te +I(Z))dZ/dTe      !
-  HeatCapacityPerAtom = 1.50*real(nZ_I(iLevel))
+  HeatCapacityPerAtom = 1.50*real(nZ_I(iMaterial))
   !----------------------------------------------------------------------!
   HeatCapacity = HeatCapacityPerAtom * cAtomicToMass / &
-                     AtomicMass_I(iLevel)
+                     AtomicMass_I(iMaterial)
 end subroutine get_e_heat_capacity_per_mass
 !========================================================================!
