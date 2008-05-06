da0408cd66a4683963ceae6ecf96c28df4d17f23
gtoth <>
2008-05-06 21:06:40 +0000

    Moved CRASH related files into util/CRASH
    Former-commit-id: 09200731cb26d4724c59b3cc3bedc510aa993c08

--- share/Library/src/CRASH_definitions.h
+++
@@ -1,5 +0,0 @@
-!^CFG COPYRIGHT UM
-         integer,parameter:: Xe_=0      !Xenon
-         integer,parameter:: Be_=1      !Beryllium
-	 integer,parameter:: Plastic_=2 ! Chemical formula:C_22 H_10 N_2 O_5
-         integer,parameter:: C_=3, H_=4, N_=5, O_=6 !Composition elements
