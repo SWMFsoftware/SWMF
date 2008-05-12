97ee341590f15e3a67e6033afec18eb5bba3ba1f
aglocer <>
2008-05-12 02:43:52 +0000

    Add Dt, from Dt_I which is the timestep used in last call.
    Former-commit-id: d9873f6d21973cde1f0b03c1defba67a7c258410

--- PW/PWOM/src/ModPass.f90
+++ PW/PWOM/src/ModPass.f90
@@ -9,8 +9,8 @@ module ModFieldLine
 
   real,allocatable    ::  PassState_CV(:,:)
 
-  logical :: IsVariableDtPW, DoLogPW
-  real    :: TimePW,MaxLineTimePW,DToutputPW, DTpolarwindPW,GeoMagLatPW,&
+  logical :: DoLogPW
+  real    :: TimePW,MaxLineTimePW,DToutputPW, DtPw,GeoMagLatPW,&
        GeoMagLonPW,JrPW,nDimPW,uJoule2PW
   real    :: wHorizontalPW
   integer :: nStepPw,iUnitInputPW,      &
@@ -29,8 +29,8 @@ contains
   subroutine put_field_line(nAlt,State_CV, &
        GeoMagLat_I,GeoMagLon_I,Jr,wHorizontal,uJoule2,                  &
        iUnitOutput, NameRestart,iLine,Time,   &
-       MaxLineTime,TypeSolver,IsVariableDt,DToutput,DoLog,&
-       nStep,r_C)
+       MaxLineTime,TypeSolver,DToutput,DoLog,&
+       nStep,r_C,Dt)
 
     use ModParameters
     use ModCommonPlanet,ONLY: nVar
@@ -42,7 +42,8 @@ contains
     integer, optional,intent(in)  :: iUnitOutput,iLine,nStep
     character*100,optional,intent(in):: NameRestart
     character(7),optional,intent(in)::TypeSolver
-    logical,optional,intent(in) :: IsVariableDt,DoLog
+    logical,optional,intent(in) :: DoLog
+    real,optional,intent(in) :: Dt
     !-------------------------------------------------------------------------
     if (.not. allocated(PassState_CV)) allocate(PassState_CV(nAlt,nVar))
     if (.not. allocated(rPW_C))        allocate(rPW_C(nAlt))
@@ -59,12 +60,12 @@ contains
     if (present(iLine))         iLinePW        = iLine
     if (present(iUnitOutput))   iUnitOutputPW  = iUnitOutput
     if (present(TypeSolver))    TypeSolverPW   = TypeSolver
-    if (present(IsVariableDt))  IsVariableDtPW = IsVariableDt
     if (present(DToutput))      DToutputPW     = DToutput
     if (present(DoLog))         DoLogPW        = DoLog
     if (present(nStep))         nStepPW        = nStep
     if (present(r_c))           rPw_C(:)       = r_C(:)
     if (present(uJoule2))       uJoule2PW      = uJoule2
+    if (present(Dt))            DtPw           = Dt
 
   end subroutine put_field_line
 
@@ -75,8 +76,8 @@ contains
   subroutine get_field_line(nAlt,State_CV,& 
        GeoMagLat_I,GeoMagLon_I,Jr,wHorizontal,uJoule2,                  &
        iUnitOutput, NameRestart,iLine,Time,   &
-       MaxLineTime,TypeSolver,IsVariableDt,DToutput,DoLog,&
-       nStep,r_C)
+       MaxLineTime,TypeSolver,DToutput,DoLog,&
+       nStep,r_C,Dt)
 
     use ModParameters
     use ModCommonPlanet, ONLY: nVar
@@ -88,9 +89,9 @@ contains
     
     character*100,optional,intent(out):: NameRestart
     character(7),optional,intent(out):: TypeSolver
-    logical,optional,intent(out)      :: IsVariableDt,DoLog
+    logical,optional,intent(out)      :: DoLog
     real, optional, intent(out)       :: Time,MaxLineTime,DToutput,uJoule2
-    real, optional, intent(out)        :: r_C(nAlt)
+    real, optional, intent(out)        :: r_C(nAlt),Dt
     integer, optional,intent(out)     :: iUnitOutput,iLine,nStep
     !--------------------------------------------------------------------------
     if (.not. allocated(PassState_CV)) allocate(PassState_CV(nAlt,nVar))
@@ -110,12 +111,12 @@ contains
     if (present(iLine))         iLine = iLinePW
     if (present(iUnitOutput))   iUnitOutput=iUnitOutputPW
     if (present(TypeSolver))    TypeSolver =TypeSolverPW
-    if (present(IsVariableDt))  IsVariableDt=IsVariableDtPW
     if (present(DToutput))      DToutput=DToutputPW
     if (present(DoLog))         DoLog=DoLogPW
     if (present(nStep))         nStep        = nStepPW
     if (present(r_C))           r_C(:)       = rPw_C(:)
     if (present(uJoule2))       uJoule2      = uJoule2PW
+    if (present(Dt))            Dt           = DtPw
   end subroutine get_field_line
 
 end module ModFieldLine
