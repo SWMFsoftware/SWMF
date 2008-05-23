dd4843a5c4227f4d125c3fc4abda0e947d496e57
plasmint <>
2008-05-23 18:02:57 +0000

    cleaning	test_ionization.f90
    Former-commit-id: e028812cf2d31f59d525a8c4457cc88e9bd20d97

--- util/CRASH/src/test_ionization.f90
+++ util/CRASH/src/test_ionization.f90
@@ -1,34 +1,28 @@
 program caxa     ! test of get_ionization()
-  use ModSaha    ! 
+  use ModSaha    
   use ModIonizPotential
   implicit NONE
 
   integer          :: i, NZ
 
-  real :: Uxe(11)=  &  !<> Xenon
-       (/ 12.1299, 21.21 ,  32.10,  52.42, 65.31, 89.80, 103.41,   &  !1-7
-       176.88,  204.09, 225.86, 255.79                        /)   !8-11
-
-  real  :: Ube(4)= &    !<> Beryllium 
-       (/ 15.5  ,  25.5 , 35.5 , 45.5 /)    
-
-
-  Ube(1) = 14.4  !<~> {-dusty -w} is  NOT required          
-
-
+  !  real :: Uxe(11)=  &  !<> Xenon
+  !       (/ 12.1299, 21.21 ,  32.10,  52.42, 65.31, 89.80, 103.41,   &  !1-7
+  !       176.88,  204.09, 225.86, 255.79                        /)   !8-11
+  !**************************************  debug perposes, only
+  !  real  :: Ube(4)= &    !<> Beryllium 
+  !       (/ 15.5  ,  25.5 , 35.5 , 45.5 /)    
+  !  Ube(1) = 14.4  !<~> {-dusty -w} is  NOT required          
   !      do i=1,11        ! 
   !         U_I(i)=Uxe(i) !  version for XENON, keep it
   !      end do           !
 
 
-  call get_ioniz_potential(25, U_I)  ! new, 1-30 materials
+  call get_ioniz_potential( 54 , U_I)  ! new, 1-30 materials in use
 
 
 
-
-
-  vNatomII    = 1.d025      ! [cm^{-3}]  <== set for default test
-  !deb:> vNatomII    = vNatomII *0.0001
+  vNatomII    = 1.d025         ! [cm^{-3}]  <== set for default test
+                               ! deb:> vNatomII    = vNatomII *0.0001
   OCTPOBA:   do i= 5,300,25    ! 5,300,25  <== set for default test
 
      vTe = 1.0*i               ! [eV]
@@ -47,7 +41,7 @@ program caxa     ! test of get_ionization()
   ! <insert  ALL internal procedures HERE>
   ! vDens  <== density, kg/m^{3}
   ! vTmas  <== temperature, K
-  ! vnUi   <== max ionization      for given mat
+  ! vnUi   <== max ionization      for given chemical element (nZ)
   ! vmUi   <== ioniz.potent.array  ------/------
   ! nMat   <== simple or compl. mat [0,1]
   !            if complex : nUi & mUi complex
@@ -71,4 +65,5 @@ subroutine CON_set_do_test(String,DoTest,DoTestMe)
   character (len=*), intent(in)  :: String
   logical          , intent(out) :: DoTest, DoTestMe
 end subroutine CON_set_do_test
+
 
