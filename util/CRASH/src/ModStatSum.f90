e955e6c370b234ef24df1665cab6bf36ff5560eb
plasmint <>
2008-05-30 17:45:23 +0000

    cleaning ModStatSum.f90
    Former-commit-id: ec07e174195c8c8e84c50929421ef1a92f4549fb

--- util/CRASH/src/ModStatSum.f90
+++ util/CRASH/src/ModStatSum.f90
@@ -1,53 +1,53 @@
 module ModStatSum
-   use ModIonizPotential
-   use ModAtomicMass,ONLY:nZMax
-   implicit none  
-   integer:: nZ           !Atomic number of element in question
-   integer::iZDominant    !Most populated ion state
-   integer::iZMin         !Numbers of the ionization states, such that &
-   integer::iZMax         !the population of ion states with iZ<iZMin or &
-                          !iZ>iZMax is negligible.
-   real,dimension(nZMax)::IonizPotential_I
-   real,dimension(nZMax)::Population_I
+  use ModIonizPotential
+  use ModAtomicMass,ONLY:nZMax
+  implicit none  
+  integer:: nZ           !Atomic number of element in question
+  integer::iZDominant    !Most populated ion state
+  integer::iZMin         !Numbers of the ionization states, such that &
+  integer::iZMax         !the population of ion states with iZ<iZMin or &
+  !iZ>iZMax is negligible.
+  real,dimension(nZMax) :: IonizPotential_I
+  real,dimension(nZMax) :: Population_I
 
 Contains
-   !Set the element and its Ionization Potentials
-   subroutine set_element(nZIn)
-        integer,intent(in)::nZIn
-        nZ=nZIn
-        call get_ioniz_potential(nZ,IonizPotential_I(1:nZ))
-   end subroutine set_element
+  !Set the element and its Ionization Potentials
+  subroutine set_element(nZIn)
+    integer,intent(in) :: nZIn
+    nZ=nZIn
+    call get_ioniz_potential(nZ,IonizPotential_I(1:nZ))
+  end subroutine set_element
 
-   !Finding the populations of the ions
-   subroutine set_population(TeInv,GeLog)
-         real,intent(in)::TeInv !the inverse of the electron temperature
-         real,intent(in)::GeLog ! Logarithm of the electron stat weight
-                                !log(1/Ne\lambda^3)
-         !The program sets iZMin and iZMax, such that the ionization state
-         !population is negligibly small for iZ<iZMin or iZ>iZMax. It also
-         !sets the ionization state populations, Population_I(iZMin:iZMax),
-         !such that:
-         !log(Population_I(I)/Population_I(I-1))=
-         !=-IonizPotential_I(I)*TeInv + GeLog
-         !and sum(Pupulation_I(iZMin:iZMax)=1
-   end subroutine set_population
+  !Finding the populations of the ions
+  subroutine set_population(TeInv,GeLog)
+    real,intent(in)::TeInv !the inverse of the electron temperature
+    real,intent(in)::GeLog ! Logarithm of the electron stat weight
+    !log(1/Ne\lambda^3)
+    !The program sets iZMin and iZMax, such that the ionization state
+    !population is negligibly small for iZ<iZMin or iZ>iZMax. It also
+    !sets the ionization state populations, Population_I(iZMin:iZMax),
+    !such that:
+    !log(Population_I(I)/Population_I(I-1))=
+    !=-IonizPotential_I(I)*TeInv + GeLog
+    !and sum(Pupulation_I(iZMin:iZMax)=1
+  end subroutine set_population
 
-   !Calculating the Z average values
-   real function z_averaged()
-     integer::iLoop
-     z_averaged=0
-     do iLoop=iZMin,iZMax
+  !Calculating the Z average values
+  real function z_averaged()
+    integer::iLoop
+    z_averaged=0
+    do iLoop=iZMin,iZMax
        z_averaged = z_averaged + Population_I(iLoop)*iLoop
-     end do
-   end function z_averaged
+    end do
+  end function z_averaged
 
-   !Calculating the Z^2 average values
-   real function z2_averaged()
-     integer::iLoop
-     z2_averaged=0
-     do iLoop=iZMin,iZMax
+  !Calculating the Z^2 average values
+  real function z2_averaged()
+    integer::iLoop
+    z2_averaged=0
+    do iLoop=iZMin,iZMax
        z2_averaged = z2_averaged + Population_I(iLoop) * real(iLoop)**2
-     end do
-   end function z2_averaged
+    end do
+  end function z2_averaged
 
-end module ModStatSum			 
+end module ModStatSum
