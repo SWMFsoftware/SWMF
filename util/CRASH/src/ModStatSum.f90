a856b116d9fe35488d052482f4deb6ea6c2b7025
igorsok <>
2008-06-02 01:32:44 +0000

    Edit comments. Separate the computation of logarithm of the statistical sum term  Modified Files:  	ModStatSum.f90
    Former-commit-id: c459e961faa46509dce0070243742c9a23e6b85e

--- util/CRASH/src/ModStatSum.f90
+++ util/CRASH/src/ModStatSum.f90
@@ -5,60 +5,80 @@ module ModStatSum
   implicit none  
   integer :: nZ                        !Atomic number of element in question
   integer,dimension(1)::iZDominant    !Most populated ion state
-  integer :: iZMin  !Numbers of the ionization states, such that &
-  integer :: iZMax  !the population of ion states with iZ<iZMin or &
-  integer :: iZ     !iZ>iZMax is negligible.
-  real    :: PiSum  !The sum of all non-normalized Pi values  
+  integer :: iZMin  !Numbers of the ionization states, such that the population
+  integer :: iZMax  !of ion states with iZ<iZMin or iZ>iZMax is negligible.
+  
+ 
   real,dimension(nZMax) :: IonizPotential_I
-  real,dimension(0:nZMax) :: Population_I
-  !real,parameter :: cBoltzmann = 1.3806503e-23 !(m^2*kg)/(s^2*K) - Bolzmann constant (only for testing purposes)
+  real,dimension(0:nZMax) :: Population_I, StatSumTermLog_I
 
 
 Contains
   !Set the element and its Ionization Potentials
+  !==========================================================================
   subroutine set_element(nZIn)
     integer,intent(in) :: nZIn
+    !--------------------------!
     nZ=nZIn
     call get_ioniz_potential(nZ,IonizPotential_I(1:nZ))
   end subroutine set_element
-
-  !Finding the populations of the ions
+  
+  !=========================================================================
+  !Finding the populations of the ion states
   subroutine set_population(TeInv,GeLog)
-         real,intent(in)::TeInv !the inverse of the electron temperature
-         real,intent(in)::GeLog !Natural logarithm of the electron stat weight: log(1/(Ne*lambda^3))
-         Population_I(0) = 0.	!First, make the sequence of ln(Pi) values; let P0=1 ( ln(P0)=0 )
-		 do iZ = 1, nZ  !Fill up the sequence using the following equation:
-		   Population_I(iZ) = Population_I(iZ-1) - IonizPotential_I(iZ)*TeInv/cBoltzmann + GeLog
-		 end do
-		 Population_I(1:nZ) = Population_I(1:nZ) - maxval(Population_I)	!Normalize the array to the maximum value of Pi
-		 iZDominant = maxloc(Population_I) !Find the location of that maximum value
-		 iZMin = count(Population_I(1:iZDominant(1)) <= -5.) + 1 !Find the lower boundary of the array 
-		                                                       !below which the values of Pi can be neglected
-		 iZMax = nZ - count(Population_I(iZDominant(1):nZ) <= -5.) !Find the similar upper boundary
-		 Population_I(iZMin:iZMax) = exp(Population_I(iZMin:iZMax)) !Convert the array back into the Pi values from ln(Pi)
-		 Population_I(1:iZMin-1) = 0. !Get rid of all the negligible values
-		 Population_I(iZMax+1:nZMax) = 0.
-		 PiSum = sum(Population_I(iZMin:iZMax))	!Add up all the values of Pi found so far
-		 Population_I(iZMin:iZMax) = Population_I(iZMin:iZMax)/PiSum !Normalize the Pi-s so that their sum =1
-		                        
-         !The program sets iZMin and iZMax, such that the ionization state
-         !population is negligibly small for iZ<iZMin or iZ>iZMax. It also
-         !sets the ionization state populations, Population_I(iZMin:iZMax),
-         !such that:
-         !log(Population_I(I)/Population_I(I-1))=
-         !=-IonizPotential_I(I)*TeInv/kB + GeLog
-         !and sum(Pupulation_I(iZMin:iZMax)=1
-  end subroutine set_population
+    real,intent(in)::TeInv !the inverse of the electron temperature, [eV]
+    real,intent(in)::GeLog !Natural logarithm of the electron stat weight: log(1/(Ne*lambda^3))
+    real::StatSumTermMax,StatSumTermMin
 
+    !ln(1.0e-2), to truncate terms of the statistical sum, which a factor of 
+    !1e-2 less than the maximal one: 
+    real,parameter::StatSumToleranceLog=4.6 
+
+    integer :: iZ
+    real:: PITotal
+    !--------------------------------------!
+
+    !First, make the sequence of ln(StatSumTerm) values; let ln(P0)=0 )
+    StatSumTermLog_I(0) = 0.	
+    do iZ = 1, nZ  !Fill up the sequence using the following equation:
+       StatSumTermLog_I(iZ) = StatSumTermLog_I(iZ-1) - IonizPotential_I(iZ)*TeInv + GeLog
+    end do
+
+	
+    iZDominant = maxloc(StatSumTermLog_I(0:nZ)) !Find the location of that maximum value
+
+    StatSumTermMax=StatSumTermLog_I(iZDominant(1))
+    StatSumTermMin=StatSumTermMax-StatSumToleranceLog
+    
+    !Find the lower boundary of the array 
+    !below which the values of Pi can be neglected
+    iZMin = count(StatSumTermLog_I(0:iZDominant(1)) <StatSumTermMin) 
+
+    !Find the similar upper boundary
+    iZMax = nZ - count(StatSumTermLog_I(iZDominant(1):nZ) <StatSumTermMin)
+
+    !Get rid of all the negligible values
+    Population_I(0:nZ) = cZero 
+
+    !Convert the array into the Pi values from ln(Pi)
+    Population_I(iZMin:iZMax) = exp(StatSumTermLog_I(iZMin:iZMax)-StatSumTermMax)
+ 
+    PITotal = sum(Population_I(iZMin:iZMax))	!Add up all the values of Pi found so far
+    Population_I(iZMin:iZMax) = Population_I(iZMin:iZMax)/PITotal !Normalize the Pi-s so that their sum =1
+		                        
+  end subroutine set_population
+  !=======================================!
   !Calculating the Z average values
   real function z_averaged()
     integer::iLoop
+    !-------------!
     z_averaged=0
     do iLoop=iZMin,iZMax
-       z_averaged = z_averaged + Population_I(iLoop)*iLoop
+       z_averaged = z_averaged + Population_I(iLoop)*real(iLoop)
     end do
   end function z_averaged
 
+  !=======================================!
   !Calculating the Z^2 average values
   real function z2_averaged()
     integer::iLoop
@@ -67,5 +87,5 @@ Contains
        z2_averaged = z2_averaged + Population_I(iLoop) * real(iLoop)**2
     end do
   end function z2_averaged
-
+  !=======================================!
 end module ModStatSum
