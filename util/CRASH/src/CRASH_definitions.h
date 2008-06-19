d846e85e3fbefc08c75b7218442070f0d6aa932f
igorsok <>
2008-06-19 13:55:51 +0000

    Removed obsolete get_ionization.f90 Modified Files:  	CRASH_definitions.h Makefile get_ionization.f90  	test_ionization.f90
    Former-commit-id: 31ef5093424b586b18229204c6ed4d137a89a0a2

--- util/CRASH/src/CRASH_definitions.h
+++ util/CRASH/src/CRASH_definitions.h
@@ -1,4 +1,36 @@
 !^CFG COPYRIGHT UM
+! In the initial CRASH treatment of materials,
+!
+! 1. We can treat any given material as having a single average Z.
+!
+! 2. Mixtures can be treated either as an average material or as mixtures.
+!
+! 3. If mixtures are treated as mixtures, then collisional rates should be 
+!    calculated using the "effective Z", which is the average of Z squared 
+!    divided by the average Z.
+!
+! 4. The average Z can be determined from the Saha equation, but must not 
+!    exceed the nuclear charge of the material in question.
+!
+! 5. We do not need to account for electron degeneracy in the initial model.
+!
+! 6. In our regime of interest, the electrons behave as an ideal gas in an 
+!    ion-sphere environment within which Coulomb interactions do affect the 
+!    electron pressure and internal energy. The electron pressure and internal 
+!    energy are best calculated using equations 3.47 through 3.50 in 
+!    R. P. Drake, High Energy Density Physics
+!
+! 7. The ion pressure is the ideal gas pressure. The ion internal energy 
+!    includes the particle energy of random motion and the energy of ionization. 
+!    The model of eqs. 3.74 through 3.76 in the mentioned book is acceptable. 
+!    Alternatively, a more complex model using actual ionization energies would 
+!    be acceptable.
+!
+! 8. The materials that matter are
+!    - Beryllium
+!    - Xenon
+!    - Polyimide (C_22 H_10 N_2 O_5)
+!
          integer,parameter:: Xe_=0      !Xenon
          integer,parameter:: Be_=1      !Beryllium
 	 integer,parameter:: Plastic_=2 ! Chemical formula:C_22 H_10 N_2 O_5
--- util/CRASH/src/test_ionization.f90
+++ util/CRASH/src/test_ionization.f90
@@ -1,18 +1,35 @@
-
-program caxa     ! test of get_ionization()
-  use ModSaha    
-
+!^CFG COPYRIGHT UM
+program saha
+  use ModStatSum
   implicit NONE
+  real :: &
+       Nao = 1.00e19,  &  ! 25 , &  ! 25, &     ! cm-3
+       vTe,nA 
 
-  integer          :: i 
+  real ::  dTe,dLogN
+  integer :: iT,nT=1000,iN
+  integer,parameter::nN=4
+  real::z_I(0:nN),z2_I(0:nN)
+  character(LEN=*),parameter,dimension(0:nN)::Separator_I='|'
+  character(LEN=*),parameter,dimension(0:nN)::Separator1_I='/'
 
+  dTe = 1.0; dLogN=log(10.0)
 
-  call proGON
+  call set_element( 54 )
+  call mod_init
 
-
-
-end program caxa
-
+  do  iT = 1,nT
+     vTe = dTe * iT
+     do iN=0,nN
+        Na  = Nao*exp(iN*dLogN)
+        call set_ionization_equilibrium(Na*1000000.0, vTe*11610.0)
+        Z_I(iN) = z_averaged() 
+        Z2_I(iN)= z2_averaged()/Z_I(iN)
+     end do
+     write(*,'(a,f4.0,a,5(a,f4.1,a,f4.1))')'|',vTe,'|',&
+          ((Z_I(iN),Separator1_I(iN),Z2_I(iN),Separator_I(iN)),(iN=0,nN))
+  end do
+end program saha
 
 !============================================================================
 ! The following subroutines are here so that we can use SWMF library routines
