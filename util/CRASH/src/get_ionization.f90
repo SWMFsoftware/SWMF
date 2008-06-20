467112e1c10a304c0b0ddacadf751f4bc6a3b673
plasmint <>
2008-06-20 16:40:29 +0000

    Modified Files: cleared testing cycles  get_ionization.f90 test_ionization.f90
    Former-commit-id: 9a5c800b257ff67cbe425a76d3acc96a0a78d514

--- util/CRASH/src/get_ionization.f90
+++ util/CRASH/src/get_ionization.f90
@@ -1,8 +1,4 @@
 !^CFG COPYRIGHT UM
 
-!***********************************************************************
-!    calculation of ionization equilibrium, single material
-!    for given - concentration of  atomic particles Co+Ciz [cm^{-3}]
-!              - electron temperature Te[eV]
-!***********************************************************************
+
 
--- util/CRASH/src/test_ionization.f90
+++ util/CRASH/src/test_ionization.f90
@@ -1,38 +1,51 @@
 !^CFG COPYRIGHT UM
+!***********************************************************************
+!    calculation of ionization equilibrium, single material
+!    for given - concentration of  atomic particles Co+Ciz [cm^{-3}]
+!              - electron temperature Te[eV]
+!***********************************************************************
 program saha
   use ModStatSum
   implicit NONE
   real :: &
        Nao = 1.00e18,  &  ! 25 , &  ! 25, &     ! cm-3
-       vTe,Na 
+       vTe, Na 
 
-  real    ::  dTe,dLogN
-  integer :: iT,nT=1000,iN
-  integer,parameter::nN=5
-  real    ::z_I(0:nN),z2_I(0:nN)
+  integer,parameter :: nN=5 , nT= 1000000
+  real    :: dTe, dLogN
+  integer :: iT , nT1=1000000, iN
+
+  real    :: z_I(0:nN),z2_I(0:nN)
   character(LEN=*),parameter,dimension(0:nN)::Separator_I='|'
   character(LEN=*),parameter,dimension(0:nN)::Separator1_I='/'
-  logical::IsDegenerated
-  dTe = 2.0; dLogN=log(10.0)
+  logical :: IsDegenerated
+
+  dTe = 0.1; dLogN=log(10.0)
 
   call set_element( 54 )
   call mod_init
 
-  do  iT = 1,nT
+  nT1 =  nN*nT/1000000 
+  write(*,*)"Start,", nT1 , " million iterations"
+
+  do iT  = 1,nT
      vTe = dTe * iT
-     do iN=0,nN
-        Na  = Nao*exp(iN*dLogN)
+     do iN = 0,nN
+        Na = Nao*exp(iN*dLogN)
         call set_ionization_equilibrium(vTe,Na*1000000.0,IsDegenerated)
         Z_I(iN) = z_averaged() 
         Z2_I(iN)= z2_averaged()/Z_I(iN)
         if(IsDegenerated)then
-           Z_I(iN)=-1.0
-           Z2_I(iN)=-1.0
+           Z_I(iN) = -1.0
+           Z2_I(iN)= -1.0
         end if
      end do
-     write(*,'(a,f5.0,a,6(f4.1,a,f4.1,a))')'|',vTe,'|',&
-          (Z_I(iN), Separator1_I(iN), Z2_I(iN), Separator_I(iN), iN=0,nN )
+
+     !     write(*,'(a,f5.0,a,6(f4.1,a,f4.1,a))')'|',vTe,'|',&
+     !          (Z_I(iN), Separator1_I(iN), Z2_I(iN), Separator_I(iN), iN=0,nN )
   end do
+  write(*,*)"End ", nT1," million iterations"
+
 end program saha
 
 !============================================================================
