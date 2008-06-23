65613332435dc996644bf0a16c1765780aae1db9
igorsok <>
2008-06-23 16:17:09 +0000

    Modified Files:  	test_ionization.f90
    Former-commit-id: bd8456a1ea79ac5a47e70a922798d17597311534

--- util/CRASH/src/test_ionization.f90
+++ util/CRASH/src/test_ionization.f90
@@ -11,26 +11,37 @@ program saha
        Nao = 1.00e18,  &  ! 25 , &  ! 25, &     ! cm-3
        vTe, Na 
 
-  integer,parameter :: nN=5 , nT=1000
+  integer,parameter :: nN=5 , nT=30
   real    :: dTe, dLogN
   integer :: iT , nT1=1000000, iN
-!  integer :: tm_1
   
   real    :: z_I(0:nN),z2_I(0:nN)
   character(LEN=*),parameter,dimension(0:nN) :: Separator_I='|'
   character(LEN=*),parameter,dimension(0:nN) :: Separator1_I='/'
   logical :: IsDegenerated
 
-  dTe = 1; dLogN=log(10.0)
+  dTe = 5.0; dLogN=log(10.0)
 
   call set_element( 54 )
   call mod_init
 
- 
+  tm_1 = diff_sec()!
 
-!  nT1  = (nN +1)*nT/1000000 
-!  write(*,*)"Start,", nT1 , " million iterations"
-!   tm_1 = diff_sec() ! start time
+  nT1 =  (nN +1)*nT/1000000 
+  write(*,*)"Start,", nT1 , " million iterations"
+
+  open(24,file='../doc/Table1.tex',status='unknown')
+  write(24,'(a)')'\begin{verbatim}'
+  write(24,'(a)')'____________________________________'//&
+'______________________________'
+  write(*,'(a)')' \ Na |   10^18 |   10^19 |   10^20 |'//& 
+'   10^21 |   10^22 |   10^23 |'
+  write(24,'(a)')'Na/cm3|   10^18 |   10^19 |   10^20 |'//& 
+'  10^21 |   10^22 |   10^23  |'
+  write(24,'(a)')'Te[K]\|z,<z^2>/z|z,<z^2>/z|z,<z^2>/z|'//& 
+'z,<z^2>/z|z,<z^2>/z|z,<z^2>/z|'
+  write(24,'(a)')'------------------------------------'//&
+'------------------------------'
 
   do iT  = 1,nT
      vTe = dTe * iT
@@ -44,16 +55,17 @@ program saha
            Z2_I(iN)= -1.0
         end if
      end do
-
-          write(*,'(a,f5.0,a,6(f4.1,a,f4.1,a))')'|',vTe,'|',&
+     write(* ,'(a,f5.0,a,6(f4.1,a,f4.1,a))')'|',vTe,'|',&
+               (Z_I(iN), Separator1_I(iN), Z2_I(iN), Separator_I(iN), iN=0,nN )
+     write(24,'(a,f5.0,a,6(f4.1,a,f4.1,a))')'|',vTe,'|',&
                (Z_I(iN), Separator1_I(iN), Z2_I(iN), Separator_I(iN), iN=0,nN )
   end do
 
+  write(24,'(a)')'------------------------------------'//&
+'------------------------------'
+  write(24,'(a)')'\end{verbatim}'
+  close(24)
 
-
- ! tm_1 = diff_sec() -tm_1;
- ! write(*,'(f4.2,a,i4,a)') nT1/(tm_1 +1.0d0)," [MOPs/s], t:",tm_1,"s" 
- ! write(*,*)"End,  ", nT1," million iterations"
 
 
 contains
