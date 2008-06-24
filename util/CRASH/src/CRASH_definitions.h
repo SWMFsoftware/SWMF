a6c961fb25ce40c0962e9fbfae5208e2872bb6b2
igorsok <>
2008-06-24 17:01:15 +0000

    Repeating array nZ_I moved to the CRASH_definitions  Modified Files:  	CRASH_definitions.h
    Former-commit-id: de6133907f8160d5748ea19650c8d5f13ed808fe

--- util/CRASH/src/CRASH_definitions.h
+++ util/CRASH/src/CRASH_definitions.h
@@ -35,4 +35,5 @@
          integer,parameter:: Be_=1      !Beryllium
 	 integer,parameter:: Plastic_=2 ! Chemical formula:C_22 H_10 N_2 O_5
          integer,parameter:: C_=3, H_=4, N_=5, O_=6 !Composition elements
-
+         integer,parameter, dimension(0:1) :: nZ_I=(/54, &
+                                               4  /)
