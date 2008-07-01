0f607c1deef20e406ed0e0591d0985ee0a9a136e
igorsok <>
2008-07-01 15:05:25 +0000

    Modified Files:  	util/CRASH/src/test_godunov.f90
    Former-commit-id: 5e5ccdbff8de0245e501cd3d3df0a1ce60dfe587

--- util/CRASH/src/test_godunov.f90
+++ util/CRASH/src/test_godunov.f90
@@ -17,17 +17,18 @@ program test_Godunov
   use ModAtomicMass
   use ModEos
   implicit none
-  integer,parameter::nStep=500,nX=1000,nDim=1,nVar=3,iMaterial=0
+  integer,parameter::nX=1000,nDim=1,nVar=3,iMaterial=0
   real::Cons_VC(nVar,1:nX),Prime_VG(nVar,0:nX+1),Flux_VF(nVar,1:nX+1)
   real::CMax_F(1:nX+1),Gamma_G(0:nX+1)
   real,parameter::cLength=5.0e-3    !5 mm
   real::Dt,Time
   real,parameter::DX=cLength/nX
-  integer::iStep,iX
+  integer::iX
   real,parameter::RhoInit=100.0 !Approximately 30 times the normal density
   real,parameter::UInit = 1.50*RhoInit*(8.31e+3/122.2)*3.0e3 !3000 K 
   real,parameter::UPiston = 3.0e+4 !30 km/s
-  real,parameter::CFL=0.9           !
+  real,parameter::CFL=0.8
+  real,parameter::TimeOut=5.0e-7 !500 ns
 !^CFG COPYRIGHT UM
 !=====================Equation Of State (EOS)===========================!!!!
 !
@@ -52,9 +53,9 @@ program test_Godunov
   end do
      
   open(24,file='Godunov_scheme_for_Xe',status='replace')
-  write(24,'(a)') 'X [m] Rho [kg/m^3]   u [m/s]  P [Pa]  Gamma'
-  do iStep=1,nStep
-     write(*,*)'iStep,StartTime=',iStep,Time
+  write(24,'(a)') 'X [mm] Rho [100 kg/m^3]   u [10,000 m/s]  P [MBar]  Gamma'
+  do 
+     write(*,*)'iStep,StartTime=',Time
      !Get primitives:
      do iX=1,nX
         Prime_VG(1,iX)=Cons_VC(1,iX)
@@ -66,9 +67,13 @@ program test_Godunov
                PTotalOut=Prime_VG(3,iX),      & !Output,pressure [Pa]
                GammaOut=Gamma_G(iX))            !Output,polytropic index
      end do
-     if(iStep==nStep)then
+     if(Time>TimeOut)then
         do iX=1,nX
-           write(24,'(5E13.6)')DX*iX,Prime_VG(:,iX),Gamma_G(iX)
+           write(24,'(5(E14.6,2X))')DX*(iX-0.5)/1.0e-3,&
+           Prime_VG(1,iX)/RhoInit,&
+           Prime_VG(2,iX)/1.0e4,&
+           Prime_VG(3,iX)/1.0e11,&
+           Gamma_G(iX)
         end do
         exit
      end if
@@ -77,24 +82,21 @@ program test_Godunov
      Prime_VG(:,nX+1)=Prime_VG(:,nX); Gamma_G(nX+1)=Gamma_G(nX)
      
      !Reflection at the left end
-     Prime_VG(2,0) = -Prime_VG(2,0)
+     Prime_VG(2,0) = - Prime_VG(2,0)
 
      
      !Get fluxes
      do iX=1,nX+1
         call get_godunov_flux(1,(/1.0/),3,Prime_VG(:,iX-1),Prime_VG(:,iX),&
              Flux_VF(:,iX),CMax_F(iX),Gamma_G(iX-1),Gamma_G(iX))
-!        if(iX==1)write(*,*)Flux_VF(:,1)
      end do
-     Cons_VC(:,1:nX)=Cons_VC(:,1:nX)+(Dt/DX)*(Flux_VF(:,1:nX)-Flux_VF(:,2:nX+1))
+     Cons_VC(:,1:nX)=Cons_VC(:,1:nX)+&
+          (Dt/DX)*(Flux_VF(:,1:nX)-Flux_VF(:,2:nX+1))
+     Dt=min(CFL*DX/maxval(CMax_F(1:nX+1)),1.001*(TimeOut-Time))
+
      Time = Time + Dt
-     call set_dt
   end do
   close(24)
-contains
-  subroutine set_dt
-    Dt=CFL*DX/maxval(CMax_F(1:nX+1))
-  end subroutine set_dt
 end program test_Godunov
 !============================================================================
 ! The following subroutines are here so that we can use SWMF library routines
