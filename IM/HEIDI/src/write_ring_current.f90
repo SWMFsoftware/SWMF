aa650251ff27ee3af3189c7fd77b347fb3803e69
ridley <>
2008-07-21 21:15:33 +0000

    bug fix in write_ring_current - finds max and min potential now....
    Former-commit-id: 6cb42d7138bbf54e2ebd9ca60e3d75ad437412bb

--- IM/HEIDI/src/write_ring_current.f90
+++ IM/HEIDI/src/write_ring_current.f90
@@ -158,15 +158,15 @@ subroutine write_ring_current
 	potfacn=potfacn+kfac*(1.-lfac)*IONO_NORTH_PHI(kk,l)
 	potfacn=potfacn+(1.-kfac)*lfac*IONO_NORTH_PHI(k,l1)
 	potfacn=potfacn+kfac*lfac*IONO_NORTH_PHI(kk,l1)
-	potfacs=(1.-kfac)*(1.-lfac)*IONO_SOUTH_PHI(IONO_nTheta-k,l)
-	potfacs=potfacs+kfac*(1.-lfac)*IONO_SOUTH_PHI(IONO_nTheta-kk,l)
-	potfacs=potfacs+(1.-kfac)*lfac*IONO_SOUTH_PHI(IONO_nTheta-k,l1)
-	potfacs=potfacs+kfac*lfac*IONO_SOUTH_PHI(IONO_nTheta-kk,l1)
-	If (abs(potfacn).le.abs(potfacs)) then
+!	potfacs=(1.-kfac)*(1.-lfac)*IONO_SOUTH_PHI(IONO_nTheta-k,l)
+!	potfacs=potfacs+kfac*(1.-lfac)*IONO_SOUTH_PHI(IONO_nTheta-kk,l)
+!	potfacs=potfacs+(1.-kfac)*lfac*IONO_SOUTH_PHI(IONO_nTheta-k,l1)
+!	potfacs=potfacs+kfac*lfac*IONO_SOUTH_PHI(IONO_nTheta-kk,l1)
+!	If (abs(potfacn).le.abs(potfacs)) then
            potfac = potfacn
-	else
-           potfac = potfacs
-	endif
+!	else
+!           potfac = potfacs
+!	endif
         
 	potdgcpm(i,j) = potdgcpm(i,j) + potfac
         
