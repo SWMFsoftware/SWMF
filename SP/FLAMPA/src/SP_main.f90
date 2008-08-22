6a71e71e3abaff3dc048a8ef585877cd18e8198a
igorsok <>
2008-08-22 21:21:33 +0000

    Fix a format  Modified Files:  	SP_main.f90
    Former-commit-id: 10d43a111aca1154740c475671869038f206502f

--- SP/FLAMPA/src/SP_main.f90
+++ SP/FLAMPA/src/SP_main.f90
@@ -523,7 +523,7 @@ Contains
       open(iFile,file=NameFile,status='replace',iostat=iError)
 
       R=sqrt(sum(X_DI(:,iShock)**2 ))/cAU
-      write(iFile,'(a,f5.3,a)')  'TITLE="Rshock=', R, ' AU, SP_time=',SP_time,'s, iX=',iShock+5,'"'
+      write(iFile,'(a,f5.3,a,F5.0,a,i5,a)')  'TITLE="Rshock=', R, ' AU, SP_time=',SP_time,'s, iX=',iShock+5,'"'
       write(iFile,*) 'VARIABLES = "k", "I+", "I-"'
 
       do iK=1,nP
