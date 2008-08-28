e954ae4357232e1f226542eacfe0157808878e20
dpawlows <>
2008-08-28 14:42:56 +0000

    added chemicalheatings
    Former-commit-id: 4a4b543a3adba4fab7a0bbbd5b5058a06643b87f

--- UA/GITM2/src/ModSources_static.f90
+++ UA/GITM2/src/ModSources_static.f90
@@ -24,6 +24,7 @@ module ModSources
 
   integer, parameter :: nReactions = 26
   real :: ChemicalHeatingSpecies(nLons, nLats, nAlts,nReactions)
+  real :: ChemicalHeatingS(nReactions)
   integer, parameter ::    in2p_e = 1
   integer, parameter ::    io2p_e = 2
   integer, parameter ::    in2p_o = 3
