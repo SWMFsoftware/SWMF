692de58d74aa9c71966d108105299ea6c8825be0
dpawlows <>
2008-09-03 15:19:30 +0000

    GLOW files
    Former-commit-id: e58d73c0a93293406268859dcc229beabdf9079f

---
+++ UA/GITM2/srcGlow/ModCIMPIT.f90
@@ -0,0 +1,11 @@
+module ModCIMPIT
+
+  use Mod_Glow, only: ALTSMAX
+  implicit none
+
+
+  real  :: ALPHA(ALTSMAX), BETA(ALTSMAX), GAMMA2(ALTSMAX), PSI(ALTSMAX),&
+       DELZ(ALTSMAX), DEL2(ALTSMAX), DELA(ALTSMAX), DELP(ALTSMAX), &
+       DELM(ALTSMAX), DELS(ALTSMAX), DEN(ALTSMAX), FAC
+
+end module ModCIMPIT
