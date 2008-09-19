53a227fa35e799c58bed727bd416924671d68c53
igorsok <>
2008-09-19 04:54:54 +0000

    Modified Files:  	MDI.pl  Added Files:  	GNG.pl
    Former-commit-id: fa3da7eace67d6f814cbebeebd38e61f559d42d8

--- util/DATAREAD/srcMagnetogram/GNG.pl
+++ util/DATAREAD/srcMagnetogram/GNG.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -s
 #
 # Script downloads the magnetograms and processes them
-# r means Carrington rotation t - Till e - execute
+#
 # MDI.pl -r=nnnn (-t=mmmm) (-e) (-mpi='mpirun -np 2')
 ###########################################################
 #  IDL program
@@ -15,21 +15,19 @@ my $MpiRun = ($mpi);
 print " Carrington rotation: first CR= $CR\n";
 print " Carrington rotation: last  $Till\n";
 print " The converted file is harmonics.dat\n" if $DoExec;
-
 system("make HARMONICS") if $DoExec;
 
 $IDL_PRO="fits_to_asciicr";
 my $Executable="../../../bin/HARMONICS.exe";
-$Executable=$MpiRun." ".$Executable if $MpiRun;
+my $ftpsite = "gong2.nso.edu";
+my $ftpdir = "QR/mqs/";
 
-$ftpsite = "soi.stanford.edu";
-
-print ("$Executable \n") if $DoExec;
+print ("$MpiRun "."$Executable\n") if $MpiRun;
 ############################################################
 # The carringon rotation cycle will start with this number +1
 #
 
-$CR = $CR -1; 
+$CR = $CR -1;
 
 #
 #############################################################
@@ -39,6 +37,7 @@ $CR = $CR -1;
   until($CR==$Till)
   {
 $CR=$CR+1;
+
 #
 ###########################################################
 # The url of the data, data location directory and filenames
@@ -47,11 +46,7 @@ $CR=$CR+1;
 print " CR= $CR\n";
 #
 
-#
-############################################################
-# Check if the magnetogram has been already downloaded    
-#
-      if (-e "CR".$CR."_MDI.fits")
+if (-e "CR".$CR."_GNG.fits")
       {
         print("The Magnetogram for CR=$CR is already downloaded \n");   
           
@@ -59,42 +54,46 @@ print " CR= $CR\n";
 #
       else 
       { 
-#
-############################################################
-#  Getting the magnetogram, renaming
-  $ftpdir  = "magnetic/synoptic/carrot/M/".$CR."/";
-  $ftpname = "synop_Mr_0.".$CR.".fits";
 
-	system("wget http://".$ftpsite."/".$ftpdir.$ftpname);
-        system("mv *".$CR.".fits CR".$CR."_MDI.fits");
+############################################################
+#  Getting the magnetogram, renaming,running the IDL program,
+#  renaming the output files
+#
+
+
+system("wget -r -nd -A \"*".$CR."_000.fits.gz\" ftp://".$ftpsite."/".$ftpdir);
+system("gzip -d *".$CR."_000.fits.gz ");
+system("mv *".$CR."_000.fits  CR".$CR."_GNG.fits");
+system("rm *".$CR."_000.fits.gz");
       }
 
-
 #
-      if (-e "CR".$CR."_MDI.DAT")
-      {
+      if (-e "CR".$CR."_GNG.DAT")
+ {
         print("The Magnetogram for CR=$CR is already converted \n");   
           
       }  
 #
       else 
       {  
+
 #
 ############################################################
 #  Running the IDL program,
 #  renaming the output files
 #
-	 system("cp CR".$CR."_MDI.fits fitsfile.fits");
+	 system("cp CR".$CR."_GNG.fits fitsfile.fits");
 
-         system("echo $IDL_PRO, \"CR=\"$CR | idl");
-	 system("mv fitsfile.dat CR".$CR."_MDI.DAT ");
-         system("mv fitsfile.H fitsfile_".$CR.".H ");
-         system("mv fitsfile_tec.dat fitsfile_tec".$CR.".dat ");
+
+system("echo $IDL_PRO, \"CR=\"$CR | idl");
+system("mv fitsfile.dat CR".$CR."_GNG.DAT");
+system("mv fitsfile.H fitsfile_".$CR.".H ");
+system("mv fitsfile_tec.dat fitsfile_tec".$CR.".dat ");
       }
 if ($DoExec){
-    system("cp CR".$CR."_MDI.DAT  fitsfile.dat");
-    system($Executable);
-    system("mv harmonics.dat CR".$CR."_MDI.dat");
+    system("cp CR".$CR."_GNG.DAT  fitsfile.dat");
+    system("$MpiRun "."$Executable");
+    system("mv harmonics.dat CR".$CR."_GNG.dat");
     system("rm fitsfile.dat")
             }
 
