9666309c6e65f46b55cf89892fe4f88ddc55dc46
igorsok <>
2008-09-29 07:30:19 +0000

    Indent Files:  	src/CRASH_definitions.h
    Former-commit-id: 5db9730ecd1a66329cbbec1dc58f927a52587c40

--- util/CRASH/src/CRASH_definitions.h
+++ util/CRASH/src/CRASH_definitions.h
@@ -33,6 +33,7 @@
 !
          integer,parameter:: Xe_=0      !Xenon
          integer,parameter:: Be_=1      !Beryllium
-	 integer,parameter:: Plastic_=2 ! Chemical formula:C_22 H_10 N_2 O_5
+		   integer,parameter:: Plastic_=2 ! Chemical formula:C_22 H_10 N_2 O_5
          integer,parameter:: C_=3, H_=4, N_=5, O_=6 !Composition elements
          integer,parameter, dimension(0:2) :: nZ_I=(/54, 4, 6 /)
+			
