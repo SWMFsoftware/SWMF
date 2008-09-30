5baa06777043691717f37e31c9c60ea6e0624f8d
igorsok <>
2008-09-30 15:40:46 +0000

    Added Files:  	util/CRASH/src/ModPolyimide.f90
    Former-commit-id: 6b4398a81ffa791e6e63c6c710409e181e47a4aa

---
+++ util/CRASH/src/ModPolyimide.f90
@@ -0,0 +1,27 @@
+!^CFG COPYRIGHT UM
+!===================Constants for polyimide============================!!!!
+
+Module ModPolyimide
+  use ModAtomicMass
+  implicit none
+  save
+  
+  !Number of elements in C_22 H_10 N_2 O_5
+  integer,parameter :: nPolyimide = 4 
+  
+  !Relative atomic concentrations in C_22 H_10 N_2 O_5
+  real,dimension(nPolyimide),parameter :: CPolyimide_I = &
+         (/22.0, 10.0, 2.0, 5.0/)/(22.0 + 10.0 + 2.0 +5.0)
+
+  !Atomic number for elements:
+  integer,dimension(nPolyimide),parameter :: nZPolyimide_I = &
+         (/6, 1, 7, 8/)
+   
+  ! Averaged atomic mass
+  real,parameter :: cAPolyimide = &
+         (cAtomicMass_I(6) * 22.0 + &
+         cAtomicMass_I(1) * 10.0 + &
+         cAtomicMass_I(7) *  2.0 + &
+         cAtomicMass_I(8) *  5.0) / &
+         (22.0 + 10.0 + 2.0 +5.0)
+end Module ModPolyimide
