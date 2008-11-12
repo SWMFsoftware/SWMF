e937f3e056e4939e74d8a6bc8200596ea916986b
ridley <>
2008-11-12 20:41:51 +0000

    This is a "good" grid for a single processor.
    Former-commit-id: c25c8639e46c8a6db40e2e0df56a190d2cd4f126

--- IE/RIM/src/ModSizeRIM.f90
+++ IE/RIM/src/ModSizeRIM.f90
@@ -3,7 +3,7 @@ module ModSizeRIM
   implicit none
 
   integer, parameter :: nLats = 90
-  integer, parameter :: nLons = 18
+  integer, parameter :: nLons = 72
   integer :: nLonsAll = 0
 
 end module ModSizeRIM
