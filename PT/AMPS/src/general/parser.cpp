6875c8baf44550160b7e8585d0784a7ca17861dc
vtenishe <>
2009-02-06 17:28:41 +0000

    *** empty log message ***
    Former-commit-id: 836e68904f2f4042c1f83b9114c5349bdec1b91b

--- PT/AMPS/src/general/parser.cpp
+++ PT/AMPS/src/general/parser.cpp
@@ -26,11 +26,10 @@
 #endif
 
 
-FILE* fd;
 char init_str[100];
 long int line=0;
 //===================================================
-//Separators:' ', ',', '=', ';', ':' 
+//Separators:' ', ',', '=', ';', ':' '"' 
 void CutInputStr(char* dest, char* src) {
   int i,j;
 
@@ -49,7 +48,7 @@ void CutInputStr(char* dest, char* src) {
   } 
 }
 //===================================================
-void GetInputStr(char* str,int n){
+void GetInputStr(char* str,int n,FILE* fd){
   int i,j; 
  
   do {
@@ -64,6 +63,7 @@ void GetInputStr(char* str,int n){
   } while ((str[0]=='\0')&&(!feof(fd)));
 
   for(i=0;str[i]!='\0';i++) { 
+    if (str[i]=='"') str[i]=' '; 
     init_str[i]=str[i];
     if ((str[i]>='a')&&(str[i]<='z')) str[i]=str[i]-32;
   }
@@ -78,14 +78,14 @@ void error() {
 
 //===================================================
 //===================================================
-void GeneralBlock() {
+void GeneralBlock(FILE* fd) {
   static bool GeneralBlockLoadedFlag=false;
   char str1[100],str[100];
   char *endptr;
 
   if (GeneralBlockLoadedFlag!=false) {
     do {
-      GetInputStr(str,sizeof(str));
+      GetInputStr(str,sizeof(str),fd);
       CutInputStr(str1,str);
     }
     while((!feof(fd))&&(strcmp("#ENDGENERAL",str1)!=0)); 
@@ -96,7 +96,7 @@ void GeneralBlock() {
   GeneralBlockLoadedFlag=true; 
 
   while (!feof(fd)) {
-    GetInputStr(str,sizeof(str));
+    GetInputStr(str,sizeof(str),fd);
     CutInputStr(str1,str);
 
     if (strcmp("TAU",str1)==0) {
@@ -157,7 +157,17 @@ void GeneralBlock() {
         SymmetryMode=cylindrical_symmetry;
       else if (strcmp("SPHERICAL",str1)==0)
         SymmetryMode=spherical_symmetry;
-      else error();}
+      else error(); 
+    }
+    else if (strcmp("SPECIESLIST",str1)==0) {
+      while (str[0]!='\0') {
+        char *newSpecieSymbol=new char[100];
+        CutInputStr(str1,str);
+        for (int i=0;str1[i]!='\0';i++) newSpecieSymbol[i]=str1[i];
+
+        mol.SpeciesList.push_back(newSpecieSymbol);
+      }
+    } 
     else if (strcmp("#ENDGENERAL",str1)==0) {
 #if (CompilationTarget==DSMCTARGET)||(CompilationTarget==PICTARGET)||(CompilationTarget==HYBRIDTARGET) 
       mol.init(NS);
@@ -170,6 +180,7 @@ void GeneralBlock() {
 void parser(char* InputFile) {
   char nonstandardBlock_endname[100]; 
   bool nonstandardBlock_flag=false;
+  FILE* fd;
 
   char str1[100],str[100];
 
@@ -182,9 +193,14 @@ void parser(char* InputFile) {
 
   fd=fopen(InputFile,"r");
   while (!feof(fd)) {
-    GetInputStr(str,sizeof(str));
+    GetInputStr(str,sizeof(str),fd);
     CutInputStr(str1,str);
-    if (strcmp("#GENERAL",str1)==0) GeneralBlock();
+    if (strcmp("#GENERAL",str1)==0) GeneralBlock(fd);
+    else if (strcmp("#INCLUDE",str1)==0) {
+      CutInputStr(str1,init_str);
+      CutInputStr(str1,init_str);
+      parser(str1);
+    }
 
 #if CompilationTarget==DSMCTARGET 
     else if (strcmp("#SPECIES",str1)==0) mol.parser(fd,line,InputFile);
@@ -208,28 +224,14 @@ void parser(char* InputFile) {
 #endif
 
     else if (strcmp("#END",str1)==0) return;
-
-    else if ((str1[0]=='#')&&(nonstandardBlock_flag==false)) {
-      printf("Found nonstandard block: %s\n",str1);
-
-      nonstandardBlock_flag=true;
-      sprintf(nonstandardBlock_endname,"#END%s",str1+1);     
-    } 
-    else if (nonstandardBlock_flag==true) {
-      if (strcmp(nonstandardBlock_endname,str1)==0) nonstandardBlock_flag=false;   
-    } 
-
-    else if (nonstandardBlock_flag==false) error();
+    else if (strcmp("",str1)!=0) error();
   }
-
-  error();
 }  
 
 void parser_readGeneralBlock(FILE* fin,long int& line_in) {
-  fd=fin;
   line=line_in;
 
-  GeneralBlock();
+  GeneralBlock(fin);
   line_in=line;
 }
 
