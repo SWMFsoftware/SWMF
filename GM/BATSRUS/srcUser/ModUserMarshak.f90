e0b196ece1a8f71182348f7831c9ffe669ba4cc4
bartvand <>
2009-03-09 14:54:49 +0000

    Su and Olson's non-equilibrium Marshak wave  Modified Files:  	src/ModGrayDiffusion.f90 src/ModTemperature.f90  	srcUser/ModUserHeatConduction.f90  Added Files:  	srcUser/ModUserMarshak.f90
    Former-commit-id: b341fa7a097d37419c1d3fb3d6855e44f2c69644

--- GM/BATSRUS/srcUser/ModUserMarshak.f90
+++ GM/BATSRUS/srcUser/ModUserMarshak.f90
@@ -3,8 +3,8 @@
 module ModUser
   use ModUserEmpty,                                     &
        IMPLEMENTED1 => user_read_inputs,                &
-       IMPLEMENTED2 => user_init_session,               &
-       IMPLEMENTED3 => user_set_ics,                    &
+       IMPLEMENTED2 => user_set_ics,                    &
+       IMPLEMENTED3 => user_set_outerbcs,               &
        IMPLEMENTED4 => user_update_states,              &
        IMPLEMENTED5 => user_set_plot_var,               &
        IMPLEMENTED6 => user_material_properties
@@ -12,44 +12,11 @@ module ModUser
   include 'user_module.h' !list of public methods
 
   real,              parameter :: VersionUserModule = 1.0
-  character (len=*), parameter :: &
-       NameUserModule = 'heat conduction'
+  character (len=*), parameter :: NameUserModule = 'Marshak Wave'
 
   character(len=20) :: TypeProblem
-  real :: HeatConductionCoef, AmplitudeTemperature, T0
 
 contains
-
-  !============================================================================
-
-  subroutine user_init_session
-
-    use ModMain,   ONLY: Time_Simulation
-    use ModProcMH, ONLY: iProc
-
-    character(len=*), parameter :: NameSub = 'user_init_session'
-    !--------------------------------------------------------------------------
-
-    if(Time_Simulation <= 0.0)then
-       if(iProc == 0) write(*,*) NameSub// &
-            ' : starting simulation time should be larger than 0'
-       call stop_mpi('reset time with #TIMESIMULATION')
-    end if
-
-    select case(TypeProblem)
-    case('gaussian')
-       HeatConductionCoef = 0.1
-       AmplitudeTemperature = 10.0
-       T0 = 10.0
-    case('rz')
-       HeatConductionCoef = 0.1
-       AmplitudeTemperature = 10.0
-       T0 = 3.0
-    case default
-       call stop_mpi(NameSub//' : undefined problem type='//TypeProblem)
-    end select
-
-  end subroutine user_init_session
 
   !============================================================================
 
@@ -69,10 +36,10 @@ contains
 
   subroutine user_read_inputs
 
-    use ModIO,          ONLY: write_prefix, write_myname, iUnitOut
-    use ModMain,        ONLY: lVerbose
-    use ModProcMH,      ONLY: iProc
-    use ModReadParam,   ONLY: read_line, read_command, read_var
+    use ModIO,        ONLY: write_prefix, write_myname, iUnitOut
+    use ModMain,      ONLY: lVerbose
+    use ModProcMH,    ONLY: iProc
+    use ModReadParam, ONLY: read_line, read_command, read_var
 
     character (len=100) :: NameCommand
     !--------------------------------------------------------------------------
@@ -86,7 +53,7 @@ contains
        if(.not.read_command(NameCommand)) CYCLE
 
        select case(NameCommand)
-       case("#HEATCONDUCTIONTEST")
+       case("#MARSHAKWAVETEST")
           call read_var('TypeProblem',TypeProblem)
 
        case('#USERINPUTEND')
@@ -115,47 +82,31 @@ contains
   subroutine user_set_ics
 
     use ModAdvance,    ONLY: State_VGB
-    use ModGeometry,   ONLY: x_Blk, y_Blk, Dx_Blk, y2
-    use ModMain,       ONLY: GlobalBlk, nI, nJ, nK, x_, Time_Simulation
-    use ModNumConst,   ONLY: cPi
-    use ModPhysics,    ONLY: gm1
-    use ModVarIndexes, ONLY: Rho_, RhoUx_, p_
+    use ModMain,       ONLY: GlobalBlk, nI, nJ, nK
+    use ModPhysics,    ONLY: inv_gm1, cRadiationNo
+    use ModVarIndexes, ONLY: Rho_, RhoUx_, RhoUz_, Erad_, ExtraEint_, p_
 
-    integer :: iBlock, i, j, k
-    real :: x, Dx, Temperature, Spread
-    real :: r, Lambda
+    integer :: i, j, k, iBlock
+    real :: Rho, Temperature, Pressure
 
     character(len=*), parameter :: NameSub = "user_set_ics"
     !--------------------------------------------------------------------------
     iBlock = GlobalBlk
 
     select case(TypeProblem)
-    case('gaussian')
-       do i = -1, nI+2
-          Spread = 4.0*HeatConductionCoef*Time_Simulation
-          Temperature = T0 + AmplitudeTemperature/(sqrt(cPi*Spread)) &
-               *exp(-x_Blk(i,1,1,iBlock)**2/Spread)
-          do k = -1, nK+2; do j = -1, nJ+2
-             State_VGB(Rho_,i,j,k,iBlock) = 1.0
-             State_VGB(RhoUx_:p_-1,i,j,k,iBlock) = 0.0
-             State_VGB(p_,i,j,k,iBlock) = Temperature
-          end do; end do
-       end do
-    case('rz')
-       Lambda = -(3.831705970/y2)**2
-       Spread = 4.0*HeatConductionCoef*Time_Simulation
-       do j = -1, nJ+2; do i = -1, nI+2
-          r = abs(y_BLK(i,j,1,iBlock))
-          Temperature = T0 + AmplitudeTemperature &
-               *exp(Lambda*HeatConductionCoef*Time_Simulation) &
-               *bessj0(sqrt(-Lambda)*r) &
-               /(sqrt(cPi*Spread))*exp(-x_Blk(i,j,1,iBlock)**2/Spread)
-          do k = -1, nK+2
-             State_VGB(Rho_,i,j,k,iBlock) = 1.0
-             State_VGB(RhoUx_:p_-1,i,j,k,iBlock) = 0.0
-             State_VGB(p_,i,j,k,iBlock) = Temperature
-          end do
-       end do; end do
+    case('suolson')
+       Rho = 1.0
+       Temperature = 1.0e-6
+       Pressure = Rho*Temperature
+       do k = -1, nK+2; do j = -1, nJ+2; do i = -1, nI+2
+          State_VGB(Rho_,i,j,k,iBlock) = Rho
+          State_VGB(RhoUx_:RhoUz_,i,j,k,iBlock) = 0.0
+          State_VGB(Erad_,i,j,k,iBlock) = cRadiationNo*Temperature**4
+          State_VGB(ExtraEint_,i,j,k,iBlock) = &
+               cRadiationNo*Temperature**4 - inv_gm1*Pressure
+          State_VGB(p_,i,j,k,iBlock) = Pressure
+       end do; end do; end do
+
     case default
        call stop_mpi(NameSub//' : undefined problem type='//TypeProblem)
     end select
@@ -164,14 +115,59 @@ contains
 
   !============================================================================
 
+  subroutine user_set_outerbcs(iBlock,iSide, TypeBc, IsFound)
+
+    use ModAdvance,  ONLY: State_VGB
+    use ModGeometry, ONLY: Dx_Blk
+    use ModImplicit, ONLY: StateSemi_VGB
+    use ModMain,     ONLY: nJ, nK
+
+    integer,          intent(in)  :: iBlock, iSide
+    character(len=20),intent(in)  :: TypeBc
+    logical,          intent(out) :: IsFound
+
+    integer :: j, k
+    real :: Dx
+
+    character (len=*), parameter :: NameSub = 'user_set_outerbcs'
+    !--------------------------------------------------------------------------
+    if(iSide /= 1)then
+       write(*,*) NameSub//' : user boundary not defined at iSide = ', iSide
+       call stop_mpi(NameSub)
+    end if
+
+    IsFound = .true.
+
+    select case(TypeBc)
+    case('user')
+
+       State_VGB(:,0,:,:,iBlock) = State_VGB(:,1,:,:,iBlock)
+       State_VGB(:,-1,:,:,iBlock) = State_VGB(:,1,:,:,iBlock)
+
+    case('usersemi')
+
+       ! Marshak boundary conditions
+
+       Dx = Dx_Blk(iBlock)
+       do k = 1, nK; do j = 1, nJ
+          StateSemi_VGB(1,0,j,k,iBlock) = &
+               (1.0 - StateSemi_VGB(1,1,j,k,iBlock) &
+               *(0.5 - 2.0/(sqrt(3.0)*Dx))) &
+               /(0.5 + 2.0/(sqrt(3.0)*Dx))
+       end do; end do
+    end select
+
+  end subroutine user_set_outerbcs
+
+  !============================================================================
+
   subroutine user_set_plot_var(iBlock, NameVar, IsDimensional, &
        PlotVar_G, PlotVarBody, UsePlotVarBody, &
        NameTecVar, NameTecUnit, NameIdlUnit, IsFound)
 
     use ModAdvance,    ONLY: State_VGB
-    use ModGeometry,   ONLY: x_Blk, y_Blk, Dx_Blk, y2
-    use ModMain,       ONLY: nI, nJ, nK, Time_Simulation
-    use ModNumConst,   ONLY: cPi
+    use ModMain,       ONLY: nI, nJ, nK
+    use ModPhysics,    ONLY: cRadiationNo
     use ModVarIndexes, ONLY: p_, Rho_
 
     integer,          intent(in)   :: iBlock
@@ -185,9 +181,6 @@ contains
     character(len=*), intent(inout):: NameIdlUnit
     logical,          intent(out)  :: IsFound
 
-    real :: Spread, Lambda, r, Temperature
-    integer :: i, j, k
-
     character (len=*), parameter :: NameSub = 'user_set_plot_var'
     !--------------------------------------------------------------------------
 
@@ -196,26 +189,9 @@ contains
 
     IsFound = .true.
     select case(NameVar)
-    case('t0','temp0')
-       select case(TypeProblem)
-       case('gaussian')
-          Spread = 4.0*HeatConductionCoef*Time_Simulation
-          PlotVar_G = T0 + AmplitudeTemperature/(sqrt(cPi*Spread)) &
-               *exp(-x_Blk(:,:,:,iBlock)**2/Spread)
-       case('rz')
-          Lambda = -(3.831705970/y2)**2
-          Spread = 4.0*HeatConductionCoef*Time_Simulation
-          do j = -1, nJ+2; do i = -1, nI+2
-             r = abs(y_BLK(i,j,1,iBlock))
-             Temperature = T0 + AmplitudeTemperature &
-                  *exp(Lambda*HeatConductionCoef*Time_Simulation) &
-                  *bessj0(sqrt(-Lambda)*r) &
-                  /(sqrt(cPi*Spread))*exp(-x_Blk(i,j,1,iBlock)**2/Spread)
-             do k = -1, nK+2
-                PlotVar_G(i,j,k) = Temperature
-             end do
-          end do; end do
-       end select
+    case('at4')
+       PlotVar_G = cRadiationNo &
+            *(State_VGB(p_,:,:,:,iBlock)/State_VGB(Rho_,:,:,:,iBlock))**4
     case default
        IsFound = .false.
     end select
@@ -233,7 +209,7 @@ contains
 
     use ModPhysics,    ONLY: gm1, inv_gm1, No2Si_V, Si2No_V, &
          UnitRho_, UnitP_, UnitEnergyDens_, UnitTemperature_, &
-         UnitX_, UnitT_, UnitU_
+         UnitX_, UnitT_, UnitU_, cRadiationNo, Clight
     use ModVarIndexes, ONLY: nVar, Rho_, p_
 
     real, intent(in) :: State_V(nVar)
@@ -257,10 +233,11 @@ contains
     RhoSi = Rho*No2Si_V(Rho_)
 
     if(present(EinternalSiIn))then
-       pSi = EinternalSiIn*gm1
-       Pressure = pSi*Si2No_V(UnitP_)
-       Temperature = Pressure/Rho
+       Temperature = sqrt(sqrt(EinternalSiIn*Si2No_V(UnitEnergyDens_) &
+            /cRadiationNo))
        TemperatureSi = Temperature*No2Si_V(UnitTemperature_)
+       Pressure = Rho*Temperature
+       pSi = Pressure*No2Si_V(UnitP_)
     elseif(present(TeSiIn))then
        TemperatureSi = TeSiIn
        Temperature = TemperatureSi*Si2No_V(UnitTemperature_)
@@ -273,55 +250,22 @@ contains
        TemperatureSi = Temperature*No2Si_V(UnitTemperature_)
     end if
 
-    if(present(EinternalSiOut)) EinternalSiOut = pSi*inv_gm1
+    if(present(EinternalSiOut)) EinternalSiOut = &
+         cRadiationNo*Temperature**4*No2Si_V(UnitEnergyDens_)
+
     if(present(TeSiOut)) TeSiOut = TemperatureSi
     if(present(PressureSiOut)) PressureSiOut = pSi
 
-    if(present(CvSiOut)) CvSiOut = inv_gm1*Rho &
+    if(present(CvSiOut)) CvSiOut = 4.0*cRadiationNo*Temperature**3 &
          *No2Si_V(UnitEnergyDens_)/No2Si_V(UnitTemperature_)
 
-    if(present(HeatConductionCoefSiOut)) &
-       HeatConductionCoefSiOut = HeatConductionCoef &
-            *No2Si_V(UnitEnergyDens_)/No2Si_V(UnitTemperature_) &
-            *No2Si_V(UnitU_)*No2Si_V(UnitX_)
+    if(present(AbsorptionOpacitySiOut)) &
+         AbsorptionOpacitySiOut = 1.0/Clight/No2Si_V(UnitX_)
+    if(present(RosselandMeanOpacitySiOut)) &
+         RosselandMeanOpacitySiOut = Clight/3.0/No2Si_V(UnitX_)
 
-    if(present(AbsorptionOpacitySiOut)) AbsorptionOpacitySiOut = 0.0
-    if(present(RosselandMeanOpacitySiOut)) RosselandMeanOpacitySiOut = 0.0
+    if(present(HeatConductionCoefSiOut)) HeatConductionCoefSiOut = 0.0
 
   end subroutine user_material_properties
-
-  !============================================================================
-
-  real function bessj0(x)
-
-    ! bessj0 from numerical recipes (changed to free format)
-
-    real :: x, z, ax, xx
-
-    real y,p1,p2,p3,p4,p5,q1,q2,q3,q4,q5,r1,r2,r3,r4,r5,r6,s1,s2,s3,s4,s5,s6
-    data p1,p2,p3,p4,p5/1.d0,-.1098628627d-2,.2734510407d-4, &
-         -.2073370639d-5,.2093887211d-6/, q1,q2,q3,q4,q5/-.1562499995d-1, &
-         .1430488765d-3,-.6911147651d-5,.7621095161d-6,-.934945152d-7/
-    data r1,r2,r3,r4,r5,r6/57568490574.d0,-13362590354.d0,651619640.7d0, &
-         -11214424.18d0,77392.33017d0,-184.9052456d0/, &
-         s1,s2,s3,s4,s5,s6/57568490411.d0,1029532985.d0, &
-         9494680.718d0,59272.64853d0,267.8532712d0,1.d0/
-
-    !--------------------------------------------------------------------------
-
-    if(abs(x).lt.8.)then
-       y=x**2
-       bessj0=(r1+y*(r2+y*(r3+y*(r4+y*(r5+y*r6))))) &
-            /(s1+y*(s2+y*(s3+y*(s4+y*(s5+y*s6)))))
-    else
-       ax=abs(x)
-       z=8./ax
-       y=z**2
-       xx=ax-.785398164
-       bessj0=sqrt(.636619772/ax)*(cos(xx)*(p1+y*(p2+y*(p3+y*(p4+y &
-            *p5))))-z*sin(xx)*(q1+y*(q2+y*(q3+y*(q4+y*q5)))))
-    endif
-
-  end function bessj0
 
 end module ModUser
