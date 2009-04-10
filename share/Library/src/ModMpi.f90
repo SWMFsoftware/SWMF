9ac1ee14bf7344d0ee732569d3bb43095dd495f4
dnajib <>
2009-04-10 16:16:41 +0000

    Fixed MPI_COMPLEX definition. Commented out the whole thing because it failed on columbia due to some ifort bug.
    Former-commit-id: f9a5e36915c8861490fe8cc86fcb0bb554c09c64

--- share/Library/src/ModMpi.f90
+++ share/Library/src/ModMpi.f90
@@ -32,14 +32,14 @@ module ModMpi
   !EOP
   !BOC
   use ModMpiInterfaces
-  use ModMpiConstants, &
-       MPI_REAL_ORIG => MPI_REAL, MPI_COMPLEX_ORIG => MPI_COMPLEX
-
-  integer, parameter :: MPI_REAL = &
-       iRealPrec*MPI_DOUBLE_PRECISION + (1-iRealPrec)*MPI_REAL_ORIG
-
-  integer, parameter :: MPI_COMPLEX = &
-       iRealPrec*MPI_DOUBLE_PRECISION + (1-iRealPrec)*MPI_COMPLEX_ORIG
+  use ModMpiConstants !, &
+!       MPI_REAL_ORIG => MPI_REAL, MPI_COMPLEX_ORIG => MPI_COMPLEX
+!
+!  integer, parameter :: MPI_REAL = &
+!       iRealPrec*MPI_DOUBLE_PRECISION + (1-iRealPrec)*MPI_REAL_ORIG
+!
+!  integer, parameter :: MPI_COMPLEX = &
+!       iRealPrec*MPI_DOUBLE_COMPLEX + (1-iRealPrec)*MPI_COMPLEX_ORIG
 
   !EOC
 end module ModMpi
