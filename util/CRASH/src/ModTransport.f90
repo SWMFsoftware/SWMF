1610b8795a9a6eaddd588094a7ab85c5f4036616
gtoth <>
2009-05-06 17:51:23 +0000

    Renamed module to CRASH_ModTransport so that the module name is unique.
    Former-commit-id: 61c1cd1bd9f9dbbacadd21f937b6b8086ec863be

--- util/CRASH/src/ModTransport.f90
+++ util/CRASH/src/ModTransport.f90
@@ -1,8 +1,8 @@
 !^CFG COPYRIGHT UM
-module ModTransport
+module CRASH_ModTransport
   use ModStatSum
   use ModStatSumMix,ONLY: Z2PerA, Z2
   !Calculation of the transport coefficients 
   !in dense plasmas: electron heat conduction,
   !Electron-ion temperature relaxation etc.
-end module ModTransport
+end module CRASH_ModTransport
