933d1077362a94e35d3a018390f215ae9b9f4c0a
gtoth <>
2009-06-03 22:24:33 +0000

    Removed cZero8. Added i_DD that can be used as a Kronecker delta.
    Former-commit-id: 06c684035d1c9f0a0205c9b7a7bc13090cea47dd

--- share/Library/src/ModNumConst.f90
+++ share/Library/src/ModNumConst.f90
@@ -20,11 +20,14 @@ Module ModNumConst
 
   real(Real8_), parameter:: &
        cTiny8     =  0.0000000001,                       &
-       cZero8     =  0.0,                                &
        cPi8       =  3.1415926535897932384626433832795,  &
        cTwoPi8    =  2*cPi8
 
-  ! Unit matrix (also Kronecker delta)
+  ! integer unit matrix
+  integer, parameter, dimension(3,3) :: i_DD = reshape( &
+       (/1,0,0, 0,1,0, 0,0,1/), (/3,3/))
+
+  ! integer unit matrix (also Kronecker delta)
   real, parameter, dimension(3,3) :: cUnit_DD = reshape( &
        (/1.0,0.0,0.0, 0.0,1.0,0.0, 0.0,0.0,1.0/),&
        (/3,3/))
