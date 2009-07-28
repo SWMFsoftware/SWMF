48ce6efd7363197b55eb71aa54266c8a42d450e5
ridley <>
2009-07-28 21:47:07 +0000

    Made a bunch of new tests.
    Former-commit-id: 9ba1e0eb605a24b777340d389fdfc17e637b8e41

--- IE/RIM/src/set_test.f90
+++ IE/RIM/src/set_test.f90
@@ -12,6 +12,8 @@ subroutine set_test
   ! Latitude, longitude, with latitude starting at the north pole and
   ! longitude starting at 12 MLT.
 
+  if (iDebugLevel > 2) write(*,*) "RIM===> TestName --->",TestName,"<---"
+
   if (TestName == "ideal") then
 
      write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
@@ -30,28 +32,116 @@ subroutine set_test
 !          -3.0e-6*exp(-abs((abs(LatitudeAll)-180.0*cDegToRad)) / &
 !          (5.0*cDegToRad)) 
 
-     OuterMagRhoAll = sin(LatitudeAll)**3 * 2.0e-19
+     OuterMagRhoAll = sin(abs(LatitudeAll))**3 * 2.0e-19
 
      OuterMagPAll = &
           1.7e-7*exp(-abs((abs(LatitudeAll)-35.0*cDegToRad)) / &
-          (2.0*cDegToRad)) * abs(sin(LongitudeAll/2 + cPi/4)) &
-          +1.7e-7*exp(-abs((abs(LatitudeAll)-155.0*cDegToRad)) / &
-          (2.0*cDegToRad)) * abs(sin(LongitudeAll/2 + cPi/4))
+          (2.0*cDegToRad)) & !* abs(sin(LongitudeAll/2 + cPi/4)) &
+          +1.7e-7*exp(-abs((abs(LatitudeAll)-145.0*cDegToRad)) / &
+          (2.0*cDegToRad)) ! * abs(sin(LongitudeAll/2 + cPi/4))
 
      OuterMagInvBAll = 1.0
-     where(LatitudeAll <  10.0*cDegToRad) OuterMagInvBAll = -1.0
-     where(LatitudeAll <  10.0*cDegToRad) OuterMagPAll = 0.0
-     where(LatitudeAll <  10.0*cDegToRad) OuterMagRhoAll = 0.0
-     where(LatitudeAll > 170.0*cDegToRad) OuterMagInvBAll = -1.0
-     where(LatitudeAll > 170.0*cDegToRad) OuterMagPAll = 0.0
-     where(LatitudeAll > 170.0*cDegToRad) OuterMagRhoAll = 0.0
+!!!     where(LatitudeAll <  10.0*cDegToRad) OuterMagInvBAll = -1.0
+!!!     where(LatitudeAll <  10.0*cDegToRad) OuterMagPAll = 0.0
+!!!     where(LatitudeAll <  10.0*cDegToRad) OuterMagRhoAll = 0.0
+!!!     where(LatitudeAll > 170.0*cDegToRad) OuterMagInvBAll = -1.0
+!!!     where(LatitudeAll > 170.0*cDegToRad) OuterMagPAll = 0.0
+!!!     where(LatitudeAll > 170.0*cDegToRad) OuterMagRhoAll = 0.0
+!!!
+!!!     where(LatitudeAll > 40.0*cDegToRad .and. &
+!!!           LatitudeAll < 150.0*cDegToRad) OuterMagRhoAll = 0.0
+!!!     where(LatitudeAll > 40.0*cDegToRad .and. &
+!!!           LatitudeAll < 150.0*cDegToRad) OuterMagPAll = 0.0
+!!!     where(LatitudeAll > 40.0*cDegToRad .and. &
+!!!           LatitudeAll < 150.0*cDegToRad) OuterMagInvBAll = -1.0
 
-     where(LatitudeAll > 40.0*cDegToRad .and. &
-           LatitudeAll < 150.0*cDegToRad) OuterMagRhoAll = 0.0
-     where(LatitudeAll > 40.0*cDegToRad .and. &
-           LatitudeAll < 150.0*cDegToRad) OuterMagPAll = 0.0
-     where(LatitudeAll > 40.0*cDegToRad .and. &
-           LatitudeAll < 150.0*cDegToRad) OuterMagInvBAll = -1.0
+  endif
+
+  if (TestName == "sym") then
+
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     write(*,*) "               IDEAL Sym TEST"
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     OuterMagJrAll = &
+          2.0e-6*exp(-abs((abs(LatitudeAll)-25.0*cDegToRad)) / &
+          (3.0*cDegToRad)) * sin(LongitudeAll) &
+          +2.0e-6*exp(-abs((abs(LatitudeAll)-155.0*cDegToRad)) / &
+          (3.0*cDegToRad)) * sin(LongitudeAll) 
+
+     OuterMagRhoAll = sin(abs(LatitudeAll))**3 * 2.0e-19
+
+     OuterMagPAll = &
+          1.7e-7*exp(-abs((abs(LatitudeAll)-35.0*cDegToRad)) / &
+          (2.0*cDegToRad)) &
+          +1.7e-7*exp(-abs((abs(LatitudeAll)-145.0*cDegToRad)) / &
+          (2.0*cDegToRad))
+
+     OuterMagInvBAll = 1.0
+
+  endif
+
+  if (TestName == "sym_wocflb") then
+
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     write(*,*) "               IDEAL Sym TEST"
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     OuterMagJrAll = &
+          2.0e-6*exp(-abs((abs(LatitudeAll)-25.0*cDegToRad)) / &
+          (3.0*cDegToRad)) * sin(LongitudeAll) &
+          +2.0e-6*exp(-abs((abs(LatitudeAll)-155.0*cDegToRad)) / &
+          (3.0*cDegToRad)) * sin(LongitudeAll) 
+
+     OuterMagRhoAll = sin(abs(LatitudeAll))**3 * 2.0e-19
+
+     OuterMagPAll = &
+          1.7e-7*exp(-abs((abs(LatitudeAll)-35.0*cDegToRad)) / &
+          (2.0*cDegToRad)) &
+          +1.7e-7*exp(-abs((abs(LatitudeAll)-145.0*cDegToRad)) / &
+          (2.0*cDegToRad))
+
+     OuterMagInvBAll = 1.0
+
+     where(LatitudeAll < 30.0*cDegToRad .and. &
+          LatitudeAll > 150.0*cDegToRad) OuterMagRhoAll = 0.0
+     where(LatitudeAll < 30.0*cDegToRad .and. &
+          LatitudeAll > 150.0*cDegToRad) OuterMagPAll = 0.0
+     where(LatitudeAll < 30.0*cDegToRad .and. &
+          LatitudeAll > 150.0*cDegToRad) OuterMagInvBAll = -1.0
+  endif
+
+  if (TestName == "nonsym_wocflb") then
+
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     write(*,*) "               IDEAL Sym TEST"
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     write(*,*) "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+     OuterMagJrAll = &
+          2.0e-6*exp(-abs((abs(LatitudeAll)-25.0*cDegToRad)) / &
+          (3.0*cDegToRad)) * sin(LongitudeAll) &
+          +4.0e-6*exp(-abs((abs(LatitudeAll)-155.0*cDegToRad)) / &
+          (3.0*cDegToRad)) * sin(LongitudeAll) 
+
+     OuterMagRhoAll = sin(abs(LatitudeAll))**3 * 2.0e-19
+
+     OuterMagPAll = &
+          1.7e-7*exp(-abs((abs(LatitudeAll)-35.0*cDegToRad)) / &
+          (2.0*cDegToRad)) &
+          +6.1e-7*exp(-abs((abs(LatitudeAll)-145.0*cDegToRad)) / &
+          (2.0*cDegToRad))
+
+     OuterMagInvBAll = 1.0
+
+     where(LatitudeAll < 15.0*cDegToRad .and. &
+          LatitudeAll > 165.0*cDegToRad) OuterMagRhoAll = 0.0
+     where(LatitudeAll < 15.0*cDegToRad .and. &
+          LatitudeAll > 165.0*cDegToRad) OuterMagPAll = 0.0
+     where(LatitudeAll < 30.0*cDegToRad .and. &
+          LatitudeAll > 150.0*cDegToRad) OuterMagInvBAll = -1.0
 
   endif
 
