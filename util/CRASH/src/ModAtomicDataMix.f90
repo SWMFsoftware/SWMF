922683fbd3e0d35ac10df1c94ca6c6384a8a8c2f
igorsok <>
2009-07-29 01:07:38 +0000

    Add excitation database and interface to databases to make it possible to use either tabular data or calculated on-the-fly.  Added Files:  	ModExcitationData.f90 ModAtomicDataMix.f90
    Former-commit-id: df05c7387159f390627adb1afcc78db1412861e9

---
+++ util/CRASH/src/ModAtomicDataMix.f90
@@ -0,0 +1,79 @@
+module CRASH_ModAtomicDataMix
+  use CRASH_ModIonization
+  use CRASH_ModAtomicMass,ONLY : nZMax
+  implicit none
+  !Interface to databases
+
+  integer, parameter, public :: nMixMax = 6
+
+  integer :: nMix = -1    !Number of components in the mixture
+
+  integer,public,dimension(nMixMax) :: nZ_I = -1  !Atomic numbers of elements in the mixture 
+
+
+  ! relative concentrations of the elements in the mixture (part of whole comprised by the element)
+  real,dimension(nMixMax) :: Concentration_I=0.0 
+
+  ! Array of ionization potentials - energy needed to create i-level ion from (i-1)-level ion
+  real,dimension(1:nZMax,nMixMax) :: IonizPotential_II
+
+  !\
+  !  Maximum principal quantum number for the excitation states.
+  !/
+  integer,parameter:: nExcitation = 10
+
+
+  !The degeneracy ('multiplicity') of the given quantum state:
+  integer,dimension(0:nExcitation-1,nExcitation,0:nZMax-1,nMixMax):: Degeneracy_IIII = 0
+
+
+  !Excitation energy in eV, with respect to the ground state energy as a function of the qunatum numbers l (first index) 
+  !and n (second index for a given charge state (third index) for the given component of the mixture (fourth index)
+  real,dimension(0:nExcitation-1,nExcitation,0:nZMax-1,nMixMax)::ExcitationEnergy_IIII = 0.0
+
+  !The same, for the compression-dependent part of the state energy, assuming that the energy growth may be approximated as
+  !\Delta E_{l,n,i)=VirialCoeff(l,n,i)*(a_0/r_{iono})**iPressureEffectIndex, 
+  !where a_0 is the Bohr radius, r_{iono} is the iono-sphere radius
+  real,dimension(0:nExcitation-1,nExcitation,0:nZMax-1,nMixMax)::VirialCoeff4Energy_IIII = 0.0
+
+  integer,parameter :: iPressureEffectIndex = 2
+contains
+  !========================================================================
+  subroutine set_data_for_mixture(nMixIn, nZIn_I, ConcentrationIn_I)
+    use ModConst,ONLY : cTiny
+    use CRASH_ModExcitationData,ONLY : get_excitation_energy,&
+         get_virial_coeff4_energy, get_degeneracy
+
+    integer,intent(in)::nMixIn
+    integer,dimension(nMixIn),intent(in) :: nZIn_I
+    real,dimension(nMixIn),intent(in) :: ConcentrationIn_I
+
+    integer            :: iZ, iMix  ! for loops
+    !--------------------------!
+    Concentration_I( 1:nMixIn ) =  ConcentrationIn_I( 1:nMixIn )
+    if(abs (sum(Concentration_I( 1:nMixIn ) ) - 1.0 ) > cTiny)then
+       write(*,*)&
+            'Wrong input - the total of relative concentrations differs from 1.0: ',&
+            Concentration_I( 1:nMixIn ) 
+       call CON_stop('Stop')
+    end if
+
+    if(nMixIn==nMix)then
+       if( all( nZIn_I( 1:nMix ) == nZ_I( 1:nMix ) ) )return
+    end if
+
+    nMix = nMixIn
+    nZ_I( 1:nMix ) = nZIn_I( 1:nMix )
+    do iMix=1,nMix
+       call get_ioniz_potential(nZ_I(iMix),IonizPotential_II(1:nZ_I(iMix),iMix))
+
+       call get_excitation_energy(nExcitation,nZ_I(iMix),&
+            ExcitationEnergy_IIII(:,:,0:nZ_I(iMix)-1,iMix))
+
+       call get_virial_coeff4_energy(nExcitation,nZ_I(iMix),&
+            VirialCoeff4Energy_IIII(:,:,0:nZ_I(iMix)-1,iMix))
+
+       call get_degeneracy(nExcitation,nZ_I(iMix),Degeneracy_IIII(:,:,0:nZ_I(iMix)-1,iMix))
+    end do
+  end subroutine set_data_for_mixture
+end module CRASH_ModAtomicDataMix
---
+++ util/CRASH/src/ModExcitationData.f90
@@ -0,0 +1,96 @@
+module CRASH_ModExcitationData
+  use CRASH_ModIonization,ONLY : get_ioniz_potential
+  implicit none
+  PRIVATE !except
+  
+  !public methods
+  public:: n_ground  !Principal quantum number as the function of the charge state and the element number
+  public:: get_excitation_energy
+  public:: get_virial_coeff4_energy
+  public:: get_degeneracy
+contains
+  !=====================================================================================
+  integer function n_ground(iZ,nZ)
+    integer,intent(in)::iZ,nZ
+    !The principal quantum number of the outermost electron in bounded with an ion
+    !with I electrons
+    integer,parameter :: nGround_I(100) = (/ 1, 1, &                                                       !  2
+         2, 2, 2, 2, 2, 2, 2, 2, &                                                                         !  8
+         3, 3, 3, 3, 3, 3, 3, 3, &                                                                         !  8
+         4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, &                                           ! 18
+         5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, &                                           ! 18
+         6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, & ! 32
+         7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7 /)                                                       !
+    !------------------------------------------------------------------------------------------------------!
+    n_ground = nGround_I(nZ - iZ)
+  end function n_ground
+  !======================================================================================
+  !Fill in the array ExcitationEnergy_III with tabulated or calculted excitation energies
+  !for quantum states defined by n, l of atoms or ions of a particular element defined
+  !by their charge state
+  subroutine get_excitation_energy(nExcitation, nZ, ExcitationEnergy_III)
+    integer :: nExcitation
+    integer :: nZ
+    real,dimension(0:nExcitation-1,nExcitation,0:nZ-1),intent(out) :: ExcitationEnergy_III
+    real,dimension(1:nZ) :: IonizPotential_I
+
+    integer :: iZ, iN, nGround
+    !--------------
+    ExcitationEnergy_III = 0.0
+
+    call get_ioniz_potential(nZ, IonizPotential_I(1:nZ))
+
+    do iZ = 0, nZ-1
+       nGround = n_ground(iZ, nZ)
+
+       do iN = nGround+1, nExcitation
+          ExcitationEnergy_III(0:iN-1,iN,iZ) = IonizPotential_I(iZ+1) * (1.0 - (real(nGround) / iN)**2.0)
+       end do
+    end do
+  end subroutine get_excitation_energy
+  !======================================================================================
+  subroutine get_virial_coeff4_energy(nExcitation, nZ, VirialCoeff4Energy_III)
+    integer :: nExcitation
+    integer :: nZ
+    real,dimension(0:nExcitation-1,nExcitation,0:nZ-1),intent(out) :: VirialCoeff4Energy_III
+    real,dimension(1:nZ) :: IonizPotential_I
+
+    integer :: iZ, iN, iL, nGround
+    !--------------
+    VirialCoeff4Energy_III = 0.0
+
+    call get_ioniz_potential(nZ, IonizPotential_I(1:nZ))
+
+    do iZ = 0, nZ-1
+       nGround = n_ground(iZ, nZ)
+
+       do iN = nGround, nExcitation
+          do iL = 0, iN-1
+             VirialCoeff4Energy_III(iL,iN,iZ) = IonizPotential_I(iZ+1) * real(nGround)**2 * real(iN - iL)**2 / real(iZ+1)**2
+          end do
+       end do
+    end do
+  end subroutine get_virial_coeff4_energy
+  !======================================================================================
+  subroutine get_degeneracy(nExcitation, nZ, Degeneracy_III)
+    integer :: nExcitation
+    integer :: nZ
+    integer,dimension(0:nExcitation-1,nExcitation,0:nZ-1),intent(out) :: Degeneracy_III
+    integer :: iMix
+
+    integer :: iZ, iN, iL, nGround
+    !--------------
+    Degeneracy_III = 0
+
+    do iZ = 0, nZ-1
+       nGround = n_ground(iZ, nZ)
+
+       do iN = nGround, nExcitation
+          do iL = 0, iN-1
+             Degeneracy_III(iZ,iN,iL) = 2 * (2*iL + 1)
+          end do
+       end do
+    end do
+  end subroutine get_degeneracy
+  !======================================================================================
+end module CRASH_ModExcitationData
