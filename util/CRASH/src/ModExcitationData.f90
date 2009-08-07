bf8103655917577cdba7f35a7d8d6c979f148ce8
igorsok <>
2009-08-07 15:10:28 +0000

    Use tabulated ground state degeneracies whenever possible  Modified Files:  	ModExcitationData.f90
    Former-commit-id: d6d466c189b16e0f4dda128917bc0c76d70c197e

--- util/CRASH/src/ModExcitationData.f90
+++ util/CRASH/src/ModExcitationData.f90
@@ -96,6 +96,28 @@ contains
     integer :: iMix
 
     integer :: iZ, iN, iL, nGround
+
+
+    !Degeneracies of ground states for positive ions of the first 10 elements.
+    !The data for the first 3 columns are taken from the book 
+    !"Allen' Astrophysical Quantities, Edn iV" p. 33
+    !by Allen, Clabon W. Editor: Cox, Arthur N.
+    !Publisher: Springer (2000) (g_0 for Y=1, Y=2, Y=3
+
+    !First 10 elements - full ionizations
+    integer, parameter, dimension(10,10) :: cDegeneracy10_II = reshape(  (/   &
+         !   I  !  II  !  III !  IV  !   V  !  VI  !  VII ! VIII !  IX  !   X  !
+         2 ,     1 ,    1 ,    1 ,    1 ,    1 ,    1 ,    1 ,    1 ,    1     ,&  ! 1 - H
+         1 ,     2 ,    1 ,    1 ,    1 ,    1 ,    1 ,    1 ,    1 ,    1     ,&  ! 2 - He
+         2 ,     1 ,    2 ,    1 ,    1 ,    1 ,    1 ,    1 ,    1 ,    1     ,&  ! 3 - Li
+         1 ,     2 ,    1 ,    2 ,    1 ,    1 ,    1 ,    1 ,    1 ,    1     ,&  ! 4 - Be
+         6 ,     1 ,    2 ,    1 ,    2 ,    1 ,    1 ,    1 ,    1 ,    1     ,&  ! 5 - B
+         9 ,     6 ,    1 ,    2 ,    1 ,    2 ,    1 ,    1 ,    1 ,    1     ,&  ! 6 - C
+         4 ,     9 ,    6 ,    1 ,    2 ,    1 ,    2 ,    1 ,    1 ,    1     ,&  ! 7 - N
+         9 ,     4 ,    9 ,    6 ,    1 ,    2 ,    1 ,    2 ,    1 ,    1     ,&  ! 8 - O
+         6 ,     9 ,    4 ,    9 ,    6 ,    1 ,    2 ,    1 ,    2 ,    1     ,&  ! 9 - F
+         1 ,     6 ,    9 ,    4 ,    9 ,    6 ,    1 ,    2 ,    1 ,    2   /),&  !10 - Ne
+                                                                                  (/10,10/))
     !--------------
     Degeneracy_III = 0
 
@@ -104,7 +126,13 @@ contains
 
        do iN = nGround, nExcitation
           do iL = 0, iN-1
-             Degeneracy_III(iZ,iN,iL) = 2 * (2*iL + 1)
+             if (iN == nGround .and. nZ <= 10) then
+                if (iL == 0)&
+                     Degeneracy_III(iZ,iN,iL) = cDegeneracy10_II(iZ+1,nZ)
+             else
+                Degeneracy_III(iZ,iN,iL) = 2 * (2*iL + 1)
+
+             end if
           end do
        end do
     end do
