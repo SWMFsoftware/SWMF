0ec0b3b977f9aaf29cc00c8d807e34a1541646a9
igorsok <>
2009-08-14 19:02:07 +0000

    Add table of excitation energies for helium. Convert nested loops over i, n, l to WHERE statement.  Modified Files:  	ModExcitationData.f90
    Former-commit-id: b5cb5f022836f7bb84c8cbe1a670e7befda8e924

--- util/CRASH/src/ModExcitationData.f90
+++ util/CRASH/src/ModExcitationData.f90
@@ -3,126 +3,132 @@ module CRASH_ModExcitationData
   implicit none
   PRIVATE !except
 
+  ! Tables of excitation energies
+  ! He, nZ = 2
   ! From SPECTR-w3
+  !          S     |      P     |      D     |      F     |      G     |
+  real, parameter, dimension(0:4, 2:5, 0:1) :: cExcitationHe_III = reshape((/&
+       !Ionization potential = 24.58741, nGround = 1
+        19.820112,   20.964486,         0.0,         0.0,         0.0,&  !n = 2
+        22.718862,   23.006505,   23.070977,         0.0,         0.0,&  !n = 3
+        23.594191,   23.708256,   23.735533,         0.0,         0.0,&  !n = 4
+        23.972342,   24.026895,   24.038054,         0.0,         0.0,&  !n = 5
+       !Ionization potential = 54.41778, nGround = 1
+        40.813085,   40.813027,         0.0,         0.0,         0.0,&  !n = 2
+        48.371310,   48.371292,   48.371507,         0.0,         0.0,&  !n = 3
+        51.016663,   51.016656,   51.016747,   51.016777,         0.0,&  !n = 4
+        52.241073,   52.241069,   52.241115,   52.241131,   52.241138 &  !n = 5
+       /), (/5, 4, 2/))
+
   ! Be, nZ = 4
-  !             S  !    P       !    D      !       F       !     G      !
-  real, parameter, dimension(0:4, 1:5, 0:3) :: cExcitationBe_III = reshape((/&
+  ! From SPECTR-w3
+  !          S     |      P     |      D     |      F     |      G     |
+  real, parameter, dimension(0:4, 2:5, 0:3) :: cExcitationBe_III = reshape((/&
        !Ionization potential = 9.32263, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    2.724652,         0.0,         0.0,         0.0,&  !n = 2
          6.455745,    7.288175,    7.693182,         0.0,         0.0,&  !n = 3
          7.949928,    8.220561,    8.366949,         0.0,         0.0,&  !n = 4
          8.484647,    8.611297,    8.680418,         0.0,         0.0,&  !n = 5
        !Ionization potential = 18.21116, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    3.956807,         0.0,         0.0,         0.0,&  !n = 2
         10.938703,   11.963904,   12.156649,         0.0,         0.0,&  !n = 3
         14.315214,   14.724362,   14.806191,         0.0,         0.0,&  !n = 4
         15.787526,   15.990240,   16.031031,         0.0,         0.0,&  !n = 5
        !Ionization potential = 153.897, nGround = 1
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
        118.590874,  121.923569,         0.0,         0.0,         0.0,&  !n = 2
        139.008589,  139.891357,  140.273228,         0.0,         0.0,&  !n = 3
        145.718613,  146.074448,  146.233148,         0.0,         0.0,&  !n = 4
        148.731429,  148.912446,  148.991796,  148.996755,         0.0,&  !n = 5
        !Ionization potential = 217.713, nGround = 1
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
        163.285338,  163.284594,         0.0,         0.0,         0.0,&  !n = 2
        193.527201,  193.526977,  193.530424,         0.0,         0.0,&  !n = 3
        204.111433,  204.111346,  204.112797,  204.113280,         0.0,&  !n = 4
        209.010234,  209.010185,  209.010928,  209.011176,  209.011300 &  !n = 5
-       /), (/5, 5, 4/))
+       /), (/5, 4, 4/))
 
+  ! C, nZ = 6
   ! From SPECTR-w3 and
   ! http://physics.nist.gov/PhysRefData/Handbook/Tables/carbontable5.htm
-  real, parameter, dimension(0:4, 1:5, 0:5) :: cExcitationC_III = reshape((/&
+  !          S     |      P     |      D     |      F     |      G     |
+  real, parameter, dimension(0:4, 2:5, 0:5) :: cExcitationC_III = reshape((/&
        !Ionization potential = 11.26030, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    0.002480,         0.0,         0.0,         0.0,&  !n = 2
          7.479966,    8.537097,    9.631092,         0.0,         0.0,&  !n = 3
          9.683165,         0.0,   10.352680,         0.0,         0.0,&  !n = 4
         10.382436,         0.0,   10.686197,         0.0,         0.0,&  !n = 5
        !Ionization potential = 24.38332, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    0.007439,         0.0,         0.0,         0.0,&  !n = 2
         14.448869,   16.331197,   18.045898,         0.0,         0.0,&  !n = 3
         19.494034,   20.149786,   20.844221,   20.950848,         0.0,&  !n = 4
         21.492411,   21.732940,   22.130557,         0.0,         0.0,&  !n = 5
        !Ionization potential = 47.888, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    6.488266,         0.0,         0.0,         0.0,&  !n = 2
         29.538612,   32.096530,   33.476226,         0.0,         0.0,&  !n = 3
         38.409805,   39.376633,   39.849757,   39.922908,         0.0,&  !n = 4
         41.969887,   42.494836,   42.734249,   43.042350,         0.0,&  !n = 5
        !Ionization potential = 64.492, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    7.994252,         0.0,         0.0,         0.0,&  !n = 2
         37.548611,   39.679981,   40.189981,         0.0,         0.0,&  !n = 3
         49.736504,   50.623983,   50.875175,   50.886829,         0.0,&  !n = 4
         55.200363,   55.651294,   55.779245,   55.785445,         0.0,&  !n = 5
        !Ionization potential = 392.08, nGround = 1
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
        298.958108,  304.399774,         0.0,         0.0,         0.0,&  !n = 2
        352.060534,  353.530987,  354.260014,         0.0,         0.0,&  !n = 3
        369.911777,  370.509381,  370.809423,  370.826781,         0.0,&  !n = 4
        378.019103,  378.316665,  378.472885,  378.480324,  378.481564,&  !n = 5
        !Ionization potential = 489.98, nGround = 1
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
        367.477261,  367.474025,         0.0,         0.0,         0.0,&  !n = 2
        435.547716,  435.546749,  435.564169,         0.0,         0.0,&  !n = 3
        459.370211,  459.369802,  459.377154,  459.379596,         0.0,&  !n = 4
        470.395666,  470.395455,  470.399212,  470.400464,  470.401096 &  !n = 5
-       /), (/5, 5, 6/))
+       /), (/5, 4, 6/))
 
+  ! N, nZ = 7
   ! From SPECTR-w3
-  real, parameter, dimension(0:4, 1:5, 0:6) :: cExcitationN_III = reshape((/&
+  !          S     |      P     |      D     |      F     |      G     |
+  real, parameter, dimension(0:4, 2:5, 0:6) :: cExcitationN_III = reshape((/&
        !Ionization potential = 14.53414, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    2.382976,         0.0,         0.0,         0.0,&  !n = 2
         10.325403,   11.844209,   12.971226,         0.0,         0.0,&  !n = 3
         12.847241,         0.0,   13.661817,         0.0,         0.0,&  !n = 4
         13.614703,         0.0,   13.980457,         0.0,         0.0,&  !n = 5
        !Ionization potential = 29.6013, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    0.006199,         0.0,         0.0,         0.0,&  !n = 2
         18.462485,   20.409037,   23.196201,         0.0,         0.0,&  !n = 3
         24.367852,   25.065883,   26.028000,         0.0,         0.0,&  !n = 4
         26.558652,         0.0,   27.338513,         0.0,         0.0,&  !n = 5
        !Ionization potential = 47.449, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    0.021077,         0.0,         0.0,         0.0,&  !n = 2
         27.437700,   30.458699,   33.133410,         0.0,         0.0,&  !n = 3
         37.329159,   38.644755,   39.395975,   39.710895,         0.0,&  !n = 4
         41.374763,   41.898472,   42.395896,   42.495580,         0.0,&  !n = 5
        !Ionization potential = 77.472, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    8.338842,         0.0,         0.0,         0.0,&  !n = 2
         46.776754,   50.142428,   52.079557,         0.0,         0.0,&  !n = 3
         60.455929,   62.448355,   63.343149,   64.045271,         0.0,&  !n = 4
         67.437230,   68.081080,   68.411498,   68.811223,         0.0,&  !n = 5
        !Ionization potential = 97.89, nGround = 2
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
               0.0,    9.975768,         0.0,         0.0,         0.0,&  !n = 2
         56.552287,   59.235304,   60.057940,         0.0,         0.0,&  !n = 3
         75.149667,   76.267632,   76.611440,   76.629542,         0.0,&  !n = 4
         83.530874,   84.098473,   84.273291,   84.283210,   84.284449,&  !n = 5
        !Ionization potential = 552.06, nGround = 1
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
        419.796815,  426.294826,         0.0,         0.0,         0.0,&  !n = 2
        494.927512,  496.684367,  497.600611,         0.0,         0.0,&  !n = 3
        520.336831,  521.052219,  521.432851,  521.453928,         0.0,&  !n = 4
        531.913234,  532.272788,  532.458764,  532.474882,         0.0,&  !n = 5
        !Ionization potential = 667.03, nGround = 1
-              0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 1
        500.252260,  500.246681,         0.0,         0.0,         0.0,&  !n = 2
        592.926844,  592.925108,  592.957344,         0.0,         0.0,&  !n = 3
        625.358875,  625.358131,  625.371769,  625.376233,         0.0,&  !n = 4
        640.368400,  640.368028,  640.375096,  640.377327,  640.378567 &  !n = 5
-       /), (/5, 5, 7/))
+       /), (/5, 4, 7/))
 
+  ! O, nZ = 8
   ! Partly from SPECTR-w3, partly from
   ! http://physics.nist.gov/PhysRefData/Handbook/Tables/oxygentable5.htm and
   ! http://physics.nist.gov/PhysRefData/Handbook/Tables/oxygentable6.htm
+  !          S     |      P     |      D     |      F     |      G     |
   real, parameter, dimension(0:4, 2:5, 0:7) :: cExcitationO_III = reshape((/&
        !Ionization potential = 13.61806, nGround = 2
               0.0,    0.019837,         0.0,         0.0,         0.0,&  !n = 2
@@ -166,10 +172,12 @@ module CRASH_ModExcitationData
        836.560728,  836.560232,  836.572134,  836.575978,  836.577962 &  !n = 5
        /), (/5, 4, 8/))
 
+  ! Al, nZ = 13
   ! From SPECTR-w3 and W.C.Martin and Romuald Zalubas,
   ! Energy Levels of Aluminium, Al I through Al XIII
 
   !Ionization potential = 5.98577, nGround = 3
+  !          S     |      P     |      D     |      F     |      G     |
   real, parameter, dimension(20) :: cExcitationAl0_II = (/&
                0.0,         0.0,         0.0,         0.0,         0.0,&  !n = 2
                0.0,     3.59807,     4.02148,         0.0,         0.0,&  !n = 3
@@ -281,12 +289,12 @@ module CRASH_ModExcitationData
        cExcitationAl12_II &
        /), (/5, 4, 13/))
 
-
+  ! Xe, nZ = 54
   ! E.B.Saloman, Energy Levels and Observed Spectral Lines of Xenon,
   ! Xe I through Xe LIV
 
   !Ionization potential = 12.129874, nGround = 5
-  real, parameter, dimension(0:4, 4:10) :: cExcitationXe0_III = reshape((/&
+  real, parameter, dimension(0:4, 4:10) :: cExcitationXe0_II = reshape((/&
              0.0,       0.0,       0.0,  11.26270,       0.0,&  !n = 4
              0.0,       0.0,   9.89038,  11.57549,  11.58275,&  !n = 5
          8.31532,   9.58015,  10.97149,  11.74563,       0.0,&  !n = 6
@@ -297,7 +305,7 @@ module CRASH_ModExcitationData
        /), (/5, 7/))
 
   !Ionization potential = 21.20979, nGround = 5
-  real, parameter, dimension(0:4, 4:8) :: cExcitationXe1_III = reshape((/&
+  real, parameter, dimension(0:4, 4:8) :: cExcitationXe1_II = reshape((/&
              0.0,       0.0,       0.0,  17.22982,       0.0,&  !n = 4
              0.0,  11.26692,  11.82769,  18.60964,  18.77645,&  !n = 5
         11.53901,  13.86046,  16.80076,  19.32030,  19.44976,&  !n = 6
@@ -306,7 +314,7 @@ module CRASH_ModExcitationData
        /), (/5, 5/))
 
   !Ionization potential = 32.12295, nGround = 5
-  real, parameter, dimension(0:3, 4:7) :: cExcitationXe2_III = reshape((/&
+  real, parameter, dimension(0:3, 4:7) :: cExcitationXe2_II = reshape((/&
              0.0,       0.0,       0.0,  20.62542,&  !n = 4
              0.0,  12.18299,  13.83731,  24.46339,&  !n = 5
         15.06110,  18.19858,  22.60701,       0.0,&  !n = 6
@@ -376,44 +384,45 @@ contains
        do iN = nGround+1, nExcitation
           ExcitationEnergy_III(0:iN-1,iN,iZ) = IonizPotential_I(iZ+1) * &
                (1.0 - (real(nGround) / iN)**2.0)
-
-          do iL = 0, iN-1
-             select case (nZ)
-                case (4)
-                   if (iL <= 4 .and. iN <= 5 .and. cExcitationBe_III(iL,iN,iZ) /= 0.0)&
-                        ExcitationEnergy_III(iL,iN,iZ) = cExcitationBe_III(iL,iN,iZ)
-
-                case (6)
-                   if (iL <= 4 .and. iN <= 5 .and. cExcitationC_III(iL,iN,iZ) /= 0.0)&
-                        ExcitationEnergy_III(iL,iN,iZ) = cExcitationC_III(iL,iN,iZ)
-
-                case (7)
-                   if (iL <= 4 .and. iN <= 5 .and. cExcitationN_III(iL,iN,iZ) /= 0.0)&
-                        ExcitationEnergy_III(iL,iN,iZ) = cExcitationN_III(iL,iN,iZ)
-
-                case (8)
-                   if (iL <= 4 .and. iN >= 2 .and. iN <= 5 .and. cExcitationO_III(iL,iN,iZ) /= 0.0)&
-                        ExcitationEnergy_III(iL,iN,iZ) = cExcitationO_III(iL,iN,iZ)
-
-                case (13)
-                   if (iL <= 4 .and. iN >= 2 .and. iN <= 5 .and. cExcitationAl_III(iL,iN,iZ) /= 0.0)&
-                        ExcitationEnergy_III(iL,iN,iZ) = cExcitationAl_III(iL,iN,iZ)
-
-                case (54)
-                   if (iZ == 0 .and. iL <= 4 .and. iN >= 4 .and. iN <= 10 .and. &
-                        cExcitationXe0_III(iL,iN) /= 0.0)&
-                        ExcitationEnergy_III(iL,iN,iZ) = cExcitationXe0_III(iL,iN)
-                   if (iZ == 1 .and. iL <= 4 .and. iN >= 4 .and. iN <= 8 .and. &
-                        cExcitationXe1_III(iL,iN) /= 0.0)&
-                        ExcitationEnergy_III(iL,iN,iZ) = cExcitationXe1_III(iL,iN)
-                   if (iZ == 2 .and. iL <= 3 .and. iN >= 4 .and. iN <= 7 .and. &
-                        cExcitationXe2_III(iL,iN) /= 0.0)&
-                        ExcitationEnergy_III(iL,iN,iZ) = cExcitationXe2_III(iL,iN)
-
-             end select
-          end do
        end do
+
     end do
+
+    select case (nZ)
+       case (2)
+          where (cExcitationHe_III(0:4,2:5,0:nZ-1) /= 0.0)&
+               ExcitationEnergy_III(0:4,2:5,0:nZ-1) = cExcitationHe_III(0:4,2:5,0:nZ-1)
+
+       case (4)
+          where (cExcitationBe_III(0:4,2:5,0:nZ-1) /= 0.0)&
+               ExcitationEnergy_III(0:4,2:5,0:nZ-1) = cExcitationBe_III(0:4,2:5,0:nZ-1)
+
+       case (6)
+          where (cExcitationC_III(0:4,2:5,0:nZ-1) /= 0.0)&
+               ExcitationEnergy_III(0:4,2:5,0:nZ-1) = cExcitationC_III(0:4,2:5,0:nZ-1)
+
+       case (7)
+          where (cExcitationN_III(0:4,2:5,0:nZ-1) /= 0.0)&
+               ExcitationEnergy_III(0:4,2:5,0:nZ-1) = cExcitationN_III(0:4,2:5,0:nZ-1)
+
+       case (8)
+          where (cExcitationO_III(0:4,2:5,0:nZ-1) /= 0.0)&
+               ExcitationEnergy_III(0:4,2:5,0:nZ-1) = cExcitationO_III(0:4,2:5,0:nZ-1)
+
+       case (13)
+          where (cExcitationAl_III(0:4,2:5,0:nZ-1) /= 0.0)&
+               ExcitationEnergy_III(0:4,2:5,0:nZ-1) = cExcitationAl_III(0:4,2:5,0:nZ-1)
+
+       case (54)
+          where (cExcitationXe0_II(0:4,4:10) /= 0.0)&
+               ExcitationEnergy_III(0:4,4:10,0) = cExcitationXe0_II(0:4,4:10)
+          where (cExcitationXe1_II(0:4,4:8) /= 0.0)&
+               ExcitationEnergy_III(0:4,4:8,1) = cExcitationXe1_II(0:4,4:8)
+          where (cExcitationXe2_II(0:3,4:7) /= 0.0)&
+               ExcitationEnergy_III(0:3,4:7,2) = cExcitationXe2_II(0:3,4:7)
+
+    end select
+
   end subroutine get_excitation_energy
   !======================================================================================
   subroutine get_virial_coeff4_energy(nExcitation, nZ, VirialCoeff4Energy_III)
