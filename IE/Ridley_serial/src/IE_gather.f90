f9510dd0d85fc343266c2b2fb5d5e6dceed8638e
yiqunyu <>
2009-08-18 22:07:39 +0000

    add jouleheating and ionnumflux in mpi reducing
    Former-commit-id: 6926de068f1f39f7e57d0114f22a65e27f9f8b72

--- IE/Ridley_serial/src/IE_gather.f90
+++ IE/Ridley_serial/src/IE_gather.f90
@@ -7,25 +7,36 @@ subroutine IE_gather
 
   implicit none
 
-  real :: localVar(2*IONO_nTheta-1, IONO_nPsi)
+  real, dimension(2*IONO_nTheta-1, IONO_nPsi):: localVar, localVar1,localVar2
   integer :: iError, iSize
   !--------------------------------------------------------------------------
   IONO_phi = -1.0e32
 
   if (iProc == 0) then
      IONO_phi(1:IONO_nTheta,:) = IONO_NORTH_Phi
+     IONO_IonNumFlux(1:IONO_nTheta,:) = IONO_NORTH_IonNumFlux
+     IONO_Joule(1:IONO_nTheta,:) = IONO_NORTH_Joule
   endif
 
   if (iProc == nProc-1) then
      IONO_phi(IONO_nTheta:2*IONO_nTheta-1,:) = IONO_SOUTH_Phi
+     IONO_IonNumFlux(1:IONO_nTheta,:) = IONO_SOUTH_IonNumFlux
+     IONO_Joule(1:IONO_nTheta,:) = IONO_SOUTH_Joule
   endif
 
   if (nProc > 1) then
      localVar = IONO_Phi
+     localVar1 = IONO_IonNumFlux
+     localVar2 = IONO_Joule
      iSize = (2*IONO_nTheta-1) * IONO_nPsi
      iError = 0
      call MPI_REDUCE(localVar, IONO_Phi, iSize, MPI_REAL, MPI_MAX, &
           0, iComm, iError)
+     call MPI_REDUCE(localVar1, IONO_IonNumFlux, iSize, MPI_REAL, MPI_MAX, &
+          0, iComm, iError)
+     call MPI_REDUCE(localVar2, IONO_Joule, iSize, MPI_REAL, MPI_MAX, &
+          0, iComm, iError)
+     
   endif
 
 end subroutine IE_gather
