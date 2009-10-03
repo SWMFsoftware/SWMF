3523494bbe90c3ae323917dbe5e7a165bc538e21
aglocer <>
2009-10-03 02:56:49 +0000

    The CreateRestart.pl script has been modified to no longer need a PARAMFILE input. Now the field-line footpoints are calculated internally for greater flexibility.  Modified Files:  	CreateRestart.pl  Removed Files:  	PARAMFILE
    Former-commit-id: 2ee5ddb51cd7d212d249878c71e0024d6564f40c

--- PW/PWOM/Scripts/CreateRestart.pl
+++ PW/PWOM/Scripts/CreateRestart.pl
@@ -5,28 +5,30 @@ use strict;
 
 &print_help if $Help;
 
-my $ParamFile = $ARGV[0];
-&print_help unless $ParamFile;
+my $nCircle = $ARGV[0];
 
-open(PARAMFILE, $ParamFile) or
-    die "Could not open parameter file $ParamFile!\n";
+if($nCircle < 6){ $nCircle=6}
 
-my $InputFile1 = <PARAMFILE> 
-    or die "Could not read first input file name from $ParamFile\n";
-chop($InputFile1);
+my$InputFile1 = 'restart_iline0001.dat';
 
-my $nInputFile = <PARAMFILE>
-    or die "ould not read number of input files from $ParamFile\n";
-chop($nInputFile);
 
-# Read in latitude-longitude pairs
-my $i;
+
+# Create latitude-longitude pairs
+my $nLine= $nCircle * 6.0 * (1.0+$nCircle)/2.0;
+my $dLat = 40.0/$nCircle;
+my $Lat = 90.0;
+my $Lon = 0.0;
+my $i = 0;
 my @LatLon;
-for $i (0..$nInputFile-1){
-    $LatLon[$i] = <PARAMFILE>
-	or die "Could not read lat-lon line $i from $ParamFile\n";
+for my $iCircle (1..$nCircle){
+    $Lat=$Lat-$dLat;
+    my $dLon = 360.0/($iCircle * 6.0);
+    for my $iPoint (0..($iCircle * 6.0-1.0)){
+	$Lon = $iPoint*$dLon;
+	$LatLon[$i] = "$Lat $Lon\n";
+	$i++;
+    }
 }
-close PARAMFILE;
 
 open (FILE, $InputFile1)
     or die "Could not open first input file $InputFile1\n";
@@ -45,7 +47,7 @@ print "Writing out input files ...\n";
 
 my $iFile;
 my $File;
-for $iFile (0..($nInputFile-1)){
+for $iFile (0..($nLine-1)){
     my $iFileString = "0" x (4 - length($iFile+1)) . ($iFile + 1);
     $File = $InputFile1;
     $File =~ s/0001/$iFileString/
@@ -66,15 +68,14 @@ exit;
 
 sub print_help{
     print "
-Usage: CreateRestart.pl PARAMFILE
+Usage: CreateRestart.pl nCircle
 
-where PARAMFILE contains the following lines
-
-FirstInputFileName
-NumberOfInputFiles
-Lat Lon
-Lat Lon
-...
+Where nCircle is the number of circles of points filling the space between 
+90 and 50 degrees latitude. nCircle default and minimum is 6. 
+The points are chosen to ensure uniform layout.
+ 
+The script must be run in a restart directory with a seed line 
+restart_iline0001.out
 
 where the Latitude Longitude (degrees) pairs define the footpoint 
 for the field lines.
