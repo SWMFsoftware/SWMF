b5f610e2754fcad6e590302b18f66a337b63173d
aglocer <>
2009-10-05 15:16:53 +0000

    Add named variables to the time array
    Former-commit-id: f07acc8f379c7c0355753ddbbf8230a53609b726

--- PW/PWOM/src/ModPwTime.f90
+++ PW/PWOM/src/ModPwTime.f90
@@ -3,6 +3,7 @@ Module ModPwTime
   
   real (kind=real8_)   ::  StartTime,CurrentTime !needed for SWMF stuff
   integer              ::  iStartTime(7)=(/1976,6,28,0,0,0,0/)
-  
+  integer, parameter   ::  Year_=1, Month_=2, Day_=3, Hour_=4, Minute_=5, &
+                           Second_=6, mSecond_=7
 
 end Module ModPwTime
