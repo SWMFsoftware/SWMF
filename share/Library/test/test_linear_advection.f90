32ec16da37e365fe83846bc2ad3e68717030035c
oran <>
2009-11-11 00:31:10 +0000

    Added Files:  	test_linear_advection.f90
    Former-commit-id: ed7ecde6478b3ee374329bd8e49ac0e7588daff1

---
+++ share/Library/test/test_linear_advection.f90
@@ -0,0 +1,16 @@
+program linear_advection_test
+
+  use ModLinearAdvectiom, ONLY: test_linear_advection
+
+  implicit none
+
+  call test_linear_advectiom
+
+end program linear_advection_test
+
+subroutine CON_stop(String)
+  implicit none
+  character(len=*), intent(in):: String
+  write(*,*)'ERROR: ',String
+  stop
+end subroutine CON_stop
