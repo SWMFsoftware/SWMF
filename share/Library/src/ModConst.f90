459536feacd27c641bea8d5fda91e4e6c4df5f37
igorsok <>
2009-12-04 17:01:19 +0000

    Spectroscopy symbols Modified Files:  	ModConst.f90
    Former-commit-id: 32737323330fc5b776392f0fe81dbb695d1d51a5

--- share/Library/src/ModConst.f90
+++ share/Library/src/ModConst.f90
@@ -156,6 +156,7 @@ Module ModConst
   real,parameter :: mSun              = 1.99E+30               ! [kg]
   real,parameter :: RotationPeriodSun = 25.38 * cSecondPerDay  ! [ s]
 
+  !Chemistry and spectroscopy
   !\
   ! The Mendeleev table
   !/
@@ -165,10 +166,39 @@ Module ModConst
      'Na','Mg',                                                  'Al','Si','P_','S_','Cl','Ar',& !11:18
      'K_','Ca','Sc','Ti','V_','Cr','Mn','Fe','Co','Ni','Cu','Zn','Ga','Ge','As','Se','Br','Kr',& !19:36
      'Rb','Sr','Y_','Zr','Nb','Mo','Tc','Ru','Rh','Pd','Ag','Cd','In','Sn','Sb','Te','I_','Xe'/) !37:54
-
-          
-  
-
+  !Orbital quantum number: transform a numerical value to a symbol:
+  character(LEN=1),parameter::TypeL_I(0:9) = (/'s','p','d','f','g','h','i','k','l','m'/)
+contains
+  !Transforms the spectroscopy term symbols to the value of the orbital quantum number
+  integer function l_orbital(TypeL)
+    character(LEN=1),intent(in)::TypeL
+    !--------------------------------!
+    l_orbital=-1
+    select case(TypeL)
+    case('s','S')
+       l_orbital = 0
+    case('p','P')
+       l_orbital = 1
+    case('d','D')
+       l_orbital = 2
+    case('f','F')
+       l_orbital = 3
+    case('g','G')
+       l_orbital = 4
+    case('h')
+       l_orbital = 5
+    case('i')
+       l_orbital = 6
+    case('k')
+       l_orbital = 7
+    case('l')
+       l_orbital = 8
+    case('m')
+       l_orbital = 9
+    case default
+       call CON_stop('The spectroscopy symbol '//TypeL//' is not implemented')
+    end select
+  end function l_orbital
 end module ModConst
 !====================================================================
 !====================================================================
