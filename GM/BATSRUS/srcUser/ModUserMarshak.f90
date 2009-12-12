c62edbf0132516f099ab2c4888c15cfa2526df09
bartvand <>
2009-12-12 16:30:20 +0000

    Fixed boundary conditions with strictly 1D with BATL.
    Former-commit-id: 51c1089f3559638e9e6f76dc4c64c9d03eb68a68

--- GM/BATSRUS/srcUser/ModUserMarshak.f90
+++ GM/BATSRUS/srcUser/ModUserMarshak.f90
@@ -157,30 +157,33 @@ contains
        select case(TypeBc)
        case('user')
           ! float, just for the sake of having filled in ghost cells
-          State_VGB(:,0,:,:,iBlock)  = State_VGB(:,1,:,:,iBlock)
-          State_VGB(:,-1,:,:,iBlock) = State_VGB(:,1,:,:,iBlock)
+          State_VGB(:,0,1:nJ,1:nK,iBlock)  = State_VGB(:,1,1:nJ,1:nK,iBlock)
+          State_VGB(:,-1,1:nJ,1:nK,iBlock) = State_VGB(:,1,1:nJ,1:nK,iBlock)
        case('usersemi')
           ! Marshak boundary conditions
           Coef = 2.0/(3.0*SpecificOpacity*Density*Dx_Blk(iBlock))
-          StateSemi_VGB(iEradImpl,0,:,:,iBlock) = &
-               (cRadiationNo*TradBc**4 + StateSemi_VGB(iEradImpl,1,:,:,iBlock)&
+          StateSemi_VGB(iEradImpl,0,1:nJ,1:nK,iBlock) = &
+               (cRadiationNo*TradBc**4 &
+               + StateSemi_VGB(iEradImpl,1,1:nJ,1:nK,iBlock)&
                *(Coef - 0.5) )/(Coef + 0.5)
        case('usersemilinear')
           Coef = 2.0/(3.0*SpecificOpacity*Density*Dx_Blk(iBlock))
-          StateSemi_VGB(iEradImpl,0,:,:,iBlock) = &
-               StateSemi_VGB(iEradImpl,1,:,:,iBlock)*(Coef - 0.5)/(Coef + 0.5)
+          StateSemi_VGB(iEradImpl,0,1:nJ,1:nK,iBlock) = &
+               StateSemi_VGB(iEradImpl,1,1:nJ,1:nK,iBlock) &
+               *(Coef - 0.5)/(Coef + 0.5)
        end select
     case(2)
        select case(TypeBc)
        case('user')
           ! float, just for the sake of having filled in ghost cells
-          State_VGB(:,nI+1,:,:,iBlock) = State_VGB(:,nI,:,:,iBlock)
-          State_VGB(:,nI+2,:,:,iBlock) = State_VGB(:,nI,:,:,iBlock)
+          State_VGB(:,nI+1,1:nJ,1:nK,iBlock) = State_VGB(:,nI,1:nJ,1:nK,iBlock)
+          State_VGB(:,nI+2,1:nJ,1:nK,iBlock) = State_VGB(:,nI,1:nJ,1:nK,iBlock)
        case('usersemi','usersemilinear')
           ! zero radiation influx boundary conditions
           Coef = 2.0/(3.0*SpecificOpacity*Density*Dx_Blk(iBlock))
-          StateSemi_VGB(iEradImpl,nI+1,:,:,iBlock) = &
-               StateSemi_VGB(iEradImpl,nI,:,:,iBlock)*(Coef - 0.5)/(Coef + 0.5)
+          StateSemi_VGB(iEradImpl,nI+1,1:nJ,1:nK,iBlock) = &
+               StateSemi_VGB(iEradImpl,nI,1:nJ,1:nK,iBlock) &
+               *(Coef - 0.5)/(Coef + 0.5)
        end select
     case default
        write(*,*) NameSub//' : user boundary not defined at iSide = ', iSide
