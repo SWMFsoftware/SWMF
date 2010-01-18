d8bbea54206912b33afb130d8f5c9e89b36ca813
igorsok <>
2010-01-18 19:00:26 +0000

    Preliminary version of electron-neutral collisions included into the transport phenomena.
    Former-commit-id: 4b29fed7a6fe47b11be9d19abf563451f2b9e6a7

--- util/CRASH/src/ModTransport.f90
+++ util/CRASH/src/ModTransport.f90
@@ -5,17 +5,19 @@ module CRASH_ModTransport
   !Calculation of the transport coefficients 
   !in dense plasmas: electron heat conduction,
   !Electron-ion temperature relaxation etc.
-
   implicit none
+
+  logical,public::UseNeutralCollision=.false.
+
 contains
   !=================================================================
   real function nu_ei()
     !The conventional auxiliary characterstic of the
     !electron-ion collisions
     use ModConst,ONLY: cSigmaThomson, cElectronMass, &
-                       cLightSpeed, cEV
+         cLightSpeed, cEV
     use ModNumConst,ONLY: cPi
-   
+
     !The rest mass energy
     real, parameter:: MeC2InEV = &
          cElectronMass * cLightSpeed * cLightSpeed /cEV
@@ -24,7 +26,7 @@ contains
     !The expression to eveluate:
     !\nu_{\rm eff} = \frac{4}{3}\sqrt{\frac{2\pi}{m_e}}*
     !                *\frac{e^4*Na*CoulombLog}{Te^{3/2}}
-    
+
     !We re-rite it in the following manner:
     ! \nu_{\rm eff} = 
     !  =\frac{8\pi}{3}\left(\frac{e^2}{m_ec^2}\right)^2 * 
@@ -32,7 +34,7 @@ contains
     ! ...*(m_ec^2 [eV]/T_e[eV])^2 *\sqrt{T_e/(2\pi m_e}
     !The last term equals c\sqrt{T_e[eV]/{2\pi* m_ec^2[eV]}
     !With this account:
-    
+
     if(Te.le.0.0 .or. Z2.le.0.0) then
        nu_ei = 0.0
        return
@@ -41,12 +43,12 @@ contains
 
     nu_ei = cSigmaThomson * cLightSpeed * Na * CoulombLog / &
          ( TePerMeC2 * sqrt(2.0 * cPi * TePerMeC2 ) ) ! s^{-1}
-    
+
   end function nu_ei
   !================
   real function te_ti_relaxation()
     use ModConst,ONLY: cElectronMass, cAtomicMass 
-    
+
     !The Landau formula, may be written only for T_i in our case:
     !d(3/2)T_i/dt =  TeTiRelaxation * (T_e - T_i)
     !so that the relaxation rate for the ion ENERGY is expressed in terms
@@ -55,10 +57,10 @@ contains
 
     real,parameter :: cMassRatio = 3.0 * cElectronMass/ cAtomicMass
     !-
-    te_ti_relaxation =  cMassRatio * ZAv * Z2PerA *  nu_ei() !s^{-1}
+    te_ti_relaxation =  cMassRatio * ZAv * (Z2PerA *  nu_ei() + nu_en_per_a()) !s^{-1}
 
     !The practical application:
-    !The ralaxation per the unit of volume:
+    !The ralaxation per the unit of volume
     !dE_i/dt = TeTiRelaxation * Na * (T_e-T_i)* 
     !* {1 cEV or k_{Bolttmann}}, 
     !depending on the unit of temperature.
@@ -81,13 +83,60 @@ contains
     !The dependence on Z as well as the numerical coefficient
     !are to be modified:
 
-    if(nu_ei()==0.0)then
+    if(nu_ei()==0.0.and.(.not.UseNeutralCollision))then
        electron_heat_conductivity = 0.0
        return
     end if
 
     electron_heat_conductivity = cBoltzmann * (Te * cEV/cElectronMass) * &
-         (Na * ZAv) /(Z2 * nu_ei())  ![J/(M*s*K)]
+         (Na * ZAv) /(Z2 * nu_ei()+nu_en())  ![J/(M*s*K)]
   end function electron_heat_conductivity
+  !=======================================
+  real function nu_en()
+    !The conventional auxiliary characterstic of the
+    !electron-neutral collisions
+    use ModConst,ONLY: cElectronMass, &
+         cLightSpeed, cEV,cBohrRadius
+    use ModNumConst,ONLY: cPi
+    use CRASH_ModAtomicDataMix,ONLY: nMix,Concentration_I
+    use CRASH_ModPartition,ONLY: Population_II,iZMin_I
 
+    !The rest mass energy
+    real, parameter:: MeC2InEV = &
+         cElectronMass * cLightSpeed * cLightSpeed /cEV
+    real :: VTe
+    !------------------
+    VTe = sqrt(Te/ MeC2InEV) * cLightSpeed
+    nu_en = VTe * Na*sum(Population_II(0,1:nMix)*Concentration_I(1:nMix),&
+         MASK=iZMin_I(1:nMix)==0)*cBohrRadius**2
+  end function nu_en
+  !========================================
+  real function nu_en_per_a()
+    !The conventional auxiliary characterstic of the
+    !electron-neutral collisions relater to A
+    use ModConst,ONLY: cElectronMass, &
+         cLightSpeed, cEV,cBohrRadius
+    use ModNumConst,ONLY: cPi
+    use CRASH_ModAtomicDataMix,ONLY: nMix,Concentration_I,nZ_I
+    use CRASH_ModAtomicMass
+    use CRASH_ModPartition,ONLY: Population_II,iZMin_I
+    !-------------------------
+
+    !The rest mass energy
+    real, parameter:: MeC2InEV = &
+         cElectronMass * cLightSpeed * cLightSpeed /cEV
+    real :: VTe
+    integer::iMix
+    !------------------
+    VTe = sqrt(Te/ MeC2InEV) * cLightSpeed
+
+    nu_en_per_a = 0.0
+    do iMix = 1,nMix
+       if(iZMin_I(iMix)/=0)CYCLE
+       nu_en_per_a = nu_en_per_a + Population_II(0,iMix)*&
+            Concentration_I(iMix)/cAtomicMass_I(nZ_I(iMix))
+            
+    end do
+    nu_en_per_a = nu_en_per_a * VTe * Na*cBohrRadius**2
+  end function nu_en_per_a
 end module CRASH_ModTransport
