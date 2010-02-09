d1a9e6318dbad09390bd310cccd09b4bd11cd399
gtoth <>
2010-02-09 19:24:41 +0000

    Added CON_stop and CON_set_do_test for standalone compilation.
    Former-commit-id: d53c4d32cd036859cb6c2b9a5c71d98297501b25

--- IM/CRCM/src/main.f90
+++ IM/CRCM/src/main.f90
@@ -107,4 +107,27 @@
   close(2)
 
 end program     ! end of the test program
+!=============================================================================
+subroutine CON_stop(StringError)
+  implicit none
+  character (len=*), intent(in) :: StringError
+
+  ! Local variables:
+  integer :: iError,nError
+  !----------------------------------------------------------------------------
+
+  write(*,*) 'Stopping execution with msg:'
+  write(*,*) StringError
+  stop
+
+end subroutine CON_stop
+!=============================================================================
+subroutine CON_set_do_test(String, DoTest, DoTestMe)
+  implicit none
+  character (len=*), intent(in)  :: String
+  logical          , intent(out) :: DoTest, DoTestMe
+
+  DoTest = .false.; DoTestMe = .false.
+
+end subroutine CON_set_do_test
 
