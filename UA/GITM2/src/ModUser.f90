8afc6e5c6305985a13b501538502f9b4d9ae44e0
dpawlows <>
2010-02-10 15:24:20 +0000

    Added a userdata1D variable
    Former-commit-id: fb08a1233c85471f4784faad05cbb69a39b8489c

--- UA/GITM2/src/ModUser.f90
+++ UA/GITM2/src/ModUser.f90
@@ -1,15 +1,17 @@
 
 module ModUserGITM
 
-  use ModGITM
+  use ModGITM, only: nBlocksmax,nLons,nLats,nAlts
 
   implicit none
 
-  integer, parameter :: nUserOutputs = 40
+  integer, parameter :: nUserOutputs = 100
   real :: UserData3D(-1:nLons+2,-1:nLats+2,-1:nAlts+2,nUserOutputs,nBlocksMax)=0.0
   real :: UserData2D(-1:nLons+2,-1:nLats+2,         1,nUserOutputs,nBlocksMax)=0.0
+  real :: UserData1D(1,1,1:nalts,nUserOutputs)=0.0
 
   integer :: nVarsUser3d=0
+  integer :: nVarsUser1d=0
   integer :: nVarsUser2d=0
 
 end module ModUserGITM
