61cf97185d26ca34c454d13aa393be7be8960b73
rilie <>
2010-03-16 03:12:18 +0000

    B1 needed multiplication by the dipole moment. Added comments.
    Former-commit-id: beb2f023a654604ce7142fffbd677f1101fd3b2c

--- GM/BATSRUS/srcUser/ModUserStretchedDipole.f90
+++ GM/BATSRUS/srcUser/ModUserStretchedDipole.f90
@@ -1,19 +1,19 @@
 !^CFG COPYRIGHT UM
 module ModUser
-  
+
   use ModUserEmpty,               &
        IMPLEMENTED1 => user_set_ics
-  
+
   include 'user_module.h' !list of public methods
-  
+
   real,              parameter :: VersionUserModule = 1.0
   character (len=*), parameter :: NameUserModule = &
        'Stretched Dipole'
-  
-  real, parameter :: alpha = 1.1, beta = 1./alpha
-  
+
+  real, parameter :: alpha = 2.0, beta = 3.0 ! Stretching factors
+
 contains
-  
+
   !=============================================================================
   subroutine user_set_ics
 
@@ -22,8 +22,10 @@ contains
     use ModAdvance,    ONLY: State_VGB
     use ModGeometry,   ONLY: x_BLK, y_BLK, z_BLK
     use ModVarIndexes, ONLY: Bx_,By_,Bz_
+    use ModPhysics,    ONLY: Bdp
+    use ModB0,         ONLY: B0_DGB
 
-    integer :: i, j, k, iBLK 
+    integer :: i, j, k, iBlock 
     logical :: oktest, oktest_me
     real    :: x, y, z, a, b
 
@@ -40,24 +42,32 @@ contains
        oktest=.false.; oktest_me=.false.
     end if
 
-    iBLK = globalBLK
+    iBlock = globalBLK
 
     do k = 1, nK; do j = 1, nJ; do i = 1, nI
-       x = x_BLK(i,j,k,iBLK)
-       y = y_BLK(i,j,k,iBLK)
-       z = z_BLK(i,j,k,iBLK)
+       x = x_BLK(i,j,k,iBlock)
+       y = y_BLK(i,j,k,iBlock)
+       z = z_BLK(i,j,k,iBlock)
+       !\
+       ! The dipole field is stretched by alpha factor in the z direction and
+       ! beta in the y direction.
+       !/
 
-       a = (x**2 + y**2 + z**2)**(5./2.)
-       b = (x**2 + (y*beta)**2 + (alpha*z)**2)**(5./2.)
+       a = (sqrt(x**2 + y**2 + z**2))**5
+       b = (sqrt(x**2 + (y*beta)**2 + (alpha*z)**2))**5
 
-       State_VGB(Bx_,i,j,k,iBLK) = (-3. * z * x * alpha)/b + (3. * z * x)/a
-       State_VGB(By_,i,j,k,iBLK) = (-3. * z * y * alpha)/b + (3. * z * y)/a
-       State_VGB(Bz_,i,j,k,iBLK) = (1./alpha *b) *      &
-            (-2. * (alpha*z)**2 + x**2 + (beta*y)**2) + &
-            (2. * z**2 - x**2 -y**2)/a
+       ! write(*,*) 'Bx', B0_DGB(1,i,j,k,iBlock), Bdp*(3*z * x)/a
+       ! write(*,*) 'By', B0_DGB(2,i,j,k,iBlock), Bdp*(3*z * y)/a
+       ! write(*,*) 'Bz', B0_DGB(3,i,j,k,iBlock), Bdp*(2*z**2  - x**2 - y**2)/a
+
+       State_VGB(Bx_,i,j,k,iBlock) = Bdp*((-3. * z * x * alpha)/b + (3. * z * x)/a)
+       State_VGB(By_,i,j,k,iBlock) = Bdp*((-3. * z * y * alpha)/b + (3. * z * y)/a)
+       State_VGB(Bz_,i,j,k,iBlock) = &
+            Bdp*((-2. * (alpha*z)**2 + x**2 + (beta*y)**2)/(alpha*b) + &
+            ( 2. *        z**2  - x**2 -        y**2)/a)
 
     end do; end do; end do
 
   end subroutine user_set_ics
-  
+
 end module ModUser
