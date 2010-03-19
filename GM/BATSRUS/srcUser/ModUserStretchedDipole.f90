6c11059c6abbc2fdd9e35d058b81acfb8b2f65f3
rilie <>
2010-03-19 18:24:09 +0000

    Sign fix.
    Former-commit-id: 436cba13c407cefca444cd713712b1c6784a1619

--- GM/BATSRUS/srcUser/ModUserStretchedDipole.f90
+++ GM/BATSRUS/srcUser/ModUserStretchedDipole.f90
@@ -60,11 +60,11 @@ contains
         !write(*,*) 'By', B0_DGB(2,i,j,k,iBlock), Bdp*(3*z * y)/a
         !write(*,*) 'Bz', B0_DGB(3,i,j,k,iBlock), Bdp*(2*z**2  - x**2 - y**2)/a
 
-       State_VGB(Bx_,i,j,k,iBlock) = Bdp*((-3. * z * x * alpha)/b - (3. * z * x)/a)
-       State_VGB(By_,i,j,k,iBlock) = Bdp*((-3. * z * y * alpha)/b - (3. * z * y)/a)
+       State_VGB(Bx_,i,j,k,iBlock) = Bdp*((3. * z * x * alpha)/b - (3. * z * x)/a)
+       State_VGB(By_,i,j,k,iBlock) = Bdp*((3. * z * y * alpha)/b - (3. * z * y)/a)
        State_VGB(Bz_,i,j,k,iBlock) = &
-            Bdp*((-2. * (alpha*z)**2 + x**2 + (beta*y)**2)/(alpha*b) - &
-            (  2.      *        z**2 - x**2 -        y**2)/a)
+            Bdp*((2. * (alpha*z)**2 - x**2 - (beta*y)**2)/(alpha*b) - &
+            (  2.      *       z**2 - x**2 -        y**2)/a)
 
     end do; end do; end do
 
