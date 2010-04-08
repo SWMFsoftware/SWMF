210309f5ebd7276abdb55d1d41223703e829b85a
bartvand <>
2010-04-08 00:52:32 +0000

    Several improvements. There is still a small shift in the longitude.
    Former-commit-id: daa1f5c031fbab3d3f4eaf09450e82f52969de7b

--- util/DATAREAD/srcMagnetogram/potential_field.f90
+++ util/DATAREAD/srcMagnetogram/potential_field.f90
@@ -8,6 +8,8 @@ module ModPotentialField
   integer         :: nR = 50, nTheta = 90, nPhi = 90
   real, parameter :: rMin = 1.0, rMax = 2.5
 
+  logical :: UseBr = .true.
+
   real, dimension(:), allocatable :: &
        Radius_I, Theta_I, Phi_I, SinTheta_I, &
        dRadius_I, dTheta_I, dPhi_I, &
@@ -25,15 +27,16 @@ contains
     ! Read the raw magnetogram file into a 2d array
 
     ! Name of input file
-    character (len=*), parameter:: NameFileIn = 'test.in'
+    character (len=*), parameter:: NameFileIn = 'fitsfile.dat'
 
-    real, parameter:: BrMax = 1900.0
+    real, parameter:: BrMax = 3500.0 ! Saturation level of MDI
     integer, parameter:: iUnit = 9   
 
     integer:: iError
     integer:: nCarringtonRotation
     integer:: nTheta0, nPhi0, nThetaRatio, nPhiRatio
     integer:: iTheta, iPhi, iTheta0, iTheta1, iPhi0, iPhi1
+    real :: BrAverage
     character (len=100) :: String
 
     real, allocatable:: Br0_II(:,:)
@@ -56,8 +59,9 @@ contains
          nCarringtonRotation, nTheta0, nPhi0
 
     allocate(Br0_II(nTheta0,nPhi0))
-    
-    do iTheta = 1, nTheta0
+
+    ! fitsfile.dat is in longitude, latitude
+    do iTheta = nTheta0, 1, -1
        do iPhi = 1, nPhi0
           read(iUnit,*) Br0_II(iTheta,iPhi)
           if (abs(Br0_II(iTheta,iPhi)) > BrMax) &
@@ -96,6 +100,10 @@ contains
        end do
     end do
 
+    ! remove monopole
+    BrAverage = 0.0 !sum(Br_II)/(nTheta*nPhi)
+    Br_II = Br_II - BrAverage
+
     deallocate(Br0_II)
 
     close(iUnit)
@@ -115,7 +123,7 @@ contains
     allocate( &
          Radius_I(0:nR+1), Theta_I(0:nTheta+1), Phi_I(0:nPhi+1), &
          dRadius_I(nR), dTheta_I(nTheta), dPhi_I(nPhi), &
-         SinTheta_I(nTheta), SinThetaNode_I(nTheta+1), &
+         SinTheta_I(0:nTheta+1), SinThetaNode_I(nTheta+1), &
          RadiusNode_I(nR+1), ThetaNode_I(nTheta+1), PhiNode_I(nPhi+1), &
          dRadiusNode_I(nR+1), dThetaNode_I(nTheta+1), dPhiNode_I(nPhi+1))
 
@@ -154,7 +162,7 @@ contains
           ThetaNode_I(iTheta) = (iTheta - 1)*dTheta
        end do
     end if
-    SinTheta_I = sin(Theta_I(1:nTheta))
+    SinTheta_I = sin(Theta_I)
     SinThetaNode_I = sin(ThetaNode_I)
 
     dTheta_I     = ThetaNode_I(2:nTheta+1) - ThetaNode_I(1:nTheta)
@@ -182,6 +190,135 @@ contains
 
   end subroutine init_potential_field
 
+  !============================================================================
+
+  subroutine save_potential_field
+
+    use ModInterpolate, ONLY: trilinear
+    use ModNumConst,    ONLY: cHalfPi
+    use ModPlotFile,    ONLY: save_plot_file
+
+    real, allocatable :: Potential_G(:,:,:)
+    real, allocatable :: Br_G(:,:,:), Btheta_G(:,:,:), Bphi_G(:,:,:)
+    real, allocatable :: B_DN(:,:,:,:)
+
+    real :: r, Theta, Phi
+    integer :: iR, iTheta, iPhi
+    !--------------------------------------------------------------------------
+
+    allocate(Potential_G(0:nR+1,0:nTheta+1,0:nPhi+1))
+    Potential_G = 0.0
+    call set_boundary(Potential_C, Potential_G)
+
+    ! Staggered components of the magnetic field
+    allocate( &
+         Br_G(1:nR+1,0:nTheta+1,0:nPhi+1), &
+         Btheta_G(0:nR+1,1:nTheta+1,0:nPhi+1), &
+         Bphi_G(0:nR+1,0:nTheta+1,1:nPhi+1) )
+
+    do iPhi = 0, nPhi+1
+       do iTheta = 0, nTheta+1
+          do iR = 1, nR+1
+             Br_G(iR,iTheta,iPhi) = &
+                  (Potential_G(iR,iTheta,iPhi)-Potential_G(iR-1,iTheta,iPhi)) &
+                  / dRadiusNode_I(iR)
+          end do
+       end do
+    end do
+
+    do iPhi = 0, nPhi+1
+       do iTheta = 1, nTheta+1
+          do iR = 0, nR+1
+             Btheta_G(iR,iTheta,iPhi) = &
+                  (Potential_G(iR,iTheta,iPhi)-Potential_G(iR,iTheta-1,iPhi)) &
+                  / (Radius_I(iR)*dThetaNode_I(iTheta))
+          end do
+       end do
+    end do
+
+    do iPhi = 1, nPhi+1
+       do iTheta = 0, nTheta+1
+          do iR = 0, nR+1
+             Bphi_G(iR,iTheta,iPhi) = &
+                  (Potential_G(iR,iTheta,iPhi)-Potential_G(iR,iTheta,iPhi-1)) &
+                  / (Radius_I(iR)*SinTheta_I(iTheta)*dPhiNode_I(iPhi))
+          end do
+       end do
+    end do
+
+    ! The magnetic field on the final grid used in BATSRUS
+    ! Note, the coordinates are in longitutude and latitude, but the
+    ! magnetic field is in Bphi, Btheta = -Blatitude
+    allocate(B_DN(3,1:nR+1,1:nPhi+1,1:nTheta))
+
+    do iPhi = 1, nPhi+1
+       do iTheta = 1, nTheta
+          do iR = 1, nR+1
+             r = RadiusNode_I(iR)
+             Theta = Theta_I(iTheta)
+             Phi = PhiNode_I(iPhi)
+
+             B_DN(1,iR,iPhi,nTheta+1-iTheta) = &
+                  0.5*sum(Br_G(iR,iTheta,iPhi-1:iPhi))
+             B_DN(3,iR,iPhi,nTheta+1-iTheta) = &
+                  trilinear(Btheta_G, 0, nR+1, 1, nTheta+1, 0, nPhi+1, &
+                  (/r,Theta,Phi/), x_I=Radius_I, y_I=ThetaNode_I, &
+                  z_I=Phi_I, DoExtrapolate=.false.)
+             B_DN(2,iR,iPhi,nTheta+1-iTheta) = &
+                  trilinear(Bphi_G, 0, nR+1, 0, nTheta+1, 1, nPhi+1, &
+                  (/r,Theta,Phi/), x_I=Radius_I, y_I=Theta_I, &
+                  z_I=PhiNode_I, DoExtrapolate=.false.)
+
+          end do
+       end do
+    end do
+
+    call save_plot_file('potentialfield.out', TypeFileIn='real8', &
+         StringHeaderIn = 'Radius [Rs] Longitude [Rad] Latitude [Rad] B [G]', &
+         nameVarIn = 'Radius Longitude Latitude Br Bphi Btheta' &
+         //' Ro_PFSSM Rs_PFSSM nRExt', ParamIn_I = (/ rMin, rMax, 0.0 /), &
+         nDimIn=3, VarIn_VIII=B_DN, &
+         Coord1In_I=RadiusNode_I, &
+         Coord2In_I=PhiNode_I, &
+         Coord3In_I=cHalfPi-Theta_I(nTheta:1:-1))
+
+  end subroutine save_potential_field
+
+  !============================================================================
+
+  subroutine set_boundary(x_C, x_G)
+
+    real, intent(in):: x_C(nR,nTheta,nPhi)
+    real, intent(inout):: x_G(0:nR+1,0:nTheta+1,0:nPhi+1)
+
+    integer :: iPhi, jPhi
+    !--------------------------------------------------------------------------
+    ! Current solution inside
+    x_G(1:nR,1:nTheta,1:nPhi) = x_C
+
+    ! The slope is forced to be Br at the inner boundary
+    if(UseBr)then
+       x_G(0,1:nTheta,1:nPhi) = x_C(1,:,:) - dRadiusNode_I(1)*Br_II
+    else
+       x_G(0,1:nTheta,1:nPhi) = x_C(1,:,:)
+    end if
+
+    ! The potential is zero at the outer boundary
+    x_G(nR+1,:,:) = 0.0
+
+    ! Symmetric in Theta but shifted by nPhi/2
+    do iPhi = 1, nPhi
+       jPhi = modulo(iPhi-1 + nPhi/2, nPhi) + 1
+       x_G(:,0,iPhi)        = x_G(:,1,jPhi)
+       x_G(:,nTheta+1,iPhi) = x_G(:,nTheta,jPhi)
+    end do
+
+    ! Periodic around phi
+    x_G(:,:,0)      = x_G(:,:,nPhi)
+    x_G(:,:,nPhi+1) = x_G(:,:,1)
+
+  end subroutine set_boundary
+
 end module ModPotentialField
 
 !==============================================================================
@@ -189,8 +326,6 @@ end module ModPotentialField
 module ModB0Matvec
 
   implicit none
-
-  logical :: UseBr = .true.
 
 contains
 
@@ -218,14 +353,14 @@ contains
   subroutine get_gradient(x_C, Grad_DG)
 
     use ModPotentialField, ONLY: nR, nTheta, nPhi, Radius_I, SinTheta_I, &
-         dRadiusNode_I, dThetaNode_I, dPhiNode_I, Br_II
+         dRadiusNode_I, dThetaNode_I, dPhiNode_I, Br_II, set_boundary
 
     real, intent(in):: x_C(nR,nTheta,nPhi)
     real, intent(out):: Grad_DG(3,nR+1,nTheta+1,nPhi+1)
 
     real, allocatable, save :: x_G(:,:,:)
 
-    integer:: iR, iTheta, iPhi, jPhi
+    integer:: iR, iTheta, iPhi
     !--------------------------------------------------------------------------
     if(.not.allocated(x_G))then
        allocate(x_G(0:nR+1,0:nTheta+1,0:nPhi+1))
@@ -233,29 +368,7 @@ contains
        x_G = 0.0
     end if
 
-    ! Current solution inside
-    x_G(1:nR,1:nTheta,1:nPhi) = x_C
-
-    ! The slope is forced to be Br at the inner boundary
-    if(UseBr)then
-       x_G(0,1:nTheta,1:nPhi) = x_C(1,:,:) - dRadiusNode_I(1)*Br_II
-    else
-       x_G(0,1:nTheta,1:nPhi) = x_C(1,:,:)
-    end if
-
-    ! The potential is zero at the outer boundary
-    x_G(nR+1,:,:) = 0.0
-
-    ! Symmetric in Theta but shifted by nPhi/2
-    do iPhi = 1, nPhi
-       jPhi = modulo(iPhi-1 + nPhi/2, nPhi) + 1
-       x_G(:,0,iPhi)        = x_G(:,1,jPhi)
-       x_G(:,nTheta+1,iPhi) = x_G(:,nTheta,jPhi)
-    end do
-
-    ! Periodic around phi
-    x_G(:,:,0)      = x_G(:,:,nPhi)
-    x_G(:,:,nPhi+1) = x_G(:,:,1)
+    call set_boundary(x_C, x_G)
 
     ! This initialization is only for the corners
     Grad_DG = 0.0
@@ -335,14 +448,14 @@ program potential_field
   ! radial field at outer boundary.
 
   use ModPotentialField
-  use ModB0Matvec, ONLY: get_gradient, get_divergence, matvec, UseBr
+  use ModB0Matvec, ONLY: get_gradient, get_divergence, matvec
   use ModLinearSolver, ONLY: gmres, bicgstab
   use ModPlotFile, ONLY: save_plot_file
 
   implicit none
 
-  integer :: nKrylov=400, nIter=1000
-  real    :: Tolerance = 0.001
+  integer :: nKrylov=400, nIter=10000
+  real    :: Tolerance = 0.0001
   integer :: n, iError, iTheta
   !--------------------------------------------------------------------------
 
@@ -393,6 +506,10 @@ program potential_field
        Coord2In_I=ThetaNode_I, &
        Coord3In_I=PhiNode_I, &
        VarIn_VIII=PlotVar_VG)
+
+  deallocate(PlotVar_VG)
+
+  call save_potential_field
 
 end program potential_field
 !==============================================================================
