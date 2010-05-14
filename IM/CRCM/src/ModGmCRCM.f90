12d3e1941abb7de000efd4b5e1100ff3d3be4f51
aglocer <>
2010-05-14 22:55:14 +0000

    rrio,ttio --> Den_IC,Temp_IC
    indices for species density and pressure also added in preparation for
    multi-fluid coupling
    
    
    Former-commit-id: 4f733688df165b8b085b10878b92119f31eacd46

--- IM/CRCM/src/ModGmCRCM.f90
+++ IM/CRCM/src/ModGmCRCM.f90
@@ -1,11 +1,13 @@
 Module ModGmCrcm
-  use ModCrcmGrid,ONLY: nLat => np, nLon => nt
+  use ModCrcmGrid,  ONLY: nLat => np, nLon => nt
+  use ModCrcmPlanet,ONLY: nspec
   real, allocatable :: StateLine_VI(:,:),StateIntegral_IIV(:,:,:)
   integer :: iLineIndex_II(nLon,1:nLat),nPoint, nIntegral
-  integer, parameter :: AveDens_=4, AveP_=5
+  integer, parameter :: AveDens_=4, AveP_=5,AveHpRho_=7,AveOpRho_=8,&
+                        AveHpP_=9,AveOpP_=10
   integer,parameter :: nVar=4
   
-  real :: rrio(nLat,nLon),ttio(nLat,nLon)
+  real :: Den_IC(nspec,nLat,nLon),Temp_IC(nspec,nLat,nLon)
   integer :: iLatMin=22 !Minimum latitude in MHD boundary
   
   logical :: UseGm =.true.,DoneGmCoupling=.false.,DoMultiFluidGMCoupling=.false.
