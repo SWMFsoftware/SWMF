3957584f3275d97c91c3e48a0edffc882a2370c6
aglocer <>
2010-05-14 22:46:03 +0000

    Pressure_C -> Pressure_IC so now individual ion pressure is calculated. similar for phot
    dt is reduced to 1 from 10
    
    SDtime is added. This is the electron diffusion time needed for the new
    inclusion of electrons in the calculation
    
    
    Former-commit-id: d31a658e8a7dd2d88984a26ce5987ef44b5e60a6

--- IM/CRCM/src/ModCrcm.f90
+++ IM/CRCM/src/ModCrcm.f90
@@ -1,13 +1,14 @@
 Module ModCrcm
   use ModCrcmGrid
+  use ModCrcmPlanet,ONLY: nspec
   implicit none
   
-  real    :: f2(nspec1,np1,nt1,nm,nk),phot(np,nt),Pressure_C(np,nt)
-  integer :: kspec(nspec)
-  real    :: dt =10., dtmax=10. ! tyical time step of crcm
+  real    :: f2(nspec,np1,nt1,nm,nk),phot(nspec,np,nt),Pressure_IC(nspec,np,nt)
+  real    :: dt =1., dtmax=1. ! tyical time step of crcm
   real    :: Time
   real    :: FAC_C(np,nt)
   logical :: UseMcLimiter=.false.
   real    :: BetaLimiter = 1.5
+  real    :: SDtime(np,nt,nm,nk)
 
 end Module ModCrcm
