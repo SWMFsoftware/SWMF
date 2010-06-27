62a33515f51afbb3152ed21c6f05d51ccb9e8631
gtoth <>
2010-06-27 20:02:17 +0000

    Major rewrite of the unit tests. All tests are done with "make test" Save outputs into *.tmp, store reference solutions in *.log, and calculate *.diff files for all tests. Will be included in nightly test.
    test_linear_advection and test_planet_field actually fail right now.
    
    Modified Files:
    	Makefile test_axes.f90 test_planet_field.f90
    Added Files:
    	test_axes.log test_freq.log test_interpolate.log
    	test_io_unit.log test_line_extract.log
    	test_linear_advection.log test_linear_solver.log
    	test_lookup_table.log test_plot_file.log test_sort.log
    	test_time_convert.log
    Removed Files:
    	linear_advection.ref
    
    
    Former-commit-id: d6ed9279d6b0ef11a93ed283883a4e833dd05fe6

--- share/Library/test/test_axes.f90
+++ share/Library/test/test_axes.f90
@@ -1,9 +1,12 @@
 program test_axes
 
   use CON_axes, ONLY: test => test_axes
+  use CON_planet, ONLY: init_planet_const, set_planet_defaults
 
   implicit none
 
+  call init_planet_const
+  call set_planet_defaults
   call test
 
 end program test_axes
--- share/Library/test/test_planet_field.f90
+++ share/Library/test/test_planet_field.f90
@@ -1,9 +1,12 @@
 program test_planet_field
 
   use CON_planet_field, ONLY: test => test_planet_field
+  use CON_planet, ONLY: init_planet_const, set_planet_defaults
 
   implicit none
 
+  call init_planet_const
+  call set_planet_defaults
   call test
 
 end program test_planet_field
