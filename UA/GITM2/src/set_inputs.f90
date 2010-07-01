73d54a850f6ee0bbcf0196cba4189de7752487c0
ridley <>
2010-07-01 22:04:55 +0000

    the subroutine is not expecting the file name to be passed, which was causing the code to crash.
    Former-commit-id: d72c9901e6a8528c83caf15565463bb5378184f0

--- UA/GITM2/src/set_inputs.f90
+++ UA/GITM2/src/set_inputs.f90
@@ -796,7 +796,8 @@ subroutine set_inputs
            call read_in_logical(UseEUVData, iError)
            call read_in_string(cEUVFile,iError)
            
-           if (UseEUVData) call Set_Euv(cEUVFile,iError)
+           if (UseEUVData) call Set_Euv(iError)
+!           if (UseEUVData) call Set_Euv(cEUVFile,iError)
            if (iError /= 0) then
               write(*,*) 'Incorrect format for #EUV_DATA'
               write(*,*) '#EUV_DATA'
