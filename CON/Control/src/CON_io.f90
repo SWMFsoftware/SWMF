b3db63235e3307dbd49235613938d3f11f2282dc
gtoth <>
2010-07-01 16:23:38 +0000

    Import NamePlanet directly from CON_planet (used to be via CON_axes?)
    Former-commit-id: ffc818f64e42c33d178dff7b00182c3efe2c517f

--- CON/Control/src/CON_io.f90
+++ CON/Control/src/CON_io.f90
@@ -14,7 +14,7 @@ module CON_io
   use CON_world
   use CON_comp_param
   use CON_wrapper
-  use CON_planet, ONLY: read_planet_var, check_planet_var
+  use CON_planet, ONLY: NamePlanet, read_planet_var, check_planet_var
   use CON_time
   use CON_axes
   use ModReadParam
