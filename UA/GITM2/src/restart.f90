2c1ef63add25586d2bb14d756aeb561a91f020c3
dpawlows <>
2010-08-18 20:32:37 +0000

    The surface temp needs to be filled on restart for Mars.
    Former-commit-id: ac57ebc2c4b6f251b2539c21dcc6bb00119e6f4b

--- UA/GITM2/src/restart.f90
+++ UA/GITM2/src/restart.f90
@@ -149,6 +149,12 @@ subroutine read_restart(dir)
 
      if (iDebugLevel > 4) write(*,*) "=====> Reading Temperature"
      read(iRestartUnit_)  Temperature(:,:,:,iBlock)
+     
+     if (isMars) then 
+        SurfaceTemp = 0.0
+        SurfaceTemp(:,:,iBlock) = Temperature(1:nLons,:1:nLats,0,iBlock)
+     endif
+
      if (iDebugLevel > 4) write(*,*) "=====> Reading ITemperature"
      read(iRestartUnit_) ITemperature(:,:,:,iBlock)
      if (iDebugLevel > 4) write(*,*) "=====> Reading eTemperature"
