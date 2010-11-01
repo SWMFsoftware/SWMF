2297efb4a18d7c35feaee74068d04a6a1fa89e6a
cvs2git <>
2010-11-01 14:40:30 +0000

    This commit was manufactured by cvs2svn to create tag 'CRASH_v2dot2'.
    Former-commit-id: 88fc8f22ff55fd3cca0a1043c79e343c8d7bd7d1

--- IE/RIM/src/ModSizeRIM.f90
+++
@@ -1,9 +0,0 @@
-
-module ModSizeRIM
-  implicit none
-
-  integer, parameter :: nLats = 91
-  integer, parameter :: nLons = 72
-  integer :: nLonsAll = 0
-
-end module ModSizeRIM
--- PS/DGCPM/src/ModConstants.f90
+++
@@ -1,7 +0,0 @@
-
-module ModConstants
-
-  real, parameter :: pi = 3.1415926
-  real, parameter :: dtor = pi/180.0
-
-end module ModConstants
