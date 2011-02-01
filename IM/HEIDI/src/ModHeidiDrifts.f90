b448b0533ffcfbeb32337e3f6f9fc7cdfd4ea409
rilie <>
2011-02-01 21:48:41 +0000

    Initialize var.
    Former-commit-id: 710d9365e4e35c507c37afa9e2a0a2b1f98d2811

--- IM/HEIDI/src/ModHeidiDrifts.f90
+++ IM/HEIDI/src/ModHeidiDrifts.f90
@@ -3,23 +3,27 @@ Module ModHeidiDrifts
   ! Drift and diffusion variable definition module for the HEIDI program.
   ! Mike Liemohn, March 2006
   !/
-
-	use ModHeidiSize
-
-! Define the atmospheric loss and charge exchange variables
-! Formerly: Common block CE
-	integer:: J6,J18
-	real :: ACHAR(NR,NT,NE,NPA,NS),ATLOS(NR,NT,NE,NPA,NS)
-
-! Define the advection drift rate variables
-! Formerly: Common block CDR
-	real :: VR(NR,NT,NE,NPA),P1(NR,NT),P2(NR,NT,NE,NPA),EDOT(NR,NT,NE,NPA)
-	real :: MUDOT(NR,NT,NE,NPA), VrConv(NR,NT,NE,NPA)
-
-! Define the pitch angle diffusion coefficient variables
-! Formerly: Common block CCOUL
-	real :: COULE(NR,NT,NE,NPA,NS),COULI(NR,NT,NE,NPA,NS)
-	real :: ATAE(NR,NT,NE,NPA,NS),BTAE(NR,NT,NE,NPA,NS),GTAE(NR,NT,NE,NPA+1,NS)
-	real :: ATAI(NR,NT,NE,NPA,NS),BTAI(NR,NT,NE,NPA,NS),GTAI(NR,NT,NE,NPA+1,NS)
-
+  
+  use ModHeidiSize
+  
+  ! Define the atmospheric loss and charge exchange variables
+  integer :: J6 = 0
+  integer :: J18 = 0
+  real    :: ACHAR(NR,NT,NE,NPA,NS)
+  real    :: ATLOS(NR,NT,NE,NPA,NS)
+  
+  ! Define the advection drift rate variables
+  real :: VR(NR,NT,NE,NPA)  ! Radial drift
+  real :: P1(NR,NT) 
+  real :: P2(NR,NT,NE,NPA) = 0.0
+  real :: EDOT(NR,NT,NE,NPA)
+  real :: MUDOT(NR,NT,NE,NPA)
+  real :: VrConv(NR,NT,NE,NPA) ! Convection velocity
+  
+  ! Define the pitch angle diffusion coefficient variables
+  
+  real :: COULE(NR,NT,NE,NPA,NS),COULI(NR,NT,NE,NPA,NS)
+  real :: ATAE(NR,NT,NE,NPA,NS),BTAE(NR,NT,NE,NPA,NS),GTAE(NR,NT,NE,NPA+1,NS)
+  real :: ATAI(NR,NT,NE,NPA,NS),BTAI(NR,NT,NE,NPA,NS),GTAI(NR,NT,NE,NPA+1,NS)
+  
 end Module ModHeidiDrifts
