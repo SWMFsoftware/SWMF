23b1fa7cb7c5544f845b214e7d5c8ce9d63a5e72
cvs2git <>
2011-02-28 22:37:36 +0000

    This commit was manufactured by cvs2svn to create tag 'CRASH_v2dot3'.
    Former-commit-id: f14486664d95ff15d56d30e0c9eafe9485ec6def

--- PW/PWOM/src/ModPwTime.f90
+++
@@ -1,9 +0,0 @@
-Module ModPwTime
-  use ModKind,only:real8_
-  
-  real (kind=real8_)   ::  StartTime,CurrentTime !needed for SWMF stuff
-  integer              ::  iStartTime(7)=(/1976,6,28,0,0,0,0/)
-  integer, parameter   ::  Year_=1, Month_=2, Day_=3, Hour_=4, Minute_=5, &
-                           Second_=6, mSecond_=7
-
-end Module ModPwTime
--- gui/check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#
-# Check the httpd.conf configuration file
-#
-
-`dirname $0`/apache/bin/apachectl configtest
-
