5f87de09783715139ed9872f049137b97748c0bc
gtoth <>
2011-04-04 16:40:12 +0000

    Fixed names and many other issues (including compiler bug) for test_process_var_name Modified Files: 	Makefile Added Files: 	test_process_var_name.f90 test_process_var_name.log Removed Files: 	test_process_var_names.f90 test_process_var_names.log
    Former-commit-id: 787eb34e4e8453cc0f9f288b0a709ebb6c3d63e3

--- share/Library/test/test_process_var_name.f90
+++ share/Library/test/test_process_var_name.f90
@@ -1,4 +1,4 @@
-program process_var_names_test
+program process_var_name_test
 
   ! This test is designed to be perfomed using, in turn, all the ModEquation
   ! files in BATSRUS.
@@ -7,7 +7,7 @@ program process_var_names_test
   ! The test converts the string array NameVar_V, defined in a specific
   ! ModEquation, to the same format it will take in a full SWMF run.
 
-  use ModProcessVarNames, ONLY: process_var_names
+  use ModProcessVarName,  ONLY: process_var_name
   use ModVarIndexes,      ONLY: NameVar_V
   use ModUtilities,       ONLY: lower_case
   implicit none
@@ -19,20 +19,21 @@ program process_var_names_test
   ! Replace '?' characters with numbers (for waves/materials)
   call set_namevar 
  
-  ! Fix string array to comply with usual input to process_var_names.
+  ! Fix string array to comply with usual input to process_var_name.
   ! String length should be fixed(=15) regardless of length in ModEquation.
   ! All characters should be lower case.
   allocate(NameVarFixed_V(nVar))
-  NameVarFixed_V = NameVar_V
   do iVar = 1, nVar
+     NameVarFixed_V(iVar) = NameVar_V(iVar)
      call lower_case(NameVarFixed_V(iVar))
   end do
 
-  call process_var_names(nVar, NameVarFixed_V, nDensity, nSpeed)
-  print*, 'Original  Standerdized  nDensity=',nDensity,'nSpeed=',nSpeed
-  print*, '--------  ----------- '
+  call process_var_name(nVar, NameVarFixed_V, nDensity, nSpeed)
+
+  write(*,*) 'Original  Standardized  nDensity=',nDensity,'nSpeed=',nSpeed
+  write(*,*) '--------  ------------'
   do iVar = 1, nVar
-     print*,  NameVar_V(iVar),'         ',trim(NameVarFixed_V(iVar))
+     write(*,*) NameVar_V(iVar),'         ',trim(NameVarFixed_V(iVar))
   end do
 
   deallocate(NameVarFixed_V)
@@ -68,7 +69,7 @@ contains
 
   end subroutine set_namevar
 
-end program process_var_names_test
+end program process_var_name_test
 ! =================================================================
 subroutine CON_stop(String)
   implicit none
