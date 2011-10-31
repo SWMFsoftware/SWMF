85bde669e613be2d976a2102f511e2c56e989d24
vtenishe <>
2011-10-31 21:28:14 +0000

    Initial revision
    Former-commit-id: ff28b4e44d345b38948e9c697d75ffb735a31b17

---
+++ PT/AMPS/src/pic/pic_internal_boundary.cpp
@@ -0,0 +1,4 @@
+//====================================================
+//$Id$
+//====================================================
+//the general functions and variables used for internal boundaries installed into the mesh
