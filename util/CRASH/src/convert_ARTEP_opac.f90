7e373c2fa4638e710b294ef414e7ff6e42c9adae
igorsok <>
2011-12-18 16:13:26 +0000

    Updated version (for Xenon). The bug in calculating the logarathm of min/max argements is fixed.
    Former-commit-id: 1197f0376c1ebf5add41f680d793968c76173d55

--- util/CRASH/src/convert_ARTEP_opac.f90
+++ util/CRASH/src/convert_ARTEP_opac.f90
@@ -10,13 +10,13 @@ contains
     real:: Aux_V(7), Aux
     !---------------
 
-    allocate(Value_VII(60,164,185))
+    allocate(Value_VII(60,201,201))
 
     iFile = io_unit_new()
-    open(iFile, file='Ay_opac_ARTEP.dat', form='formatted',status='old')
+    open(iFile, file='Xe_opac_ARTEP.dat', form='formatted',status='old')
     line=0
-    do iTe = 1, 185
-       do iRho = 1,164
+    do iTe = 1, 201
+       do iRho = 1,201
           line = line+1;write(*,*)line
           read(iFile,*)Aux_V,Value_VII(1:30, iRho, iTe), Aux, Value_VII(31:60, iRho, iTe)
        end do
@@ -34,19 +34,22 @@ program save_eos_table
   use CRASH_ModAtomicDataMix
   use CRASH_ModExcitationData,ONLY : n_ground, cExcitationN_III
   use CRASH_ModIonMix
-  use CRASH_ModEos, ONLY:UseEosTable_I, check_eos_table
   use ModReadArtep
   use ModPlotFile
   use ModMpi
+  use CRASH_ModAtomicMass,ONLY: cAtomicMass_I
+  use ModConst
   implicit none
   integer:: iError, iMaterial
+  real:: AtomicMass
   !----------------
   call read_artep
+  AtomicMass = cAtomicMass_I(54) * cAtomicMass
   Value_VII = 0.10 * Value_VII  !cm^2/g = 1e-4 m^2/(1e-3 kg) = 0.1 m2/kg
   call save_plot_file( &
-         'Ay_opac.dat',                                &
+         'Xe_opac.dat',                                &
          TypeFileIn     = 'real8',                     &
-         StringHeaderIn = 'ARTEP Opacity for Acrylic', &
+         StringHeaderIn = 'ARTEP Opacity for Xenon', &
          NameVarIn      = 'logRho logTe Planck01 Planck02 Planck03 Planck04 Planck05'//&
                                       ' Planck06 Planck07 Planck08 Planck09 Planck10'//&
                                       ' Planck11 Planck12 Planck13 Planck14 Planck15'//&
@@ -59,8 +62,8 @@ program save_eos_table
                                       ' Ross16 Ross17 Ross18 Ross19 Ross20'//&
                                       ' Ross21 Ross22 Ross23 Ross24 Ross25'//&
                                       ' Ross26 Ross27 Ross28 Ross29 Ross30', &
-         CoordMinIn_D   = (/1.136e-1, 2.002/),             &                             
-         CoordMaxIn_D   = (/6.151e+4 ,1.0e4/),             &
+         CoordMinIn_D   = (/log10(AtomicMass*1.0e+24), log10(0.030)/),       &                             
+         CoordMaxIn_D   = (/log10(AtomicMass*1.0e+29),          3.0/),       &
          VarIn_VII      = Value_VII)
   stop
 end program save_eos_table
