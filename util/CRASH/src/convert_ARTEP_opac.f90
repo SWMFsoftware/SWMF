95bdc7e2480b3494a28350e999e49a4234f18d3b
gtoth <>
2012-01-07 16:35:26 +0000

    Some improvements.
    Former-commit-id: c897f5fee20f34639784f2ddf9a62252d9e43cff

--- util/CRASH/src/convert_ARTEP_opac.f90
+++ util/CRASH/src/convert_ARTEP_opac.f90
@@ -1,30 +1,38 @@
-Module ModReadArtep
+module ModReadArtep
   use ModIoUnit, ONLY: io_unit_new
   implicit none
+
+  integer, parameter:: nRho = 201, nTe = 201, nGroup = 30
+
   SAVE
   real, allocatable:: Value_VII(:,:,:) 
   integer::line
 contains
+  !==================================================================================
   subroutine read_artep
     integer:: iFile, iRho, iTe
     real:: Aux_V(7), Aux
-    !---------------
+    !--------------------------------------------------------------------------------
 
-    allocate(Value_VII(60,201,201))
+    allocate(Value_VII(2*nGroup,nRho,nTe))
 
     iFile = io_unit_new()
     open(iFile, file='xz_opac.artep', form='formatted',status='old')
     line=0
-    do iTe = 1, 201
-       do iRho = 1,201
+    do iTe = 1, nTe
+       do iRho = 1, nRho
           line = line+1;write(*,*)line
-          read(iFile,*)Aux_V,Value_VII(1:30, iRho, iTe), Aux, Value_VII(31:60, iRho, iTe)
+          read(iFile,*) Aux_V, Value_VII(1:nGroup,iRho,iTe), &
+               Aux,            Value_VII(nGroup+1:2*nGroup,iRho,iTe)
        end do
     end do
     close(iFile)
   end subroutine read_artep
+
 end Module ModReadArtep
+!====================================================================================
 program save_eos_table
+
   use CRASH_ModStatSum
   use CRASH_ModPartition, ONLY: UseCoulombCorrection
   use CRASH_ModPolyimide
@@ -42,33 +50,22 @@ program save_eos_table
   implicit none
   integer:: iError, iMaterial
   real:: AtomicMass
-  !----------------
+  !----------------------------------------------------------------------------------
   call read_artep
   AtomicMass = cAtomicMass_I(54) * cAtomicMass
   Value_VII = 0.10 * Value_VII  !cm^2/g = 1e-4 m^2/(1e-3 kg) = 0.1 m2/kg
   call save_plot_file( &
-         'Xe_opac.dat',                                &
-         TypeFileIn     = 'real8',                     &
-         StringHeaderIn = 'ARTEP Opacity for Xenon', &
-         NameVarIn      = 'logRho logTe Planck01 Planck02 Planck03 Planck04 Planck05'//&
-                                      ' Planck06 Planck07 Planck08 Planck09 Planck10'//&
-                                      ' Planck11 Planck12 Planck13 Planck14 Planck15'//&
-                                      ' Planck16 Planck17 Planck18 Planck19 Planck20'//&
-                                      ' Planck21 Planck22 Planck23 Planck24 Planck25'//&
-                                      ' Planck26 Planck27 Planck28 Planck29 Planck30'//&
-                                      ' Ross01 Ross02 Ross03 Ross04 Ross05'//&
-                                      ' Ross06 Ross07 Ross08 Ross09 Ross10'//&
-                                      ' Ross11 Ross12 Ross13 Ross14 Ross15'//&
-                                      ' Ross16 Ross17 Ross18 Ross19 Ross20'//&
-                                      ' Ross21 Ross22 Ross23 Ross24 Ross25'//&
-                                      ' Ross26 Ross27 Ross28 Ross29 Ross30 EvMin EvMax', &
-         CoordMinIn_D   = (/log10(AtomicMass*1.0e+24), log10(0.030)/),       &                             
-         CoordMaxIn_D   = (/log10(AtomicMass*1.0e+29),          3.0/),       &
-         ParamIn_I      = (/0.1,2.0e+4/),                                    &
+         'Xe_opac.dat',                                                      &
+         TypeFileIn     = 'real8',                                           &
+         StringHeaderIn = 'ARTEP Opacity for Xenon',                         &
+         NameVarIn      = 'logRho logTe Planck(30) Ross(30) EvMin EvMax',    &
+         CoordMinIn_D   = (/log10(AtomicMass*1.0e+24), log10(0.03)/),        &
+         CoordMaxIn_D   = (/log10(AtomicMass*1.0e+29),       3.0  /),        &
+         ParamIn_I      = (/0.1, 2e4/),                                      &
          VarIn_VII      = Value_VII)
-  stop
+
 end program save_eos_table
-!============================================================================
+!====================================================================================
 ! The following subroutines are here so that we can use SWMF library routines
 ! Also some features available in SWMF mode only require empty subroutines
 ! for compilation of the stand alone code.
