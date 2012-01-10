af28250ea6b2341730d8c6b4a29a61aa919d2fbf
cvs2git <>
2012-01-10 22:58:16 +0000

    This commit was manufactured by cvs2svn to create tag 'CRASH_v3dot1'.
    Former-commit-id: 0347fed5d2c57245bf534863ff28ed2f8f2f40a3

--- gui/htdocs/site_header_empty.php
+++
@@ -1,7 +0,0 @@
-<?php echo "<!-- BEGIN " . basename(__FILE__) . " -->\n"; ?>
-
-<?php require("config.php"); ?>
-
-<link rel="stylesheet" type="text/css" href="/style.css" title="Blue Theme" />
-
-<?php echo "<!-- END " . basename(__FILE__) . " -->\n"; ?>
