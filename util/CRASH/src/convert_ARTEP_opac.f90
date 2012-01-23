298cd30da9d1fe34a39133f0b2426885c2784dbd
igorsok <>
2012-01-23 07:13:47 +0000

    Add the header file output
    Former-commit-id: 7460ac54f48ce7a71bae65328b6f7e14089eca2d

--- util/CRASH/src/convert_ARTEP_opac.f90
+++ util/CRASH/src/convert_ARTEP_opac.f90
@@ -17,8 +17,8 @@ contains
     real:: Aux_V(7), Aux
     !-----------------------------------------
     iFile = io_unit_new()
-    open(iFile, file='header.C5O2H8',status = 'old')
-    do line = 1,17
+    open(iFile, file='header.x4',status = 'old')
+    do line = 1,13
        read(iFile,*)
     end do
     read(iFile,*)NameEmpty, TMin, TMax, nTe
@@ -27,8 +27,8 @@ contains
     write(*,*)'NameEmpty, RhoMin, RhoMax, nRho=', &
          NameEmpty, RhoMin, RhoMax, nRho
     read(iFile,*)
-    !read(iFile,*)
-    !read(iFile,*)
+    read(iFile,*)
+    read(iFile,*)
     read(iFile,*)NameEmpty,NameEmpty,NameEmpty,nGroup
     write(*,*)'nGroup=', nGroup
     allocate(Value_VII(2*nGroup,nRho,nTe), EGroup_I(1+nGroup))
@@ -38,7 +38,7 @@ contains
     write(*,*)EGroup_I
 
     iFile = io_unit_new()
-    open(iFile, file='opa.C5O2H8', form='formatted',status='old')
+    open(iFile, file='x4_opac.artep.rev', form='formatted',status='old')
     line=0
     do iTe = 1, nTe
        do iRho = 1, nRho
@@ -69,19 +69,54 @@ program save_eos_table
   use ModMpi
   use CRASH_ModAtomicMass,ONLY: cAtomicMass_I
   use ModConst
+  use ModIoUnit
   implicit none
-  !----------------------------------------------------------------------------
+
+  integer:: iError, iMaterial, iFile
+  real:: AtomicMass
+  !----------------------------------------------------------------------------------
+
   call read_artep
   Value_VII = 0.10 * Value_VII  !cm^2/g = 1e-4 m^2/(1e-3 kg) = 0.1 m2/kg
+  RhoMin = 1.0e6 * RhoMin * cAtomicMass * cAtomicMass_I(54) !1/cm3=>kg/m3
+  RhoMax = 1.0e6 * RhoMax * cAtomicMass * cAtomicMass_I(54) !1/cm3=>kg/m3
   call save_plot_file( &
-         'Ay_opac.dat',                                                      &
+         'Xe_opac.dat',                                                      &
          TypeFileIn     = 'real8',                                           &
-         StringHeaderIn = 'ARTEP Opacity for Ay',                            &
-         NameVarIn      = 'logRho logTe Planck(30) Ross(30) Ev(31)',         &
-         CoordMinIn_D   = (/log10(1.0e3*RhoMin), log10(TMin)/),              &
-         CoordMaxIn_D   = (/log10(1.0e3*RhoMax), log10(TMax)/),              &
-         ParamIn_I      = EGroup_I,                                          &
+         StringHeaderIn = 'ARTEP Opacity for Xe',                            &
+         NameVarIn      = 'logRho logTe Planck(44) Ross(44) EGropu00 EGroup(44)',&
+         CoordMinIn_D   = (/log10(RhoMin), log10(TMin)/),        &
+         CoordMaxIn_D   = (/log10(RhoMax), log10(TMax)/),        &
+         ParamIn_I      = EGroup_I,                                     &
          VarIn_VII      = Value_VII)
+  iFile = io_unit_new()
+  open(iFile,file='Xe_opac_ARTEP44.head')
+       write(iFile,'(a)')'  '
+       write(iFile,'(a)')'----------------OPACITY TABLE----------'
+       write(iFile,'(a)')'  '
+       write(iFile,'(a)')'#LOOKUPTABLE'
+       write(iFile,'(a)')'Xe_opac                 NameTable'
+       write(iFile,'(a)')'use param               NameCommand'
+       write(iFile,'(a)')'Tables/Xe_opac_ARTEP44.dat'
+       write(iFile,'(a)')'real8                   TypeFile'
+       write(iFile,'(a)')'EGroup00 EGroup(44)'
+       write(iFile,'(e13.7)')EGroup_I
+       write(iFile,'(a)')'Opacity(rho,Te) for Xe'
+       write(iFile,'(a)')'logRho LogT Planck(44) Ross(44)'
+       write(iFile,'(i4,a)')nRho,'                    nIndex1'
+       write(iFile,'(e13.7,a)')RhoMin,&
+            '           Index1Min (kg/m3)'
+       write(iFile,'(e13.7,a)')RhoMax,& 
+            '           Index1Max (kg/m3)'
+       write(iFile,'(i4,a)')nTe,'                    nIndex2'
+       write(iFile,'(e13.7,a)')TMin,&
+            '           Index2Min (eV)'
+       write(iFile,'(e13.7,a)')TMax,&
+            '           Index2Max (eV)'
+       write(iFile,'(a)')'  '
+       write(iFile,'(a)')'#END'
+       write(iFile,'(a)')'  '
+       close(iFile)
 
 end program save_eos_table
 !============================================================================
