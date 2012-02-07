aeb884036d958e6389576c25a0db226e3766ab15
pauldast <>
2012-02-07 09:29:24 +0000

    added some more generic quick sort routines
    Former-commit-id: 9ec96092aad29ac919f951c902b5eaf4214218bd

--- share/Library/src/ModSort.f90
+++ share/Library/src/ModSort.f90
@@ -8,6 +8,9 @@ module ModSort
   public :: sort_sum   ! add up numbers but first sort them by magnitude
   public :: sort_test  ! unit test
 
+  public :: qsort_integer_2D
+  public :: qsort_real_2D
+
 contains
 
   subroutine sort_quick(n, arr, indx)
@@ -124,8 +127,197 @@ contains
 
   end function sort_sum
 
-  !==========================================================================
+!=======================================================================
 
+  !======================================================================
+
+  subroutine qsort_integer_2D(n, d, arr, testFunc, indx)
+
+    ! Quick sort algorithm: sorts indx array according to 'arr' using
+    ! user defined function 'func'
+    ! 'func(a,b)' is any function that returns -1 for a < b, 0 for a = b
+    ! or 1 for a > b
+
+    ! Based on the F77 code indexx from Numerical Recipes
+    integer, external :: testFunc
+    integer, intent(in)  :: n, d
+
+    integer, intent(in)     :: arr(d,n)
+    integer, intent(out) :: indx(n)
+
+    integer, parameter   :: M=7, NSTACK=1000 ! should be > 2*log_2(n)
+
+    integer :: i,indxt,ir,itemp,j,jstack,k,l, iStack(NSTACK)
+    integer    :: a(d)
+
+    !-------------------------------------------------------------------
+    do j=1,n
+       indx(j)=j
+    end do
+    jstack=0
+    l=1
+    ir=n
+1   if(ir-l < M)then
+       do j=l+1,ir
+          indxt=indx(j)
+          a=arr(:,indxt)
+          do i=j-1,1,-1
+             if(testFunc(arr(:,indx(i)), a) .le. 0)goto 2
+             indx(i+1)=indx(i)
+          end do
+          i=0
+2         indx(i+1)=indxt
+       end do
+       if(jstack.eq.0)return
+       ir=istack(jstack)
+       l=istack(jstack-1)
+       jstack=jstack-2
+    else
+       k=(l+ir)/2
+       itemp=indx(k)
+       indx(k)=indx(l+1)
+       indx(l+1)=itemp
+       if(testFunc(arr(:,indx(l+1)),arr(:,indx(ir))) == 1)then
+          itemp=indx(l+1)
+          indx(l+1)=indx(ir)
+          indx(ir)=itemp
+       endif
+       if(testFunc(arr(:,indx(l)), arr(:,indx(ir))) == 1) then
+          itemp=indx(l)
+          indx(l)=indx(ir)
+          indx(ir)=itemp
+       endif
+       if(testFunc(arr(:,indx(l+1)), arr(:,indx(l))) == 1)then
+          itemp=indx(l+1)
+          indx(l+1)=indx(l)
+          indx(l)=itemp
+       endif
+       i=l+1
+       j=ir
+       indxt=indx(l)
+       a=arr(:,indxt)
+3      continue
+       i=i+1
+       if(testFunc(arr(:,indx(i)), a) == -1)goto 3
+4      continue
+       j=j-1
+       if(testFunc(arr(:,indx(j)), a) == 1)goto 4
+       if(j < i)goto 5
+       itemp=indx(i)
+       indx(i)=indx(j)
+       indx(j)=itemp
+       goto 3
+5      indx(l)=indx(j)
+       indx(j)=indxt
+       jstack=jstack+2
+       if(ir-i+1.ge.j-l)then
+          istack(jstack)=ir
+          istack(jstack-1)=i
+          ir=j-1
+       else
+          istack(jstack)=j-1
+          istack(jstack-1)=l
+          l=i
+       endif
+    endif
+    goto 1
+
+  end subroutine qsort_integer_2D
+
+  !=====================================================================
+  subroutine qsort_real_2D(n, d, arr, testFunc, indx)
+
+    ! Quick sort algorithm: sorts indx array according to 'arr' using
+    ! user defined function 'func'
+    ! 'func(a,b)' is any function that returns -1 for a < b, 0 for a = b
+    ! or 1 for a > b
+
+    ! Based on the F77 code indexx from Numerical Recipes
+    integer, external :: testFunc
+    integer, intent(in)  :: n, d
+
+    real, intent(in)     :: arr(d,n)
+    integer, intent(out) :: indx(n)
+
+    integer, parameter   :: M=7, NSTACK=1000 ! should be > 2*log_2(n)
+
+    integer :: i,indxt,ir,itemp,j,jstack,k,l, iStack(NSTACK)
+    real    :: a(d)
+
+    !-------------------------------------------------------------------
+    do j=1,n
+       indx(j)=j
+    end do
+    jstack=0
+    l=1
+    ir=n
+1   if(ir-l < M)then
+       do j=l+1,ir
+          indxt=indx(j)
+          a=arr(:,indxt)
+          do i=j-1,1,-1
+             if(testFunc(arr(:,indx(i)), a) .le. 0)goto 2
+             indx(i+1)=indx(i)
+          end do
+          i=0
+2         indx(i+1)=indxt
+       end do
+       if(jstack.eq.0)return
+       ir=istack(jstack)
+       l=istack(jstack-1)
+       jstack=jstack-2
+    else
+       k=(l+ir)/2
+       itemp=indx(k)
+       indx(k)=indx(l+1)
+       indx(l+1)=itemp
+       if(testFunc(arr(:,indx(l+1)),arr(:,indx(ir))) == 1)then
+          itemp=indx(l+1)
+          indx(l+1)=indx(ir)
+          indx(ir)=itemp
+       endif
+       if(testFunc(arr(:,indx(l)), arr(:,indx(ir))) == 1) then
+          itemp=indx(l)
+          indx(l)=indx(ir)
+          indx(ir)=itemp
+       endif
+       if(testFunc(arr(:,indx(l+1)), arr(:,indx(l))) == 1)then
+          itemp=indx(l+1)
+          indx(l+1)=indx(l)
+          indx(l)=itemp
+       endif
+       i=l+1
+       j=ir
+       indxt=indx(l)
+       a=arr(:,indxt)
+3      continue
+       i=i+1
+       if(testFunc(arr(:,indx(i)), a) == -1)goto 3
+4      continue
+       j=j-1
+       if(testFunc(arr(:,indx(j)), a) == 1)goto 4
+       if(j < i)goto 5
+       itemp=indx(i)
+       indx(i)=indx(j)
+       indx(j)=itemp
+       goto 3
+5      indx(l)=indx(j)
+       indx(j)=indxt
+       jstack=jstack+2
+       if(ir-i+1.ge.j-l)then
+          istack(jstack)=ir
+          istack(jstack-1)=i
+          ir=j-1
+       else
+          istack(jstack)=j-1
+          istack(jstack-1)=l
+          l=i
+       endif
+    endif
+    goto 1
+
+  end subroutine qsort_real_2D
+!======================================================================
   subroutine sort_test
 
     integer, parameter :: n=5
