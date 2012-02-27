557d490f8ad85b27b1e1ef9d1abe1929780cd37d
ridley <>
2012-02-27 00:51:33 +0000

    I am adding .1 to the version number.
    Former-commit-id: e6c1985a0c3a6b8da202dcfdf8fc42d8d14c4da3

--- UA/GITM2/src/output_common.f90
+++ UA/GITM2/src/output_common.f90
@@ -712,7 +712,7 @@ contains
   subroutine write_head_version
 
     write(iOutputUnit_,*) "VERSION"
-    write(iOutputUnit_,*) 3.0+PlanetNum
+    write(iOutputUnit_,*) 3.1+PlanetNum
     write(iOutputUnit_,*) ""
 
   end subroutine write_head_version
