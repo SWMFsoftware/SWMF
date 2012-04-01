ef987f34f5899c3d1e70d4a76bbea7e7ea56c8ba
ridley <>
2012-04-01 19:58:16 +0000

    added the ability to enter the background file.
    Former-commit-id: d6345e952d2478ffd87cb7466c5074cf484142da

--- UA/GITM2/srcPython/gitm_diff_images.py
+++ UA/GITM2/srcPython/gitm_diff_images.py
@@ -9,7 +9,7 @@ using syntax like,
 That's how most pybats stuff works right now.
 '''
 
-def quickplot(infile, outfile, min, max):
+def quickplot(infile, outfile, min, max, backfile):
     '''
     Takes file name, infile, and creates a PNG plot thing.  Yeah.
     '''
@@ -21,35 +21,33 @@ def quickplot(infile, outfile, min, max):
     from matplotlib.cm import get_cmap
     from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
 
-    # Open file.
-    a=gitm.GitmBin(infile)
-    base = gitm.GitmBin('3DALL_t020321_120100.bin')
-
-    # Make contour of rho at lowest altitude (index 0).
     # Convert lat lon from rad to degrees.
     p=180.0/np.pi
     
     f=plt.figure(figsize=(6,7)) #make a fig.
     ax=f.add_subplot(111) #make an ax.
 
-    # Create the contour for an altitude slice and call it 'cnt'.
-    # The '61' is the number of contours; you could use a vector of values to set
-    # levels manually if you wish.  get_cmap accepts any of the color map names
-    # from the colormap demo pic from the Matplotlib gallery; adding '_r' 
-    # reverses the colormap.
+    # Open file.
+    a=gitm.GitmBin(infile)
+
     t = a['Temperature'][:,:,:]
     sz = t.shape
     c = sz[0]/2.0
-    diff = 100*(a['Rho'][c,:,:] - base['Rho'][c,:,:])/base['Rho'][c,:,:]
 
-    diff[diff>max] = max
-    diff[diff<min] = min
+    IsNotDiff = re.match(r'none',backfile)
+    if IsNotDiff:
+        diff = np.log10(a['Rho'][c,:,:])
+    else:
+        base = gitm.GitmBin(backfile)
+        diff = 100*(a['Rho'][c,:,:] - base['Rho'][c,:,:])/base['Rho'][c,:,:]
+        diff[diff>max] = max
+        diff[diff<min] = min
 
-#    diff = 100*(a['Temperature'][c,:,:] - base['Temperature'][c,:,:])/base['Temperature'][c,:,:]
-#    lev = (np.arange(60)-30.0)
-
-#    diff = np.log10(a['Rho'][c,:,:])
-#    diff = a['e-'][c,:,:]
+    # Create the contour for an altitude slice and call it 'cnt'.
+    # The 61 is the number of contours; you could use a vector of values to set
+    # levels manually if you wish.  get_cmap accepts any of the color map names
+    # from the colormap demo pic from the Matplotlib gallery; adding '_r' 
+    # reverses the colormap.
 
     u = a['V!Dn!N (north)'][c,:,:]
     v = a['V!Dn!N (up)'][c,:,:]
@@ -64,18 +62,16 @@ def quickplot(infile, outfile, min, max):
                         levels=lev)
     else:
         cnt=ax.contourf(x,y,
-                        diff, 61, cmap=get_cmap('RdBu_r')) #,
-#        cnt=ax.contourf(a['Latitude'][c,:,:]*p,
-#                        a['Altitude'][c,:,:]/1000.0,
-#                        diff, 61, cmap=get_cmap('RdBu_r')) #,
-#                    levels=lev)
-#                    diff, 61, cmap=get_cmap('BrBG'),
-#                    np.log10(a['Rho'][5,:,:]), 61, cmap=get_cmap('Spectral_r'))
+                        diff, 61, cmap=get_cmap('RdBu_r'))
 
     # Configure axis.
     ax.set_xlabel('Latitude (in km)')
     ax.set_ylabel('Altitude (km)')
-    ax.set_title(r'Pecent Diff of Rho at longitude=%5.2f$^{\circ}$' % (a['Longitude'][c,0,0]*p))
+    if IsNotDiff:
+        ax.set_title(r'Rho at longitude=%5.2f$^{\circ}$' % (a['Longitude'][c,0,0]*p))
+    else:
+        ax.set_title(r'Pecent Diff of Rho at longitude=%5.2f$^{\circ}$' % (a['Longitude'][c,0,0]*p))
+
     f.suptitle('File=%s'%(a.attrs['file']))
     
     q = plt.quiver(x,y,u,v,angles='xy',scale=5000,color='b')
@@ -110,26 +106,35 @@ import re
 min = -1.0e32
 max =  1.0e32
 
+backfile = 'none'
 outfile = 'image'
 
 for arg in sys.argv:
+
     matchObj = re.match(r'-min=(.*)',arg)
     if matchObj:
         min=float(matchObj.group(1))
         print 'min set to ',repr(min)
+
     matchObj = re.match(r'-max=(.*)',arg)
     if matchObj:
         max=float(matchObj.group(1))
         print 'max set to ',repr(max)
+
     matchObj = re.match(r'-out=(.*)',arg)
     if matchObj:
         outfile=matchObj.group(1)
         print 'output file set to ',outfile
+
+    matchObj = re.match(r'-back=(.*)',arg)
+    if matchObj:
+        backfile=matchObj.group(1)
+        print 'background file set to ',backfile
 
 i = 0
 for f in glob('3DALL*.bin'):
     s = repr(i).zfill(4)
     o = outfile+'.{}'.format(s)
     print f
-    quickplot(f, o, min, max)
+    quickplot(f, o, min, max, backfile)
     i=i+1
