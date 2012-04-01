dc8d54700cab2548be8d95cbfd1e8d42da23cda6
ridley <>
2012-04-01 14:59:32 +0000

    Updated python script
    Former-commit-id: 2471303cea7259aed33f13d6cc4e3b07f0445396

--- UA/GITM2/srcPython/gitm_diff_images.py
+++ UA/GITM2/srcPython/gitm_diff_images.py
@@ -9,7 +9,7 @@ using syntax like,
 That's how most pybats stuff works right now.
 '''
 
-def quickplot(infile, outfile):
+def quickplot(infile, outfile, min, max):
     '''
     Takes file name, infile, and creates a PNG plot thing.  Yeah.
     '''
@@ -23,33 +23,63 @@ def quickplot(infile, outfile):
 
     # Open file.
     a=gitm.GitmBin(infile)
-    base = gitm.GitmBin('3DALL_t020321_120010.bin')
+    base = gitm.GitmBin('3DALL_t020321_120100.bin')
 
     # Make contour of rho at lowest altitude (index 0).
     # Convert lat lon from rad to degrees.
     p=180.0/np.pi
     
-    f=plt.figure(figsize=(5,7)) #make a fig.
+    f=plt.figure(figsize=(6,7)) #make a fig.
     ax=f.add_subplot(111) #make an ax.
 
-    # Create the contour for an altitude slice and call it 'cnt' (no jokes, please.)
+    # Create the contour for an altitude slice and call it 'cnt'.
     # The '61' is the number of contours; you could use a vector of values to set
     # levels manually if you wish.  get_cmap accepts any of the color map names
     # from the colormap demo pic from the Matplotlib gallery; adding '_r' 
     # reverses the colormap.
-    diff = (a['Rho'][5,:,:] - base['Rho'][5,:,:])/base['Rho'][5,:,:]
+    t = a['Temperature'][:,:,:]
+    sz = t.shape
+    c = sz[0]/2.0
+    diff = 100*(a['Rho'][c,:,:] - base['Rho'][c,:,:])/base['Rho'][c,:,:]
 
-    cnt=ax.contourf(a['Latitude'][5,:,:]*p,
-                    a['Altitude'][5,:,:],
-                    diff, 61, cmap=get_cmap('Spectral_r'))
+    diff[diff>max] = max
+    diff[diff<min] = min
+
+#    diff = 100*(a['Temperature'][c,:,:] - base['Temperature'][c,:,:])/base['Temperature'][c,:,:]
+#    lev = (np.arange(60)-30.0)
+
+#    diff = np.log10(a['Rho'][c,:,:])
+#    diff = a['e-'][c,:,:]
+
+    u = a['V!Dn!N (north)'][c,:,:]
+    v = a['V!Dn!N (up)'][c,:,:]
+
+    x = a['Latitude'][c,:,:]*p * 111.0
+    y = a['Altitude'][c,:,:]/1000.0
+
+    if (min > -1.0e31 and max < 1.0e32):
+        lev = (np.arange(61))/60.0 * (max-min) + min
+        cnt=ax.contourf(x,y,
+                        diff, 61, cmap=get_cmap('RdBu_r'),
+                        levels=lev)
+    else:
+        cnt=ax.contourf(x,y,
+                        diff, 61, cmap=get_cmap('RdBu_r')) #,
+#        cnt=ax.contourf(a['Latitude'][c,:,:]*p,
+#                        a['Altitude'][c,:,:]/1000.0,
+#                        diff, 61, cmap=get_cmap('RdBu_r')) #,
+#                    levels=lev)
+#                    diff, 61, cmap=get_cmap('BrBG'),
 #                    np.log10(a['Rho'][5,:,:]), 61, cmap=get_cmap('Spectral_r'))
 
     # Configure axis.
-    ax.set_xlabel('Longitude')
-    ax.set_ylabel('Latitude')
-    ax.set_title(r'$\rho$ at Altitude=%5.2f$km$' % (a['Altitude'][0,0,0]/1000.0))
+    ax.set_xlabel('Latitude (in km)')
+    ax.set_ylabel('Altitude (km)')
+    ax.set_title(r'Pecent Diff of Rho at longitude=%5.2f$^{\circ}$' % (a['Longitude'][c,0,0]*p))
     f.suptitle('File=%s'%(a.attrs['file']))
     
+    q = plt.quiver(x,y,u,v,angles='xy',scale=5000,color='b')
+
     # Add a colorbar and set the tick format to exponential notation.
     cb=plt.colorbar(cnt)
     cb.formatter=FormatStrFormatter('%7.2E')
@@ -74,9 +104,32 @@ def quickplot(infile, outfile):
     plt.close(f)
 
 from glob import glob
+import sys
+import re
+
+min = -1.0e32
+max =  1.0e32
+
+outfile = 'image'
+
+for arg in sys.argv:
+    matchObj = re.match(r'-min=(.*)',arg)
+    if matchObj:
+        min=float(matchObj.group(1))
+        print 'min set to ',repr(min)
+    matchObj = re.match(r'-max=(.*)',arg)
+    if matchObj:
+        max=float(matchObj.group(1))
+        print 'max set to ',repr(max)
+    matchObj = re.match(r'-out=(.*)',arg)
+    if matchObj:
+        outfile=matchObj.group(1)
+        print 'output file set to ',outfile
+
 i = 0
 for f in glob('3DALL*.bin'):
     s = repr(i).zfill(4)
-    o = 'image.{}'.format(s)
-    quickplot(f, o)
+    o = outfile+'.{}'.format(s)
+    print f
+    quickplot(f, o, min, max)
     i=i+1
