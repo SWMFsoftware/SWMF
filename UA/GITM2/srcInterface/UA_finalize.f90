e692bf4f2d984b71c445facda2026947f8617d60
cvs2git <>
2012-04-22 09:36:05 +0000

    This commit was manufactured by cvs2svn to create tag 'EGU-2012-Mercury'.
    Former-commit-id: 65407ff518fc8f7cd8a0dd44b37291f8802a299e

--- UA/GITM2/srcInterface/UA_finalize.f90
+++
@@ -1,8 +0,0 @@
-
-subroutine UA_finalize
-
-  implicit none
-
-  call finalize_gitm
-
-end subroutine UA_finalize
--- gui/htdocs/plot_results/paths.php
+++
@@ -1,7 +0,0 @@
-<?php    // Set posted value
-$tecplot = "/usr/local/bin/tecplot";
-$pltview = "/usr/local/bin/pltview";
-$gs = "/sw/bin/gs";
-$convert = "/sw/bin/convert";
-
-?>
