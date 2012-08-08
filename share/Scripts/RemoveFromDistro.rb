e8d863273329852714cdca50e3f6919e44e08791
daldorff <>
2012-08-08 18:48:55 +0000

    Fix problem with not installed and some coments an fixes
    Former-commit-id: 0436129a2050be48198e304d9971f4ac4e8d76b2

--- share/Scripts/RemoveFromDistro.rb
+++ share/Scripts/RemoveFromDistro.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/ruby
 
+#
+# The script remove files containing the header :
+# !#NOTPUBLIC  email:<email address>  expires:<month>/<day>/<year>
+# from the repository if has not expierd or are used by core part
+# of the code (defined as used by make test). A woring is printed if
+# the expiering date is closer then NdaysWarning
+#
+
 require 'date'
 require 'Open3'
 
@@ -9,15 +17,34 @@ def colorize(text, color_code)
 	  "\e[#{color_code}m#{text}\e[0m"
 end
 
+def runcode(cmd)
+	stdin, stdout, stderr, wait_thr = Open3.popen3(cmd)
+	txt = stdout.read
+	stdin.close 
+	stdout.close
+	stderr.close
+	return txt
+end
+
 def red(text); colorize(text, 31); end
 def green(text); colorize(text, 32); end
 
-stdin, stdout, stderr, wait_thr = Open3.popen3("make -n test | fgrep Config.pl")
+# If code is not installed we will install it to get the 
+# proper configurations used by make test
+installed = !`./Config.pl -show`.include?("is not installed")
 
-maketest = stdout.read
-stdin.close  # stdin, stdout and stderr should be closed explicitly in this form.
-stdout.close
-stderr.close
+if !installed
+	out = runcode("./Config.pl -install")
+	#puts("Installing the code \n")
+end
+
+#Get the makefile comands run when runing test
+maketest = runcode("make -n test | fgrep Config.pl")
+
+if !installed
+	out = runcode("./Config.pl -uninstall")
+	#puts("Uninstalling the code \n")
+end
 
 #puts(" maketest = #{maketest}\n\n")
 
@@ -34,14 +61,16 @@ dirs.each{ |dir|
 
 	# If we have expier date and email address give 
 	if ids.length==1
-		puts("WARNING file #{filename} has no contact persion or expiration date.\n")
+		puts(red("WARNING file #{filename} has no contact persion or expiration date.\n"))
         else 
         	contactname = ids[1].split(":").last
 	        enddate = ids[2].split(":").last.split("/").map{|a| a.to_i}
+                # If expire date is today or in the past it will be public
 		if Date.new(enddate[2],enddate[0],enddate[1]) <= Date.today
 			filename=""
 			# TODO remove NOTPUBLIC line and comit the change
 	        end
+		# NdaysWarning WARNING befor it get public
 		if Date.new(enddate[2],enddate[0],enddate[1])- Date.today < NdaysWarning
 			daysleft = Date.new(enddate[2],enddate[0],enddate[1])- Date.today
 			puts(red("File : #{filename} from #{contactname} will be public in #{daysleft} days.\n"))
@@ -52,7 +81,7 @@ dirs.each{ |dir|
 	usermode = filename.split("/").last.scan(/ModUser(.*)\.f90/).flatten.first
         ## Module used in standard test, will not be removed. 
         if maketest.scan(/u=#{usermode}/).length > 0
-		filename=""
+		#filename=""
 		# TODO remove NOTPUBLIC line and comit the change
 	end
 
