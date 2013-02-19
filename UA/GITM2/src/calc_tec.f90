e81fddce76d47f9087cbba22f435678eebd681a4
agburr <>
2013-02-19 22:35:24 +0000

    Added interpolation routine for single TEC point
    Former-commit-id: acf04164979fc4d46b720270f74b64b640da7d44

--- UA/GITM2/src/calc_tec.f90
+++ UA/GITM2/src/calc_tec.f90
@@ -4,7 +4,14 @@
 !
 ! calc_tec
 !
-! Author: Angeline G. Burrell, UMichigan, December 2012
+! Author: Angeline G. Burrell (AGB), UMichigan, December 2012
+!
+! Modified: Alexey Morozov (AM), UMichigan, Feb 2013 - added
+!                                                      calc_single_vtec_interp
+!           AGB, UMichian, Feb 2013 - Removed useless lines in
+!                                     calc_single_vtec_interp and added use
+!                                     of BlockLocationIndex to reduce code
+!                                     duplication.
 !
 ! Comments: Routines to compute the total electron content (TEC) from
 !           the GITM electron density profiles.  TEC is defined as the integral
@@ -24,6 +31,8 @@
 !                               and longitude, using the latitude, longitude
 !                               and block indeces
 !           calc_vtec <- computes the VTEC at all locations
+!           calc_single_vtec_interp <- computes VTEC by first interpolating 
+!                                      IDensityS to lonfind and latfind
 !-----------------------------------------------------------------------------
 
 subroutine calc_single_vtec(iLon, iLat, iBlock, single_vtec)
@@ -88,3 +97,72 @@ subroutine calc_vtec(iBlock)
 
   return
 end subroutine calc_vtec
+
+subroutine calc_single_vtec_interp(LonFind, LatFind, single_vtec)
+
+  use ModGITM
+
+  implicit none
+
+  real, intent(in) :: LonFind, LatFind
+  real, intent(out) :: single_vtec
+
+  integer :: i, m, iiLon, iiLat, iiBlock
+  real :: rLon, rLat, height
+  real, dimension(0:nAlts+1) :: column !what to integrate over
+  real :: Tmp(0:nLons+1,0:nLats+1,0:nAlts+1)
+
+  ! Initialize VTEC to an unpysical value as an error flag
+
+  single_vtec = -1.0e32
+
+  ! Identify the latitude and longitude indexes as well as their interpolated
+  ! values
+
+  call LocationIndex(LonFind, LatFind, iiBlock, iiLon, iiLat, rLon, rLat)
+
+  if(iiLon.lt.0 .or. iiLat.lt.0 .or. iiLon.gt.nLons .or. iiLat.gt.nLats) then
+     return
+  end if
+
+  ! Interpolate electron density
+
+  Tmp = IDensityS(0:nLons+1,0:nLats+1,0:nAlts+1,ie_,iiBlock)
+  call inter2(Tmp,iiLon,iiLat,rlon,rlat,column)
+
+  ! Integrate using the trapezoidal rule
+
+  single_vtec = 0.0
+  m           = nAlts - 1
+
+  do i = 1, m
+     height      = Altitude_GB(iiLon,iiLat,i+1,iiBlock) &
+          - Altitude_GB(iiLon,iiLat,i,iiBlock)
+     single_vtec = single_vtec + height * (column(i) + column(i+1)) / 2.0
+  enddo
+
+  !  VTEC is in TECU while electron density is in m^-3
+
+  single_vtec = single_vtec * (10.0**(-16.0))
+
+ write(*,*) "AGB TEST: got VTEC of", single_vtec
+
+contains
+
+  subroutine inter2(variable, iiLon, iiLat, rLon, rLat, out) 
+
+    implicit none
+
+    real, intent(in) :: variable(0:nLons+1, 0:nLats+1, 0:nAlts+1), rLon, rLat
+    integer, intent(in) :: iiLon, iiLat
+    real, intent(out) :: out(0:nAlts+1)
+
+    out = &  
+          (  rLon)*(  rLat)*Variable(iiLon  ,iiLat  ,:) + &
+          (1-rLon)*(  rLat)*Variable(iiLon+1,iiLat  ,:) + &
+          (  rLon)*(1-rLat)*Variable(iiLon  ,iiLat+1,:) + &
+          (1-rLon)*(1-rLat)*Variable(iiLon+1,iiLat+1,:)
+
+  end subroutine inter2
+
+end subroutine calc_single_vtec_interp
