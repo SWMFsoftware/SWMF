7995e8a88e10114e71c0e679c795cec7ef865d07
agburr <>
2013-07-03 16:29:02 +0000

    Added several routines to better determine parameter ranges
    Former-commit-id: 6d4ad53575a75f4ded2240b73b007eb6512b7d78

--- UA/GITM2/srcPython/gitm_plot_rout.py
+++ UA/GITM2/srcPython/gitm_plot_rout.py
@@ -11,9 +11,16 @@
 #                                           polar plot
 #           find_data_limits              - find the upper and lower limits
 #                                           for a list of GITM data arrays
+#           find_data_limits_irange       - find the upper and lower limits
+#                                           for a list of GITM data arrays and
+#                                           a range of lon/lat/alt indexes
 #           localtime_to_glon             - convert local time to longitude
 #           find_lat_lon_index            - find the appropriate index for
 #                                           a specified location
+#           retrieve_key_from_web_name    - a routine to retrieve the data key
+#                                           from a website-friendly data name
+#           find_alt_index - A routine to find the appropriate index for
+#                            a specified altitude
 #----------------------------------------------------------------------------
 
 '''
@@ -133,6 +140,91 @@ def find_data_limits(gDataList, xkey, lat_index=-1, lon_index=-1, alt_index=-2,
 
     return xmin, xmax
 
+def find_data_limits_irange(gDataList, xkey, min_ilat=-1, max_ilat = -1,
+                            min_ilon=-1, max_ilon=-1, min_ialt=-2, max_ialt=-2,
+                            inc=6, *args, **kwargs):
+    '''
+    Establish the appropriate axis limits for a list of GitmBin files at
+    a specified range of indexes.  If you only want one index, the maximum
+    index should have a value on integer higher than the minimum index, which
+    contains the desired index integer.
+
+    Input: gDataList = A list of GitmBin data structures
+           xkey      = key for the desired values
+           min_ilat  = minimum latitude index (default -1 for no index)
+           max_ilat  = maximum latitude index (default -1 for no index)
+           min_ilon  = minimum longitude index (default -1 for no index)
+           max_ilon  = maximum llongitude index (default -1 for no index)
+           min_ialt  = minimum altitude index (default -2 for no index, -1
+                       for 2D)
+           max_ialt  = maximum laltitude index (default -2 for no index, -1
+                       for 2D)
+           inc       = number of tick incriments (default is 6)
+    '''
+    import math
+
+    hold_min = []
+    hold_max = []
+
+    for gData in gDataList:
+        if(min_ilat < 0 and min_ilon < 0):
+            if(min_ialt > -2):
+                flat = gData[xkey][:,:,min_ialt:max_ialt].reshape(-1)
+            else:
+                flat = gData[xkey][:,:,:].reshape(-1)
+        elif(min_ilat < 0):
+            if(min_ialt > -2):
+                flat = gData[xkey][min_ilon:max_ilon,:,
+                                   min_ialt:max_ialt].reshape(-1)
+            else:
+                flat = gData[xkey][min_ilon:max_ilon,:,:].reshape(-1)
+        elif(min_ilon < 0):
+            if(min_ialt > -2):
+                flat = gData[xkey][:,min_ilat:max_ilat,
+                                     min_ialt:max_ialt].reshape(-1)
+            else:
+                flat = gData[xkey][:,min_ilat:max_ilat,:].reshape(-1)
+        else:
+            if(min_ialt > -1):
+                flat = gData[xkey][min_ilon:max_ilon,min_ilat:max_ilat,
+                                   min_ialt:max_ialt].reshape(-1)
+            else:
+                flat = gData[xkey][min_ilon:max_ilon,min_ilat:max_ilat,
+                                   :].reshape(-1)
+
+        hold_min.append(min(flat))
+        hold_max.append(max(flat))
+
+    xmin = min(hold_min)
+    xmax = max(hold_max)
+    xran = round((xmax-xmin)/inc)
+
+    if(xran != 0.0):
+        xmin = math.floor(float("%.14f" % (xmin / xran))) * xran
+        xmax = math.ceil(float("%.14f" % (xmax / xran))) * xran
+
+    # Consider physical limits for Latitude and Longitude keys.
+
+    if(xkey == "dLat"):
+        if(xmin < -90.0):
+            xmin = -90.0
+        if(xmax > 90.0):
+            xmax = 90.0
+    elif(xkey == "Latitude"):
+        if(xmin < -np.pi / 2.0):
+            xmin = -np.pi / 2.0
+        if(xmax > np.pi / 2.0):
+            xmax = np.pi / 2.0
+    elif(xkey == "dLon" or xkey == "Longitude"):
+        if(xmin < 0.0):
+            xmin = 0.0
+        if(xkey == "dLon" and xmax > 360.0):
+            xmax = 360
+        elif(xkey == "Longitude" and xmax > np.pi):
+            xmax = np.pi
+
+    return xmin, xmax
+
 def localtime_to_glon(ut_datetime, localtime):
     '''
     Routine to compute the longitude where the local time is at a specified
@@ -191,41 +283,41 @@ def retrieve_key_from_web_name(name):
     key_dict = {"Altitude":"Altitude", "Argon Mixing Ratio":"Ar Mixing Ratio",
                 "[Ar]":"Ar","Methane Mixing Ratio":"CH4 Mixing Ratio",
                 "Conduction":"Conduction", "EUV Heating":"EuvHeating",
-                "[H]":"H", "[H$^+$]":"H!U+!N", "[He]":"He",
-                "H$_2$ Mixing Ratio":"H2 Mixing Ratio", "[He$^+$]":"He!U+!N",
+                "[H]":"H", "[H+]":"H!U+!N", "[He]":"He",
+                "H2 Mixing Ratio":"H2 Mixing Ratio", "[He+]":"He!U+!N",
                 "Hydrogen Cyanide Mixing Ratio":"HCN Mixing Ratio",
                 "Heating Efficiency":"Heating Efficiency",
                 "Heat Balance Total":"Heat Balance Total",
                 "Latitude (rad)":"Latitude", "Longitude (rad)":"Longitude",
-                "[N$_2$]":"N!D2!N", "[N$_2$$^+$]":"N!D2!U+!N",
-                "[N$^+$]":"N!U+!N", "[N($^2$D)]":"N(!U2!ND)",
-                "[N($^2$P)]":"N(!U2!NP)", "[N($^4$S)]":"N(!U4!NS)",
-                "N$_2$ Mixing Ratio":"N2 Mixing Ratio", "[NO]":"NO",
-                "[NO$^+$]":"NO!U+!N", "[O($^4$SP)$^+$]":"O_4SP_!U+!N",
-                "[O($^1$D)]":"O(!U1!ND)", "[O$_2$$^+$]":"O!D2!U+!N",
-                "[O($^2$D)]":"O(!U2!ND)!", "[O($^2$D)]":"O(!U2!ND)!U+!N",
-                "[O($^2$P)$^+$]":"O(!U2!NP)!U+!N", "[O$_2$]":"O!D2!N",
-                "[O($^2$P)]":"O(!U2!NP)!U+!N", "[O($^3$P)]":"O(!U3!NP)",
+                "[N2]":"N!D2!N", "[N2+]":"N!D2!U+!N",
+                "[N+]":"N!U+!N", "[N(2D)]":"N(!U2!ND)",
+                "[N(2P)]":"N(!U2!NP)", "[N(4S)]":"N(!U4!NS)",
+                "N2 Mixing Ratio":"N2 Mixing Ratio", "[NO]":"NO",
+                "[NO+]":"NO!U+!N", "[O(4SP)+]":"O_4SP_!U+!N",
+                "[O(1D)]":"O(!U1!ND)", "[O2+]":"O!D2!U+!N",
+                "[O(2D)]":"O(!U2!ND)!", "[O(2D)]":"O(!U2!ND)!U+!N",
+                "[O(2P)+]":"O(!U2!NP)!U+!N", "[O2]":"O!D2!N",
+                "[O(2P)]":"O(!U2!NP)!U+!N", "[O(3P)]":"O(!U3!NP)",
                 "Radiative Cooling":"RadCooling", "Neutral Density":"Rho",
-                "T$_n$":"Temperature", "v$_{East}$":"V!Di!N (east)",
-                "v$_{North}$":"V!Di!N (north)", "v$_{Up}$":"V!Di!N (up)",
-                "u$_{East}$":"V!Dn!N (east)","u$_{North}$":"V!Dn!N (north)",
-                "u$_{Up}$":"V!Dn!N (up)", "[e-]":"e-",
-                "u$_{Up, N_2}$":"V!Dn!N (up,N!D2!N              )",
-                "u$_{Up, N(^4S)}$":"V!Dn!N (up,N(!U4!NS)           )",
-                "u$_{Up, NO}$":"V!Dn!N (up,NO                  )",
-                "u$_{Up, O_2}$":"V!Dn!N (up,O!D2!N              )",
-                "u$_{Up, O(^3P)}$":"V!Dn!N (up,O(!U3!NP)           )",
+                "Tn":"Temperature", "v(East)":"V!Di!N (east)",
+                "v(North)":"V!Di!N (north)", "v(Up)":"V!Di!N (up)",
+                "u(East)":"V!Dn!N (east)","u(North)":"V!Dn!N (north)",
+                "u(Up)":"V!Dn!N (up)", "[e-]":"e-",
+                "u(Up, N_2)":"V!Dn!N (up,N!D2!N              )",
+                "u(Up, N(4S))":"V!Dn!N (up,N(!U4!NS)           )",
+                "u(Up, NO)":"V!Dn!N (up,NO                  )",
+                "u(Up, O_2)":"V!Dn!N (up,O!D2!N              )",
+                "u(Up, O(3P))":"V!Dn!N (up,O(!U3!NP)           )",
                 "Electron Average Energy":"Electron_Average_Energy",
-                "T$_e$":"eTemperature", "T$_i$":"iTemperature",
-                "Solar Zenith Angle":"Solar Zenith Angle", "[CO$_2$]":"CO!D2!N",
+                "Te":"eTemperature", "Ti":"iTemperature",
+                "Solar Zenith Angle":"Solar Zenith Angle", "[CO2]":"CO!D2!N",
                 "Vertical TEC":"Vertical TEC", "DivJu FL":"DivJu FL",
                 "DivJuAlt":"DivJuAlt", "Field Line Length":"FL Length",
                 "Electron Energy Flux":"Electron_Energy_Flux",
-                "$\sigma_P$":"Pedersen FL Conductance", "Potential":"Potential",
-                "$\Sigma_P$":"Pedersen Conductance", "Region 2 Current":"Je2",
-                "$\sigma_H$":"Hall FL Conductance", "Region 1 Current":"Je1",
-                "Hall Conductance":"$\Sigma_H$", "Ed1":"Ed1", "Ed2":"Ed2",
+                "sigmaP":"Pedersen FL Conductance", "Potential":"Potential",
+                "SigmaP":"Pedersen Conductance", "Region 2 Current":"Je2",
+                "sigmaH":"Hall FL Conductance", "Region 1 Current":"Je1",
+                "Hall Conductance":"SigmaH", "Ed1":"Ed1", "Ed2":"Ed2",
                 "Vertical Electric Field":"E.F. Vertical",
                 "Eastward Electric Field":"E.F. East", "dLat":"Latitude (deg)",
                 "Northward Electric Field":"E.F. North",
@@ -240,5 +332,36 @@ def retrieve_key_from_web_name(name):
         print "ERROR: unknown data type [", name, "], known names are: "
         print key_dict.keys()
 
-#End
+def find_alt_index(gData, ilon, ilat, alt, units="km"):
+    '''
+    Routine to locate the appropriate altitude index in a given array.
+    The altitude may be specified in km (default) or m.
+    '''
+
+    import string
+    import numpy as np
+
+    if string.lower(units) == "km":
+        alt *= 1000.0
+
+    # The GITM arrays are sorted, so we can use searchsorted to find
+    # a close index
+
+    ialt = np.searchsorted(gData['Altitude'][ilon,ilat,:], alt)
+
+    # If the search index is zero, it is at or below the minimum altitude
+    # and doesn't need to be changed.  If the distance between the desired
+    # altitude is closer to the search index than the the previous index,
+    # it does not need to be changed either
+
+    if(ialt >= gData.attrs['nAlt'] or
+       (ialt> 0 and abs(alt - gData['Altitude'][ilon,ilat,ialt]) >
+        abs(alt + gData['Altitude'][ilon,ilat,ialt-1]))):
+        # If this location is above the maximum height, return maximum index 
+        # by reducing the count by one, or if this location is closer to the
+        # previous altitude than the one at this index, reduce the count by one
+        ialt -= 1
+        
+    return(ialt)
+#End find_alt_index
 
