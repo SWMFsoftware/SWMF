57883d727e1eaaecd963ab6cb1ef75970f20e028
gtoth <>
2013-09-20 17:02:08 +0000

    Use generic share/Scripts/Config.pl and copy one of the share/build/Makefile* into Makfile.conf. The -mpi and -np flags are not reimplemented, since I am not sure what they are needed for.
    Former-commit-id: 13094e730e1c043097fefc79dee110be5b09b41b

--- PT/AMPS/Config.pl
+++ PT/AMPS/Config.pl
@@ -1,53 +1,32 @@
 #!/usr/bin/perl -i
 use strict;
 
-my $Install;
-my $Uninstall;
-my $Compiler;
-my $IsComponent;
+our $Component       = 'PT';
+our $Code            = 'amps';
+our $MakefileDefOrig = 'Makefile.def.amps';
+our @Arguments       = @ARGV;
+
 my $Application;
 my $MpiLocation;
 my $Mpirun="mpirun";
 my $TestRunProcessorNumber=4;
 
-foreach (@ARGV){
-    if(/^-install(=.*)?$/)    {my $value=$1;
-                               $IsComponent=1 if $value =~ /^=c/i;
-                               $IsComponent=0 if $value =~ /^=s/i;
-                               $Install=1;                     next;}
-    if(/^-uninstall$/i)       {$Uninstall=1;                   next};
-    if(/^-compiler=(.*)$/i)   {$Compiler=$1;                   next};
+# Run the shared Config.pl script
+my $config     = "share/Scripts/Config.pl";
+if(-f $config){
+    require $config;
+}else{
+    require "../../$config";
+}
+
+our %Remaining;   # Arguments not handled by share/Scripts/Config.pl
+
+foreach (@Arguments){
     if(/^-application=(.*)/i) {$Application=$1;                next};
     if(/^-mpi=(.*)$/i)        {$MpiLocation=$1;                next}; 
     if(/^-np=(.*)$/i)         {$TestRunProcessorNumber=$1;     next}; 
-}
 
-if($Install){
-
-    $Compiler = "mpicxx" unless $Compiler;
-
-    if ($MpiLocation) {
-      $Compiler=$MpiLocation."/".$Compiler;
-      $Mpirun=$MpiLocation."/".$Mpirun;
-    }
-
-    my $pwd=`pwd`;
-    `echo "MYDIR=$pwd" > Makefile.def`;
-    if($IsComponent){
-	`echo "include ../../Makefile.def" >> Makefile.def`;
-	`echo "include ../../Makefile.conf" > Makefile.conf`;
-    }else{
-	`cat Makefile.def.amps >> Makefile.def`;
-
-	`echo "COMPILE.mpicxx = $Compiler" > Makefile.conf`;
-        `echo "MPIRUN = $Mpirun -np $TestRunProcessorNumber" >> Makefile.conf`;
-    }
-    `make install`;
-}
-
-if($Uninstall){
-    `make allclean`;
-    `rm -f Makefile.def Makefile.conf`;
+    warn "WARNING: Unknown flag $_\n" if $Remaining{$_};
 }
 
 if($Application){
