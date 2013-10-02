e8b3b2f78a7ac24aa2769942765c2f90520e8f96
gtoth <>
2013-10-02 20:53:04 +0000

    The whole thing is in extern("C") now so it compiles better. Commented out unused amps_init and amps_timestep that would not compile.
    Former-commit-id: cf57303863e439c10ffe3c15e0829eb8ade50c51

--- PT/AMPS/srcInterface/amps2swmf.cpp
+++ PT/AMPS/srcInterface/amps2swmf.cpp
@@ -18,7 +18,6 @@
 #include <fstream>
 #include <signal.h>
 
-
 #include <sys/time.h>
 #include <sys/resource.h>
 
@@ -26,59 +25,54 @@
 
 using namespace std;
 
-
+  //void amps_init();
+  //void amps_time_step();
 
 extern "C" { 
   void amps_timestep_(double* TimeSimulation, double* TimeSimulationLimit);
   int initamps_();
   void amps_setmpicommunicator_(signed int* iComm,signed int* iProc,signed int* nProc);
-} 
 
+  void amps_setmpicommunicator_(signed int* iComm,signed int* iProc,signed int* nProc) {
+    MPI_GLOBAL_COMMUNICATOR=MPI_Comm_f2c(*iComm);
+    PIC::InitMPI();
 
-void amps_init();
-void amps_time_step();
-
-
-void amps_setmpicommunicator_(signed int* iComm,signed int* iProc,signed int* nProc) {
-  MPI_GLOBAL_COMMUNICATOR=MPI_Comm_f2c(*iComm);
-  PIC::InitMPI();
-
-  if (PIC::ThisThread==0) {
-    printf("AMPS: MPI Communicatior is imported from SWMF, size=%i\n",PIC::nTotalThreads);
-  }
-}
-
-
-void amps_timestep_(double* TimeSimulation, double* TimeSimulationLimit) {
-  static bool InitFlag=false;
-
-  if (InitFlag==false) {
-    //initamps_();
-
-    amps_init();
-
-    InitFlag=true;
+    if (PIC::ThisThread==0) {
+      printf("AMPS: MPI Communicatior is imported from SWMF, size=%i\n",PIC::nTotalThreads);
+    }
   }
 
+  void amps_timestep_(double* TimeSimulation, double* TimeSimulationLimit) {
+    static bool InitFlag=false;
 
-  static int counter=0;
-  counter++;
+    if (InitFlag==false) {
+      //initamps_();
 
-  amps_time_step();
+      //amps_init();
 
-  if (counter==100) {
-    char fname[400];
+      InitFlag=true;
+    }
 
-    sprintf(fname,"%s/amsp.dat",PIC::OutputDataFileDirectory);
-    PIC::RunTimeSystemState::GetMeanParticleMicroscopicParameters(fname);
 
-    exit(0);
-  }    
+    static int counter=0;
+    counter++;
+
+    //amps_time_step();
+
+    if (counter==100) {
+      char fname[400];
+
+      sprintf(fname,"%s/amsp.dat",PIC::OutputDataFileDirectory);
+      PIC::RunTimeSystemState::GetMeanParticleMicroscopicParameters(fname);
+
+      exit(0);
+    }    
+
+  }
 
 }
 
 /*
 int main () {
   return 1;
-}
 */
