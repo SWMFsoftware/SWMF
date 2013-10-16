bdef522be6291e0e4b29c7eecb76111f7d1a3e07
vtenishe <>
2013-10-16 17:18:46 +0000

    initial submission of the used-defined model sources for the model of Mercury's pick-up ions and neutral exosphere
    Former-commit-id: e4c56adaf12bf55164b488e8d93a2dce753d4994

---
+++ PT/AMPS/srcMercury/UserDefinition.PIC.PhysicalModelHeaderList.h
@@ -0,0 +1,12 @@
+//==============================================================
+//$Id$
+//include the headers for the comet-specific models
+
+#ifndef USERDEFINITION_PIC_PHYSICALMODELHEADERLIST_H_
+#define USERDEFINITION_PIC_PHYSICALMODELHEADERLIST_H_
+
+
+#include "Mercury.h"
+
+
+#endif /* USERDEFINITION_PIC_PHYSICALMODELHEADERLIST_H_ */
