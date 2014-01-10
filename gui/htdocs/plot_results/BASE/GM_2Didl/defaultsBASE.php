6c3000b8e0c073acfbc7b121140d42c5c83121d5
cvs2git <>
2014-01-10 18:43:36 +0000

    This commit was manufactured by cvs2svn to create tag 'cut-cells-works-with- srcBullet--high-resolution'.
    Former-commit-id: 86bba953f5dcd18ca46cb54277e9e3ba3d590b18

--- gui/htdocs/plot_results/BASE/GM_2Didl/defaultsBASE.php
+++
@@ -1,9 +0,0 @@
-<?php 
-#  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
-#  For more information, see http://csem.engin.umich.edu/tools/swmf 
-?>
-<?php // idl
-$plotextension = ".out";
-$macroextension = ".pro";
-$loadfile = "2Didl";
-?>
