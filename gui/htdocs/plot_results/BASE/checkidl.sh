d500728f60ef88e1df65ae141bf3ec095767f172
cvs2git <>
2014-01-23 16:52:54 +0000

    This commit was manufactured by cvs2svn to create tag 'SWPC_01_23_2014'.
    Former-commit-id: fe9349a7124af8a1e710fba6a63bc845c6f94813

--- gui/htdocs/plot_results/BASE/checkidl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
-#  For more information, see http://csem.engin.umich.edu/tools/swmf
-
-IDL_PATH=Idl:${IDL_PATH}
-IDL_STARTUP=Idl/idlrc_gui
-export IDL_PATH IDL_STARTUP
-
-echo show_head,"'"$1"'" | idl
