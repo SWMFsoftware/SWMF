a45be3e5125c93fa03b5acee8caab3a9094df7dc
vtenishe <>
2014-01-27 00:37:36 +0000

    1. Makefile.local is created (if not exists); 2. Compiling and code settings are saved in either .ampsConfig.Settings or Makefile.local (depending if they are used to modify the sources or to control the compilation)
    Former-commit-id: 17ba6e3712986e0ba310ffb6df7c3f293377afb2

--- PT/AMPS/Config.pl
+++ PT/AMPS/Config.pl
@@ -13,6 +13,12 @@ my $MpiLocation;
 my $Mpirun="mpirun";
 my $TestRunProcessorNumber=4;
 
+#create Makefile.local
+if (! -e "Makefile.local") {
+  `echo " " > Makefile.local`;
+}
+
+
 # Run the shared Config.pl script
 my $config     = "share/Scripts/Config.pl";
 if(-f $config){
@@ -23,18 +29,61 @@ if(-f $config){
 
 our %Remaining;   # Arguments not handled by share/Scripts/Config.pl
 
-foreach (@Arguments){
-    if(/^-application=(.*)/i) {$Application=$1;                next};
-    if(/^-mpi=(.*)$/i)        {$MpiLocation=$1;                next}; 
-    if(/^-np=(.*)$/i)         {$TestRunProcessorNumber=$1;     next}; 
+#sort the argument line to push the 'application' argument first
+my $cntArgument=0;
 
-    warn "WARNING: Unknown flag $_\n" if $Remaining{$_};
+foreach (@Arguments) {
+  if (/^-application=(.*)/i) {
+    if ($cntArgument!=0) {
+      my $t;
+      
+      $t=$Arguments[0];
+      $Arguments[0]=$Arguments[$cntArgument];
+      $Arguments[$cntArgument]=$t;
+      
+      last;
+    }
+  }
+  
+  $cntArgument++;
 }
 
-if($Application){
-    my $application = lc($Application);
-#   `rm -rf main; cp -r src$Application main; rm -rf main/CVS`;
+#process the configuration settings
+foreach (@Arguments) {
+  if (/^-application=(.*)/i) {
+    my $application = lc($1);
+    
     `cp -f input/$application.* input/species.input .`;
+    `echo "InputFileAMPS=$application.input" >> Makefile.local`;
+      
+    #copy all makefile settings from the input file into 'Makefile.local'
+    #print "Config.pl modifies Makefile.local settings for application $1\n";
+    open (INPUTFILE,"<","$application.input") || die "Cannot open $application.input\n";
+      
+    while (my $line=<INPUTFILE>) {
+      my ($p0,$p1);
+        
+      chomp($line);
+      ($p0,$p1)=split(' ',$line,2);
+      $p0=lc($p0);
+        
+      if ($p0 =~ /makefile/) {
+        `echo "$p1" >> Makefile.local`;
+      }
+    }
+      
+    close (INPUTFILE);     
+    next
+  };
+    
+  if (/^-mpi=(.*)$/i)        {$MpiLocation=$1;                next}; 
+  if (/^-np=(.*)$/i)         {$TestRunProcessorNumber=$1;     next};
+  if (/^-spice=(.*)$/i)      {`echo "SPICE=$1" >> Makefile.local`;     next}; 
+  if (/^-kernels=(.*)$/i)    {`echo "SPICEKERNELS=$1" >> .ampsConfig.Settings`;     next};
+  if (/^-ices=(.*)$/i)       {`echo "ICESLOCATION=$1" >> .ampsConfig.Settings`;     next};
+  
+  warn "WARNING: Unknown flag $_\n" if $Remaining{$_};
 }
+
 
 exit 0;
