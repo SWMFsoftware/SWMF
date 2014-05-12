20f6307a151a1c35a41e92b77ae2417ca0a60a59
gtoth <>
2014-05-12 21:33:20 +0000

    Use CZ_wrapper module
    Former-commit-id: 10f7b8093061eebea37e7c882d92a18c27e89ccd

--- CZ/Empty/src/CZ_wrapper.f90
+++ CZ/Empty/src/CZ_wrapper.f90
@@ -1,94 +1,101 @@
 !  Copyright (C) 2002 Regents of the University of Michigan, 
 !  portions used with permission 
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
-! Wrapper for the empty Convection Zone (CZ) component
-!==========================================================================
-subroutine CZ_set_param(CompInfo, TypeAction)
 
-  use CON_comp_info
+module CZ_wrapper
 
-  implicit none
-
-  character (len=*), parameter :: NameSub='CZ_set_param'
-
-  ! Arguments
-  type(CompInfoType), intent(inout) :: CompInfo   ! Information for this comp.
-  character (len=*), intent(in)     :: TypeAction ! What to do
-  !-------------------------------------------------------------------------
-  select case(TypeAction)
-  case('VERSION')
-     call put(CompInfo,&
-          Use        =.false., &
-          NameVersion='Empty', &
-          Version    =0.0)
-
-  case default
-     call CON_stop(NameSub//': CZ_ERROR: empty version cannot be used!')
-  end select
-
-end subroutine CZ_set_param
-
-!==============================================================================
-
-subroutine CZ_init_session(iSession, TimeSimulation)
+  ! Wrapper for the empty Convection Zone (CZ) component
 
   implicit none
 
-  !INPUT PARAMETERS:
-  integer,  intent(in) :: iSession         ! session number (starting from 1)
-  real,     intent(in) :: TimeSimulation   ! seconds from start time
+  private ! except
 
-  character(len=*), parameter :: NameSub='CZ_init_session'
+  public:: CZ_set_param
+  public:: CZ_init_session
+  public:: CZ_run
+  public:: CZ_save_restart
+  public:: CZ_finalize
 
-  call CON_stop(NameSub//': CZ_ERROR: empty version cannot be used!')
+contains
 
-end subroutine CZ_init_session
+  !==========================================================================
+  subroutine CZ_set_param(CompInfo, TypeAction)
 
-!==============================================================================
+    use CON_comp_info
 
-subroutine CZ_finalize(TimeSimulation)
+    character (len=*), parameter :: NameSub='CZ_set_param'
 
-  implicit none
+    ! Arguments
+    type(CompInfoType), intent(inout):: CompInfo   ! Information for this comp.
+    character (len=*), intent(in)    :: TypeAction ! What to do
+    !-------------------------------------------------------------------------
+    select case(TypeAction)
+    case('VERSION')
+       call put(CompInfo,&
+            Use        =.false., &
+            NameVersion='Empty', &
+            Version    =0.0)
 
-  !INPUT PARAMETERS:
-  real,     intent(in) :: TimeSimulation   ! seconds from start time
+    case default
+       call CON_stop(NameSub//': CZ_ERROR: empty version cannot be used!')
+    end select
 
-  character(len=*), parameter :: NameSub='CZ_finalize'
+  end subroutine CZ_set_param
 
-  call CON_stop(NameSub//': CZ_ERROR: empty version cannot be used!')
+  !============================================================================
 
-end subroutine CZ_finalize
+  subroutine CZ_init_session(iSession, TimeSimulation)
 
-!==============================================================================
+    !INPUT PARAMETERS:
+    integer,  intent(in) :: iSession         ! session number (starting from 1)
+    real,     intent(in) :: TimeSimulation   ! seconds from start time
 
-subroutine CZ_save_restart(TimeSimulation)
+    character(len=*), parameter :: NameSub='CZ_init_session'
 
-  implicit none
+    call CON_stop(NameSub//': CZ_ERROR: empty version cannot be used!')
 
-  !INPUT PARAMETERS:
-  real,     intent(in) :: TimeSimulation   ! seconds from start time
+  end subroutine CZ_init_session
 
-  character(len=*), parameter :: NameSub='CZ_save_restart'
+  !============================================================================
 
-  call CON_stop(NameSub//': CZ_ERROR: empty version cannot be used!')
+  subroutine CZ_finalize(TimeSimulation)
 
-end subroutine CZ_save_restart
+    !INPUT PARAMETERS:
+    real,     intent(in) :: TimeSimulation   ! seconds from start time
 
-!==============================================================================
+    character(len=*), parameter :: NameSub='CZ_finalize'
 
-subroutine CZ_run(TimeSimulation,TimeSimulationLimit)
+    call CON_stop(NameSub//': CZ_ERROR: empty version cannot be used!')
 
-  implicit none
+  end subroutine CZ_finalize
 
-  !INPUT/OUTPUT ARGUMENTS:
-  real, intent(inout) :: TimeSimulation   ! current time of component
+  !============================================================================
 
-  !INPUT ARGUMENTS:
-  real, intent(in) :: TimeSimulationLimit ! simulation time not to be exceeded
+  subroutine CZ_save_restart(TimeSimulation)
 
-  character(len=*), parameter :: NameSub='CZ_run'
+    !INPUT PARAMETERS:
+    real,     intent(in) :: TimeSimulation   ! seconds from start time
 
-  call CON_stop(NameSub//': CZ_ERROR: empty version cannot be used!')
+    character(len=*), parameter :: NameSub='CZ_save_restart'
 
-end subroutine CZ_run
+    call CON_stop(NameSub//': CZ_ERROR: empty version cannot be used!')
 
+  end subroutine CZ_save_restart
+
+  !============================================================================
+
+  subroutine CZ_run(TimeSimulation,TimeSimulationLimit)
+
+    !INPUT/OUTPUT ARGUMENTS:
+    real, intent(inout):: TimeSimulation   ! current time of component
+
+    !INPUT ARGUMENTS:
+    real, intent(in):: TimeSimulationLimit ! simulation time not to be exceeded
+
+    character(len=*), parameter :: NameSub='CZ_run'
+
+    call CON_stop(NameSub//': CZ_ERROR: empty version cannot be used!')
+
+  end subroutine CZ_run
+
+end module CZ_wrapper
--- CZ/FSAM/srcInterface/CZ_wrapper.f90
+++ CZ/FSAM/srcInterface/CZ_wrapper.f90
@@ -1,245 +1,257 @@
-!^CFG COPYRIGHT UM
+!  Copyright (C) 2002 Regents of the University of Michigan, 
+!  portions used with permission 
+!  For more information, see http://csem.engin.umich.edu/tools/swmf
 
-!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-!               Space Weather Modeling Framework (SWMF)                !
-!    Center for Space Environment Modeling, The University of Michigan !
-!-----------------------------------------------------------------------
-subroutine CZ_set_param(CompInfo, TypeAction)
-  
-  !USES:
-  use ModPar
-  use CON_comp_info,      ONLY: get, put, CompInfoType
-  use CON_time,           ONLY: MaxIteration, tSimulationMax, CpuTimeMax
-  use ModFSAM,            ONLY: iComm
-  use ModInitialization,  ONLY: atmoin, input_param
-  use ModControl,         ONLY: itmax, wtlimit, tend
-  use ModPhysunits,       ONLY: unit_t
+module CZ_wrapper
+
+  ! Wrapper for the FSAM Convection Zone (CZ) component
+
   implicit none
 
-  character (len=*), intent(in)     :: TypeAction ! which action to perform
-  type(CompInfoType), intent(inout) :: CompInfo   ! component information
-  
-  character (len=*), parameter :: NameSub='CZ_set_param'
-  integer :: iError, isize
-  character (len=100) :: NameCommand, NameItNow
+  private ! except
 
-  !------------------------------------------------------------------------
-  
-  select case(TypeAction)
-  case('VERSION')
-     call put(CompInfo,                                     &
-          Use=.true.,                                       &
-          NameVersion='Finite-difference Spherical Anelastic MHD, Y. Fan', &
-          Version=1.0)
-  case('STDOUT')
-     if(myid==0) then
-        write(6,'(a)') '------------------------ FSAM starts -------------------------- '
-        write(6,'(a,i4,a)') ' Running FSAM on ', nproc, ' CPUs'
-        write(6,'(3(a,i4))') ' inmax=', inmax, ' , jnmax=', jnmax, ', knmax=', knmax
-        write(6,'(3(a,i4))') ' in = ', in, ', jn = ', jn, ', kn = ', kn
-        write(6,'(a)') '--------------------------------------------------------------- '
-        write(6,*)
-        call flush(6)
-     endif
-  case('FILEOUT')
-     !call get(CompInfo,iUnitOut=iUnitOut)
-     !StringPrefix=''
-  case('MPI')
-     call get(CompInfo, iComm=iComm, iProc=myid, nProc=isize)
-     ! check processors
-     if(myid==0.and.(isize .ne. nproc)) then
-        write(*, *) 'wrong number of processors'
-        write(*, *) nproc, ' specified in par.h, ', isize, ' allocated'
-        call MPI_ABORT(iComm, 1,iError)
-     endif
-     call flush(6)
-     myid2 = myid/nproc1
-     myid1 = mod(myid,nproc1)
-     ! input solar atmosphere and define units
-     call atmoin
-  case('READ')
-     ! read PARAM.in
-     call input_param
-     ! set control parameter for the run
-     MaxIteration = itmax
-     tSimulationMax = tend*unit_t
-     CpuTimeMax = wtlimit
-  case('CHECK')
-  case('GRID')
-  case default
-     call CON_stop(NameSub//' CZ_ERROR: invalid TypeAction='//TypeAction)
-  end select
-  
-end subroutine CZ_set_param
+  public:: CZ_set_param
+  public:: CZ_init_session
+  public:: CZ_run
+  public:: CZ_save_restart
+  public:: CZ_finalize
 
-!==============================================================================
+contains
+  !===========================================================================
+  subroutine CZ_set_param(CompInfo, TypeAction)
 
-subroutine CZ_init_session(iSession, TimeSimulation)
-  use ModUserSetup,  ONLY: field_init, grid
-  use ModBval,       ONLY: bvalv
-  use ModInitialization
-  use ModGetQtys
-  use ModFSAM
-  use ModPar
-  use ModIoFSAM
-  use ModPhysunits
-  use ModIteration
-  use ModOutfile
-  use ModRHS,     ONLY: p_init
-  use ModSundry,  ONLY: dt, time, ctime
-  use ModControl, ONLY: itnow, tout
-  implicit none
+    !USES:
+    use ModPar
+    use CON_comp_info,      ONLY: get, put, CompInfoType
+    use CON_time,           ONLY: MaxIteration, tSimulationMax, CpuTimeMax
+    use ModFSAM,            ONLY: iComm
+    use ModInitialization,  ONLY: atmoin, input_param
+    use ModControl,         ONLY: itmax, wtlimit, tend
+    use ModPhysunits,       ONLY: unit_t
 
-  integer,  intent(in) :: iSession         ! session number (starting from 1)
-  real,     intent(in) :: TimeSimulation   ! seconds from start time
+    character (len=*), intent(in)     :: TypeAction ! which action to perform
+    type(CompInfoType), intent(inout) :: CompInfo   ! component information
 
-  character(len=*), parameter :: NameSub='CZ_init_session'
+    character (len=*), parameter :: NameSub='CZ_set_param'
+    integer :: iError, isize
+    character (len=100) :: NameCommand, NameItNow
 
-  real :: em, ek, ek1, ek2, ek3, eth, anglm, anglmnorm, smeanbot, smeantop
-  integer :: iError, ntcond
-  !------------------------------------------------------------------------
+    !------------------------------------------------------------------------
 
-  ! define grid
-  call grid
+    select case(TypeAction)
+    case('VERSION')
+       call put(CompInfo,                                     &
+            Use=.true.,                                       &
+            NameVersion='Finite-difference Spherical Anelastic MHD, Y. Fan', &
+            Version=1.0)
+    case('STDOUT')
+       if(myid==0) then
+          write(*,'(a)') &
+               '---------------------- FSAM starts --------------------------'
+          write(*,'(a,i4,a)') &
+               ' Running FSAM on ', nproc, ' CPUs'
+          write(*,'(3(a,i4))') &
+               ' inmax=', inmax, ' , jnmax=', jnmax, ', knmax=', knmax
+          write(*,'(3(a,i4))') &
+               ' in = ', in, ', jn = ', jn, ', kn = ', kn
+          write(*,'(a)') &
+               '-------------------------------------------------------------'
+          write(*,*)
+       endif
+    case('FILEOUT')
+       !call get(CompInfo,iUnitOut=iUnitOut)
+       !StringPrefix=''
+    case('MPI')
+       call get(CompInfo, iComm=iComm, iProc=myid, nProc=isize)
+       ! check processors
+       if(myid==0.and.(isize .ne. nproc)) then
+          write(*, *) 'wrong number of processors'
+          write(*, *) nproc, ' specified in par.h, ', isize, ' allocated'
+          call MPI_ABORT(iComm, 1,iError)
+       endif
+       myid2 = myid/nproc1
+       myid1 = mod(myid,nproc1)
+       ! input solar atmosphere and define units
+       call atmoin
+    case('READ')
+       ! read PARAM.in
+       call input_param
+       ! set control parameter for the run
+       MaxIteration = itmax
+       tSimulationMax = tend*unit_t
+       CpuTimeMax = wtlimit
+    case('CHECK')
+    case('GRID')
+    case default
+       call CON_stop(NameSub//' CZ_ERROR: invalid TypeAction='//TypeAction)
+    end select
 
-  ! define blk matrix 
-  call blk_init
+  end subroutine CZ_set_param
 
-  ! initialize sundry.h and initial fields 
-  call field_init
-  if(.not. DoRestart) then
-     itnow = 0
-     time  = 0.D0
-     ctime = time
-  else
-     call readrst_mpi
-     ctime = time
-     if(myid==0) write(6,*) 'Restart from restart files at time t = ', time
-     call bvalv
-  endif
-  call p_init
-  call nudt
+  !============================================================================
 
-  ! open monitoring file
-  ntcond = 0
-  call get_global(em,ek,ek1,ek2,ek3,eth,anglm,anglmnorm)
-  call get_mean_entropy(smeanbot,smeantop)
-  if(myid==0) then
-     open(unit=16,file=engfile,status='replace')
-     write(16,'(i8,13e23.14,i8)') itnow,time,dt,em,ek,eth, &
-          ek+eth+em,smeanbot,smeantop,anglm,anglmnorm, &
-          ek1,ek2,ek3,ntcond
-     call flush(16)
-  endif
+  subroutine CZ_init_session(iSession, TimeSimulation)
+    use ModUserSetup,  ONLY: field_init, grid
+    use ModBval,       ONLY: bvalv
+    use ModInitialization
+    use ModGetQtys
+    use ModFSAM
+    use ModPar
+    use ModIoFSAM
+    use ModPhysunits
+    use ModIteration
+    use ModOutfile
+    use ModRHS,     ONLY: p_init
+    use ModSundry,  ONLY: dt, time, ctime
+    use ModControl, ONLY: itnow, tout
 
-  ! output grid and initial fields and set up for future outputs 
-  call writegrid
-  if(ifile .lt. 0) then
-     ifile = time/tout
-     tfile = tout*ifile
-  else
-     tfile=time
-  endif
-  if(.not.DoRestart) call writedata_mpi
-  ifile = ifile + 1
-  tfile = tfile + tout
+    integer,  intent(in) :: iSession         ! session number (starting from 1)
+    real,     intent(in) :: TimeSimulation   ! seconds from start time
 
-  if(UseImplCond) then
-     if(myid==0) write(*,*) 'Initialize Implict Heat Conduction Solver..........'
-     ! call init_impl_cond
-  else
-     if(myid==0) write(*,*) 'Solving Conduction Explicitly......................'
-     call nudt_cond
-  endif
+    character(len=*), parameter :: NameSub='CZ_init_session'
 
-end subroutine CZ_init_session
+    real :: em, ek, ek1, ek2, ek3, eth, anglm, anglmnorm, smeanbot, smeantop
+    integer :: iError, ntcond
+    !------------------------------------------------------------------------
 
-!==============================================================================
+    ! define grid
+    call grid
 
-subroutine CZ_run(TimeSimulation,TimeSimulationLimit)
-  use ModInitialization
-  use ModGetQtys
-  use ModFSAM
-  use ModPar
-  use ModIoFSAM
-  use ModPhysunits
-  use ModIteration
-  use ModOutfile
-  use ModSundry,  ONLY: dt, time, ctime
-  use ModControl, ONLY: itnow, tout, itintv, itout
-  implicit none
+    ! define blk matrix 
+    call blk_init
 
-  real, intent(in) :: TimeSimulationLimit ! simulation time not to be exceeded
-  real, intent(inout) :: TimeSimulation   ! current time of component
-  
-  character(len=*), parameter :: NameSub='CZ_run'
-  
-  real :: em, ek, ek1, ek2, ek3, eth, anglm, anglmnorm, smeanbot, smeantop
+    ! initialize sundry.h and initial fields 
+    call field_init
+    if(.not. DoRestart) then
+       itnow = 0
+       time  = 0.D0
+       ctime = time
+    else
+       call readrst_mpi
+       ctime = time
+       if(myid==0) write(6,*) 'Restart from restart files at time t = ', time
+       call bvalv
+    endif
+    call p_init
+    call nudt
 
-  integer :: ntcond
-  !------------------------------------------------------------------------
+    ! open monitoring file
+    ntcond = 0
+    call get_global(em,ek,ek1,ek2,ek3,eth,anglm,anglmnorm)
+    call get_mean_entropy(smeanbot,smeantop)
+    if(myid==0) then
+       open(unit=16,file=engfile,status='replace')
+       write(16,'(i8,13e23.14,i8)') itnow,time,dt,em,ek,eth, &
+            ek+eth+em,smeanbot,smeantop,anglm,anglmnorm, &
+            ek1,ek2,ek3,ntcond
+       call flush(16)
+    endif
 
-  call nudt
-  call pred_corr_step
-  if(UseImplCond)then
-     !  it = itnow                                                                       
-     !  call conduct_impl(ntcond)                         
-  else
-     call conductstep(ntcond)
-  endif
+    ! output grid and initial fields and set up for future outputs 
+    call writegrid
+    if(ifile .lt. 0) then
+       ifile = time/tout
+       tfile = tout*ifile
+    else
+       tfile=time
+    endif
+    if(.not.DoRestart) call writedata_mpi
+    ifile = ifile + 1
+    tfile = tfile + tout
 
-  time = time + dt
-  ctime = time
-  itnow = itnow + 1
-  
-  ! monitoring global quantities                                                       
-  if(mod(itnow,itintv)==0) then
-     call get_global(em,ek,ek1,ek2,ek3,eth,anglm,anglmnorm)
-     call get_mean_entropy(smeanbot,smeantop)
-     if(myid==0) then
-        write(16,'(i8,13e23.14,i8)') itnow,time,dt,em,ek,eth, &
-             ek+eth+em,smeanbot,smeantop,anglm,anglmnorm, ek1,ek2,ek3,ntcond
-        call flush(16)
-     endif
-  endif
-  
-  ! check for output time                                                              
-  if(mod(itnow,itout)==itout-1) call writedata_mpi(DoWriteRestart=.true.)
-  if(time > tfile) then
-     call writedata_mpi
-     ifile = ifile + 1
-     tfile = tfile + tout
-  endif
+    if(UseImplCond) then
+       if(myid==0) write(*,*) 'Initialize Implict Heat Conduction Solver.....'
+       ! call init_impl_cond
+    else
+       if(myid==0) write(*,*) 'Solving Conduction Explicitly.................'
+       call nudt_cond
+    endif
 
-  TimeSimulation = time*unit_t
+  end subroutine CZ_init_session
 
-end subroutine CZ_run
-!===========================================================================
+  !============================================================================
 
-subroutine CZ_finalize(TimeSimulation)
+  subroutine CZ_run(TimeSimulation,TimeSimulationLimit)
+    use ModInitialization
+    use ModGetQtys
+    use ModFSAM
+    use ModPar
+    use ModIoFSAM
+    use ModPhysunits
+    use ModIteration
+    use ModOutfile
+    use ModSundry,  ONLY: dt, time, ctime
+    use ModControl, ONLY: itnow, tout, itintv, itout
 
-  !USES:
-  implicit none
+    real, intent(in):: TimeSimulationLimit ! simulation time not to be exceeded
+    real, intent(inout) :: TimeSimulation   ! current time of component
 
-  real,     intent(in) :: TimeSimulation   ! seconds from start time
-  character(len=*), parameter :: NameSub='CZ_finalize'
+    character(len=*), parameter :: NameSub='CZ_run'
 
-  !-------------------------------------------------------------------------
+    real :: em, ek, ek1, ek2, ek3, eth, anglm, anglmnorm, smeanbot, smeantop
 
-end subroutine CZ_finalize
-!===========================================================================
+    integer :: ntcond
+    !------------------------------------------------------------------------
 
-subroutine CZ_save_restart(TimeSimulation)
-  use ModIoFSAM,  ONLY: writedata_mpi
-  implicit none
+    call nudt
+    call pred_corr_step
+    if(UseImplCond)then
+       !  it = itnow
+       !  call conduct_impl(ntcond)
+    else
+       call conductstep(ntcond)
+    endif
 
-  real,     intent(in) :: TimeSimulation   ! seconds from start time
-  character(len=*), parameter :: NameSub='CZ_save_restart'
+    time = time + dt
+    ctime = time
+    itnow = itnow + 1
 
-  !-------------------------------------------------------------------------
+    ! monitoring global quantities
+    if(mod(itnow,itintv)==0) then
+       call get_global(em,ek,ek1,ek2,ek3,eth,anglm,anglmnorm)
+       call get_mean_entropy(smeanbot,smeantop)
+       if(myid==0) then
+          write(16,'(i8,13e23.14,i8)') itnow,time,dt,em,ek,eth, &
+               ek+eth+em,smeanbot,smeantop,anglm,anglmnorm, ek1,ek2,ek3,ntcond
+          call flush(16)
+       endif
+    endif
 
-  call writedata_mpi(DoWriteRestart=.true.)
+    ! check for output time
+    if(mod(itnow,itout)==itout-1) call writedata_mpi(DoWriteRestart=.true.)
+    if(time > tfile) then
+       call writedata_mpi
+       ifile = ifile + 1
+       tfile = tfile + tout
+    endif
 
-end subroutine CZ_save_restart
-!===========================================================================
+    TimeSimulation = time*unit_t
 
+  end subroutine CZ_run
+  !===========================================================================
+
+  subroutine CZ_finalize(TimeSimulation)
+
+    real,     intent(in) :: TimeSimulation   ! seconds from start time
+    character(len=*), parameter :: NameSub='CZ_finalize'
+
+    !-------------------------------------------------------------------------
+
+  end subroutine CZ_finalize
+  !===========================================================================
+
+  subroutine CZ_save_restart(TimeSimulation)
+
+    use ModIoFSAM,  ONLY: writedata_mpi
+
+
+    real,     intent(in) :: TimeSimulation   ! seconds from start time
+    character(len=*), parameter :: NameSub='CZ_save_restart'
+    !-------------------------------------------------------------------------
+
+    call writedata_mpi(DoWriteRestart=.true.)
+
+  end subroutine CZ_save_restart
+  !===========================================================================
+
+end module CZ_wrapper
