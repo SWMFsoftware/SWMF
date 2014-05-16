1e3b3f257a379b0542a2ff42cfc0fea0cb21e45f
yfan <>
2014-05-16 17:42:18 +0000

    Make the test run with a smaller grid and 6 processors only
    Former-commit-id: 042d09424f682d4a77be1f4bbc11b0ddc9b19b03

--- CZ/FSAM/srcProblem/ModPar.test.f90
+++ CZ/FSAM/srcProblem/ModPar.test.f90
@@ -1,9 +1,7 @@
 module ModPar
 
   ! Primary parameters:
-  
-  integer, parameter :: inmax = 101, jnmax = 517, knmax = 773
-  integer, parameter :: ijknmax = max(inmax, jnmax, knmax)
+  integer, parameter :: inmax = 101, jnmax = 101, knmax = 101
   real, parameter    :: gg = 6.67231D-8, rgas = 8.3170000D+07, kboltz = 1.381D-16, &
        mproton = 1.673D-24
   real, parameter    :: pi = 3.1415926535898D0, tiny = 1.0D-99, huge = 1.0D+99
@@ -11,10 +9,11 @@ module ModPar
   ! NOTE that inmax-5 must be divisible by nproc1
   ! jnmax-5 must be divisible by nproc2
   ! knmax-5 must be divisible by nproc
-  integer, parameter :: nproc1 = 12, nproc2 = 64
+  integer, parameter :: nproc1 = 3, nproc2 = 2
   integer, parameter :: nproc = nproc1*nproc2
   integer, parameter :: in = (inmax-5)/nproc1+5, jn = (jnmax-5)/nproc2+5, &
-       kns = (knmax-5)/nproc, kn = knmax, ijkn = ijknmax
+       kns = (knmax-5)/nproc, kn = knmax
+  integer, parameter :: ijkn = max(in,jn,kn)
   integer :: myid, myid1, myid2
 
 end module ModPar
