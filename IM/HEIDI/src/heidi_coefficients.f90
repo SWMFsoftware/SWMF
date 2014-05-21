00359dd0cc050b4825bad0bc63f761c661f4f627
gtoth <>
2014-05-21 15:01:07 +0000

    Forgot to commit this one: ModProcIM --> ModProcHeidi
    Former-commit-id: b39cba4eb5bb3e460bef36adf113b79b531bd1ed

--- IM/HEIDI/src/heidi_coefficients.f90
+++ IM/HEIDI/src/heidi_coefficients.f90
@@ -664,7 +664,7 @@ subroutine MAGCONV(I3,NST)
   use ModHeidiCurrents, ONLY: Jfac, Jion1, Lats, Lsh, LatFac, fpot, basepot, fpot, ilfac, irfac, &
        lonfac, iR
   use ModHeidiDGCPM,    ONLY: potdgcpm, vthetacells, vmltcells, vphicells, vlzcells
-  use ModProcIM,        ONLY: iComm, iError, iProc, nProc
+  use ModProcHeidi,     ONLY: iComm, iError, iProc, nProc
   use ModIoUnit,        ONLY: io_unit_new
   use ModHeidiInput,    ONLY: TypeBCalc
   use ModNumConst,      ONLY: cPi
