09ae612f1978f28e3993294b70c293099d1f3ca8
gtoth <>
2014-06-25 01:44:42 +0000

    Use CON_transfer_data. It compiles and runs, but test_rb does not pass on most computers, so it is difficult to tell  if it works as before. Some bug related to the satellites is now fixed.
    Former-commit-id: 1a079d24f56c0f7efb29e694817da52fabbbafad

--- CON/Interface/src/CON_couple_gm_rb.f90
+++ CON/Interface/src/CON_couple_gm_rb.f90
@@ -15,6 +15,8 @@ module CON_couple_gm_rb
 
   !USES:
   use CON_coupler
+  use CON_transfer_data, ONLY: &
+       transfer_integer, transfer_real_array, transfer_string_array
 
   use GM_wrapper, ONLY: &
        GM_get_for_rb, GM_get_for_rb_trace, GM_get_sat_for_rb, GM_satinit_for_rb
@@ -36,11 +38,10 @@ module CON_couple_gm_rb
   !EOP
 
   ! Communicator and logicals to simplify message passing and execution
-  integer, save :: iCommGMRB
-  logical :: IsInitialized = .false., UseMe=.true.
+  logical :: IsInitialized = .false.
 
   ! Size of the 2D spherical structured (possibly non-uniform) RB grid
-  integer, save :: iSize, jSize, nCells_D(2)
+  integer, save :: iSize, jSize
 
   ! Number of satellites in GM that will also be traced in RB
   integer, save :: nShareSats
@@ -50,33 +51,22 @@ contains
   !IROUTINE: couple_gm_rb_init - initialize GM-RB coupling
   !INTERFACE:
   subroutine couple_gm_rb_init
-    integer :: iError
-    ! MPI status variable
-    integer :: iStatus_I(MPI_STATUS_SIZE)
+
     !DESCRIPTION:
-    ! Store RB grid size.
+    ! Store RB grid size. Transfer number of shared satellites from GM to RB.
     !EOP
     !------------------------------------------------------------------------
-    if(IsInitialized) return
+    if(IsInitialized) RETURN
     IsInitialized = .true.
 
-    UseMe = is_proc(RB_) .or. is_proc(GM_)
-    if(.not.UseMe) return
+    if(.not.(is_proc(RB_) .or. is_proc(GM_))) RETURN
 
-    nCells_D=ncells_decomposition_d(RB_)
-    iSize=nCells_D(1); jSize=nCells_D(2)
+    iSize = Grid_C(RB_) % nCoord_D(1)
+    jSize = Grid_C(RB_) % nCoord_D(2)
 
     ! Set number of satellites shared between GM and RB for tracing.
-    call GM_satinit_for_rb(nShareSats)
-
-    if(i_proc0(RB_) /= i_proc0(GM_))then
-       if(is_proc0(GM_)) &
-            call MPI_send(nShareSats,1,MPI_INTEGER,i_proc0(RB_),&
-            1,i_comm(),iError)
-       if(is_proc0(RB_)) &
-            call MPI_recv(nShareSats,1,MPI_INTEGER,i_proc0(GM_),&
-            1,i_comm(),iStatus_I,iError)
-    end if
+    if(is_proc(GM_))call GM_satinit_for_rb(nShareSats)
+    call transfer_integer(GM_, RB_, nShareSats, UseSourceRootOnly=.false.)
 
   end subroutine couple_gm_rb_init
 
@@ -105,173 +95,70 @@ contains
     integer, parameter :: nIntegral=6
 
     ! Names of variables to pass
-    character (len=*), parameter :: NameVar='x:y:bmin:I_I:S_I:R_I:B_I:rho:p'
+    character (len=*), parameter:: NameVar='x:y:bmin:I_I:S_I:R_I:B_I:rho:p'
 
     ! Number of variables and points saved into the line data
+    ! Probably nVarLine should be a parameter (=4)
     integer :: nVarLine, nPointLine
 
     ! Buffer for the variables on the 2D RB grid and line data
     real, allocatable :: Integral_IIV(:,:,:), BufferLine_VI(:,:)
 
     ! Buffer for satellite locations   
-    real, dimension(:,:,:), allocatable :: SatPos_DII
+    real, allocatable :: SatPos_DII(:,:,:)
     
     ! Buffer for satellite names   
-    character (len=100), dimension(:), allocatable:: NameSat_I
+    character (len=100), allocatable:: NameSat_I(:)
     
-    ! MPI related variables
-
-    ! MPI status variable
-    integer :: iStatus_I(MPI_STATUS_SIZE)
-
-    ! General error code
-    integer :: iError
-
-    ! Message size
-    integer :: nSize
-
-    ! Name of this method
-    character (len=*), parameter :: NameSub='couple_gm_rb'
-
     logical :: DoTest, DoTestMe
-    integer :: iProcWorld
-
+    character (len=*), parameter :: NameSub='couple_gm_rb'
     !-------------------------------------------------------------------------
     call CON_set_do_test(NameSub,DoTest,DoTestMe)
 
-    iProcWorld = i_proc()
-
-    ! After everything is initialized exclude PEs which are not involved
-    if(.not.UseMe) RETURN
-
-    if(DoTest)write(*,*)NameSub,' starting, iProc=',iProcWorld
-    if(DoTest)write(*,*)NameSub,', iProc, GMi_iProc0, i_proc0(RB_)=', &
-         iProcWorld,i_proc0(GM_),i_proc0(RB_)
-
-    !\
-    ! Allocate buffers both in GM and RB
-    !/
-    allocate(Integral_IIV(iSize,jSize,nIntegral), stat=iError)
-    call check_allocate(iError,NameSub//": Integral_IIV")
+    if(DoTest)write(*,*)NameSub,' starting, iProc=', i_proc()
 
     if (nShareSats > 0) then
-       allocate(SatPos_DII(4,2,nShareSats), stat=iError)
-       call check_allocate(iError,NameSub//": SatPos_DII")
-       allocate(NameSat_I(nShareSats),       stat=iError)
-       call check_allocate(iError,NameSub//": NameSat_I")
-    end if
+       ! If RB sat tracing is enabled, get sat locations from GM
+       allocate(SatPos_DII(4,2,nShareSats), NameSat_I(nShareSats))
+       if(is_proc(GM_)) &
+            call GM_get_sat_for_rb(SatPos_DII, NameSat_I, nShareSats)
 
-    if(DoTest)write(*,*)NameSub,', variables allocated',&
-         ', iProc:',iProcWorld
+       ! The satellite trace is only on the root Source
+       call transfer_real_array(GM_, RB_, 4*2*nShareSats, SatPos_DII)
 
-    !\
-    ! Get field line integrals from GM
-    !/
-    if(is_proc(GM_)) then
-       call GM_get_for_rb_trace(iSize, jSize, NameVar, nVarLine, nPointLine)
-       allocate(BufferLine_VI(nVarLine, nPointLine))
-       call GM_get_for_rb(Integral_IIV, iSize, jSize, nIntegral, &
-            BufferLine_VI, nVarLine, nPointLine, NameVar)
-    end if
-    !\
-    ! If RB sat tracing is enabled, get sat locations from GM
-    !/
-    if(is_proc(GM_).AND.(nShareSats > 0)) &
-         call GM_get_sat_for_rb(SatPos_DII, NameSat_I, nShareSats)
+       ! The satellite names are known all source procs
+       call transfer_string_array(GM_, RB_, nShareSats, NameSat_I, &
+            UseSourceRootOnly = .false.)
 
-    !\
-    ! Transfer variables from GM to RB
-    !/
-    if(i_proc0(RB_) /= i_proc0(GM_))then
-
-       
-       nSize = iSize*jSize*nIntegral
-       if(is_proc0(GM_)) then
-          call MPI_send(nVarLine,1,MPI_INTEGER,i_proc0(RB_),&
-               1,i_comm(),iError)
-          call MPI_send(nPointLine,1,MPI_INTEGER,i_proc0(RB_),&
-               1,i_comm(),iError)
-          call MPI_send(Integral_IIV,nSize,MPI_REAL,&
-               i_proc0(RB_),1,i_comm(),iError)
-          call MPI_send(BufferLine_VI,nVarLine*nPointLine,MPI_REAL,&
-               i_proc0(RB_),2,i_comm(),iError)
-       end if
-       if(is_proc0(RB_))then
-          !setup BufferLine in RB when not sharing proc with GM
-          call MPI_recv(nVarLine,1,MPI_INTEGER,i_proc0(GM_),&
-               1,i_comm(),iStatus_I,iError)
-          call MPI_recv(nPointLine,1,MPI_INTEGER,i_proc0(GM_),&
-               1,i_comm(),iStatus_I,iError)
-          if (.not. allocated(BufferLine_VI)) &
-               allocate(BufferLine_VI(nVarLine, nPointLine))
-          !recieve variables from GM
-          call MPI_recv(Integral_IIV,nSize,MPI_REAL,&
-               i_proc0(GM_),1,i_comm(),iStatus_I,iError)
-          call MPI_recv(BufferLine_VI,nVarLine*nPointLine,MPI_REAL,&
-               i_proc0(GM_),2,i_comm(),iStatus_I,iError)
-       end if
-    end if
-
-    !\
-    ! Transfer satellite names from GM to RB
-    !/   
-    
-    if(nShareSats > 0 .and. i_proc0(RB_) /= i_proc0(GM_))then
-       nSize = nShareSats*100
-       if(is_proc0(GM_)) then
-          call MPI_send(NameSat_I,nSize,MPI_BYTE,i_proc0(RB_),&
-               1,i_comm(),iError)
-       endif
-       if(is_proc0(RB_)) then
-          call MPI_recv(NameSat_I,nSize,MPI_BYTE,i_proc0(GM_),&
-               1,i_comm(),iStatus_I,iError)
-       endif
-    
-   ! Transfer satellite locations from GM to RB
-       
-       nSize = 3*2*nShareSats
-       if(is_proc0(GM_)) then
-          call MPI_send(SatPos_DII,nSize,MPI_REAL,i_proc0(RB_),&
-               1,i_comm(),iError)
-       endif
-       if(is_proc0(RB_)) then
-          call MPI_recv(SatPos_DII,nSize,MPI_REAL,i_proc0(GM_),&
-               1,i_comm(),iStatus_I,iError)
-       endif
-    end if
-    
-    if(DoTest)write(*,*)NameSub,', variables transferred',&
-         ', iProc:',iProcWorld
-       
-    !\
-    ! Put variables into RB
-    !/
-    if(is_proc0(RB_))then
-       call RB_put_from_gm(Integral_IIV,iSize,jSize,nIntegral,&
-            BufferLine_VI,nVarLine,nPointLine,NameVar,tSimulation)
-       if(nShareSats > 0) &
+       if(is_proc(RB_)) &
             call RB_put_sat_from_gm(nShareSats, NameSat_I, SatPos_DII)
-       if(DoTest) &
-            write(*,*)'RB got from GM: RB iProc, Buffer(1,1)=',&
-            iProcWorld,Integral_IIV(1,1,:)
+       deallocate(SatPos_DII, NameSat_I)
     end if
 
-    !\
-    ! Deallocate buffer to save memory
-    !/
+    ! Allocate buffers both in GM and RB
+    allocate(Integral_IIV(iSize,jSize,nIntegral))
+
+    ! Get field line integrals from GM
+    if(is_proc(GM_)) &
+         call GM_get_for_rb_trace(iSize, jSize, NameVar, nVarLine, nPointLine)
+
+    ! Send over size information
+    call transfer_integer(GM_, RB_, nVarLine)
+    call transfer_integer(GM_, RB_, nPointLine)
+    allocate(BufferLine_VI(nVarLine, nPointLine))
+
+    if(is_proc(GM_))call GM_get_for_rb(Integral_IIV, iSize, jSize, nIntegral, &
+            BufferLine_VI, nVarLine, nPointLine, NameVar)
+
+    call transfer_real_array(GM_, RB_, nVarLine*nPointLine, BufferLine_VI)
+    call transfer_real_array(GM_, RB_, iSize*jSize*nIntegral, Integral_IIV)
+       
+    if(is_proc(RB_)) call RB_put_from_gm(Integral_IIV,iSize,jSize,nIntegral,&
+         BufferLine_VI,nVarLine,nPointLine,NameVar,tSimulation)
+
     deallocate(Integral_IIV, BufferLine_VI)
 
-    if (nShareSats > 0) then
-       deallocate(NameSat_I)
-       deallocate(SatPos_DII)
-    end if
-
-    if(DoTest)write(*,*)NameSub,', variables deallocated',&
-         ', iProc:',iProcWorld
-
-    if(DoTest)write(*,*)NameSub,' finished, iProc=',iProcWorld
-
-    ! if(DoTest.and.is_proc0(RB_)) call RB_print_variables('GM',iError)
+    if(DoTest)write(*,*)NameSub,' finished, iProc=', i_proc()
 
   end subroutine couple_gm_rb
 
