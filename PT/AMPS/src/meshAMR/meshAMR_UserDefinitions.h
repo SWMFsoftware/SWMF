d85b262751cbff54c8806b1cb234f6044ae8065b
vtenishe <>
2014-08-16 04:06:57 +0000

    the place holder for the user-defiend model data  structures stored with the mesh
    Former-commit-id: a968adebf0ebd1f3cd5bd865a7eafd556e4d7ec0

---
+++ PT/AMPS/src/meshAMR/meshAMR_UserDefinitions.h
@@ -0,0 +1,12 @@
+//$Id$
+//By default this is an empty file. But configuration scripts (e.g ampsConfig.pl or exosphere.pl) can add some settings into
+//the file after 'MARKER--add-definitions-after-this-line' that later is included into the mesh headers
+
+#ifndef _MESH_AMR_USER_DEFINITION_
+#define _MESH_AMR_USER_DEFINITION_
+
+
+//MARKER--add-definitions-after-this-line
+
+
+#endif
