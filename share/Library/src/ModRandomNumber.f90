5f525549a60d62db8e5099769d424b57e6a2d3db
gtoth <>
2014-09-04 21:06:36 +0000

    Added unit test.
    Former-commit-id: a4e8ebb728a40949b75d754836c975d581c7ef3b

--- share/Library/src/ModRandomNumber.f90
+++ share/Library/src/ModRandomNumber.f90
@@ -2,8 +2,9 @@ module ModRandomNumber
 
   implicit none
 
-  public :: random_integer ! Returns a random integer number between 1 and N
-  public :: random_real    ! Returns a random real number between 0 and 1
+  public:: random_integer ! Returns a random integer number between 1 and N
+  public:: random_real    ! Returns a random real number between 0 and 1
+  public:: test_random_number ! unit test
 
 contains
   !============================================================================
@@ -14,10 +15,10 @@ contains
     ! iSeed needs to be initialized and saved by the calling routine
 
     integer, intent(inout)  :: iSeed
-    !---------------------------------------------------------
+    !--------------------------------------------------------------------------
     iSeed = iSeed*48828125
 
-    if(iSeed < 0) iSeed = (iSeed + 2147483647) + 1
+    if(iSeed < 0)  iSeed = (iSeed + 2147483647) + 1
     if(iSeed == 0) iSeed = 1
 
     random_real = iSeed/2147483647.0
@@ -64,5 +65,58 @@ contains
     random_integer = n*u + 1.0
 
   end function random_integer
+  !============================================================================
+  subroutine test_random_number
 
+    integer, parameter:: nSample = 100000
+    integer:: iSeed=1, iSeed1=11, iSeed2=222, iSeed3=3333
+    integer:: i, iRandom
+    real:: Random, Average, Average2, StdDev, AverageGood, StdDevGood
+    !--------------------------------------------------------------------------
+    write(*,'(a)')'Testing function random_integer(6) - rolling the dice'
+    Average  = 0.0
+    Average2 = 0.0
+    do i = 1, nSample
+       iRandom = random_integer(6, iSeed1, iSeed2, iSeed3)
+       Average = Average + iRandom
+       Average2 = Average2 + iRandom**2
+    end do
+    Average  = Average/nSample
+    Average2 = Average2/nSample
+    StdDev   = sqrt((Average2 - Average**2))
+
+    AverageGood = (1+6)/2.0
+    StdDevGood  = sqrt(sum( (/1,2,3,4,5,6/)**2 )/6.0 - AverageGood**2)
+
+    if(abs(Average - AverageGood) > 0.01) &
+         write(*,*)'Test failed: Average       = ', Average, &
+         ' should be about', AverageGood
+    if(abs(StdDev - StdDevGood) > 0.01) &
+         write(*,*)'Test failed: Std Deviation = ', StdDev, &
+         ' should be about', StdDevGood
+
+    write(*,'(a)')'Testing function random_real'
+    Average  = 0.0
+    Average2 = 0.0
+    do i = 1, nSample
+       Random = random_real(iSeed)
+       Average = Average + Random
+       Average2 = Average2 + Random**2
+    end do
+    Average  = Average/nSample
+    Average2 = Average2/nSample
+    StdDev   = sqrt((Average2 - Average**2))
+
+    AverageGood = 0.5
+    StdDevGood  = sqrt(0.25/3.0)
+
+    if(abs(Average - AverageGood) > 0.001) &
+         write(*,*)'Test failed: Average       = ', Average,&
+         ' should be about', AverageGood
+    if(abs(StdDev - StdDevGood) > 0.001) &
+         write(*,*)'Test failed: Std Deviation = ', StdDev,&
+         ' should be about', StdDevGood
+
+  end subroutine test_random_number
+  
 end module ModRandomNumber
