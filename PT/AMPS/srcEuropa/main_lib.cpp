fd1ee06f8bceeacc6d19438920bdfd0713008610
vtenishe <>
2014-10-21 18:14:03 +0000

    updated
    Former-commit-id: 87d2f15091f89c8a7a9ce3662cfff0e11a0d411f

--- PT/AMPS/srcEuropa/main_lib.cpp
+++ PT/AMPS/srcEuropa/main_lib.cpp
@@ -1082,8 +1082,10 @@ PIC::InitMPI();
 
 	//init particle weight of neutral species that primary source is sputtering
 
+#if  _EXOSPHERE_SOURCE__SOLAR_WIND_SPUTTERING_ == _EXOSPHERE_SOURCE__ON_
 	if(_O2_SPEC_>=0)
 	  PIC::ParticleWeightTimeStep::initParticleWeight_ConstantWeight(_O2_SPEC_);
+#endif
 
 	//prepopulate the solar wind protons
 	//  prePopulateSWprotons(PIC::Mesh::mesh.rootTree);
