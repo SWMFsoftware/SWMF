b3afc79ada8ed5464d4075bf6b4ae7c5d88d2da8
cvs2git <>
2014-11-22 23:18:23 +0000

    This commit was manufactured by cvs2svn to create tag 'trajecory-tracking- added'.
    Former-commit-id: b07af96ea4772ccb74c44cb87df3e92a7dc4dc60

--- gui/htdocs/plot_results/BASE/GM_1Dsat/defaultsBASE.php
+++
@@ -1,9 +0,0 @@
-<?php 
-#  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
-#  For more information, see http://csem.engin.umich.edu/tools/swmf 
-?>
-<?php // 1D Tecplot
-$plotextension = ".plt";
-$macroextension = ".mcr";
-$loadfile = "1Dsat";
-?>
