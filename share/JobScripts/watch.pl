91dc2468e97e82e926a434e61f97708293557674
gtoth <>
2014-12-03 22:26:13 +0000

    Simplified code and improved output.
    Former-commit-id: 3eaf33233fda9f7cc3cf4bdb51f6c937d6a64b64

--- share/JobScripts/watch.pl
+++ share/JobScripts/watch.pl
@@ -20,27 +20,28 @@ watch.pl SWMF >& watch.log &
 }
 
 my @results;
-my %status;
 my $running;
 LOOP:{
     @results = `qstat -u \$USER | grep $pattern`;
+    my $ids;
     foreach (@results){
 	/^(\d+)[^:]+:\d\d ([A-Z]) (\d\d:\d\d)/;
 	print "id=$1 status=$2 wait=$3\n";
-	$status{$1} = $2;
+	$ids .= " $1";
 	$running = $1 if $2 eq "R";
     }
     print "-------------------------\n";
     if($running){
-	my $id;
-	foreach $id (keys %status){
-	    `qdel $id` unless $id eq $running;
-	}
+	$ids =~ s/ $running//;
+	print "qdel $ids\n";
+	`qdel $ids`;
 	last LOOP;
     }
 
     sleep 5;
     redo;
 }
+
+print `qstat -u \$USER`;
 
 print "Finished watch, job $running is running\n";
