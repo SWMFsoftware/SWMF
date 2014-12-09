688ed6d70f21fa3cee1e2e836f162fbed1e45450
vtenishe <>
2014-12-09 05:16:33 +0000

    the coulumn integration of the exospheric species is added
    Former-commit-id: e2fca7d45e3f05c5a6928eea1859a9251d9175f5

--- PT/AMPS/srcEuropa/Europa.cpp
+++ PT/AMPS/srcEuropa/Europa.cpp
@@ -1069,16 +1069,36 @@ double Exosphere::OrbitalMotion::GetTAA(SpiceDouble et) {
 //calcualte the column integrals
 //calculate the sodium column density and plot
 int Exosphere::ColumnIntegral::GetVariableList(char *vlist) {
-  int nVariables=0;
+  int spec,nVariables=0;
+
+  //column density
+  for (spec=0;spec<PIC::nTotalSpecies;spec++) {
+    if (vlist!=NULL) sprintf(vlist,"%s,  \"Column Integral(%s)\"",vlist,PIC::MolecularData::GetChemSymbol(spec));
+    nVariables+=1;
+  }
 
   return nVariables;
 }
 
 void Exosphere::ColumnIntegral::ProcessColumnIntegrationVector(double *res,int resLength) {
+//do nothing
 }
 
 void Exosphere::ColumnIntegral::CoulumnDensityIntegrant(double *res,int resLength,double* x,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node) {
-  int cnt=0;
+  int i,j,k,nd,cnt=0,spec;
+  double NumberDensity;
+
+
+  nd=PIC::Mesh::mesh.fingCellIndex(x,i,j,k,node);
+  for (i=0;i<resLength;i++) res[i]=0.0;
+
+  for (spec=0;spec<PIC::nTotalSpecies;spec++) {
+    //get the local density number
+    NumberDensity=node->block->GetCenterNode(nd)->GetNumberDensity(spec);
+    res[cnt++]=NumberDensity;
+//    res[cnt++]=NumberDensity*node->block->GetCenterNode(nd)->GetMeanParticleSpeed(spec);
+  }
+
 
   if (cnt!=resLength) exit(__LINE__,__FILE__,"Error: the length of the vector is not coinsistent with the number of integrated variables");
 }
