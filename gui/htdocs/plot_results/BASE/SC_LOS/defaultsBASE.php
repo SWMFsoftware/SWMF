dee481b87c568ec0787f45253e609d066004e931
cvs2git <>
2014-12-12 19:26:58 +0000

    This commit was manufactured by cvs2svn to create tag 'rosetta-model-with- background-exosphere--26e6-cells--tower-left'.
    Former-commit-id: b4a6480b47251b997eae28557d140f89eb1c8582

--- gui/htdocs/plot_results/BASE/SC_LOS/defaultsBASE.php
+++
@@ -1,9 +0,0 @@
-<?php 
-#  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
-#  For more information, see http://csem.engin.umich.edu/tools/swmf 
-?>
-<?php // Line of Sight (LOS)
-$plotextension = ".plt";
-$macroextension = ".mcr";
-$loadfile = "LOS";
-?>
