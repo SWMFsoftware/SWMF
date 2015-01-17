0ba891549529127e1980166527a4f3e8e1d9126f
vtenishe <>
2015-01-17 20:24:30 +0000

    the defienition of PIC::CPLR::CCMC::LFM::GetDomainLimits() is added
    Former-commit-id: de167f7cbb279b472bc25bf5b9d2140ad76a5225

--- PT/AMPS/src/pic/pic.h
+++ PT/AMPS/src/pic/pic.h
@@ -2408,6 +2408,7 @@ namespace PIC {
 
       namespace LFM {
         void LoadDataFile(const char *fname,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode=PIC::Mesh::mesh.rootTree);
+        void GetDomainLimits(double *xmin,double *xmax,const char *fname);
       }
 
     }
