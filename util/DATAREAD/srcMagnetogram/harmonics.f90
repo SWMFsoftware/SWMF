7d0a5535f347622f50b1b67bbe3474b058a6c2a8
gtoth <>
2015-01-24 17:07:15 +0000

    Renamed main.f90 --> harmonics.f90
    Former-commit-id: 7b82054cda9a850633153f1ae361ef510579b6e9

--- util/DATAREAD/srcMagnetogram/harmonics.f90
+++ util/DATAREAD/srcMagnetogram/harmonics.f90
@@ -1,15 +1,24 @@
-!  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
+!  Copyright (C) 2002 Regents of the University of Michigan, 
+!  portions used with permission 
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
-program Harmonics
+program harmonics
 
   ! Transform raw magnetogram file into spherical harmonics file
 
   use ModMagHarmonics
+  use ModMpi
+
   implicit none
+  integer:: iError
   !----------------------------------------------------------------------------
+  call MPI_init(iError)
+  call read_harmonics_param
   call read_raw_magnetogram 
   call calc_harmonics
-end program Harmonics
+  call MPI_finalize(iError)
+
+end program harmonics
+!==============================================================================
 subroutine CON_stop(TypeMessage)
   character(LEN=*),intent(in)::TypeMessage
   stop
