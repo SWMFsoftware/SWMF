50839b566e682e0f0d6372c188c08aa558a6cf9c
vtenishe <>
2015-02-05 20:56:01 +0000

    the tables of the photolytic reaction rates and reaction products
    Former-commit-id: 44a53fafb9436fcd0266f3ff2ce286b83c471cbc

---
+++ PT/AMPS/src/models/photolytic_reactions/PhotolyticReactions.h
@@ -0,0 +1,127 @@
+/*
+ * PhotolyticReactions.h
+ *
+ *  Created on: Feb 5, 2015
+ *      Author: vtenishe
+ */
+
+//$Id$
+//the data base of the photolytic reaction parameters for different species
+
+#ifndef _PHOTOLYTICREACTIONS_
+#define _PHOTOLYTICREACTIONS_
+
+#include "pic.h"
+#include "PhotolyticReactions.dfn"
+
+namespace PhotolyticReactions {
+
+  //the general functions and variables used by particular model
+  namespace Huebner1992ASS {
+    inline void GenerateReactionProducts(int &ReactionChannel,int &nReactionProducts, int* ReturnReactionProductTable,
+        double *ReactionRateTable, int nReactionChannels,int* TotalReactionProductTable,int nMaxReactionProducts,double TotalReactionRate) {
+      int i;
+      double summ=0.0;
+
+      //1. Determine the reaction channel
+      for (TotalReactionRate*=rnd(),i=0,summ=0.0;i<nReactionChannels;i++) {
+        summ+=ReactionRateTable[i];
+        if (summ>TotalReactionRate) break;
+      }
+
+      if (i==nReactionChannels) i=-1;
+      ReactionChannel=i;
+
+      //2. Init the list of the reaction products
+      for (i=0,nReactionProducts=0;i<nMaxReactionProducts;i++) {
+        if (TotalReactionProductTable[i+ReactionChannel*nMaxReactionProducts]>=0) {
+          ReturnReactionProductTable[nReactionProducts++]=TotalReactionProductTable[i+ReactionChannel*nMaxReactionProducts];
+        }
+      }
+    }
+  }
+
+  //reaction rates for H2O
+  namespace H2O {
+    namespace Huebner1992ASS {
+      const int nMaxReactionProducts=3;
+      const int nReactionChannels=7;
+      const double TableHeliocentricDistance=1.0*_AU_;
+
+      extern int ReactionProducts[nReactionChannels][nMaxReactionProducts];
+      extern double EmissionWaveLength[nReactionChannels];
+
+      //the table of the rates for each channel
+      extern double ReactionRateTable_QuietSun[nReactionChannels],ReactionRateTable_ActiveSun[nReactionChannels];
+      extern double *ReactionRateTable,*ExcessEnergyTable;
+      extern double TotalReactionRate;
+
+      //the buffer to return the list of the reaction products
+      extern int ReturnReactionProductList[nMaxReactionProducts];
+
+      void Init();
+
+      inline double GetTotalReactionRate(double HeliocentricDistance) {
+        return TotalReactionRate*pow(TableHeliocentricDistance/HeliocentricDistance,2);
+      }
+
+      inline void GenerateReactionProducts(int &ReactionChannel,int &nReactionProducts, int* &ReactionProductTable) {
+        PhotolyticReactions::Huebner1992ASS::GenerateReactionProducts(ReactionChannel,nReactionProducts,ReturnReactionProductList,
+            ReactionRateTable,nReactionChannels,&ReactionProducts[0][0],nMaxReactionProducts,TotalReactionRate);
+
+        ReactionProductTable=ReturnReactionProductList;
+      }
+    }
+
+    inline double GetTotalReactionRate(double HeliocentricDistance) {
+      return Huebner1992ASS::GetTotalReactionRate(HeliocentricDistance);
+    }
+
+    inline void GenerateReactionProducts(int &ReactionChannel,int &nReactionProducts, int* &ReactionProductTable) {
+      Huebner1992ASS::GenerateReactionProducts(ReactionChannel,nReactionProducts,ReactionProductTable);
+    }
+
+  }
+
+  //reaction rates for O2
+  namespace O2 {
+    namespace Huebner1992ASS {
+      using namespace PhotolyticReactions::H2O::Huebner1992ASS;
+
+      const int nMaxReactionProducts=3;
+      const int nReactionChannels=5;
+
+      extern int ReactionProducts[nReactionChannels][nMaxReactionProducts];
+      extern double EmissionWaveLength[nReactionChannels];
+
+      //the table of the rates for each channel
+      extern double ReactionRateTable_QuietSun[nReactionChannels],ReactionRateTable_ActiveSun[nReactionChannels];
+      extern double ExcessEnergyTable_QuietSun[nReactionChannels],ExcessEnergyTable_ActiveSun[nReactionChannels];
+
+      void Init();
+
+      inline double GetTotalReactionRate(double HeliocentricDistance) {
+        return TotalReactionRate*pow(TableHeliocentricDistance/HeliocentricDistance,2);
+      }
+
+      inline void GenerateReactionProducts(int &ReactionChannel,int &nReactionProducts, int* &ReactionProductTable) {
+        PhotolyticReactions::Huebner1992ASS::GenerateReactionProducts(ReactionChannel,nReactionProducts,ReturnReactionProductList,
+            ReactionRateTable,nReactionChannels,&ReactionProducts[0][0],nMaxReactionProducts,TotalReactionRate);
+
+        ReactionProductTable=ReturnReactionProductList;
+      }
+    }
+
+    inline double GetTotalReactionRate(double HeliocentricDistance) {
+      return Huebner1992ASS::GetTotalReactionRate(HeliocentricDistance);
+    }
+
+    inline void GenerateReactionProducts(int &ReactionChannel,int &nReactionProducts, int* &ReactionProductTable) {
+      Huebner1992ASS::GenerateReactionProducts(ReactionChannel,nReactionProducts,ReactionProductTable);
+    }
+  }
+}
+
+
+
+#endif /* PHOTOLYTICREACTIONS_H_ */
---
+++ PT/AMPS/src/models/photolytic_reactions/PhotolyticReactions_Huebner1992ASS.cpp
@@ -0,0 +1,88 @@
+//$Id$
+//data se tof the photolytic reaction model (Huebner 1992, ASS)
+
+
+#include "PhotolyticReactions.h"
+
+
+
+//-----------------------------------   H2O ----------------------------------------------
+int PhotolyticReactions::H2O::Huebner1992ASS::ReactionProducts[nReactionChannels][nMaxReactionProducts]={
+    {_OH_SPEC_,_H_SPEC_,-1},
+    {_H2_SPEC_,_O_SPEC_,-1},
+    {_H_SPEC_,_H_SPEC_,_O_SPEC_},
+    {_H2O_PLUS_SPEC_,_ELECTRON_SPEC_,-1},
+    {_H_SPEC_,_OH_PLUS_SPEC_,_ELECTRON_SPEC_},
+    {_H2_SPEC_,_O_PLUS_SPEC_,_ELECTRON_SPEC_},
+    {_OH_SPEC_,_H_PLUS_SPEC_,_ELECTRON_SPEC_}
+};
+
+double PhotolyticReactions::H2O::Huebner1992ASS::EmissionWaveLength[nReactionChannels]={
+    -1.0,-1.0,-1.0,-1.0,684.4*_A_,684.8*_A_,662.3*_A_
+};
+
+double PhotolyticReactions::H2O::Huebner1992ASS::ReactionRateTable_QuietSun[nReactionChannels]={
+  1.03E-5,5.97E-7,7.55E-7,3.31E-7,5.54E-8,5.85E-9,1.31E-8
+};
+
+double PhotolyticReactions::H2O::Huebner1992ASS::ReactionRateTable_ActiveSun[nReactionChannels]={
+    1.76E-5,1.48E-6,1.91E-6,8.28E-7,1.51E-7,2.21E-8,4.07E-8
+};
+
+double *PhotolyticReactions::H2O::Huebner1992ASS::ReactionRateTable=NULL;
+double PhotolyticReactions::H2O::Huebner1992ASS::TotalReactionRate=0.0;
+double *PhotolyticReactions::H2O::Huebner1992ASS::ExcessEnergyTable=NULL;
+
+void PhotolyticReactions::H2O::Huebner1992ASS::Init() {
+  //init the tables
+  if (_PHOTOLYTIC_REACTIONS__SUN_ == _PHOTOLYTIC_REACTIONS__SUN__ACTIVE_) ReactionRateTable=ReactionRateTable_ActiveSun;
+  else if  (_PHOTOLYTIC_REACTIONS__SUN_ == _PHOTOLYTIC_REACTIONS__SUN__QUIET_) ReactionRateTable=ReactionRateTable_QuietSun;
+  else exit(__LINE__,__FILE__,"Error: the option is unknown");
+
+  //calculate the total rate
+  for (int nChannel=0;nChannel<nReactionChannels;nChannel++) TotalReactionRate+=ReactionRateTable[nChannel];
+}
+
+
+//-----------------------------------   O2 ----------------------------------------------
+int PhotolyticReactions::O2::Huebner1992ASS::ReactionProducts[nReactionChannels][nMaxReactionProducts]={
+    {_O_SPEC_,_O_SPEC_,-1},
+    {_O_SPEC_,_O_SPEC_,-1},
+    {_O_SPEC_,_O_SPEC_,-1},
+    {_O2_PLUS_SPEC_,_ELECTRON_SPEC_,-1},
+    {_O_SPEC_,_O_PLUS_SPEC_,_ELECTRON_SPEC_}
+};
+
+double PhotolyticReactions::O2::Huebner1992ASS::ReactionRateTable_QuietSun[nReactionChannels]={
+  1.45E-7,4.05E-6,3.90E-8,4.46E-7,1.10E-7
+};
+
+double PhotolyticReactions::O2::Huebner1992ASS::ReactionRateTable_ActiveSun[nReactionChannels]={
+  2.15E-7,6.47E-6,9.35E-8,1.18E-6,3.47E-7
+};
+
+double PhotolyticReactions::O2::Huebner1992ASS::EmissionWaveLength[nReactionChannels]={
+    2423.7*_A_,1759.0*_A_,923.0*_A_,-1.0,-1.0
+};
+
+double PhotolyticReactions::O2::Huebner1992ASS::ExcessEnergyTable_QuietSun[nReactionChannels]={
+    4.39*eV2J,1.33*eV2J,0.74*eV2J,15.9*eV2J,23.8*eV2J
+};
+
+double PhotolyticReactions::O2::Huebner1992ASS::ExcessEnergyTable_ActiveSun[nReactionChannels]={
+    5.85*eV2J,1.55*eV2J,0.74*eV2J,19.3*eV2J,27.3*eV2J
+};
+
+void PhotolyticReactions::O2::Huebner1992ASS::Init() {
+  //init the tables
+  if (_PHOTOLYTIC_REACTIONS__SUN_ == _PHOTOLYTIC_REACTIONS__SUN__ACTIVE_) {
+    ReactionRateTable=ReactionRateTable_ActiveSun,ExcessEnergyTable=ExcessEnergyTable_ActiveSun;
+  }
+  else if (_PHOTOLYTIC_REACTIONS__SUN_ == _PHOTOLYTIC_REACTIONS__SUN__QUIET_) {
+    ReactionRateTable=ReactionRateTable_QuietSun,ExcessEnergyTable=ExcessEnergyTable_QuietSun;
+  }
+  else exit(__LINE__,__FILE__,"Error: the option is unknown");
+
+  //calculate the total rate
+  for (int nChannel=0;nChannel<nReactionChannels;nChannel++) TotalReactionRate+=ReactionRateTable[nChannel];
+}
