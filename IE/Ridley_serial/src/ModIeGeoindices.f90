513b176cc041586eda841b54c74c9968c2bed662
gtoth <>
2015-02-05 18:04:58 +0000

    Fixed errors in the message passing.
    Former-commit-id: 5b005a64ab2c6176941682fed0e4d3fa2cdaf66a

--- IE/Ridley_serial/src/ModIeGeoindices.f90
+++ IE/Ridley_serial/src/ModIeGeoindices.f90
@@ -76,10 +76,7 @@ contains
     real, intent(out), dimension(3,nIndexMag) :: MagJhOut_DI, MagJpOut_DI 
 
     integer:: i, iError
-    real, dimension(3,nIndexMag) :: &
-         XyzSmg_DI, MagPerturbJh_DI, MagPerturbJp_DI, &
-         MagPertTotalJh_DI, MagPertTotalJp_DI
-
+    real, dimension(3,nIndexMag) :: XyzSmg_DI, MagJhLocal_DI, MagJpLocal_DI
 
     character(len=*), parameter :: NameSub='get_index_mags'
     logical :: DoTest, DoTestMe
@@ -102,21 +99,20 @@ contains
     end if
 
     ! Check to ensure indexing of array was correct.
-    if( nIndexMag .ne. (i-1) ) call CON_stop(&
+    if( i /= nIndexMag + 1 ) call CON_stop(&
          NameSub//' Indexing error!  Not all magnetometers accounted for.')
 
     ! Collect Hall and Pedersen B perturbations.
-    call iono_mag_perturb(nIndexMag, XyzSmg_DI, MagPerturbJh_DI, MagPerturbJp_DI)
+    call iono_mag_perturb(nIndexMag, XyzSmg_DI, MagJhOut_DI, MagJpOut_DI)
 
-    ! Collect the variables from all the PEs
-    if(nProc>1)then 
-       call MPI_allreduce(MagPertTotalJh_DI, MagJhOut_DI, 3*nIndexMag, &
+    ! Add up perturbations from all the PEs
+    if(nProc > 1)then 
+       MagJhLocal_DI = MagJhOut_DI
+       MagJpLocal_DI = MagJpOut_DI
+       call MPI_allreduce(MagJhLocal_DI, MagJhOut_DI, 3*nIndexMag, &
             MPI_REAL, MPI_SUM, iComm, iError)
-       call MPI_allreduce(MagPertTotalJp_DI, MagJpOut_DI, 3*nIndexMag, &
+       call MPI_allreduce(MagJpLocal_DI, MagJpOut_DI, 3*nIndexMag, &
             MPI_REAL, MPI_SUM, iComm, iError)
-    else
-       MagJhOut_DI = MagPertTotalJh_DI
-       MagJpOut_DI = MagPertTotalJp_DI
     end if
 
   end subroutine get_index_mags
