1b66069bf0598b1625ef3bbf34daad35b6e1698e
vtenishe <>
2015-03-03 11:21:24 +0000

    the definition of PIC::Restart::SavedSamplingDataRestartFileName is moved into pic_restart.cpp
    Former-commit-id: ef77f12e3356f6e1658d583125761babfc4b2a2f

--- PT/AMPS/src/pic/pic_init_const.cpp
+++ PT/AMPS/src/pic/pic_init_const.cpp
@@ -37,5 +37,3 @@ char PIC::InputDataFileDirectory[_MAX_STRING_LENGTH_PIC_]=".";
 bool PIC::ModelTestRun::mode=false;
 int PIC::ModelTestRun::nTotalIteraction=-1;
 
-//the name of the resdtart files
-char PIC::Restart::SavedSamplingDataRestartFileName[_MAX_STRING_LENGTH_PIC_]="";
