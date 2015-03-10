d8fd2a6bb1e13f01c8412b66517afb3f886c7668
dborovik <>
2015-03-10 19:03:09 +0000

    outer heliosphere model header file
    Former-commit-id: 07d2a359649629895a9a9fe4080810ac37ffbb03

---
+++ PT/AMPS/srcOH/OH.h
@@ -0,0 +1,33 @@
+#ifndef _OH_H_
+#define _OH_H_
+
+// AMPS core code
+#include "pic.h"
+
+// Exosphere model
+#include "Exosphere.h"
+
+// self-explanatory
+#include "constants.h"
+
+// SPICE is not used for this application
+// use empty functions to fill in
+#include "SpiceEmptyDefinitions.h"
+
+namespace OH {
+  using namespace Exosphere;
+
+  namespace Sampling{
+    using namespace Exosphere::Sampling;
+  }
+
+  void inline TotalParticleAcceleration(double *accl,int spec,long int ptr,double *x,double *v,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>  *startNode) {
+
+    accl[0]=0.0; accl[1]=0.0;  accl[2]=0.0; 
+
+  }
+  
+
+}
+
+#endif
