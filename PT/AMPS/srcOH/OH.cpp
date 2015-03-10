fb5fbaeb87d2701e2b8d409428f93028a017a127
dborovik <>
2015-03-10 19:03:28 +0000

    outer heliosphere model source code
    Former-commit-id: fbb57c6c4f12fc538910dd5b539022d2570ce1eb

---
+++ PT/AMPS/srcOH/OH.cpp
@@ -0,0 +1,59 @@
+
+
+#include "OH.h"
+
+
+//substitutes for Exosphere functions
+
+//calcualte the true anomaly angle
+double Exosphere::OrbitalMotion::GetTAA(SpiceDouble et) {
+  return 0.0;
+}
+
+int Exosphere::ColumnIntegral::GetVariableList(char *vlist) {
+  int spec,nVariables=0;
+
+  //column density
+  for (spec=0;spec<PIC::nTotalSpecies;spec++) {
+    if (vlist!=NULL) sprintf(vlist,"%s,  \"Column Integral(%s)\"",vlist,PIC::MolecularData::GetChemSymbol(spec));
+    nVariables+=1;
+  }
+
+  return nVariables;
+}
+
+void Exosphere::ColumnIntegral::CoulumnDensityIntegrant(double *res,int resLength,double* x,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node) {
+  int i,j,k,nd,cnt=0,spec;
+  double NumberDensity;
+
+
+  nd=PIC::Mesh::mesh.fingCellIndex(x,i,j,k,node);
+  for (i=0;i<resLength;i++) res[i]=0.0;
+
+  for (spec=0;spec<PIC::nTotalSpecies;spec++) {
+    //get the local density number
+    NumberDensity=node->block->GetCenterNode(nd)->GetNumberDensity(spec);
+    res[cnt++]=NumberDensity;
+    //    res[cnt++]=NumberDensity*node->block->GetCenterNode(nd)->GetMeanParticleSpeed(spec);
+  }
+
+
+  if (cnt!=resLength) exit(__LINE__,__FILE__,"Error: the length of the vector is not coinsistent with the number of integrated variables");
+}
+
+void Exosphere::ColumnIntegral::ProcessColumnIntegrationVector(double *res,int resLength) {
+  //do nothing
+}
+
+double Exosphere::SurfaceInteraction::StickingProbability(int spec,double& ReemissionParticleFraction,double Temp) {
+  ReemissionParticleFraction=0.0;
+
+  return 1.0;
+}
+
+
+double Exosphere::GetSurfaceTemeprature(double cosSubsolarAngle,double *x_LOCAL_SO_OBJECT) {
+
+
+  return 100.0;
+}
