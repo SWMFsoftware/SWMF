7782df7a368e001c8018ae03da648c995156f94c
dborovik <>
2015-03-11 21:18:03 +0000

    implementation for namespace Output
    Former-commit-id: 296ef805e033928ee58b0aec7419dc4e94584edf

--- PT/AMPS/srcOH/OH.cpp
+++ PT/AMPS/srcOH/OH.cpp
@@ -1,8 +1,107 @@
-
-
 #include "OH.h"
+int OH::Output::TotalDataLength = 0; 
+int OH::Output::ohSourceDensityOffset =-1; 
+int OH::Output::ohSourceMomentumOffset=-1;
+int OH::Output::ohSourceEnergyOffset  =-1;
 
 
+void OH::Output::PrintVariableList(FILE* fout,int DataSetNumber) {
+  fprintf(fout,",\"ohSourceDensity\",\"ohSourceMomentum\",\"ohSourceEnergy\"");
+}
+
+void OH::Output::Interpolate(PIC::Mesh::cDataCenterNode** InterpolationList,double *InterpolationCoeficients,int nInterpolationCoeficients,PIC::Mesh::cDataCenterNode *CenterNode){
+
+  double S1=0.0, S2=0.0, S3=0.0;
+  int i,idim;
+  char *SamplingBuffer;
+
+  for (i=0;i<nInterpolationCoeficients;i++) {
+
+    S1+=(*((double*)(InterpolationList[i]->GetAssociatedDataBufferPointer()+OH::Output::ohSourceDensityOffset)))*InterpolationCoeficients[i];
+
+    S2+=(*((double*)(InterpolationList[i]->GetAssociatedDataBufferPointer()+OH::Output::ohSourceMomentumOffset)))*InterpolationCoeficients[i];
+
+    S3+=(*((double*)(InterpolationList[i]->GetAssociatedDataBufferPointer()+OH::Output::ohSourceEnergyOffset)))*InterpolationCoeficients[i];
+  }
+
+  memcpy(CenterNode->GetAssociatedDataBufferPointer()+OH::Output::ohSourceDensityOffset,&S1,sizeof(double));
+  memcpy(CenterNode->GetAssociatedDataBufferPointer()+OH::Output::ohSourceMomentumOffset,&S2,sizeof(double));
+  memcpy(CenterNode->GetAssociatedDataBufferPointer()+OH::Output::ohSourceEnergyOffset,&S3,sizeof(double));
+
+}
+
+void OH::Output::PrintData(FILE* fout,int DataSetNumber,CMPI_channel *pipe,int CenterNodeThread,PIC::Mesh::cDataCenterNode *CenterNode){
+  double t;
+
+  //SourceDensity
+  if (pipe->ThisThread==CenterNodeThread) {
+    t= *((double*)(CenterNode->GetAssociatedDataBufferPointer()+OH::Output::ohSourceDensityOffset));
+  }
+
+  if (pipe->ThisThread==0) {
+    if (CenterNodeThread!=0) pipe->recv(t,CenterNodeThread);
+
+    fprintf(fout,"%e ",t);
+  }
+  else pipe->send(t);
+
+  //SourceMomentum
+  if (pipe->ThisThread==CenterNodeThread) {
+    t= *((double*)(CenterNode->GetAssociatedDataBufferPointer()+OH::Output::ohSourceMomentumOffset));
+  }
+
+  if (pipe->ThisThread==0) {
+    if (CenterNodeThread!=0) pipe->recv(t,CenterNodeThread);
+
+    fprintf(fout,"%e ",t);
+  }
+  else pipe->send(t);
+
+  //SourceDensity
+  if (pipe->ThisThread==CenterNodeThread) {
+    t= *((double*)(CenterNode->GetAssociatedDataBufferPointer()+OH::Output::ohSourceEnergyOffset));
+  }
+
+  if (pipe->ThisThread==0) {
+    if (CenterNodeThread!=0) pipe->recv(t,CenterNodeThread);
+
+    fprintf(fout,"%e ",t);
+  }
+  else pipe->send(t);
+
+}
+
+int OH::Output::RequestDataBuffer(int offset){
+  OH::Output::ohSourceDensityOffset=offset;
+  OH::Output::TotalDataLength = 1;
+
+  OH::Output::ohSourceMomentumOffset=offset;
+  OH::Output::TotalDataLength++;
+
+  OH::Output::ohSourceEnergyOffset=offset;
+  OH::Output::TotalDataLength++;
+
+  return OH::Output::TotalDataLength*sizeof(double);
+}
+
+
+void OH::Output::Init() {
+  //request sampling buffer and particle fields
+  PIC::IndividualModelSampling::RequestStaticCellData.push_back(OH::Output::RequestDataBuffer);
+
+  //print out of the otuput file
+  PIC::Mesh::PrintVariableListCenterNode.push_back(OH::Output::PrintVariableList);
+  PIC::Mesh::PrintDataCenterNode.push_back(OH::Output::PrintData);
+  PIC::Mesh::InterpolateCenterNode.push_back(OH::Output::Interpolate);
+}
+
+void OH::Init_BeforeParser(){
+  Exosphere::Init_BeforeParser();
+  OH::Output::Init();
+}
+
+
+//-----------------------------------------------------------------------------
 //substitutes for Exosphere functions
 
 //calcualte the true anomaly angle
