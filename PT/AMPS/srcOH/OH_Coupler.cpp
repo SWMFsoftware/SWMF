1c6cc09671b165bcad5a304df02716f2a15e2702
vtenishe <>
2015-03-20 18:58:15 +0000

    function OH::Coupling::Send() is added to send AMPS' data to OH
    Former-commit-id: 28a63072a0a1cc41823a54f3033b8925cb61cb08

---
+++ PT/AMPS/srcOH/OH_Coupler.cpp
@@ -0,0 +1,72 @@
+/*
+ * OH_Coupler.cpp
+ *
+ *  Created on: Mar 20, 2015
+ *      Author: vtenishe
+ */
+//$Id$
+//Coupler functions for OH
+
+#include "OH.h"
+
+void OH::Coupling::Send(char *NameVar, int *nVarIn, int *nDimIn, int *nPoint, double *Xyz_DI, double *Data_VI) {
+  int i0=0,i1=0;
+  char vname[200];
+
+  //get the offsets
+  int Density_AMPS2OH=-1;
+
+  while ((NameVar[i0]!=0)&&(NameVar[i0]==' ')) i0++;
+
+  for (int n=0;n<(*nVarIn);n++) {
+    i1=i0;
+    while (NameVar[i1]!=' ') {
+      vname[i1-i0]=tolower(NameVar[i1]);
+      i1++;
+    }
+
+    vname[i1-i0]=0;
+
+    if (strcmp(vname,"rho")==0) Density_AMPS2OH=n;
+
+    n++;
+    i0=i1;
+
+    while ((NameVar[i0]!=0)&&(NameVar[i0]==' ')) i0++;
+  }
+
+  //save the data
+  cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node=NULL;
+  PIC::Mesh::cDataCenterNode *cell;
+  char *AssociatedDataPointer;
+  int i,j,k,LocalCellNumber;
+
+  for (int pt=0;pt<(*nPoint);pt++) {
+    double x[3]={0.0,0.0,0.0}; //the location of the point; to be sure that it has all 3 components
+
+    //find the block
+    memcpy(x,Xyz_DI+pt*(*nDimIn),(unsigned int)(*nDimIn)*sizeof(double));
+    if ((node=PIC::Mesh::mesh.findTreeNode(x,node))==NULL) exit(__LINE__,__FILE__,"Error: can not find the block");
+    if (node->Thread!=PIC::ThisThread) exit(__LINE__,__FILE__,"Error: the point data is located on another processor");
+
+    //find the cell
+    if ((LocalCellNumber=PIC::Mesh::mesh.fingCellIndex(x,i,j,k,node,false))==-1) exit(__LINE__,__FILE__,"Error: cannot find the cell");
+
+    cell=node->block->GetCenterNode(LocalCellNumber);
+    AssociatedDataPointer=cell->GetAssociatedDataBufferPointer();
+
+    if (PIC::LastSampleLength!=0) {
+      Data_VI[Density_AMPS2OH+pt*(*nVarIn)]=
+          (*(0+(double*)(AssociatedDataPointer+PIC::Mesh::completedCellSampleDataPointerOffset+OH::Output::ohSourceDensityOffset)))/PIC::LastSampleLength;
+
+    }
+    else {
+      Data_VI[Density_AMPS2OH+pt*(*nVarIn)]=0.0;
+    }
+
+  }
+
+}
+
+
+
