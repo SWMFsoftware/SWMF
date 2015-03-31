f20dd7179ca7de3efb9c6453c6d9b0bc5f258561
dborovik <>
2015-03-31 20:16:11 +0000

    added a fix to avoid possible division by zero
    Former-commit-id: 6a8164ba2c8e4706432f389381846305f31b92bd

--- PT/AMPS/src/models/charge_exchange/ChargeExchange_MaherTinsley.cpp
+++ PT/AMPS/src/models/charge_exchange/ChargeExchange_MaherTinsley.cpp
@@ -15,7 +15,12 @@ double ChargeExchange::MaherTinsley::LifeTime(int      spec,
   for(int idim = 0; idim < 3; idim++ )
     omega += pow(vParticle[idim]-vPlasma[idim], 2.0);
   omega = pow(omega, 0.5) / v_th;
-  v_rel = v_th * ( exp(-omega*omega)/sqrtPi + (omega + 0.5/omega)*erf(omega));
+  if(omega > 1E-2)
+    v_rel = v_th* ( exp(-omega*omega)/sqrtPi + (omega + 0.5/omega)*erf(omega));
+  else
+    // Taylor expansion in vicinity of omega = 0 to avoid divison by zero;
+    // relative error < 1E-4
+    v_rel = v_th* 2.0 * (1.0 + omega*omega/3.0) / sqrtPi;
 
   sigma = pow(1.6 - 0.0695 * log(v_rel), 2.0)*1E-14*1E-4; // m^2
   return 1.0 / (PlasmaNumberDensity * v_rel * sigma);
