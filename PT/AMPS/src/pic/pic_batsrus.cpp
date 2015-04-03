b2f6b3f3af4dfeed0729ce3579b02f16cbb1e02b
vtenishe <>
2015-04-03 20:21:29 +0000

    parsing of the units line is added
    Former-commit-id: 2492aa63c63b24fd6e3bcaa6ec8237c85f42ab01

--- PT/AMPS/src/pic/pic_batsrus.cpp
+++ PT/AMPS/src/pic/pic_batsrus.cpp
@@ -17,23 +17,60 @@
 //the definition of the fortran wrapper functinos from pic_batsrus_wrapper.f90
 extern "C"
 {
-  void batsrus2amps_get_nvar_(int *nVar);
-  void batsrus2amps_domain_limits_(double *xmin,double *xmax);
-  void batsrus2amps_openfile_(char *FileName,int *FileNameLength);
+  //manipulate the file
   void batsrus2amps_read_file_header_(char *FileName,int *FileNameLength);
+  void batsrus2amps_openfile_(char *FileName,int *FileNameLength);
   void batsrus2amps_closefile_();
-  void batsrus2amps_get_data_point_(double* x,double *res,int *FoundFlag);
-  void batsrus2amps_get_namevardata_(char *varlist,int *varlistlength);
+
+  //get the side of the domain
+  void batsrus2amps_domain_limits_(double *xmin,double *xmax);
+
+  //set MPI data
   void batsrus2amps_set_mpi_parameters_(int* ThisThread,int *nTotalThreads,int *Communicator);
+
+  //get number of the variable, the list of the variables and their uinits
+  void batsrus2amps_get_nvar_(int *nVar);
+  void batsrus2amps_get_namevardata_(char *varlist,int *varlistlength);
+  void batsrus2amps_get_nameunitdata_(char *unitlist,int *unitlistlength);
+
+  //get the value of the variables
+  void batsrus2amps_get_data_point_(double* x,double *res,int *FoundFlag);
 }
 
 //definition of the variabled from BATSRUS namespace
 double PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::PlasmaSpeciesAtomicMass=_AMU_;
 double PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::UnitLength=1.0;
+char PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::filename[_MAX_STRING_LENGTH_PIC_]="";
+bool PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::InitFlag=false;
+
+//init the BATSRUS data file AMR information
+void PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::Init(const char *fname) {
+  int length;
+  
+  InitFlag=true;
+
+  //get the full file name
+  sprintf(filename,"%s/%s",PIC::CPLR::DATAFILE::path,fname);
+
+  //export MPI parameters into BATL
+  int iComm=MPI_Comm_c2f(MPI_GLOBAL_COMMUNICATOR);
+  batsrus2amps_set_mpi_parameters_(&PIC::ThisThread,&PIC::nTotalThreads,&iComm);
+  
+  //init BATSRUS' fiile reader
+  for (length=0;length<_MAX_STRING_LENGTH_PIC_;length++) if (filename[length]==0) break;
+  batsrus2amps_read_file_header_(filename,&length);
+} 
+
+void  PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::GetDomainLimits(double *xmin,double *xmax) {
+  int idim;
+
+  batsrus2amps_domain_limits_(xmin,xmax);
+  for (idim=0;idim<3;idim++) xmin[idim]*=UnitLength,xmax[idim]*=UnitLength;
+}
 
 //read BATSRUS' .idl file
-void PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::LoadDataFile(const char *fname,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode) {
-  static double *StateLocal=NULL,*State=NULL;
+void PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::LoadDataFile(cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode) {
+  static double *StateLocal=NULL,*State=NULL,*PhysicalVariableUnitConversionTable=NULL;
   static int nVar;
 
   //the offsets of the physical variables in the .idl file
@@ -42,22 +79,16 @@ void PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::LoadDataFile(const char *fname,cTreeN
   static int bxBATSRUS2AMPS=-1,byBATSRUS2AMPS=-1,bzBATSRUS2AMPS=-1;
   static int pBATSRUS2AMPS=-1;
 
+  if (InitFlag==false) exit(__LINE__,__FILE__,"Error: the reader needs to be initialized first! Call PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::InitAMR before PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::LoadDataFile");
+
   if (startNode==PIC::Mesh::mesh.rootTree) {
     //open data file
-    char fullname[_MAX_STRING_LENGTH_PIC_],NameVar[_MAX_STRING_LENGTH_PIC_];
+    char NameVar[_MAX_STRING_LENGTH_PIC_];
     int length=_MAX_STRING_LENGTH_PIC_;
 
-    //get the full file name
-    sprintf(fullname,"%s/%s",PIC::CPLR::DATAFILE::path,fname);
-
-    //export MPI parameters into BATL
-    int iComm=MPI_Comm_c2f(MPI_GLOBAL_COMMUNICATOR);
-    batsrus2amps_set_mpi_parameters_(&PIC::ThisThread,&PIC::nTotalThreads,&iComm);
-
-
     //open the data file
-    for (length=0;length<_MAX_STRING_LENGTH_PIC_;length++) if (fullname[length]==0) break;
-    batsrus2amps_openfile_(fullname,&length);
+    for (length=0;length<_MAX_STRING_LENGTH_PIC_;length++) if (filename[length]==0) break;
+    batsrus2amps_openfile_(filename,&length);
 
     //read the variable nabmer and string
     length=_MAX_STRING_LENGTH_PIC_;
@@ -65,6 +96,9 @@ void PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::LoadDataFile(const char *fname,cTreeN
     batsrus2amps_get_nvar_(&nVar);
     State=new double [nVar+1];
     StateLocal=new double [nVar+1];
+    PhysicalVariableUnitConversionTable=new double [nVar+1];
+
+    for (int i=0;i<nVar+1;i++) PhysicalVariableUnitConversionTable[i]=0.0;
 
     //parse the variable line
     char vname[200];
@@ -110,13 +144,49 @@ void PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::LoadDataFile(const char *fname,cTreeN
 
     if (pBATSRUS2AMPS==-1) exit(__LINE__,__FILE__,"Error: p is not present in the BARSRUS .idl file. Please add this variable to the .idl file.");
 
-    //the first element in the state vector is the "weight" -> adjust the offsets
-    rhoBATSRUS2AMPS++;
-    mxBATSRUS2AMPS++,myBATSRUS2AMPS++,mzBATSRUS2AMPS++;
-    bxBATSRUS2AMPS++,byBATSRUS2AMPS++,bzBATSRUS2AMPS++;
-    pBATSRUS2AMPS++;
+
+
+    //parse the unit line
+    char UnitVar[_MAX_STRING_LENGTH_PIC_],uname[200];
+
+    length=_MAX_STRING_LENGTH_PIC_;
+    batsrus2amps_get_nameunitdata_(UnitVar,&length);
+    i0=0,i1=0;
+
+    while ((UnitVar[i0]!=0)&&(UnitVar[i0]==' ')) i0++;
+
+    for (int n=0;n<nVar;n++) {
+      i1=i0;
+      while (UnitVar[i1]!=' ') {
+        uname[i1-i0]=tolower(UnitVar[i1]);
+        i1++;
+      }
+
+      uname[i1-i0]=0;
+
+      if (strcmp(uname,"mp/cc")==0) PhysicalVariableUnitConversionTable[n+1]=1.0E6/PlasmaSpeciesAtomicMass;
+      if (strcmp(uname,"km/s")==0) PhysicalVariableUnitConversionTable[n+1]=1.0E3;
+      if (strcmp(uname,"nt")==0) PhysicalVariableUnitConversionTable[n+1]=1.0E-9;
+      if (strcmp(uname,"npa")==0) PhysicalVariableUnitConversionTable[n+1]=1.0E-9;
+
+      i0=i1;
+      while ((UnitVar[i0]!=0)&&(UnitVar[i0]==' ')) i0++;
+    }
   }
 
+  //the first element in the state vector is the "weight" -> adjust the offsets
+  rhoBATSRUS2AMPS++;
+  mxBATSRUS2AMPS++,myBATSRUS2AMPS++,mzBATSRUS2AMPS++;
+  bxBATSRUS2AMPS++,byBATSRUS2AMPS++,bzBATSRUS2AMPS++;
+  pBATSRUS2AMPS++;
+
+  //check whether the unit conversion factors are defined foe all physical variables
+  if ((PhysicalVariableUnitConversionTable[rhoBATSRUS2AMPS]==0.0) || \
+      (PhysicalVariableUnitConversionTable[mxBATSRUS2AMPS]==0.0) || (PhysicalVariableUnitConversionTable[myBATSRUS2AMPS]==0.0)  || (PhysicalVariableUnitConversionTable[mzBATSRUS2AMPS]==0.0) || \
+      (PhysicalVariableUnitConversionTable[bxBATSRUS2AMPS]==0.0) || (PhysicalVariableUnitConversionTable[byBATSRUS2AMPS]==0.0)  || (PhysicalVariableUnitConversionTable[bzBATSRUS2AMPS]==0.0) || \
+      (PhysicalVariableUnitConversionTable[pBATSRUS2AMPS]==0.0)) {
+    exit(__LINE__,__FILE__,"Error: the physical variable unit conversion factor is not defined");
+  }
 
   //perform the interpolation loop
   int i,j,k,nd,idim;
@@ -158,20 +228,17 @@ void PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::LoadDataFile(const char *fname,cTreeN
       if (IsFound==true) {
         int idim;
 
-        //convert momentum into velocity
-        if (State[rhoBATSRUS2AMPS]>0.0) for (idim=0;idim<3;idim++) State[mxBATSRUS2AMPS+idim]/=State[rhoBATSRUS2AMPS];
-
         //the order of the state vector: number density, temperature
-        *((double*)(offset+PlasmaNumberDensityOffset))=State[rhoBATSRUS2AMPS]/PlasmaSpeciesAtomicMass;
-        *((double*)(offset+PlasmaTemperatureOffset))=(State[rhoBATSRUS2AMPS]>0.0) ? State[pBATSRUS2AMPS]/(Kbol*State[rhoBATSRUS2AMPS]/PlasmaSpeciesAtomicMass) : 0.0;
+        *((double*)(offset+PlasmaNumberDensityOffset))=State[rhoBATSRUS2AMPS]*PhysicalVariableUnitConversionTable[rhoBATSRUS2AMPS];
+        *((double*)(offset+PlasmaTemperatureOffset))=(State[rhoBATSRUS2AMPS]>0.0) ? PhysicalVariableUnitConversionTable[pBATSRUS2AMPS]*State[pBATSRUS2AMPS]/(Kbol*State[rhoBATSRUS2AMPS]*PhysicalVariableUnitConversionTable[rhoBATSRUS2AMPS]) : 0.0;
 
         //get pressure
-        *((double*)(offset+PlasmaPressureOffset))=State[pBATSRUS2AMPS];
+        *((double*)(offset+PlasmaPressureOffset))=State[pBATSRUS2AMPS]*State[pBATSRUS2AMPS];
 
         //bulk velocity and magnetic field
         for (idim=0;idim<3;idim++) {
-          *((double*)(offset+PlasmaBulkVelocityOffset+idim*sizeof(double)))=State[mxBATSRUS2AMPS+idim];
-          *((double*)(offset+MagneticFieldOffset+idim*sizeof(double)))=State[bxBATSRUS2AMPS+idim];
+          *((double*)(offset+PlasmaBulkVelocityOffset+idim*sizeof(double)))=State[mxBATSRUS2AMPS+idim]*PhysicalVariableUnitConversionTable[mxBATSRUS2AMPS];
+          *((double*)(offset+MagneticFieldOffset+idim*sizeof(double)))=State[bxBATSRUS2AMPS+idim]*PhysicalVariableUnitConversionTable[bxBATSRUS2AMPS];
         }
 
       }
@@ -188,7 +255,7 @@ void PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::LoadDataFile(const char *fname,cTreeN
     }
   }
   else {
-    for (int nDownNode=0;nDownNode<(1<<3);nDownNode++) if (startNode->downNode[nDownNode]!=NULL) LoadDataFile(fname,startNode->downNode[nDownNode]);
+    for (int nDownNode=0;nDownNode<(1<<3);nDownNode++) if (startNode->downNode[nDownNode]!=NULL) LoadDataFile(startNode->downNode[nDownNode]);
   }
 
 
@@ -197,6 +264,7 @@ void PIC::CPLR::DATAFILE::BATSRUS::OUTPUT::LoadDataFile(const char *fname,cTreeN
 
     delete [] State;
     delete [] StateLocal;
+    delete [] PhysicalVariableUnitConversionTable;
 
     batsrus2amps_closefile_();
   }
