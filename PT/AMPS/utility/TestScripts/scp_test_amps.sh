32573a488bd14c35855555a26abe6d7a2f9f22d2
dborovik <>
2015-05-09 18:07:46 +0000

    the script to transfer nightly tests' results to server
    Former-commit-id: f19be75ea3bc8d9e59ef3c17c1c3d50fb6b2a8bc

---
+++ PT/AMPS/utility/TestScripts/scp_test_amps.sh
@@ -0,0 +1,64 @@
+#!/bin/csh
+# This sends the results of AMPS nightly tests to the server:
+#   herot.engin.umich.edu 
+# for further processing
+# This script is meant to be used by the AMPS developers.
+
+# The script can be executed by simply typing
+#
+# ./scp_run_amps.sh
+#
+# To scp the AMPS tests' resuults perodically, you can use the crontab facility.
+# Type 'crontab -e' to add a new entry to your crontab.
+# Here is an example entry for for scp'ing nightly runs results at 8:00 am:
+#
+# 30 8 * * * $HOME/bin/scp_test_amps.sh
+
+# set the working directory
+set WorkDir = $HOME  
+
+#>Pleiades ##############
+#set WorkDir =         <#
+
+#>Yellowstone ###########
+#set WorkDir =         <#
+
+# Go to your home directory
+cd $WorkDir/Tmp_AMPS_test
+
+# Create file with results' summary and scp to server
+
+# GNU compiled test
+#>GNUAll ################################################################
+#cd GNU/AMPS                                                            #
+#ls -ltr  *diff > test_amps.res                                         #
+#echo '=============================================================='\ #
+#                >> test_amps.res                                       # 
+#head -100 *diff >> test_amps.res                                       #
+#scp test_amps.res test_amps.log \                                      #
+#    dborovik@herot.engin.umich.edu:Sites/Current/`hostname -s`_gnu/    #
+#cd ../..                                                              <#
+
+
+# Intel compiled test
+#>IntelAll ##############################################################
+#cd Intel/AMPS                                                          #
+#ls -ltr  *diff > test_amps.res                                         #
+#echo '=============================================================='\ #
+#                >> test_amps.res                                       #
+#head -100 *diff >> test_amps.res                                       #
+#scp test_amps.res test_amps.log \                                      #
+#    dborovik@herot.engin.umich.edu:Sites/Current/`hostname -s`_intel/  #
+#cd ../..                                                              <#
+
+# PGI compiled test
+#>PGIAll ################################################################
+#cd PGI/AMPS                                                            #
+#ls -ltr  *diff > test_amps.res                                         #
+#echo '=============================================================='\ #
+#                >> test_amps.res                                       #
+#head -100 *diff >> test_amps.res                                       #
+#scp test_amps.res test_amps.log \                                      #
+#    dborovik@herot.engin.umich.edu:Sites/Current/`hostname -s`_pgi/    #
+#cd ../..                                                              <#
+
