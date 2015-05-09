f997edc0a643d96b3e3c514031503852ab632911
dborovik <>
2015-05-09 18:08:56 +0000

    adjasted to the new structure of the script running nightly tests
    Former-commit-id: 1e53b305b509ec06b46d9e3bf13b1108faa89fda

--- PT/AMPS/utility/TestScripts/SetNightlyTest.pl
+++ PT/AMPS/utility/TestScripts/SetNightlyTest.pl
@@ -2,28 +2,6 @@
 # The scrpit sets up a nightly tests of AMPS on the current machine
 use strict;
 
-
-# the server name
-my $server = 'dborovik@herot.engin.umich.edu';
-
-# the full hostname of the current machine
-my $hostname = `hostname -f`;
-
-# home directory
-my $home = $ENV{"HOME"};
-
-# the machine is from a list of machines with special scripts
-# (Pleiades, Yelowstone, etc.)
-my $IsSpecial='';
-# the name for the test (if chosen)
-our $TestName;
-
-# pfe number on Pleiades to set tests on
-my $PFE = "26";
-
-# name of the script to be installed
-my $script="";
-
 # read CRONTAB file to check whether the tests have already been installed
 my $cron = `crontab -l 2> /dev/null`;
 
@@ -41,13 +19,81 @@ if($cron =~ m/run_test_amps/) {
     return 1;
 };
 
-# set MAILTO to empty string unless it has already been set to some value
-$cron = "MAILTO=\"\"\n" . $cron unless ($cron =~ m/^MAILTO/);
+#------------------------------------------------------------------------------
+# Proceed with tests' installation
 
-# add test execution to CRONTAB based on the current machine
+# the server name
+my $server = 'dborovik@herot.engin.umich.edu';
+
+# the full hostname of the current machine
+my $hostname = `hostname -f`;
+
+# home directory
+my $home = $ENV{"HOME"};
+
+# pfe number on Pleiades to set tests on
+my $PFE = "26";
+
+# date on the machine
+my $date = `date`;
+
+# hash for installing a test time based on time zone
+my %TZ2CRON_run = (
+    "EDT" => "30 23", "EST" => "30 23", 
+    "CDT" => "30 22", "CST" => "30 22",
+    "MDT" => "30 21", "MST" => "30 21",
+    "PDT" => "30 20", "PST" => "30 20");
+my %TZ2CRON_scp = (
+    "EDT" => "30 8", "EST" => "30 8", 
+    "CDT" => "30 7", "CST" => "30 7",
+    "MDT" => "30 6", "MST" => "30 6",
+    "PDT" => "30 5", "PST" => "30 5");
+
+# name of the script to be installed
+my $run_script="run_test_amps.sh";
+my $scp_script="scp_test_amps.sh";
+
+# the name for the test (if chosen)
+our $TestName;
+# check if a name for the test has been chosen
+$TestName = `hostname -s` unless($TestName);
+chomp($TestName);
+
+
+# list of compilers to use
+our @Compilers;
+
+# list of supported compilers' 
+#naming is case sensitive and the same as in %Blocks hash table
+my @CompilersSupported = ("GNU", "Intel","PGI");
+
+# HASH TABLE with blocks in the $run_script
+# the body of the named script is divided into blocks of the format
+#  #>BlockName ##############
+#  #command_1               #
+#  # ...                    #
+#  #command_last           <#
+# certain blocks should be uncommented at the test installation,
+# e.g. if test is GNU compiled, then block #>GNU <# should be uncommented,
+#      if test is set on Pleiades, then #>Pleiades <# should be uncommented
+
+# the table below is initialized to 0 for each block
+# further, this scripts determines which blocks should be activated
+my %Blocks = (
+    "GNUAll"       => "0",
+    "GNUOther"     => "0",
+    "IntelAll"     => "0",
+    "IntelOther"   => "0",
+    "PGIAll"       => "0",
+    "PGIOther"     => "0",
+    "Pleiades"     => "0",
+    "Yellowstone"  => "0"
+    );
+
+# fill the hash table %Blocks
 if($hostname =~ m/^pfe(.*).nas.nasa.gov/){
     # Pleiades
-    $IsSpecial = '1';
+    $Blocks{'Pleiades'} = "1";
     if($1 ne $PFE){
 	print 
 	    "WARNING: nightly tests on Pleiades should be set on pfe$PFE,\n"
@@ -59,39 +105,55 @@ if($hostname =~ m/^pfe(.*).nas.nasa.gov/){
 	    "Tests\' installation has been ABORTED!\n";
 	return 1;	
     }
-    else {
-	# edit the CRONTAB file & choose the script to install
-	$cron = $cron . "30 0 * * * \$HOME/bin/run_test_amps.pleiades.sh\n";
-	$script = "run_test_amps.pleiades.sh"
+}
+elsif($hostname =~ m/^yslogin(.*)/){ 
+    # Yellowstone
+    $Blocks{"Yellowstone"} = "1";
+}
+
+# set hashes for compilers (already converted to lower case)
+if($Compilers[0] eq "all"){
+    foreach my $CompilerSupported(@CompilersSupported){
+	$Blocks{$CompilerSupported."All"  } = "1";
+	unless($Blocks{"Pleiades"} or $Blocks{"Yellowstone"}){
+	    $Blocks{$CompilerSupported."Other"  } = "1";
+	}
     }
 }
-else{ 
-    # any other machine
-    # edit the CRONTAB file & choose the script to install
-    $cron = $cron . "30 0 * * * \$HOME/bin/run_test_amps.sh\n";
-    $script = "run_test_amps.sh"
+else{
+  LoopCompiler:
+    foreach my $Compiler (@Compilers){
+	foreach my $CompilerSupported(@CompilersSupported){
+	    if ($Compiler eq lc($CompilerSupported)){
+		$Blocks{$CompilerSupported."All"  } = "1";
+		unless($Blocks{"Pleiades"} or $Blocks{"Yellowstone"}){
+		    $Blocks{$CompilerSupported."Other"  } = "1";
+		}
+		next LoopCompiler;
+	    }
+	die "ERROR: Compiler is not recognized!";
+	}
+    }
 }
 
+# copy $run_script to $HOME/bin
 system('mkdir -p $HOME/bin');
-system("cp ./utility/TestScripts/$script $home/bin");
+system("cp ./utility/TestScripts/$run_script $home/bin");
+system("cp ./utility/TestScripts/$scp_script $home/bin");
 
-# check if a name for a test has been chosen
-$TestName = `hostname -s` unless($TestName);
-chomp($TestName);
-unless($IsSpecial) {
-    # read the script's content
-    open(my $fin,'<',"$home/bin/$script");
-    local $/ = undef;
-    my $fcontent = <$fin>;
-    close $fin;
-    # put a test's name into the content of script
-    $fcontent =~ s/Sites\/Current\/\`hostname -s\`/Sites\/Current\/$TestName/g;
-    # write the changed content into the script
-    open(my $fout,'>',"$home/bin/$script");
-    print $fout $fcontent;
-    close $fout;
-    system("ssh $server mkdir -p Sites/Current/$TestName");
-}
+&prepare_script("$home/bin/$run_script");
+&prepare_script("$home/bin/$scp_script");
+
+#------------------------------------------------------------------------------
+# Set the CRONTAB script
+
+# set MAILTO to empty string unless it has already been set to some value
+$cron = "MAILTO=\"\"\n" . $cron unless ($cron =~ m/^MAILTO/);
+
+# set scripts execution
+my $TZ = `date +%Z`; chomp($TZ);
+$cron .= $TZ2CRON_run{$TZ}." * * * \$HOME/bin/$run_script\n";
+$cron .= $TZ2CRON_scp{$TZ}." * * * \$HOME/bin/$scp_script\n";
 
 
 # write the new CRONTAB content to a temporary file
@@ -103,6 +165,51 @@ close $fh;
 `crontab ./tmp_crontab`;
 
 # remove the temporary file
-`rm -rf tmp_crontab`;
+#`rm -rf tmp_crontab`;
+
+
+
+#------------------------------------------------------------------------------
+# create test's folder on the server
+if($Compilers[0] eq "all"){
+    foreach my $CompilerSupported (@CompilersSupported){
+	system("ssh $server mkdir -p Sites/Current/$TestName\_".
+	       lc($CompilerSupported));
+    }
+}
+else{
+    foreach my $Compiler (@Compilers){
+	system("ssh $server mkdir -p Sites/Current/$TestName\_$Compiler");
+    }
+}
 
 return 1;
+
+
+sub prepare_script {
+# introduce changes in $run_script based on hash table $Blocks                 
+    foreach my $Block(keys %Blocks){
+        next unless ($Blocks{$Block});
+        open(my $fh,'<',$_[0]);
+        my @lines = <$fh>;
+        close($fh);
+        my $IsInBlock ='0';
+        open(my $fh,'>',$_[0]);
+        foreach my $line (@lines){
+            if($line =~ m/>$Block/){
+                $IsInBlock = '1';
+            }
+            elsif($IsInBlock){
+		if ($line =~ m/<#/){$IsInBlock ='0'; $line =~ s/<#//g;}
+                $line =~ s/^#//g;
+            }
+	    # put a test's name into the content of script 
+	    $line =~ s/\/Current\/\`hostname -s\`/\/Current\/$TestName/g;
+
+            print $fh $line;
+        }
+        close($fh);
+    }
+    
+    
+}
