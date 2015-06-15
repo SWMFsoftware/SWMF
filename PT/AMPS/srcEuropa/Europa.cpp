61fb8a5189e463836083a69f334455328b969019
vtenishe <>
2015-06-15 20:35:06 +0000

    thte dust source rate is fixed
    Former-commit-id: 063df891c2c7511f1ae6ad7b7c9d3206e57044e2

--- PT/AMPS/srcEuropa/Europa.cpp
+++ PT/AMPS/srcEuropa/Europa.cpp
@@ -35,7 +35,7 @@ double Europa::vEuropaRadial=0.0,Europa::xEuropaRadial=0.0;
 // dust parameters
 double DustSizeMin=1.0e-7;
 double DustSizeMax=1.0e-2;
-double DustTotalMassProductionRate=1e23;
+double DustTotalMassProductionRate=1.0e23*_H2O__MASS_;
 int    DustSampleIntervals=10;
 double DustSizeDistribution=0.0;
 
