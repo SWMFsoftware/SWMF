b1c388878cb652f53d3bad97a3e7be4f17b66129
vtenishe <>
2015-06-15 20:38:24 +0000

    additional run time into is printed on the screen
    Former-commit-id: fe7bd2044dfed2688135f3165eac44651a37b1eb

--- PT/AMPS/srcEuropa/main.cpp
+++ PT/AMPS/srcEuropa/main.cpp
@@ -50,6 +50,8 @@ int main(int argc,char **argv) {
   amps_init_mesh();
   amps_init();
 
+PIC::RequiredSampleLength=600;
+
 
 
 
@@ -58,7 +60,12 @@ int main(int argc,char **argv) {
 
 	  amps_time_step();
 
+    if (PIC::Mesh::mesh.ThisThread==0) {
+      time_t TimeValue=time(NULL);
+      tm *ct=localtime(&TimeValue);
 
+      printf(": (%i/%i %i:%i:%i), Iteration: %ld  (currect sample length:%ld, %ld interations to the next output)\n",ct->tm_mon+1,ct->tm_mday,ct->tm_hour,ct->tm_min,ct->tm_sec,niter,PIC::RequiredSampleLength,PIC::RequiredSampleLength-PIC::CollectingSampleCounter);
+    }
 
 
 	}
