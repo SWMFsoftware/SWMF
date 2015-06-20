0a165bf79aba547a2cae0a7062b9b5c58063f1fa
vtenishe <>
2015-06-20 22:38:30 +0000

    1. when _TARGET_ != _TARGET_NONE_ gravity and interaction with the internal spherical body are accounted for 2. the declaration of PIC::Mover::Mover_Boris_Axisymmetric_noObject() is changed
    Former-commit-id: 6b90c66c967ae7efa746f8ff6c158ab28840597d

--- PT/AMPS/src/pic/pic_mover_boris.cpp
+++ PT/AMPS/src/pic/pic_mover_boris.cpp
@@ -28,12 +28,11 @@ void PIC::Mover::BorisSplitAcceleration_default(double *accl, double *rotation, 
   int i,j,k;    // cell's coordinates in the block
   
   // fail-safe check: if the block doesn't exist => exit
-  if (startNode->block==NULL) 
-    exit(__LINE__,__FILE__,"Error: the block is not initialized");
+  if (startNode->block==NULL) exit(__LINE__,__FILE__,"Error: the block is not initialized");
 
   // flag: true - exit if a point is not found in the block / false: don't
   nd = PIC::Mesh::mesh.fingCellIndex(x,i,j,k,startNode,false);
-  
+
   // fail-safe check: if a point isn't found, try seacrhing in other blocks
   if (nd==-1) {
     // try to found the block the point is in;
@@ -41,8 +40,8 @@ void PIC::Mover::BorisSplitAcceleration_default(double *accl, double *rotation, 
     startNode=PIC::Mesh::mesh.findTreeNode(x,startNode);
     nd=PIC::Mesh::mesh.fingCellIndex(x,i,j,k,startNode,false);
     // if still not found => exit
-    if (nd==-1) 
-      exit(__LINE__,__FILE__,"Error: the cell is not found");
+
+    if (nd==-1) exit(__LINE__,__FILE__,"Error: the cell is not found");
   }
   
   //......................................................................
@@ -51,21 +50,57 @@ void PIC::Mover::BorisSplitAcceleration_default(double *accl, double *rotation, 
   PIC::CPLR::GetBackgroundMagneticField(B,x,nd,startNode);
 #endif//_PIC_COUPLER_MODE_
   
-      //......................................................................
-      // calculate acceleraton due to Lorentz force
-      double Charge2Mass=PIC::MolecularData::GetElectricCharge(spec)/PIC::MolecularData::GetMass(spec);
-      
-      for(int idim = 0; idim<DIM; idim++){
-	accl_LOCAL[idim]    += Charge2Mass*E[idim];
-	rotation_LOCAL[idim]-= Charge2Mass*B[idim];
-      }
+  //......................................................................
+  // calculate acceleraton due to Lorentz force
+  double ElectricCharge=0.0,mass,Charge2Mass;
+
+#if _PIC_MODEL__DUST__MODE_ == _PIC_MODEL__DUST__MODE__ON_
+  if ((_DUST_SPEC_<=spec) && (spec<_DUST_SPEC_+ElectricallyChargedDust::GrainVelocityGroup::nGroups)) {
+    char ParticleData[PIC::ParticleBuffer::ParticleDataLength];
+
+    memcpy((void*)ParticleData,(void*)PIC::ParticleBuffer::GetParticleDataPointer(ptr),PIC::ParticleBuffer::ParticleDataLength);
+
+    ElectricCharge=ElectricallyChargedDust::GetGrainCharge((PIC::ParticleBuffer::byte*)ParticleData);
+    mass=ElectricallyChargedDust::GetGrainMass((PIC::ParticleBuffer::byte*)ParticleData);
+  }
+  else {
+    ElectricCharge=PIC::MolecularData::GetElectricCharge(spec);
+    mass=PIC::MolecularData::GetMass(spec);
+  }
+#else
+  ElectricCharge=PIC::MolecularData::GetElectricCharge(spec);
+  mass=PIC::MolecularData::GetMass(spec);
+#endif //_PIC_MODEL__DUST__MODE_ == _PIC_MODEL__DUST__MODE__ON_
+
+  if (ElectricCharge!=0.0) {
+    Charge2Mass=ElectricCharge/mass;
+
+    for (int idim = 0; idim<DIM; idim++){
+      accl_LOCAL[idim]    += Charge2Mass*E[idim];
+      rotation_LOCAL[idim]-= Charge2Mass*B[idim];
+    }
+  }
 #endif//_FORCE_LORENTZ_MODE_
-      memcpy(accl,    accl_LOCAL,    3*sizeof(double));
-      memcpy(rotation,rotation_LOCAL,3*sizeof(double));
+
+
+  //calculate the acceleration due to gravity where applied
+#if _TARGET_ID_(_TARGET_) != _TARGET_NONE__ID_
+  double r2=x[0]*x[0]+x[1]*x[1]+x[2]*x[2];
+  double r=sqrt(r2);
+  int idim;
+
+  for (idim=0;idim<DIM;idim++) {
+    accl_LOCAL[idim]-=GravityConstant*_MASS_(_TARGET_)/r2*x[idim]/r;
+  }
+#endif //_TARGET_ != _TARGET_NONE_
+
+
+  memcpy(accl,    accl_LOCAL,    3*sizeof(double));
+  memcpy(rotation,rotation_LOCAL,3*sizeof(double));
 }
 
 
-int PIC::Mover::Mover_Boris_Axisymmetric_noObject(long int ptr, double dtTotal,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode){
+int PIC::Mover::Boris(long int ptr, double dtTotal,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode){
   cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *newNode=NULL;
   double dtTemp;
   PIC::ParticleBuffer::byte *ParticleData;
@@ -85,11 +120,7 @@ int PIC::Mover::Mover_Boris_Axisymmetric_noObject(long int ptr, double dtTotal,c
   PIC::ParticleBuffer::GetX(xInit,ParticleData);
   spec=PIC::ParticleBuffer::GetI(ParticleData);
 
-
-
   static long int nCall=0;
-
-
   nCall++;
 
   memcpy(xminBlock,startNode->xmin,DIM*sizeof(double));
@@ -100,10 +131,7 @@ int PIC::Mover::Mover_Boris_Axisymmetric_noObject(long int ptr, double dtTotal,c
   double acclInit[3],rotInit[3],vv;
   
 #if _PIC_PARTICLE_MOVER__FORCE_INTEGRTAION_MODE_ == _PIC_PARTICLE_MOVER__FORCE_INTEGRTAION_MODE__ON_
-  
   _PIC_PARTICLE_MOVER__BORIS_SPLIT_ACCELERATION_(acclInit,rotInit,spec,ptr,xInit,vInit,startNode);
-
-
 #endif
   
 
@@ -146,7 +174,7 @@ int PIC::Mover::Mover_Boris_Axisymmetric_noObject(long int ptr, double dtTotal,c
   
 
   //rotate the final position
-  {
+#if _PIC_SYMMETRY_MODE_ == _PIC_SYMMETRY_MODE__AXIAL_
     // rotate to the y=0 plane
     double cosPhi, sinPhi, vTmpX, vTmpY;
     double xNormFinal = pow(xFinal[0]*xFinal[0]+xFinal[1]*xFinal[1], 0.5);
@@ -157,16 +185,40 @@ int PIC::Mover::Mover_Boris_Axisymmetric_noObject(long int ptr, double dtTotal,c
     vTmpX = vFinal[0]; vTmpY = vFinal[1];
     vFinal[0] = vTmpX*cosPhi + vTmpY*sinPhi;
     vFinal[1] =-vTmpX*sinPhi + vTmpY*cosPhi;
+#endif //_PIC_SYMMETRY_MODE_ == _PIC_SYMMETRY_MODE__AXIAL_
 
-  }
   newNode=PIC::Mesh::mesh.findTreeNode(xFinal,startNode);
 
-    
   //advance the particle's position and velocity
   //interaction with the faces of the block and internal surfaces
   
+  //check whether the particle trajectory is intersected the spherical body
+#if _TARGET_ID_(_TARGET_) != _TARGET_NONE__ID_
+  double rFinal2;
+
+  if ((rFinal2=xFinal[0]*xFinal[0]+xFinal[1]*xFinal[1]+xFinal[2]*xFinal[2])<_RADIUS_(_TARGET_)*_RADIUS_(_TARGET_)) {
+    //the particle is inside the sphere -> apply the boundary condition procedure
+    int code;
+    cInternalBoundaryConditionsDescriptor *InternalBoundaryDescriptor=startNode->InternalBoundaryDescriptorList;
+    cInternalSphericalData *Sphere=(cInternalSphericalData*)(InternalBoundaryDescriptor->BoundaryElement);
+
+    //move the particle location at the surface of the sphere
+    double r=sqrt(rFinal2);
+
+    for (int idim=0;idim<DIM;idim++) xFinal[idim]*=_RADIUS_(_TARGET_)/r;
+
+    //apply the boundary condition
+    code=Sphere->ParticleSphereInteraction(spec,ptr,xFinal,vFinal,dtTotal,(void*)startNode,InternalBoundaryDescriptor->BoundaryElement);
+
+    if (code==_PARTICLE_DELETED_ON_THE_FACE_) {
+      PIC::ParticleBuffer::DeleteParticle(ptr);
+      return _PARTICLE_LEFT_THE_DOMAIN_;
+    }
+  }
+#endif //_TARGET_ == _TARGET_NONE_
+
+
   if (newNode==NULL) {
-    
     //the particle left the computational domain
     int code=_PARTICLE_DELETED_ON_THE_FACE_;
     
@@ -179,7 +231,6 @@ int PIC::Mover::Mover_Boris_Axisymmetric_noObject(long int ptr, double dtTotal,c
     default:
       exit(__LINE__,__FILE__,"Error: not implemented");
     }
-    
   }
 
 
