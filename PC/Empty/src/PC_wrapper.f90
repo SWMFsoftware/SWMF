c59f6d7ae8ac74db6024afd47b58ae5fe1c8590b
gtoth <>
2015-09-04 19:14:38 +0000

    PC_put_from_gm_init parameters changed. Fixed formatting.
    Former-commit-id: 3e97796bc6a8bd102d02d65010006d5c1a0cd956

--- PC/Empty/src/PC_wrapper.f90
+++ PC/Empty/src/PC_wrapper.f90
@@ -116,13 +116,11 @@ contains
 
   end subroutine PC_run
 
-  !==============================================================================
+  !============================================================================
 
   subroutine PC_find_points(nDimIn, nPoint, Xyz_DI, iProc_I)
 
-    implicit none
-
-    integer, intent(in) :: nDimIn                ! dimension of position vectors
+    integer, intent(in) :: nDimIn                ! dimension of position vector
     integer, intent(in) :: nPoint                ! number of positions
     real,    intent(in) :: Xyz_DI(nDimIn,nPoint) ! positions
     integer, intent(out):: iProc_I(nPoint)       ! processor owning position
@@ -134,14 +132,12 @@ contains
 
   end subroutine PC_find_points
 
-  !==============================================================================
+  !============================================================================
 
   subroutine PC_get_for_gm(IsNew, NameVar, nVarIn, nDimIn, nPoint, Xyz_DI, &
        Data_VI)
 
     use CON_coupler, ONLY: i_proc, PC_, n_proc
-
-    implicit none
 
     logical,          intent(in):: IsNew   ! true for new point array
     character(len=*), intent(in):: NameVar ! List of variables
@@ -159,38 +155,33 @@ contains
 
   end subroutine PC_get_for_gm
 
-  !==============================================================================
+  !============================================================================
 
   subroutine PC_get_grid_info(nDimOut, iGridOut, iDecompOut)
-
-    implicit none
 
     integer, intent(out):: nDimOut    ! grid dimensionality
     integer, intent(out):: iGridOut   ! grid index (increases with AMR)
     integer, intent(out):: iDecompOut ! decomposition index
 
     character(len=*), parameter :: NameSub = 'PC_get_grid_info'
-    !---------------------------------------------------------------------------
+    !--------------------------------------------------------------------------
 
     call CON_stop(NameSub//': PC_ERROR: empty version cannot be used!')
 
   end subroutine PC_get_grid_info
 
-  !==============================================================================
+  !============================================================================
 
-  subroutine PC_put_from_gm( &
-       NameVar, nVar, nPoint, Data_VI, iPoint_I, Pos_DI)
+  subroutine PC_put_from_gm(NameVar, nVar, nPoint, Data_VI, iPoint_I, Pos_DI)
 
     use CON_coupler, ONLY: i_proc, PC_, n_proc
-
-    implicit none
 
     character(len=*), intent(inout):: NameVar ! List of variables
     integer,          intent(inout):: nVar    ! Number of variables in Data_VI
     integer,          intent(inout):: nPoint  ! Number of points in Pos_DI
-    real,    intent(in), optional  :: Data_VI(:,:)    ! Recv data array
-    integer, intent(in), optional  :: iPoint_I(nPoint)! Order of data
-    real, intent(out), allocatable, optional:: Pos_DI(:,:)               ! Position vectors
+    real,    intent(in), optional  :: Data_VI(:,:)         ! Recv data array
+    integer, intent(in), optional  :: iPoint_I(nPoint)     ! Order of data
+    real, intent(out), allocatable, optional:: Pos_DI(:,:) ! Position vectors
 
     character(len=*), parameter :: NameSub='PC_put_from_gm'
     !--------------------------------------------------------------------------
@@ -199,33 +190,30 @@ contains
 
   end subroutine PC_put_from_gm
 
-  !==============================================================================
+  !============================================================================
 
   subroutine PC_put_from_gm_dt(DtSi)
-
-    implicit none
 
     real,    intent(in) :: DtSi
 
     character(len=*), parameter :: NameSub = 'PC_put_from_gm_dt'
-    !---------------------------------------------------------------------------
+    !--------------------------------------------------------------------------
 
     call CON_stop(NameSub//': PC_ERROR: empty version cannot be used!')
 
   end subroutine PC_put_from_gm_dt
-  !==============================================================================
-  subroutine PC_put_from_gm_init(ParamInt_I, ParamReal_I, n, NameVar, nNameVar)
+  !============================================================================
+  subroutine PC_put_from_gm_init(nParamInt, nParamReal, iParam_I, Param_I, &
+       NameVar, nVar)
 
-    implicit none
+    integer, intent(in)         :: nParamInt, nParamReal
+    integer, intent(in)         :: iParam_I(nParamInt)
+    real,    intent(in)         :: Param_I(nParamReal)
+    character(len=*), intent(in):: NameVar ! List of variables
+    integer, intent(in)         :: nVar
 
-    integer, intent(in) :: n
-    integer, intent(in) :: ParamInt_I(3)
-    real,    intent(in) :: ParamReal_I(n)
-    character(len=*), intent(inout):: NameVar ! List of variables
-    integer, intent(in) :: nNameVar
-    
     character(len=*), parameter :: NameSub = 'PC_put_from_gm_init'
-    !---------------------------------------------------------------------------
+    !--------------------------------------------------------------------------
 
     call CON_stop(NameSub//': PC_ERROR: empty version cannot be used!')
 
