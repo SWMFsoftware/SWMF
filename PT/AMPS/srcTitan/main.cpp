b9c3e336468035c4cca62aa590fdbb7c1d1ec558
vtenishe <>
2015-09-15 14:07:40 +0000

    the sample length is incremented between outputs of the data files
    Former-commit-id: a4fa9385fe451f79fdac7ebfdb96fe1b1f8d57d5

--- PT/AMPS/srcTitan/main.cpp
+++ PT/AMPS/srcTitan/main.cpp
@@ -42,10 +42,11 @@ int main(int argc,char **argv) {
   amps_init();
 
 
-
   //determine the total number of the iterations to perform
   //in the test-mode run 100 iterations and than output the particle data statistics
   int nIterations,nTotalIterations=100000001;
+  static int LastDataOutputFileNumber=0;
+
 
   if (_PIC_NIGHTLY_TEST_MODE_ == _PIC_MODE_ON_) nTotalIterations=100;
 
@@ -53,6 +54,23 @@ int main(int argc,char **argv) {
   //time step
   for (long int niter=0;niter<nTotalIterations;niter++) {
     amps_time_step();
+
+     //print the iteration number
+     if (PIC::Mesh::mesh.ThisThread==0) {
+       time_t TimeValue=time(NULL);
+       tm *ct=localtime(&TimeValue);
+
+       printf(": (%i/%i %i:%i:%i), Iteration: %ld  (currect sample length:%ld, %ld interations to the next output)\n",ct->tm_mon+1,ct->tm_mday,ct->tm_hour,ct->tm_min,ct->tm_sec,niter,PIC::RequiredSampleLength,PIC::RequiredSampleLength-PIC::CollectingSampleCounter);
+     }
+
+     if ((PIC::DataOutputFileNumber!=0)&&(PIC::DataOutputFileNumber!=LastDataOutputFileNumber)) {
+       PIC::RequiredSampleLength*=2;
+       if (PIC::RequiredSampleLength>40000) PIC::RequiredSampleLength=40000;
+
+
+       LastDataOutputFileNumber=PIC::DataOutputFileNumber;
+       if (PIC::Mesh::mesh.ThisThread==0) cout << "The new sample length is " << PIC::RequiredSampleLength << endl;
+     }
 
   }
 
