9ab9e39af774d3f63c8208f6fd10bb95fda9cc34
vtenishe <>
2015-10-28 05:43:25 +0000

    definition of CON_stop() is commented. Now CON_stop() is defined in BATL/lib/libREADAMR.a(wrap_read_amr.o)
    Former-commit-id: 5405441d60864bfd78b1a9db58bb07875278a61f

--- PT/AMPS/src/pic/pic_batsrus_wrapper.f90
+++ PT/AMPS/src/pic/pic_batsrus_wrapper.f90
@@ -3,14 +3,14 @@
 
 ! The substitute for the subroutine called by SWMF shared subroutines
 ! in the case of an error
-subroutine CON_stop(StringError)
-  implicit none
-  character (len=*), intent(in) :: StringError
-  !----------------------------------------------------------------------------
-  write(*,'(a)')StringError
-  write(*,'(a)')'!!! AMPS_ABORT !!!'
-  stop
-end subroutine CON_stop
+!subroutine CON_stop(StringError)
+!  implicit none
+!  character (len=*), intent(in) :: StringError
+!  !----------------------------------------------------------------------------
+!  write(*,'(a)')StringError
+!  write(*,'(a)')'!!! AMPS_ABORT !!!'
+!  stop
+!end subroutine CON_stop
 
 !==============================================================================                                                   
 ! The substitute for the timing subroutines called by SWMF
