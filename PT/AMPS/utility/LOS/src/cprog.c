16a46a3b5ee8ddb25e8b8b783690d5b734343432
abieler <>
2015-11-24 21:36:52 +0000

    clib template
    Former-commit-id: 1c9e3f039e176d2a9614eab37a418ecf036c9b77

---
+++ PT/AMPS/utility/LOS/src/cprog.c
@@ -0,0 +1,8 @@
+void
+ColumnIntegrationFactor (double minSize,
+                         double maxSize,
+                         double r,
+                         double * result){
+
+result[0]=1.0;
+}
