1eba1f70c615f4d963a751beeb1b672f058d42ec
vtenishe <>
2016-01-11 16:53:35 +0000

    the definitions are updated
    Former-commit-id: 2b71d7c2da4a38dfc1017a896be095a069f011ac

--- PT/AMPS/utility/PostProcess/PostProcess3D.h
+++ PT/AMPS/utility/PostProcess/PostProcess3D.h
@@ -19,7 +19,9 @@
 #include <algorithm>
 
 #include "meshAMRcutcell.h"
+#include "specfunc.h"
 
+#define _POST_PROCESS_MAP_SIZE_ (1<<8)
 
 class cPostProcess3D {
 public:
@@ -33,7 +35,7 @@ public:
   public:
     cCell ***cell;
     double xmin[3],xmax[3],dx[3];
-    int nCellX,nCellY,nCellZ;
+    int nCellX,nCellY,nCellZ,id;
 
     void Init(int nx,int ny,int nz);
   };
@@ -41,7 +43,7 @@ public:
 
   cCell *ConnectivityList;
   cBlock *Block;
-  cBlock ****Mesh; //contains pointers to the blocks
+  vector<cBlock*> Mesh[_POST_PROCESS_MAP_SIZE_][_POST_PROCESS_MAP_SIZE_][_POST_PROCESS_MAP_SIZE_]; //contains pointers to the blocks
   double xmin[3],xmax[3],dxBlockMin[3];
   int nNodes,nCells,nBlocks;
   int nBlockCellX,nBlockCellY,nBlockCellZ;
@@ -68,6 +70,26 @@ public:
   //column integration
   class cColumnIntegral {
   public:
+    cPostProcess3D *PostProcess3D;
+
+    //class that contains the user-defined integtated parametes
+    struct cColumnIntegrationSet {
+      int (*IntegrantVectorLength)();
+      void (*IntegrantVector)(double* Data,double *Location);
+      void (*PrintVariableList)(FILE* fout);
+    };
+
+    class cMap {
+    public:
+      cColumnIntegral *ColumnIntegral;
+
+      //Types of the column maps integration
+      void Circular(double *xObservation,double *PrimaryAxisDirection,double *SecondaryAxisDirection,double HalfMapAngle,int nAzimuthPoints,int nZenithPoints,const char *fname,cColumnIntegrationSet* IntegrationSet);
+    };
+
+    cMap Map;
+
+  public:
     //define 3 nodes on the surface of a bounding plane; index value: 0 -> xmin component of the coordinate, 1 -> xmax component of the coordinate
     static int x0PlaneNodeIndex[6][3]; //={ {0,0,0},{1,0,0},       {0,0,0},{0,1,0},           {0,0,0},{0,0,1}};
     static int x1PlaneNodeIndex[6][3]; //={ {0,1,0},{1,1,0},       {1,0,0},{1,1,0},           {1,0,0},{1,0,1}};
@@ -89,6 +111,11 @@ public:
     void Init(double *xGlobalMin,double *xGlobalMax);
     bool FindIntegrationLimits(double *x0,double *l,double& IntegrationPathLength,double *xStart,double *xFinish,double *xGlobalMin,double *xGlobalMax);
 
+    //calculate the integral
+    void GetCoulumnIntegral(double *ResultVector,int ResultVectorLength,double *xStart,double *l,double IntegrationPathLength,void (*Integrand)(double*,double*));
+    void GetCoulumnIntegral(double *ResultVector,int ResultVectorLength,double *x0,double *l,void (*Integrand)(double*,double*));
+
+
     //constructor
     cColumnIntegral() {
     }
@@ -96,14 +123,10 @@ public:
 
   cColumnIntegral ColumnIntegral;
 
-  //calculate the integral
-  void GetCoulumnIntegral(double *ResultVector,int ResultVectorLength,double *xStart,double *l,double IntegrationPathLength,void (*Integrand)(double*,int,double*));
-  void GetCoulumnIntegral(double *ResultVector,int ResultVectorLength,double *x0,double *l,void (*Integrand)(double*,int,double*));
-
-
   //mesh operations
   cBlock* GetBlock(double *x);
   double CharacteristicCellSize(double *x);
+  bool IsInsideDomain(double *x);
 
   //data file operations
   void LoadDataFile(const char *fname,const char* path);
@@ -118,5 +141,7 @@ public:
   //constructor
   cPostProcess3D() {
     data.nVariables=0,data.nNodes=0;
+    ColumnIntegral.PostProcess3D=this;
+    ColumnIntegral.Map.ColumnIntegral=&ColumnIntegral;
   }
 };
