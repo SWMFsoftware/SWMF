94a34314a40823dc3fa8af36e76d0184f0aad295
vtenishe <>
2016-01-11 22:00:55 +0000

    dust is added
    Former-commit-id: c3172b90da9b23f26741d5dd50a3fafbcc307cf4

--- PT/AMPS/srcCG-PostProcess/main.cpp
+++ PT/AMPS/srcCG-PostProcess/main.cpp
@@ -1,3 +1,5 @@
+//$Id$
+
 /*
  * main.cpp
  *
@@ -7,6 +9,13 @@
 
 #include "PostProcess3D.h"
 #include "SpiceUsr.h"
+
+
+#define _DUST_MODE_  0
+#define _GAS_MODE_   1
+
+#define _MODE_ _GAS_MODE_
+//#define _MODE_  _DUST_MODE_
 
 cPostProcess3D amps;
 
@@ -43,7 +52,49 @@ namespace GAS {
   }
 }
 
-void ProcessGasOutputFile() {
+namespace DUST {
+  int nRadii=10;
+
+  struct cVariablePair {
+    int nVar;
+    double GrainRadius;
+  };
+
+  cVariablePair VariablePair[]={
+      {51,0.5*(1.000000E-07+1.995262E-07)},
+      {56,0.5*(1.995262E-07+3.981072E-07)},
+      {61,0.5*(3.981072E-07+7.943282E-07)},
+      {66,0.5*(7.943282E-07+1.584893E-06)},
+      {71,0.5*(1.584893E-06+3.162278E-06)},
+      {76,0.5*(3.162278E-06+6.309573E-06)},
+      {81,0.5*(6.309573E-06+1.258925E-05)},
+      {86,0.5*(1.258925E-05+2.511886E-05)},
+      {91,0.5*(2.511886E-05+5.011872E-05)},
+      {96,0.5*(5.011872E-05+1.000000E-04)}
+  };
+
+  int IntegrantVectorLength() {return 1+nRadii;}
+
+  void PrintVariableList(FILE* fout) {
+    fprintf(fout," \"Column Densty\"");
+
+    for (int i=0;i<nRadii;i++) fprintf(fout,", \"Column Density(%e)\"",VariablePair[i].GrainRadius);
+  }
+
+  void IntegrantVector(double *data,double *x) {
+    cPostProcess3D::cStencil Stencil;
+    int i,iRadius;
+
+    amps.GetInterpolationStencil(x,&Stencil);
+
+    for (i=0;i<IntegrantVectorLength();i++) data[i]=0.0;
+
+    for (i=0;i<8;i++) data[0]+=Stencil.Weight[i]*amps.data.data[Stencil.Node[i]][46];
+
+    for (iRadius=0;iRadius<nRadii;iRadius++) for (i=0;i<8;i++) {
+      data[1+iRadius]+=Stencil.Weight[i]*amps.data.data[Stencil.Node[i]][VariablePair[iRadius].nVar];
+    }
+  }
 
 }
 
@@ -70,9 +121,17 @@ int main(int argc,char **argv) {
   }
 
 
-
+  amps.InitMPI();
   amps.SetBlockSize(5,5,5);
-  amps.LoadDataFile("pic.H2O.s=0.out=10.dat",".");
+
+  if (_MODE_==_GAS_MODE_) {
+    amps.LoadDataFile("pic.H2O.s=0.out=10.dat",".");
+  }
+  else if (_MODE_==_DUST_MODE_) {
+    amps.LoadDataFile("pic.DUST%0.s=2.out=10.dat",".");
+  }
+
+
   amps.PrintVariableList();
 
   //load the nucleus mesh
@@ -85,16 +144,24 @@ int main(int argc,char **argv) {
 
 
   //process the gas output file
-  cPostProcess3D::cColumnIntegral::cColumnIntegrationSet Gas;
+  cPostProcess3D::cColumnIntegral::cColumnIntegrationSet IntegrationSet;
 
-  Gas.IntegrantVector=GAS::IntegrantVector;
-  Gas.IntegrantVectorLength=GAS::IntegrantVectorLength;
-  Gas.PrintVariableList=GAS::PrintVariableList;
+  if (_MODE_==_GAS_MODE_) {
+    IntegrationSet.IntegrantVector=GAS::IntegrantVector;
+    IntegrationSet.IntegrantVectorLength=GAS::IntegrantVectorLength;
+    IntegrationSet.PrintVariableList=GAS::PrintVariableList;
+  }
+  else {
+    IntegrationSet.IntegrantVector=DUST::IntegrantVector;
+    IntegrationSet.IntegrantVectorLength=DUST::IntegrantVectorLength;
+    IntegrationSet.PrintVariableList=DUST::PrintVariableList;
+  }
 
-  amps.ColumnIntegral.Map.Circular(xObservation,xPrimary,xSecondary,1.5,100,100,"map.dat",&Gas);
+  amps.ColumnIntegral.Map.Circular(xObservation,xPrimary,xSecondary,1.5,100,100,"map.dat",&IntegrationSet);
   amps.SaveDataFile("Res.dat", amps.data);
 
 
+  amps.MPI_Finalize();
   return 1;
 }
 
