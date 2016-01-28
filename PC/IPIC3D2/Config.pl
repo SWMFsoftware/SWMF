6e9da61e444e2e1116b287d59fef46d66c20cbd9
yuxichen <>
2016-01-28 20:38:10 +0000

    1) Processor number in each direction is set from PARAM.in now. 2) Boundary condition is automatically determined based on the cell numbers in each direction.
    Former-commit-id: 36b86f977a5da90b650cafedd5ebe8cf52d6ff08

--- PC/IPIC3D2/Config.pl
+++ PC/IPIC3D2/Config.pl
@@ -24,49 +24,24 @@ our $WARNING;
 our $NewGridSize;
 our $ShowGridSize;
 
-my $npx;
-my $npy;
-my $npz;
-
-my $mytopology;
-my $mybc;
 my @mycpp;
-
-my $periodicX;
-my $periodicY;
-my $periodicZ;
-
 my $ng;
 my $pseudrand; # TRUE
 my $h5hut; #FALSE
 
 my $makefilelocal = "src/Makefile.local";
-my $VCtopologyfile = "src/include/VCtopology3D.h";
-
 
 #print "IPIC3D argv = @Arguments \n";
 
 foreach (@Arguments){
     if(/^-s$/)                 {$Show=1;  next};
-    if(/^-npx=(.*)/i)          {$npx.="$1";  next};
-    if(/^-npy=(.*)/i)          {$npy.="$1";  next};
-    if(/^-npz=(.*)/i)          {$npz.="$1";  next};
-    if(/^-periodicx=(.*)/i)    {$periodicX.="$1";  next};
-    if(/^-periodicy=(.*)/i)    {$periodicY.="$1";  next};
-    if(/^-periodicz=(.*)/i)    {$periodicZ.="$1";  next};
     if(/^-ng=(.*)/i)           {$ng="$1";  next};
     if(/^-pseudrand=(.*)/i)    {$pseudrand="$1";  next};
     if(/^-h5hut=(.*)/i)        {$h5hut="$1";  next};
     warn "WARNING: Unknown flag $_\n" if $Remaining{$_};
 }
 
-
 &get_settings;
-
-# Set the number of proc in each direction
-&set_topolegy;
-# Set periodic boundary or not per direction
-&set_boundary;
 
 # set local Makefile options
 &set_cpp_options;
@@ -78,29 +53,6 @@ exit 0;
 #############################################################################
 
 sub get_settings{
-
-    my $mynpx;
-    my $mynpy;
-    my $mynpz;
-    my $myperiodicx;
-    my $myperiodicy;
-    my $myperiodicz;
-
-    # Read size of the grid from topology file
-    open(FILE, $VCtopologyfile) or die "$ERROR could not open $VCtopologyfile\n";
-    while(<FILE>){
-	next if /^\s*!/; # skip commented out lines
-        $mynpx=$1     if /\bXLEN\s*=\s*(\d+)/i;
-        $mynpy=$1     if /\bYLEN\s*=\s*(\d+)/i;
-        $mynpz=$1     if /\bZLEN\s*=\s*(\d+)/i;
-	$myperiodicx=$1 if/\bPERIODICX\s*=\s*(true|false)/i;
-	$myperiodicy=$1 if/\bPERIODICY\s*=\s*(true|false)/i;
-	$myperiodicz=$1 if/\bPERIODICZ\s*=\s*(true|false)/i;
-    }
-    close($VCtopologyfile);
-
-    $mytopology = "$mynpx, $mynpy, $mynpz";
-    $mybc       = "$myperiodicx, $myperiodicy, $myperiodicz";
     my $mypseudorand = "false";
     my $myh5hut      = "false";
     my $myngp;
@@ -118,51 +70,14 @@ sub get_settings{
     @mycpp =  ($mypseudorand, $myh5hut, $myngp);
 
 }
-################################################################################
-sub set_topolegy{
 
-    if($mytopology ne "$npx, $npy, $npz"){  
-
-	die "$ERROR File $VCtopologyfile  does not exist!\n" unless -f $VCtopologyfile ;
-
-	@ARGV = ($VCtopologyfile);
-	while(<>){
-	    # should test for that its a number
-	    if($npx ne ""){s/\b(XLEN\s*=[^0-9]*)(\d+)/$1$npx/i; }
-	    if($npy ne ""){s/\b(YLEN\s*=[^0-9]*)(\d+)/$1$npy/i; }
-	    if($npz ne ""){s/\b(ZLEN\s*=[^0-9]*)(\d+)/$1$npz/i; }
-	    print;
-	}
-    }
-}
-################################################################################
-sub set_boundary{
-
-
-    if($mybc ne "$periodicX, $periodicY, $periodicZ"){
-
-	die "$ERROR File $VCtopologyfile  does not exist!\n" unless -f $VCtopologyfile ;
-
-	@ARGV = ($VCtopologyfile);
-	while(<>){
-	    # instead of testing for not empty we should test equal "true" or "false"
-	    if($periodicX ne ""){s/\b(PERIODICX\s*=[^.]*;)/PERIODICX = $periodicX;/i;}
-	    if($periodicX ne ""){s/\b(PERIODICX_P\s*=[^.]*;)/PERIODICX_P = $periodicX;/i;}
-	    if($periodicY ne ""){s/\b(PERIODICY\s*=[^.]*;)/PERIODICY = $periodicY;/i;}
-	    if($periodicY ne ""){s/\b(PERIODICY_P\s*=[^.]*;)/PERIODICY_P = $periodicY;/i;}
-	    if($periodicZ ne ""){s/\b(PERIODICZ\s*=[^.]*;)/PERIODICZ = $periodicZ;/i;}
-	    if($periodicZ ne ""){s/\b(PERIODICZ_P\s*=[^.]*;)/PERIODICZ_P = $periodicZ;/i;}
-	    print;
-	}
-    }
-}
 ################################################################################
 sub set_cpp_options{
 
     die "$ERROR File $makefilelocal does not exist!\n" unless -f $makefilelocal;
 
     if($h5hut eq "true"){
-	print "\nh5hut is not included yeat! \n";
+	print "\nh5hut is not included yet! \n";
 	print "http://www-vis.lbl.gov/Research/H5hut/ \n";
 	$h5hut = "false";
     }
@@ -194,9 +109,6 @@ sub set_cpp_options{
 
 ################################################################################
 sub show_settings{
-
-    print "Number of subdomains in X, Y and Z directions: $mytopology\n";
-    print "Periodicity          in X, Y and Z directions: $mybc\n";
     print "Number of ghost cell layers for the particles: $mycpp[2]\n";
     print "Use pseudo-random numbers (instead of rand()): $mycpp[0]\n";
     print "Use the H5hut library                        : $mycpp[1]\n";
@@ -208,14 +120,6 @@ sub print_help{
     print "
 Additional options for IPIC3D/Config.pl:
 
--npx=NPX             NPX is the number of subdomains in the X direction
--npy=NPY             NPY is the number of subdomains in the X direction
--npz=NPZ             NPZ is the number of subdomains in the X direction
-
--periodicx=BOOLEAN   Periodic in X direction: BOOLEAN is true or false
--periodicy=BOOLEAN   Periodic in Y direction: BOOLEAN is true or false
--periodicz=BOOLEAN   Periodic in Z direction: BOOLEAN is true or false
-
 -ng=NG               Use NG ghost cell layers for particles. Default is 3.
 
 -pseudrand=BOOLEAN   Use pseudo-random numbers or the built-in random numbers.
@@ -225,11 +129,6 @@ Additional options for IPIC3D/Config.pl:
 -s                   Show IPIC3D settings
 
 Examples:
-
-Set 2 x 2 subdomains in the X and Y directions and periodicity in the Z direction.
-
-   Config.pl -npx=2 -npy=2 -npz=1 -periodicx=false -periodicy=false -periodicz=true
-
 Set number of ghostcells to 1, switch on pseudo-random numbers and H5HUT library
 
    Config.pl -ng=1 -pseudorand=true -h5hut=true
