c4de13c6b4c85e120fd5d6d92c69bdf47f93cf57
vtenishe <>
2016-02-24 21:48:49 +0000

    the header is updated
    Former-commit-id: 7426182f68c753d35e7e3f10133ae072b5475692

--- PT/AMPS/test/srcModelChemistryTest/ModelChemistryTest.h
+++ PT/AMPS/test/srcModelChemistryTest/ModelChemistryTest.h
@@ -2,5 +2,5 @@
 //header of the functions used in the test
 
 double TheoreticalLifeTime(double *x,int spec,long int ptr,bool &PhotolyticReactionAllowedFlag,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *node);
-int ReactionProcessor(double *xInit,double *xFinal,double *vFinal,long int ptr,int &spec,PIC::ParticleBuffer::byte *ParticleData, cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node);
+int ReactionProcessor(double *xInit,double *xFinal,double *vFinal,long int ptr,int &spec,PIC::ParticleBuffer::byte *ParticleData, double ReactionTimeInterval,double ReactionTimeIntervalStarts,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node);
 
