baf30a833d61cf58ca99ec1ef411f5e3e34a3020
vtenishe <>
2016-02-27 02:54:33 +0000

    header Exosphere.h is added
    Former-commit-id: 40257d76ebdb2fe36911f0a8b00385ed13543517

--- PT/AMPS/test/srcModelChemistryTest/ModelChemistryTest.h
+++ PT/AMPS/test/srcModelChemistryTest/ModelChemistryTest.h
@@ -1,6 +1,8 @@
 //$Id$
 //header of the functions used in the test
 
+#include "Exosphere.h"
+
 double TheoreticalLifeTime(double *x,int spec,long int ptr,bool &PhotolyticReactionAllowedFlag,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *node);
 int ReactionProcessor(double *xInit,double *xFinal,double *vFinal,long int ptr,int &spec,PIC::ParticleBuffer::byte *ParticleData, double ReactionTimeInterval,double ReactionTimeIntervalStarts,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node);
 
