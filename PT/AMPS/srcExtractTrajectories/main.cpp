afd0c58a0ad9c641001067b05caf962d146d4b98
vtenishe <>
2016-03-22 22:19:24 +0000

    the application for post-processing creating of the individual particle trajectory files
    Former-commit-id: 463af47386fd78958d29269f6342283eba7391b4

---
+++ PT/AMPS/srcExtractTrajectories/main.cpp
@@ -0,0 +1,36 @@
+//$Id$
+
+
+#include "pic.h"
+#include "constants.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <list>
+#include <math.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <iostream>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+
+#include <sys/time.h>
+#include <sys/resource.h>
+
+
+//#include "meshAMRcutcell.h"
+//#include "cCutBlockSet.h"
+//#include "Comet.h"
+//#include "Exosphere.h"
+
+
+int main(int argc,char **argv) {
+  PIC::InitMPI(); 
+
+  return EXIT_SUCCESS;
+}
