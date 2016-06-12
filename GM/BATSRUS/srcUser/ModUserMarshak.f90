dfa569cc2f64e85b3238558877eed2a4c0feb1dd
bartvand <>
2016-06-12 21:21:26 +0000

    numerous CRASHTEST fixed Modified Files: 	CRASHTEST/HeatConduction/output/test_lowrie3.ref 	CRASHTEST/Mhd/Biermann/output/test_wave.ref 	CRASHTEST/Mhd/Ideal/output/test_alfven_1d.ref 	CRASHTEST/Mhd/Ideal/output/test_alfven_2d.ref 	CRASHTEST/Mhd/Ideal/output/test_alfven_amr.ref 	CRASHTEST/Mhd/Resistivity/output/test_wavesemi.ref 	CRASHTEST/Radiation/GrayDiffusion/Makefile 	CRASHTEST/Radiation/GrayDiffusion/README 	CRASHTEST/Radiation/GrayDiffusion/input/PARAM.in.suolson_1d 	CRASHTEST/Radiation/GrayDiffusion/input/PARAM.in.suolson_1d_amr 	CRASHTEST/Radiation/GrayDiffusion/output/test_infinitemedium.pro 	CRASHTEST/Radiation/GrayDiffusion/output/test_infinitemedium.ref 	CRASHTEST/Radiation/GrayDiffusion/output/test_suolson_1d.pro 	CRASHTEST/Radiation/GrayDiffusion/output/test_suolson_1d.ref 	CRASHTEST/Radiation/GrayDiffusion/output/test_suolson_1d_amr.pro 	CRASHTEST/Radiation/GrayDiffusion/output/test_suolson_1d_amr.ref 	CRASHTEST/Radiation/MultiGroup/output/test_infinitemedium.pro 	CRASHTEST/Radiation/MultiGroup/output/test_infinitemedium.ref 	CRASHTEST/Radiography/Makefile 	CRASHTEST/Radiography/output/test_tube_3d.pro 	srcUser/ModUserMarshak.f90
    Former-commit-id: 34481da69b88c8a4514d7f59cb1eb52778f9b3cf

--- GM/BATSRUS/srcUser/ModUserMarshak.f90
+++ GM/BATSRUS/srcUser/ModUserMarshak.f90
@@ -163,8 +163,6 @@ contains
           State_VGB(:,0,:,:,iBlock)  = State_VGB(:,1,:,:,iBlock)
           State_VGB(:,-1,:,:,iBlock) = State_VGB(:,1,:,:,iBlock)
        case('usersemi')
-          ! Set temperature in case we use radcond
-          StateSemi_VGB(:,0,:,:,iBlock) = 0.0
           ! Marshak boundary conditions
           Coef = 2.0/(3.0*SpecificOpacity*Density*CellSize_DB(x_,iBlock))
           StateSemi_VGB(iEradImpl,0,1:nJ,1:nK,iBlock) = &
@@ -184,8 +182,6 @@ contains
           State_VGB(:,nI+1,:,:,iBlock) = State_VGB(:,nI,:,:,iBlock)
           State_VGB(:,nI+2,:,:,iBlock) = State_VGB(:,nI,:,:,iBlock)
        case('usersemi','usersemilinear')
-          ! Set temperature in case we use radcond
-          StateSemi_VGB(:,nI+1,:,:,iBlock) = 0.0
           ! zero radiation influx boundary conditions
           Coef = 2.0/(3.0*SpecificOpacity*Density*CellSize_DB(x_,iBlock))
           StateSemi_VGB(iEradImpl,nI+1,1:nJ,1:nK,iBlock) = &
