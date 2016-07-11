20fe758b982ec41f06f1d690a2b5b5451c0624c1
cvs2git <>
2016-07-11 19:50:15 +0000

    This commit was manufactured by cvs2svn to create tag 'BEFORE_MERGING__HEAD_AND__OPENMP_DEV__072016'.
    Former-commit-id: 477b7fa9a5a460a985fe16b55f877a4c6c2f0c70

--- PT/AMPS/src/general/global.cpp
+++
@@ -1,8 +0,0 @@
-//  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
-//  For more information, see http://csem.engin.umich.edu/tools/swmf
-//$Id$
-//contained the difinitions of the global variables used in the simulation
-
-#include "global.h"
-
-MPI_Comm MPI_GLOBAL_COMMUNICATOR;
