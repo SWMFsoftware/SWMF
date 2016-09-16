d11fc9e20744e1508a6055dccf1ebb086ea82264
cvs2git <>
2016-09-16 21:51:32 +0000

    This commit was manufactured by cvs2svn to create tag 'Mars_HotO_DRofO2p_PERMIN'.
    Former-commit-id: 22ff5b622a079a11bddb29942ff6672b6c836427

--- gui/htdocs/plot_results/BASE/IE_idl/defaultsBASE.php
+++
@@ -1,9 +0,0 @@
-<?php 
-#  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
-#  For more information, see http://csem.engin.umich.edu/tools/swmf 
-?>
-<?php // idl
-$plotextension = ".idl";
-$macroextension = ".pro";
-$loadfile = "IEidl";
-?>
