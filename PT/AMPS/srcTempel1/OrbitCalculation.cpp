4e448523e6e17a9dd5cbab14048c8af3bfbee680
fougere <>
2016-09-20 17:44:51 +0000

    srcTempel1
    Former-commit-id: f25dcf5e3f011fd0555b8454ca787cda5138c13b

---
+++ PT/AMPS/srcTempel1/OrbitCalculation.cpp
@@ -0,0 +1,19 @@
+
+//$Id$
+
+/*
+ * OrbitCalculation.cpp
+ *
+ *  Created on: Jul 12, 2015
+ *      Author: vtenishe
+ */
+
+
+#include "pic.h"
+#include "Exosphere.h"
+
+double Exosphere::OrbitalMotion::GetTAA(double t) {return 0.0;}
+void Exosphere::ColumnIntegral::ProcessColumnIntegrationVector(double *res,int resLength) {}
+double Exosphere::SurfaceInteraction::StickingProbability(int spec,double& ReemissionParticleFraction,double Temp) {return 0.0;} 
+
+
