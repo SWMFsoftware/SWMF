17ebd8d3f6ee318d55550c4684d7570c1570c8d9
vtenishe <>
2016-10-22 02:20:36 +0000

    added: definitions of the SEP and GCR injection models
    Former-commit-id: 98871a677d3f0dd14cf6c58d8193aee5d262038a

--- PT/AMPS/srcEarth/Earth.h
+++ PT/AMPS/srcEarth/Earth.h
@@ -1,7 +1,7 @@
 //$Id$
 
-#ifndef EARTH_H_
-#define EARTH_H_
+#ifndef _PIC_EARTH_H_
+#define _PIC_EARTH_H_
 
 #include <math.h>
 #include <stdlib.h>
@@ -20,6 +20,19 @@ using namespace std;
 
 
 #include "constants.h"
+#include "Earth.dfn"
+
+//class that is used for keeping information of the injected faces
+class cBoundaryFaceDescriptor {
+public:
+  int nface;
+  cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node;
+
+  cBoundaryFaceDescriptor() {
+    nface=-1,node=NULL;
+  }
+};
+
 
 namespace Earth {
   using namespace Exosphere;
@@ -29,6 +42,72 @@ namespace Earth {
     extern char sign[_MAX_STRING_LENGTH_PIC_];
   }
   
+  //injection of new particles into the system
+  namespace BoundingBoxInjection {
+    //Energy limis of the injected particles
+    const double minEnergy=1.0*MeV2J;
+    const double maxEnergy=100.0*MeV2J;
+
+    //the list of the faces located on the domain boundary through which particles can be injected
+    extern cBoundaryFaceDescriptor *BoundaryFaceDescriptor;
+    extern int nTotalBoundaryInjectionFaces;
+
+    //init and populate the tables used by the particle injectino procedure
+    void InitBoundingBoxInjectionTable(double** &BoundaryFaceLocalInjectionRate,double* &maxBoundaryFaceLocalInjectionRate,
+        double* &BoundaryFaceTotalInjectionRate,double (*InjectionRate)(int spec,int nface,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode));
+
+    //model that specify injectino of the gakactic cosmic rays
+    namespace GCR {
+      //data buffers used for injection of GCR thought the boundary of the computational domain
+      extern double **BoundaryFaceLocalInjectionRate;
+      extern double *maxBoundaryFaceLocalInjectionRate;
+      extern double *BoundaryFaceTotalInjectionRate;
+
+      //init the model
+      void Init();
+
+      //init and populate the tables used by the particle injectino procedure
+      void InitBoundingBoxInjectionTable();
+
+      //source rate and geenration of GCRs
+      double InjectionRate(int spec,int nface,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode);
+      void GetNewParticle(PIC::ParticleBuffer::byte *ParticleData,double *x,int spec,int nface,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode,double *ExternalNormal);
+
+      //init the model
+      void Init();
+    }
+
+    //model that specifies injectino of SEP
+    namespace SEP {
+
+      //source rate and generation of SEP
+      double InjectionRate(int spec,int nface,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode);
+      void GetNewParticle(PIC::ParticleBuffer::byte *ParticleData,double *x,int spec,int nface,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode,double *ExternalNormal);
+
+      //init the model
+      void Init();
+    }
+
+    //general injection functions
+    bool InjectionIndicator(cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode);
+    long int InjectionProcessor(int spec,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode);
+
+    long int InjectionProcessor(int spec,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode);
+    long int InjectionProcessor(cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode);
+    double InjectionRate(int spec,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *startNode);
+
+
+  }
+
+  //interaction of the particles with the surface of the Earth
+  namespace BC {
+    int ParticleSphereInteraction(int spec,long int ptr,double *x,double *v, double &dtTotal, void *NodeDataPonter,void *SphereDataPointer);
+    double sphereInjectionRate(int spec,void *SphereDataPointer);
+  }
+
+  //Init the model
+  void Init();
+
 }
 
 #endif /* EARTH_H_ */
