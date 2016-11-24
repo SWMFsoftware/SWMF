8394a6e1e5ec483af8752d2933d591ca02374a6a
gtoth <>
2016-11-24 16:00:35 +0000

    Tecplot conversion code: Fixed multiple bugs (goto into loop, incorrect type for index). Fixed variables names to match data naming standard. Fixed index order to be optimal for Fortran. Added comments. Not tested.
    Former-commit-id: 0fee2d70a7ac3e948d7c3f2fd30e3c8171704c6d

--- share/Library/src/PostIDL.f90
+++ share/Library/src/PostIDL.f90
@@ -111,12 +111,14 @@ program post_idl
   ! Control verbose output
   logical:: IsVerbose = .false.
 
-  ! PLT variables for reading tecplot .dat to save as idl file
-  logical :: UsePLTconvert = .false.
-  integer :: nPLTheader=0, nPLTnodes=0, nPLTcells=0, iPLT, jPLT
-  character (len=500) :: PLTempty
-  real :: PLTminx, PLTmaxx
-  real, allocatable :: PLTnodesXyz_ND(:,:), PLTnodes_NV(:,:), PLTcells_CI(:,:)
+  ! Tecplot related variables for reading tecplot .dat to save as idl file
+  logical :: DoReadTecplot = .false.
+  integer :: nHeaderTec=0, nNodeTec=0, nCellTec=0
+  integer :: iHeaderTec, iNodeTec, iCellTec, iNeiTec
+  character(len=500) :: StringTecHeader
+  real :: xMinTec, xMaxTec
+  real, allocatable :: XyzTec_DI(:,:), PlotVarTec_VI(:,:)
+  integer, allocatable:: iNodeTec_II(:,:)
   
   character (len=lStringLine) :: NameCommand, StringLine
   !---------------------------------------------------------------------------
@@ -237,11 +239,11 @@ program post_idl
      case('#ROOTBLOCK', '#GRIDBLOCKSIZE')
         ! Ignore these commands
         
-     case('#PLTCONVERT')
-        call read_var('UsePLTconvert', UsePLTconvert)
-        call read_var('nPLTheader', nPLTheader)
-        call read_var('nPLTnodes',  nPLTnodes)
-        call read_var('nPLTcells',  nPLTcells)
+     case('#TECPLOTCONVERT')
+        call read_var('DoReadTecplot', DoReadTecplot)
+        call read_var('nHeaderTec', nHeaderTec)
+        call read_var('nNodeTec',  nNodeTec)
+        call read_var('nCellTec',  nCellTec)
 
      case default
         write(*,*) 'WARNING: unknown command ', NameCommand
@@ -413,30 +415,32 @@ program post_idl
      end if
   end if
 
-  ! Logic for PLT conversion
-  if(UsePLTconvert)then
+  ! Logic for Tecplot conversion
+  if(DoReadTecplot)then
      l=len_trim(NameFileHead)
      write(NameFile,'(a)')NameFileHead(1:l-2)//".dat"
      write(*,*)'reading file=',trim(NameFile),' ...'
 
      allocate( &
-          PLTnodesXyz_ND(nPLTnodes,3), &
-          PLTnodes_NV(nPLTnodes,nPlotVar), &
-          PLTcells_CI(nPLTcells,8), &
+          XyzTec_DI(3,nNodeTec), &
+          PlotVarTec_VI(nPlotVar,nNodeTec), &
+          iNodeTec_II(8,nCellTec), &
           STAT=iError)
      if(iError /= 0) stop 'PostIDL.exe ERROR: could not allocate arrays'
 
      open(UnitTmp_, file=NameFile, status='old', iostat=iError)
 
-     ! Read file
-     do iPLT=1,nPLTheader
-        read(UnitTmp_,*,ERR=999,END=999) PLTempty
+     ! Read Tecplot file header
+     do iHeaderTec = 1, nHeaderTec
+        read(UnitTmp_,*) StringTecHeader
      end do
-     do iPLT=1,nPLTnodes
-        read(UnitTmp_,*,ERR=999,END=999) PLTnodesXyz_ND(iPLT,:),PLTnodes_NV(iPLT,:)
+     ! Read the coordinates and variables at the nodes
+     do iNodeTec = 1, nNodeTec
+        read(UnitTmp_,*) XyzTec_DI(:,iNodeTec), PlotVarTec_VI(:,iNodeTec)
      end do
-     do iPLT=1,nPLTcells
-        read(UnitTmp_,*,ERR=999,END=999) PLTcells_CI(iPLT,:)
+     ! Read the node indexes of the nodes surrounding each cell center
+     do iCellTec = 1, nCellTec
+        read(UnitTmp_,*) iNodeTec_II(:,iCellTec)
      end do
 
      close(UnitTmp_)
@@ -448,7 +452,7 @@ program post_idl
   nCellCheck = 0
   l=len_trim(NameFileHead)
   do iProc = 0, nProc-1
-     if(.not.UsePLTconvert)then
+     if(.not.DoReadTecplot)then
         if(    nProc > 100000)then
            write(NameFile,'(a,i6.6,a)')NameFileHead(1:l-2)//"_pe",iProc,'.idl'
         elseif(nProc > 10000)then
@@ -473,34 +477,37 @@ program post_idl
 
      ! Read file
      do
-        if(UsePLTconvert)then
-           iPLT = nCellCheck + 1
-           if(iPLT .gt. nPLTcells) goto 999
-           PLTminx =  99999.
-           PLTmaxx = -99999.
-           Xyz_D = 0.
+        if(DoReadTecplot)then
+           ! Collect information from the Tecplot arrays already read above
+           iCellTec = nCellCheck + 1
+           if(iCellTec > nCellTec) EXIT
+           xMinTec   =  1e30
+           xMaxTec   = -1e30
+           Xyz_D     = 0.
            PlotVar_V = 0.
-           do jPLT=1,8
-              PLTminx = min(PLTminx, PLTnodesXyz_ND(PLTcells_CI(iPLT,jPLT),1))
-              PLTmaxx = max(PLTmaxx, PLTnodesXyz_ND(PLTcells_CI(iPLT,jPLT),1))
-              Xyz_D = Xyz_D + PLTnodesXyz_ND(PLTcells_CI(iPLT,jPLT),:)
-              PlotVar_V = PlotVar_V + PLTnodes_NV(PLTcells_CI(iPLT,jPLT),:)
+           ! Loop over nodes surrounding the cell center,
+           ! average out the coordinates and the plot variables
+           ! and calculate the cell size (only works for Cartesian)
+           do iNeiTec = 1, 8
+              iNodeTec  = iNodeTec_II(iNeiTec,iCellTec)
+              xMinTec   = min(xMinTec, XyzTec_DI(1,iNodeTec))
+              xMaxTec   = max(xMaxTec, XyzTec_DI(1,iNodeTec))
+              Xyz_D     = Xyz_D + XyzTec_DI(:,iNodeTec)
+              PlotVar_V = PlotVar_V + PlotVarTec_VI(:,iNodeTec)
            end do
-           CellSize1 = PLTmaxx - PLTminx
-           Xyz_D = Xyz_D /8.
-           PlotVar_V = PlotVar_V /8.
-        else
-           if(DoReadBinary)then
-              if(nByteRealRead == 4)then
-                 read(UnitTmp_,ERR=999,END=999) DxCell4, Xyz4_D, PlotVar4_V
-                 CellSize1 = DxCell4; Xyz_D = Xyz4_D; PlotVar_V = PlotVar4_V
-              else
-                 read(UnitTmp_,ERR=999,END=999) DxCell8, Xyz8_D, PlotVar8_V
-                 CellSize1 = DxCell8; Xyz_D = Xyz8_D; PlotVar_V = PlotVar8_V
-              end if
+           CellSize1 = xMaxTec - xMinTec
+           Xyz_D     = Xyz_D/8.
+           PlotVar_V = PlotVar_V/8.
+        else if(DoReadBinary)then
+           if(nByteRealRead == 4)then
+              read(UnitTmp_,ERR=999,END=999) DxCell4, Xyz4_D, PlotVar4_V
+              CellSize1 = DxCell4; Xyz_D = Xyz4_D; PlotVar_V = PlotVar4_V
            else
-              read(UnitTmp_,*,ERR=999,END=999) CellSize1, Xyz_D, PlotVar_V
+              read(UnitTmp_,ERR=999,END=999) DxCell8, Xyz8_D, PlotVar8_V
+              CellSize1 = DxCell8; Xyz_D = Xyz8_D; PlotVar_V = PlotVar8_V
            end if
+        else
+           read(UnitTmp_,*,ERR=999,END=999) CellSize1, Xyz_D, PlotVar_V
         end if
 
         nCellCheck = nCellCheck + 1
@@ -580,7 +587,7 @@ program post_idl
 
 999  continue
 
-     if(.not.UsePLTconvert) close(UnitTmp_)
+     if(.not.DoReadTecplot) close(UnitTmp_)
   end do ! iProc
 
   if(IsVerbose)write(*,*)'nCellCheck=', nCellCheck, &                  
