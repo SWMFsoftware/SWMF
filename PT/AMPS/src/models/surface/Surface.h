bff2eedc537818d7c38eda73d8886c08118b9bcf
vtenishe <>
2016-11-29 22:20:02 +0000

    header of the gas/surface interaction model
    Former-commit-id: 05f580efaa50074307138b5c54267e9322804a9a

---
+++ PT/AMPS/src/models/surface/Surface.h
@@ -0,0 +1,62 @@
+/*
+ * Surface.h
+ *
+ *  Created on: Nov 29, 2016
+ *      Author: vtenishe
+ */
+
+#ifndef _MODELS_SURFACE_SURFACE_H_
+#define _MODELS_SURFACE_SURFACE_H_
+
+#include "Surface.dfn"
+#include "pic.h"
+
+//the model of the gas-surface interaction
+namespace Surface {
+
+   //the models of the surface temeprature
+   namespace Temeprature {
+
+     //isothermal
+     namespace Isothremal {
+       extern double Temp;
+     }
+
+     //Radiative Equilibrium
+     namespace RadiativeEquilibrium {
+
+     }
+   }
+
+   //diffuse refrelction
+   namespace DiffuseReflection {
+     int Processor(long int ptr,double* xInit,double* vInit,CutCell::cTriangleFace *TriangleCutFace,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode);
+   }
+
+   //specular refrelction
+   namespace SpecularReflection {
+     int Processor(long int ptr,double* xInit,double* vInit,CutCell::cTriangleFace *TriangleCutFace,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode);
+   }
+
+   //quasi-specular reflection
+   namespace QuasiSpecularReflection {
+     int Processor(long int ptr,double* xInit,double* vInit,CutCell::cTriangleFace *TriangleCutFace,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode);
+   }
+
+   //Cercignani-Lampis-Lord model
+   namespace CCL {
+     int Processor(long int ptr,double* xInit,double* vInit,CutCell::cTriangleFace *TriangleCutFace,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode);
+   }
+
+   //Maxwell model
+   namespace MaxwellReflection {
+     int Processor(long int ptr,double* xInit,double* vInit,CutCell::cTriangleFace *TriangleCutFace,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode);
+   }
+
+   //process gas/surface interaction event
+   int ParticleInteractionProcessor(long int ptr,double* xInit,double* vInit,CutCell::cTriangleFace *TriangleCutFace,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode);
+}
+
+
+
+#endif /* _MODELS_SURFACE_SURFACE_H_ */
