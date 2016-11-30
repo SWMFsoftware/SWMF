f9d2e886e11f3300027e76f2e00707340a1c41e4
vtenishe <>
2016-11-30 03:03:26 +0000

    interface of the gas/surface interaction model
    Former-commit-id: 226bb7a88ad276c3c645480e7cf37408ded30e19

---
+++ PT/AMPS/src/models/surface/Surface.cpp
@@ -0,0 +1,27 @@
+//$Id$
+
+/*
+ * Surface.cpp
+ *
+ *  Created on: Nov 29, 2016
+ *      Author: vtenishe
+ */
+
+#include "Surface.h"
+
+
+int Surface::ParticleInteractionProcessor(long int ptr,double* xInit,double* vInit,CutCell::cTriangleFace *TriangleCutFace,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode) {
+  int res;
+
+  //call approptiate particle/surface interation model
+  #if _SURFACE__GAS_SURFACE_INTERACTION_MODEL_ == _SURFACE__GAS_SURFACE_INTERACTION_MODEL__SPECULAR_REFLECTION_
+  res=SpecularReflection::Processor(ptr,xInit,vInit,TriangleCutFace,startNode);
+  #else
+  exit(__LINE__,__FILE__,"Error: the option is unknown");
+  #endif
+
+  return res;
+}
+
+
+
