f6ac2283d4b717b744f3f4243e9c3402c39c8d02
vtenishe <>
2016-11-30 03:07:31 +0000

    surface temperature model in the model of the gas/surface interaction
    Former-commit-id: 2f0aed99d2476bf6c08ec19197155b0e849188d1

---
+++ PT/AMPS/src/models/surface/Surface_temperature.cpp
@@ -0,0 +1,17 @@
+//$Id$
+
+/*
+ * Surface_temperature.cpp
+ *
+ *  Created on: Nov 29, 2016
+ *      Author: vtenishe
+ */
+
+#include "Surface.h"
+
+
+//surface temeprature in the model of the isothremal temeprature
+double Surface::Temeprature::Isothremal::Temp=0.0;
+
+
+
