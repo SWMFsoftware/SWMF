42db14ba8887ebb09ec8b6fbe466a127f2296c04
vtenishe <>
2016-12-27 15:44:14 +0000

    1. CLL model is implemented; 2. Surface::DiffuseReflection::Processor is redone
    Former-commit-id: 3e1d0b2bb3efbfa8bdc86dc0bb30ce4c7b649675

--- PT/AMPS/src/models/surface/Surface_particle_interaction.cpp
+++ PT/AMPS/src/models/surface/Surface_particle_interaction.cpp
@@ -27,14 +27,30 @@ int Surface::SpecularReflection::Processor(long int ptr,double* xInit,double* vI
 
 //diffuse reflection
 int Surface::DiffuseReflection::Processor(long int ptr,double* xInit,double* vInit,CutCell::cTriangleFace *TriangleCutFace,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode) {
-  double BulkFlowVelocity[3]={0.0,0.0,0.0},temp,*ExternalNormal;
+  double BulkFlowVelocity[3]={0.0,0.0,0.0},temp,*e0,*e1,*e2,ksi_n_r,ksi_t1_r,ksi_t2_r,beta,phi,c;
   int spec;
 
   spec=PIC::ParticleBuffer::GetI(ptr);
   temp=Surface::GetSurfaceTemeprature(TriangleCutFace,startNode);
-  ExternalNormal=TriangleCutFace->ExternalNormal;
+  beta=sqrt(PIC::MolecularData::GetMass(spec)/(2.0*Kbol*temp));
 
-  PIC::Distribution::InjectMaxwellianDistribution(vInit,BulkFlowVelocity,temp,ExternalNormal,spec,_PIC_DISTRIBUTION_WEIGHT_CORRECTION_MODE__NO_WEIGHT_CORRECTION_);
+  //get the internal frame of reference related to the surface element
+  e0=TriangleCutFace->e0Orthogonal;
+  e1=TriangleCutFace->e1Orthogonal;
+  e2=TriangleCutFace->ExternalNormal;
+
+  //the normal component of the reflected velocity
+  ksi_n_r=sqrt(-log(rnd()))/beta;
+
+  //tangenetial component of thereflected velocity
+  phi=PiTimes2*rnd();
+  c=sqrt(-log(rnd()))/beta;
+
+  ksi_t1_r=c*cos(phi);
+  ksi_t2_r=c*sin(phi);
+
+  //combine the reflection velocity vector
+  for (int idim=0;idim<3;idim++) vInit[idim]=ksi_t1_r*e0[idim]+ksi_t2_r*e1[idim]+ksi_n_r*e2[idim];
 
   return _PARTICLE_REJECTED_ON_THE_FACE_;
 }
@@ -50,3 +66,49 @@ int Surface::MaxwellReflection::Processor(long int ptr,double* xInit,double* vIn
 
   return res;
 }
+
+//CLL model. Implements from Padilla-2009-JTHT, Padilla-2008-PHD (as in MONACO)
+int Surface::CLL::Processor(long int ptr,double* xInit,double* vInit,CutCell::cTriangleFace *TriangleCutFace,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode) {
+  double *e0,*e1,*e2;
+  int idim,spec;
+  double ksi_n_i=0.0,ksi_t1_i=0.0;
+  double r1,phi2,ksi_n_m,ksi_mp_w,ksi_n_r;
+  double r3,phi4,ksi_t_m,ksi_t1_r;
+  double r5,phi6,ksi_t2_r;
+  double alpha_n,alpha_t;
+
+  //get the internal frame of reference related to the surface element
+  e0=TriangleCutFace->e0Orthogonal;
+  e1=TriangleCutFace->e1Orthogonal;
+  e2=TriangleCutFace->ExternalNormal;
+
+  //get thenorml and tangential components of the incident velocities
+  for (idim=0;idim<3;idim++) ksi_n_i+=vInit[idim]*e2[idim],ksi_t1_i+=vInit[idim]*e1[idim];
+
+  spec=PIC::ParticleBuffer::GetI(ptr);
+  ksi_mp_w=sqrt(2.0*Kbol/PIC::MolecularData::GetMass(spec)*Surface::GetSurfaceTemeprature(TriangleCutFace,startNode));
+
+  //get the normal component of the refrected velocity
+  r1=sqrt(-alpha_n*log(rnd()));
+  phi2=PiTimes2*rnd();
+  ksi_n_m=fabs(ksi_n_i/ksi_mp_w)*sqrt(1.0-alpha_n);
+  ksi_n_r=ksi_mp_w*sqrt(r1*r1+ksi_n_m*ksi_n_m+2.0*r1*ksi_n_m*cos(phi2));
+
+  //get the first tangential component of the refrected velocity
+  r3=sqrt(-alpha_t*log(rnd()));
+  phi4=PiTimes2*rnd();
+  ksi_t_m=fabs(ksi_t1_i/ksi_mp_w)*sqrt(1-alpha_t);
+  ksi_t1_r=ksi_mp_w*(ksi_t_m+r3*cos(phi4));
+
+  //get the second tangential component of the refrected velocity
+  r5=sqrt(-alpha_t*log(rnd()));
+  phi6=PiTimes2*rnd();
+  ksi_t2_r=ksi_mp_w*r5*cos(phi6);
+
+  //combine the reflection velocity vector
+  for (idim=0;idim<3;idim++) vInit[idim]=ksi_t1_r*e0[idim]+ksi_t2_r*e1[idim]+ksi_n_r*e2[idim];
+
+  return _PARTICLE_REJECTED_ON_THE_FACE_;
+}
+
+
