4cefaf888acd3d606126c0e3b82a102268c1173a
vtenishe <>
2017-01-17 22:09:23 +0000

    added: sampling of the injected particles gyro frequency, and gyro radius
    Former-commit-id: 245bd675cbc076ed36188ed2bd61590cb88cf7a1

--- PT/AMPS/srcEarth/ImpulseSource.cpp
+++ PT/AMPS/srcEarth/ImpulseSource.cpp
@@ -23,9 +23,22 @@ long int Earth::ImpulseSource::InjectParticles() {
   int nTotalInjectedParticles=0;
   int idim,spec,iSource;
   cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>  *startNode;
-  double mass,a,v[3];
+  double mass,ElectricCharge,a,v[3];
   long int newParticle;
   PIC::ParticleBuffer::byte *newParticleData;
+
+  //the number of the OpenMP threads
+  #if _COMPILATION_MODE_ == _COMPILATION_MODE__HYBRID_
+  nThreadsOpenMP=omp_get_num_threads();
+  #else
+  int nThreadsOpenMP=1;
+  #endif
+
+  //sampling buffer of the gyro-frequency and gyro-radii
+  double *GyroFrequencySample=new double[nThreadsOpenMP];
+  double *GyroRadiiSample=new double [nThreadsOpenMP];
+  int *SampleCounter=new int [nThreadsOpenMP];
+  double SourceLocationB[3];
 
   for (iSource=0;iSource<nTotalSourceLocations;iSource++) {
     if ((TimeCounter>=ImpulseSourceData[iSource].time)&&(ImpulseSourceData[iSource].ProcessedFlag==false)) {
@@ -43,6 +56,18 @@ long int Earth::ImpulseSource::InjectParticles() {
       if (rnd()<a) nTotalInjectedParticles++;
 
       mass=PIC::MolecularData::GetMass(spec);
+      ElectricCharge=PIC::MolecularData::GetElectricCharge(spec);
+
+      //get magnetic field at the source location
+      PIC::CPLR::InitInterpolationStencil(ImpulseSourceData[iSource].x,startNode);
+      PIC::CPLR::GetBackgroundMagneticField(SourceLocationB);
+
+      //reset the sampling buffers
+      for (int thread=0;thread<nThreadsOpenMP;thread++) {
+        GyroFrequencySample[thread]=0.0,GyroRadiiSample[thread]=0.0;
+        SampleCounter[thread]=0;
+      }
+
 
 /*
 #if _COMPILATION_MODE_ == _COMPILATION_MODE__HYBRID_
@@ -65,7 +90,7 @@ long int Earth::ImpulseSource::InjectParticles() {
 
 #if _COMPILATION_MODE_ == _COMPILATION_MODE__HYBRID_
         #pragma omp task default (none) firstprivate (newParticle) private (idim,newParticleData)  \
-        shared (TimeCounter,iSource,nTotalInjectedParticles,startNode,spec,mass,EnergySpectrum::Mode,EnergySpectrum::Mode_Constatant,EnergySpectrum::Constant::e,ImpulseSourceData)
+        shared (SourceLocationB,GyroFrequencySample,GyroRadiiSample,SampleCounter,TimeCounter,iSource,nTotalInjectedParticles,startNode,spec,mass,ElectricCharge,EnergySpectrum::Mode,EnergySpectrum::Mode_Constatant,EnergySpectrum::Constant::e,ImpulseSourceData)
         {
 #endif
 
@@ -92,6 +117,17 @@ long int Earth::ImpulseSource::InjectParticles() {
         PIC::ParticleBuffer::SetI(spec,newParticleData);
         PIC::ParticleBuffer::SetIndividualStatWeightCorrection(1.0,newParticleData);
 
+        //sample the gyrofequency and gyroradius of the source
+        #if _COMPILATION_MODE_ == _COMPILATION_MODE__HYBRID_
+        int thread=omp_get_num_threads();
+        #else
+        int thread=0;
+        #endif
+
+        GyroFrequencySample[thread]+=Relativistic::GetGyroFrequency(v,mass,ElectricCharge,SourceLocationB);
+        GyroRadiiSample[thread]+=Relativistic::GetGyroRadius(v,mass,ElectricCharge,SourceLocationB);
+        SampleCounter[thread]++;
+
         //inject the particle into the system
         _PIC_PARTICLE_MOVER__MOVE_PARTICLE_TIME_STEP_(newParticle,TimeCounter-ImpulseSourceData[iSource].time,startNode);
 
@@ -106,12 +142,28 @@ long int Earth::ImpulseSource::InjectParticles() {
      }}
 #endif
 
+
+      //output sampled information
+      for (int thread=1;thread<nThreadsOpenMP;thread++) {
+        GyroFrequencySample[0]+=GyroFrequencySample[thread];
+        GyroRadiiSample[0]+=GyroRadiiSample[thread];
+        SampleCounter[0]+=SampleCounter[thread];
+      }
+
+      if (SampleCounter[0]!=0) GyroRadiiSample[0]/=SampleCounter[0],GyroFrequencySample[0]/=SampleCounter[0];
+      printf("$PREFIX: Impulse source location %i:  spec=%i\n",iSource,spec);
+      printf("$PREFIX: Impulse source location %i:  Mean Gyro Radii=%e\n",iSource,GyroRadiiSample[0]);
+      printf("$PREFIX: Impulse source location %i:  Mean Gyro Frequency=%e\n",iSource,GyroFrequencySample[0]);
     }
 
     //increment the species dependent time counter
 
   }
 
+  //deallocate the sampling buffers
+  delete [] GyroFrequencySample;
+  delete [] GyroRadiiSample;
+  delete [] SampleCounter;
 }
 
 //set weights of the model particles
