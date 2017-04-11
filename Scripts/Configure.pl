47741be464e325b5cea0773c1feaabe7bbbbe073
cvs2git <>
2017-04-11 01:07:11 +0000

    This commit was manufactured by cvs2svn to create tag 'RAM_AND_NUDE_GAUGES_MODEL_ARE_CONSISTENT_WITH_COPS_MEASUREMENTS_INDIVIDUALLY__041017'.
    Former-commit-id: 19c73d9757069649a072af76941410a0449648de

--- Scripts/Configure.pl
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
-#  For more information, see http://csem.engin.umich.edu/tools/swmf
-#^CMP FILE CONFIGURE
-share/Scripts/Configure.pl -o=Scripts/Configure.options $*
