db6c7cdde17ba5a4320e25b2c3ff2c403138138f
vtenishe <>
2017-05-05 20:17:25 +0000

    added: definition of the namespace 'Sampling'
    Former-commit-id: 7799f0f535121546c6e6539cbd54564b43bb4f75

--- PT/AMPS/srcMars-ions/mars-ions.h
+++ PT/AMPS/srcMars-ions/mars-ions.h
@@ -56,10 +56,41 @@ namespace MarsIon {
   extern double DomainDXMax;
   /* _MARS_ION_H_ */
 
+  int ParticleMover(long int ptr,double dtTotal,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* startNode);
+
+  //sampling of the ions fluxs
+  namespace Sampling {
+    //sampling of the fluxes passing through the spherical shells
+
+    void PrintManager(int nDataSet);
+    void SamplingManager();
+
+    namespace SphericalShells {
+      extern bool SamplingMode;
+
+      const int nSphericalShells=1;
+      extern double SampleSphereRadii[nSphericalShells];
+      extern cInternalSphericalData SamplingSphericlaShell[nSphericalShells];
+
+      //energy limis of the sampled fluxes
+      extern double minSampledEnergy,maxSampledEnergy,dLogEnergy;
+      extern int nSampledLevels;
+
+      namespace Output {
+        void PrintTitle(FILE* fout);
+        void PrintDataStateVector(FILE* fout,long int nZenithPoint,
+            long int nAzimuthalPoint,long int *SurfaceElementsInterpolationList,long int SurfaceElementsInterpolationListLength,cInternalSphericalData *Sphere,
+            int spec,CMPI_channel* pipe,int ThisThread,int nTotalThreads);
+        void PrintVariableList(FILE* fout);
+      }
+
+    }
+
+  }
 
   //init the model
   void Init_BeforeParser();
-  inline void Init_AfterParser() {}
+  void Init_AfterParser();
 
   //process interaction of the particles with the boundaries of the domain and the surface of the planet
   int ParticleSphereInteraction(int spec,long int ptr,double *x,double *v,double &dtTotal,void *NodeDataPonter,void *SphereDataPointer);
