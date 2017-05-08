0c02e617f2ea54de74ec654a025dc5fb4895b0ac
vtenishe <>
2017-05-08 02:42:52 +0000

    sampled particle speed, and the local value of the magnetic field are printed the output file of the spherical shells
    Former-commit-id: aecf37e2b8c3cbce03097b871cad377ee147b453

--- PT/AMPS/srcMars-ions/mars-sampling.cpp
+++ PT/AMPS/srcMars-ions/mars-sampling.cpp
@@ -37,7 +37,7 @@ void MarsIon::Sampling::SphericalShells::Output::PrintTitle(FILE* fout) {
 }
 
 void MarsIon::Sampling::SphericalShells::Output::PrintVariableList(FILE* fout) {
-  fprintf(fout,", \"Total Flux Up\", \"Total Flux Down\"");
+  fprintf(fout,", \"Total Flux Up [m^{-1} s^{-1}]\", \"Total Flux Down [m^{-1} s^{-1}]\", \"Mean Speed Up [m/s]\", \"Mean Speed Down [m/s]\", \"abs Magnetic Field [T]\"");
 
   for (int iEnergyLevel=0;iEnergyLevel<MarsIon::Sampling::SphericalShells::nSampledLevels;iEnergyLevel++) {
     double emin=MarsIon::Sampling::SphericalShells::minSampledEnergy*pow(10.0,iEnergyLevel*MarsIon::Sampling::SphericalShells::dLogEnergy);
@@ -61,12 +61,18 @@ void MarsIon::Sampling::SphericalShells::Output::PrintDataStateVector(FILE* fout
   static double *ParticleFluenceUp=NULL;
   static double *ParticleFluenceDown=NULL;
 
+  cInternalSphericalData::cVelocitySample SampledVelocityUp,SampledVelocityDown,tSampledVelocityUp,tSampledVelocityDown;
+
+
   if (ParticleFluenceUp==NULL) {
     ParticleFluenceUp=new double [MarsIon::Sampling::SphericalShells::nSampledLevels];
     ParticleFluenceDown=new double [MarsIon::Sampling::SphericalShells::nSampledLevels];
   }
 
   for (i=0;i<MarsIon::Sampling::SphericalShells::nSampledLevels;i++) ParticleFluenceDown[i]=0.0,ParticleFluenceUp[i]=0.0;
+
+  tSampledVelocityUp.SampledParticleSpeed=0.0,tSampledVelocityUp.SampledParticleWeight=0.0;
+  tSampledVelocityDown.SampledParticleSpeed=0.0,tSampledVelocityDown.SampledParticleWeight=0.0;
 
   //get averaged on a given processor
   for (i=0;i<SurfaceElementsInterpolationListLength;i++) {
@@ -78,6 +84,12 @@ void MarsIon::Sampling::SphericalShells::Output::PrintDataStateVector(FILE* fout
     Flux+=Sphere->Flux[spec][el];
     ParticleFluxUp+=Sphere->ParticleFluxUp[spec][el];
     ParticleFluxDown+=Sphere->ParticleFluxDown[spec][el];
+
+    tSampledVelocityUp.SampledParticleSpeed+=Sphere->VelocitySampleUp[spec][el].SampledParticleSpeed;
+    tSampledVelocityUp.SampledParticleWeight+=Sphere->VelocitySampleUp[spec][el].SampledParticleWeight;
+
+    tSampledVelocityDown.SampledParticleSpeed+=Sphere->VelocitySampleDown[spec][el].SampledParticleSpeed;
+    tSampledVelocityDown.SampledParticleWeight+=Sphere->VelocitySampleDown[spec][el].SampledParticleWeight;
 
     for (int iLevel=0;iLevel<MarsIon::Sampling::SphericalShells::nSampledLevels;iLevel++) {
       ParticleFluenceDown[iLevel]+=Sphere->ParticleFluencyDown[spec][el][iLevel];
@@ -103,16 +115,45 @@ void MarsIon::Sampling::SphericalShells::Output::PrintDataStateVector(FILE* fout
   ParticleFluxDown/=TotalStencilArea*SamplingTime;
   for (i=0;i<MarsIon::Sampling::SphericalShells::nSampledLevels;i++) ParticleFluenceDown[i]/=TotalStencilArea*SamplingTime,ParticleFluenceUp[i]/=TotalStencilArea*SamplingTime;
 
+  //calcualte the local magnetic field
+  double x[3],B[3],absB=0.0;
+  cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *node;
+
+  Sphere->GetSurfaceCoordinate(x,nZenithPoint,nAzimuthalPoint);
+  node=PIC::Mesh::mesh.findTreeNode(x,NULL);
+
+  if (node!=NULL) {
+    if (PIC::ThisThread==node->Thread) {
+      PIC::CPLR::InitInterpolationStencil(x,node);
+      PIC::CPLR::GetBackgroundMagneticField(B);
+      absB=Vector3D::Length(B);
+
+      if (PIC::ThisThread!=0) pipe->send(absB);
+    }
+    else if (PIC::ThisThread==0) pipe->recv(absB,node->Thread);
+  }
+
   //send data to the root processor, and print the data
   if (PIC::ThisThread==0) {
     double t;
     int thread;
+
+    SampledVelocityUp=tSampledVelocityUp;
+    SampledVelocityDown=tSampledVelocityDown;
 
     for (thread=1;thread<PIC::nTotalThreads;thread++) {
       Flux+=pipe->recv<double>(thread);
 
       ParticleFluxUp+=pipe->recv<double>(thread);
       ParticleFluxDown+=pipe->recv<double>(thread);
+
+      pipe->recv(tSampledVelocityDown,thread);
+      SampledVelocityDown.SampledParticleSpeed+=tSampledVelocityDown.SampledParticleSpeed;
+      SampledVelocityDown.SampledParticleWeight+=tSampledVelocityDown.SampledParticleWeight;
+
+      pipe->recv(tSampledVelocityUp,thread);
+      SampledVelocityUp.SampledParticleSpeed+=tSampledVelocityUp.SampledParticleSpeed;
+      SampledVelocityUp.SampledParticleWeight+=tSampledVelocityUp.SampledParticleWeight;
 
       for (i=0;i<MarsIon::Sampling::SphericalShells::nSampledLevels;i++) {
         ParticleFluenceDown[i]+=pipe->recv<double>(thread);
@@ -123,7 +164,10 @@ void MarsIon::Sampling::SphericalShells::Output::PrintDataStateVector(FILE* fout
       if ((t>0.0) && ((t<Rigidity)||(Rigidity<0.0)) ) Rigidity=t;
     }
 
-    fprintf(fout," %e  %e  ",ParticleFluxUp,ParticleFluxDown);
+    if (SampledVelocityUp.SampledParticleWeight>0.0) SampledVelocityUp.SampledParticleSpeed/=SampledVelocityUp.SampledParticleWeight;
+    if (SampledVelocityDown.SampledParticleWeight>0.0) SampledVelocityDown.SampledParticleSpeed/=SampledVelocityDown.SampledParticleWeight;
+
+    fprintf(fout," %e  %e  %e  %e   %e  ",ParticleFluxUp,ParticleFluxDown,SampledVelocityUp.SampledParticleSpeed,SampledVelocityDown.SampledParticleSpeed,absB);
     for (i=0;i<MarsIon::Sampling::SphericalShells::nSampledLevels;i++) fprintf(fout," %e  %e ", ParticleFluenceUp[i],ParticleFluenceDown[i]);
   }
   else {
@@ -131,6 +175,9 @@ void MarsIon::Sampling::SphericalShells::Output::PrintDataStateVector(FILE* fout
 
     pipe->send(ParticleFluxUp);
     pipe->send(ParticleFluxDown);
+
+    pipe->send(tSampledVelocityDown);
+    pipe->send(tSampledVelocityUp);
 
     for (i=0;i<MarsIon::Sampling::SphericalShells::nSampledLevels;i++) {
       pipe->send(ParticleFluenceDown[i]);
