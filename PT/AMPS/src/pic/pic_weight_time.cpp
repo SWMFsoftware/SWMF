d5f19f04cae38ecd9d51d4dc07bbf007d512274c
shyinsi <>
2017-05-15 18:32:55 +0000

    enable _SINGLE_GLOBAL_TIME_STEP_ mode in functions SimlulationTime::update(), initTimeStep, and copyLocalTimeStep.
    Former-commit-id: 74a4b12ee07e26a50d830ab485f1e889679a7a79

--- PT/AMPS/src/pic/pic_weight_time.cpp
+++ PT/AMPS/src/pic/pic_weight_time.cpp
@@ -38,10 +38,10 @@ void PIC::SimulationTime::Update() {
   if (PIC::nTotalSpecies!=1) exit(__LINE__,__FILE__,"Error: the time simulation glocal counting cannot be applied when the species number is greated thatn one");
   TimeCounter+=PIC::ParticleWeightTimeStep::GlobalTimeStep[0];
   #elif _SIMULATION_TIME_STEP_MODE_ == _SINGLE_GLOBAL_TIME_STEP_
-  exit(__LINE__,__FILE__,"Error: not implemented");
-#else
+  TimeCounter+=PIC::ParticleWeightTimeStep::GlobalTimeStep[0];
+  #else
   exit(__LINE__,__FILE__,"Error: the global time counter cannot be applied for this case");
-#endif
+  #endif
 
 }
 
@@ -394,6 +394,36 @@ void PIC::ParticleWeightTimeStep::initTimeStep(cTreeNodeAMR<PIC::Mesh::cDataBloc
         MPI_Bcast(&t,1,MPI_DOUBLE,0,MPI_GLOBAL_COMMUNICATOR);
         PIC::Mesh::mesh.ParallelNodesDistributionList[PIC::Mesh::mesh.ThisThread]->block->SetLocalTimeStep(t,s);
       }
+
+#elif _SIMULATION_TIME_STEP_MODE_ == _SINGLE_GLOBAL_TIME_STEP_
+      double tGlobal=-1.0;
+      for (s=0;s<PIC::nTotalSpecies;s++) {
+        double t;
+
+        t=PIC::Mesh::mesh.ParallelNodesDistributionList[PIC::Mesh::mesh.ThisThread]->block->GetLocalTimeStep(s);
+
+        MPI_Gather(&t,1,MPI_DOUBLE,buffer,1,MPI_DOUBLE,0,MPI_GLOBAL_COMMUNICATOR);
+
+        if (PIC::ThisThread==0) {
+          for (thread=1;thread<PIC::nTotalThreads;thread++) if (buffer[thread]<t) t=buffer[thread];
+	  if (tGlobal<0.0) {
+	    tGlobal=t;
+	  }else{
+	    if (t<tGlobal) tGlobal=t; 
+	    //tGlobal is the min time step of all species.
+	  }
+	}
+	
+      }
+
+      MPI_Bcast(&tGlobal,1,MPI_DOUBLE,0,MPI_GLOBAL_COMMUNICATOR);
+      for (s=0;s<PIC::nTotalSpecies;s++) {
+	if (PIC::ThisThread==0) {
+	  printf("$PREFIX: Global Time Step (%s) = %e (file=%s,line=%i)\n",PIC::MolecularData::GetChemSymbol(s),tGlobal,__FILE__,__LINE__);
+        }
+        PIC::Mesh::mesh.ParallelNodesDistributionList[PIC::Mesh::mesh.ThisThread]->block->SetLocalTimeStep(tGlobal,s);
+      } 
+
 #elif _SIMULATION_TIME_STEP_MODE_ == _SPECIES_DEPENDENT_LOCAL_TIME_STEP_
       //do nothing
 #else
@@ -417,6 +447,9 @@ void PIC::ParticleWeightTimeStep::copyLocalParticleWeightDistribution(int specTa
 void PIC::ParticleWeightTimeStep::copyLocalTimeStepDistribution(int specTarget,int specSource,double ProportionaltyCoefficient) {
 #if _SIMULATION_TIME_STEP_MODE_ == _SPECIES_DEPENDENT_GLOBAL_TIME_STEP_
   GlobalTimeStep[specTarget]=ProportionaltyCoefficient*GlobalTimeStep[specSource];
+#elif _SIMULATION_TIME_STEP_MODE_ == _SINGLE_GLOBAL_TIME_STEP_
+  GlobalTimeStep[specTarget]=GlobalTimeStep[specSource];
+  //global time step is the same for all   
 #elif _SIMULATION_TIME_STEP_MODE_ == _SPECIES_DEPENDENT_LOCAL_TIME_STEP_
 
   class cCopyTimeStepDistribution {
