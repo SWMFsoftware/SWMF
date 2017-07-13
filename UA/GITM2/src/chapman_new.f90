c66b14bd524e6a4ecfba5110dc21409f9332fcb6
jmbell <>
2017-07-13 14:21:22 +0000

    JMB 07/13/2017
    Added an Update to the Chapman Function.
    
    For SZA beyond the terminator, I use log-linear interpolation to provide smooth
    transition across the terminator onto the nightside.
    
    previous version had a "stair-step" variation with time, as no interpolation was used.
    This caused problems on the nightside of Titan and Jupiter.
    
    
    Former-commit-id: 4223382dd86b93d4beae51f50fcf5b1d78cb9da4

--- UA/GITM2/src/chapman_new.f90
+++ UA/GITM2/src/chapman_new.f90
@@ -15,11 +15,15 @@ subroutine chapman_integrals(iBlock)
   real :: ScaleHeightS
 
   real, dimension(nLons, nLats, nAlts, nSpecies) :: &
-       integrals, xp, erfcy
+       integrals, xp, erfcy, Hp
 
   real, dimension(nSpecies) :: MaxCh
 
   real :: a,b,c,d,f,g, y
+  real :: LocalLogNS, GradLogNS, Ng, Np
+  real :: LocalLogIntS, GradLogIntS, Intg, Intp
+  real :: GradHs, Hg, GradXp, Xg
+  real :: HpUp, HpDown
 
   Chapman(:,:,:,:,iBlock) = 1.0e26
   Integrals = 0.0
@@ -34,6 +38,8 @@ subroutine chapman_integrals(iBlock)
   ! JMB Update: 05/2017.  Corrected a small error in the y-calc for 
   ! erfc(y)
   !
+  ! Also Updated the Grazing Integral for SZA > 90.0
+  ! We now do log-linear interpolation for smoother transitions
 
   a = 1.06069630
   b = 0.55643831
@@ -43,12 +49,9 @@ subroutine chapman_integrals(iBlock)
   g = 0.06651874
 
   do iSpecies = 1, nSpecies
-
      MaxCh(iSpecies) = 0.0
-
      do iLon = 1, nLons
         do iLat = 1, nLats
-
            do iAlt = nAlts, 1, -1
 
               if (iAlt < nAlts) then
@@ -61,6 +64,7 @@ subroutine chapman_integrals(iBlock)
                       Temperature(iLon,iLat,iAlt,iBlock) &
                       * TempUnit(iLon,iLat,iAlt) * Boltzmanns_Constant &
                       / (-Gravity_GB(iLon,iLat,iAlt,iBlock) * Mass(iSpecies))
+
                  Integrals(iLon,iLat,iAlt,iSpecies) = &
                       NDensityS(iLon,iLat,iAlt,iSpecies, iBlock) * ScaleHeightS
               endif
@@ -69,18 +73,13 @@ subroutine chapman_integrals(iBlock)
                    *TempUnit(iLon,iLat,iAlt) * Boltzmanns_Constant &
                    / (-Gravity_GB(iLon,iLat,iAlt,iBlock) * Mass(iSpecies))
 
-!              xp(iLon,iLat,iAlt,iSpecies) = &
-!                   sqrt(0.5 * pi * &
-!                   RadialDistance_GB(iLon, iLat, iAlt, iBlock) / ScaleHeightS)
-!              y = xp(iLon,iLat,iAlt,iSpecies) * abs(cos(SZA(iLon,iLat,iBlock)))
-
               ! JMB Update
               xp(iLon,iLat,iAlt,iSpecies) = &
                    RadialDistance_GB(iLon, iLat, iAlt, iBlock) / ScaleHeightS
+
               ! Eqn (10) Smith & Smith
               y = sqrt(0.5*xp(iLon,iLat,iAlt,iSpecies)) * &
                   abs(cos(SZA(iLon,iLat,iBlock)))
-!              SpeciesChapY(iLon,iLat,iAlt,iSpecies) = y
 
               ! Eqn (12) Smith and Smith
               if (y < 8) then
@@ -89,7 +88,6 @@ subroutine chapman_integrals(iBlock)
                  erfcy(iLon,iLat,iAlt,iSpecies) = f / (g + y)
               endif
            enddo
-
         enddo
      enddo
 
@@ -99,27 +97,17 @@ subroutine chapman_integrals(iBlock)
 
               if (SZA(iLon,iLat,iBlock)<TwoPi/4 .or. &
                    SZA(iLon,iLat,iBlock)>3*TwoPi/4) then
-
-
-!                 chapman(iLon,iLat,iAlt,iSpecies,iBlock) = &
-!                      Integrals(iLon,iLat,iAlt,iSpecies) * &
-!                      xp(iLon,iLat,iAlt,iSpecies) * &
-!                      erfcy(iLon,iLat,iAlt,iSpecies)
-
-                 ! JMB: 05/04/2017
                  ! Eqn (13) Smith & Smith
                  chapman(iLon,iLat,iAlt,iSpecies,iBlock) = &
                       Integrals(iLon,iLat,iAlt,iSpecies) * &
                       sqrt(0.5*pi*xp(iLon,iLat,iAlt,iSpecies)) * &
                       erfcy(iLon,iLat,iAlt,iSpecies)
-
                  if (chapman(iLon,iLat,iAlt,iSpecies,iBlock) > &
-                      MaxCh(iSpecies)) then
+                    MaxCh(iSpecies)) then
                     MaxCh(iSpecies) = chapman(iLon,iLat,iAlt,iSpecies,iBlock)
                  endif
 
               else
-
                 ! Determine if the grazing angle (X = 90 deg) lies
                 ! within our modeling domain.
                 ! This y is a projection of the r-vector at point P
@@ -127,41 +115,54 @@ subroutine chapman_integrals(iBlock)
                  y = RadialDistance_GB(iLon, iLat, iAlt, iBlock) &
                       *abs(cos(SZA(iLon,iLat,iBlock)-pi/2))
 
-                 if (y > RadialDistance_GB(iLon, iLat, 2, iBlock)) then
-
+                 if (y > RadialDistance_GB(iLon, iLat, 1, iBlock)) then
                     iiAlt = iAlt
                     do while (RadialDistance_GB(iLon, iLat, iiAlt-1, iBlock)>y)
                        iiAlt = iiAlt - 1
                     enddo
+                    iiAlt = iiAlt-1
 
-                    ! This is approximate.  We should do linear interpolation
-                    ! here, but the integral is going to be huge anyways...
+                  ! Variables for Linear Interpolation:
+                   HpUp = ( Temperature(iLon,iLat,iiAlt+1,iBlock) &
+                   *TempUnit(iLon,iLat,iiAlt+1) * Boltzmanns_Constant &
+                   / (-Gravity_GB(iLon,iLat,iiAlt+1,iBlock) * Mass(iSpecies)) ) 
 
-                    ScaleHeightS = &
-                         Temperature(iLon,iLat,iiAlt,iBlock) &
-                         *TempUnit(iLon,iLat,iiAlt) * Boltzmanns_Constant &
-                         / (-Gravity_GB(iLon,iLat,iiAlt,iBlock)*Mass(iSpecies))
+                   HpDown = ( Temperature(iLon,iLat,iiAlt,iBlock) &
+                   *TempUnit(iLon,iLat,iiAlt) * Boltzmanns_Constant &
+                   / (-Gravity_GB(iLon,iLat,iiAlt,iBlock) * Mass(iSpecies)) ) 
 
-                    ! xp is evaluated at the tangent point, which is where
-                    ! at a radial distance of r*cos(sza)
+                  GradHs = ( HpUp - HpDown)/&
+                    dAlt_GB(iLon,iLat,iiAlt+1,iBlock)
+                    
 
+                  GradXp =  (xp(iLon,iLat,iiAlt+1,iSpecies) - &
+                             xp(iLon,iLat,iiAlt  ,iSpecies) )/&
+                             dAlt_GB(iLon,iLat,iiAlt+1,iBlock)
 
-!                    chapman(iLon,iLat,iAlt,iSpecies,iBlock) = &
-!                         xp(iLon,iLat,iiAlt,iSpecies) * &
-!                         ScaleHeightS * ( &
-!                         2 * NDensityS(iLon,iLat,iiAlt,iSpecies,iBlock) - &
-!                         NDensityS(iLon,iLat,iAlt,iSpecies,iBlock) * &
-!                         erfcy(iLon,iLat,iAlt,iSpecies))
+                  Hg = HpDown + &
+                       (y - RadialDistance_GB(iLon,iLat,iiAlt,iBlock))*&
+                       GradHs
 
+                  Xg = xp(iLon,iLAt,iiAlt,iSpecies) + &
+                       (y - RadialDistance_GB(iLon,iLat,iiAlt,iBlock))*&
+                       GradXp
+
+                  GradLogIntS = &
+                      (alog(Integrals(iLon,iLat,iiAlt+1,iSpecies)) - &
+                       alog(Integrals(iLon,iLat,iiAlt  ,iSpecies)))/ &
+                       (dAlt_GB(iLon,iLat,iiAlt+1,iBlock))
+
+                  LocalLogIntS = alog(Integrals(iLon,iLat,iiAlt,iSpecies)) + &
+                          (y - RadialDistance_GB(iLon,iLat,iiAlt,iBlock))*&
+                          GradLogIntS
+
+                  Intg = exp(LocalLogIntS)
+                  Intp = Integrals(iLon,iLat,iAlt,iSpecies)
                     ! JMB Update.  Equation (19) Smith & Smith
                     chapman(iLon,iLat,iAlt,iSpecies,iBlock) = &
-                         sqrt(0.5*pi*xp(iLon,iLat,iiAlt,iSpecies)) * &
-                         ScaleHeightS * ( &
-                         2 * NDensityS(iLon,iLat,iiAlt,iSpecies,iBlock) - &
-                         NDensityS(iLon,iLat,iAlt,iSpecies,iBlock) * &
+                         sqrt(0.5*pi*Xg) * &
+                         ( 2.0 * Intg - Intp*&
                          erfcy(iLon,iLat,iAlt,iSpecies))
-
-
 
                     if (chapman(iLon,iLat,iAlt,iSpecies,iBlock) > &
                          MaxCh(iSpecies)) then
@@ -170,7 +171,6 @@ subroutine chapman_integrals(iBlock)
 
                  ! you're within the "shadow" and so make champan huge
                  else
-
                     chapman(iLon, iLat, iAlt, iSpecies, iBlock) = 1.0e26
 
                  endif
