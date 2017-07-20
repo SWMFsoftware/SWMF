bb1833c329f4285c8de59ec30228bcb186740847
vtenishe <>
2017-07-20 22:49:52 +0000

    added: a custom build of test_CG-PostProcess--Read-Trajectories--off on tower-left.engin.umich.edu
    Former-commit-id: 9877b713113d9661c391460a33598b8f2b115910

--- PT/AMPS/utility/TestScripts/Valeriy/AllGNU.sh
+++ PT/AMPS/utility/TestScripts/Valeriy/AllGNU.sh
@@ -8,6 +8,15 @@ echo -n "Compiling GNU....."
 cd $WorkDir/Tmp_AMPS_test/GNU/AMPS  
 ./Config.pl -link-option=-lmpi_cxx
 make test_compile >>& test_amps.log
+
+#making of test_CG-PostProcess--Read-Trajectories--off is not standard - used a custom build
+cd $WorkDir/Tmp_AMPS_test/GNU/AMPS/srcCG-PostProcess; ./Config.pl -extraflag=-fopenmp 
+cd $WorkDir/Tmp_AMPS_test/GNU/AMPS/utility/PostProcess; ./Config.pl -extraflag=-fopenmp
+
+cd $WorkDir/Tmp_AMPS_test/GNU/AMPS/
+make EXTRALINKEROPTIONS="./utility/PostProcess/pproc.a -fopenmp" test_CG-PostProcess--Read-Trajectories--off_compile -j
+make test_CG-PostProcess--Read-Trajectories--off_rundir
+
 echo " done."
 
 echo -n "Executing tests GNU....."
--- PT/AMPS/utility/TestScripts/Valeriy/AllIntel.sh
+++ PT/AMPS/utility/TestScripts/Valeriy/AllIntel.sh
@@ -7,6 +7,16 @@ echo -n "Compiling Intel....."
 
 cd $WorkDir/Tmp_AMPS_test/Intel/AMPS; 
 make test_compile >>& test_amps.log
+
+#making of test_CG-PostProcess--Read-Trajectories--off is not standard - used a custom build
+cd $WorkDir/Tmp_AMPS_test/Intel/AMPS/srcCG-PostProcess; ./Config.pl -extraflag=-fopenmp
+cd $WorkDir/Tmp_AMPS_test/Intel/AMPS/utility/PostProcess; ./Config.pl -extraflag=-fopenmp
+
+cd $WorkDir/Tmp_AMPS_test/Intel/AMPS/
+make EXTRALINKEROPTIONS="./utility/PostProcess/pproc.a -fopenmp" test_CG-PostProcess--Read-Trajectories--off_compile -j
+mpicxx -o amps srcTemp/main/main.a srcTemp/libAMPS.a -lstdc++ -lmpi_cxx    /Users/vtenishe/SPICE/Toolkit/cspice/lib/cspice.a ./utility/PostProcess/pproc.a /Users/vtenishe/Debugger/eclipse-workspace/MERCURYAMPS/AMPS/libiomp5.a
+make test_CG-PostProcess--Read-Trajectories--off_rundir
+
 echo " done."
 
 echo -n "Executing tests Intel....."
