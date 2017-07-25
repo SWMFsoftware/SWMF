5b8b8796fbf1c91cb97b4d40730dd1d8e9737382
vtenishe <>
2017-07-25 21:21:16 +0000

    a module name is corrected
    Former-commit-id: 6ffd3c94a5b4bc9366a5c048fd99064135eb261b

--- PT/AMPS/utility/TestScripts/Pleiades/RunIntelPleiades.sh
+++ PT/AMPS/utility/TestScripts/Pleiades/RunIntelPleiades.sh
@@ -3,7 +3,7 @@
 cd Intel/AMPS
 
 module purge
-module load comp-intel/2015.0.090
+module load comp-intel/2016.2.181 
 module load mpi-sgi
 module load tecplot/2017r2
 make $1 MPIRUN=mpiexec >>& test_amps.log
