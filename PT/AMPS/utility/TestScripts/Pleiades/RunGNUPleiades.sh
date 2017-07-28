e386055f559c182fbd3e9779e2df1fdae032c00f
vtenishe <>
2017-07-28 20:25:19 +0000

    each thread of tests is assegned to a particular set of cores
    Former-commit-id: e19aef23b57098f2b36e8e7f6c41a741f7a27a88

--- PT/AMPS/utility/TestScripts/Pleiades/RunGNUPleiades.sh
+++ PT/AMPS/utility/TestScripts/Pleiades/RunGNUPleiades.sh
@@ -5,7 +5,7 @@ module purge
 module load gcc/6.2 
 module load mpi-sgi
 module load tecplot/2017r2
-make $1 "MPIRUN=mpiexec -n 8" >>& test_amps.log
+make $1 "MPIRUN=mpiexec -n 8 dplace -s1 -c 16-23" >>& test_amps.log
 
 cd ../..
 echo Done > AmpsTestGNUComplete 
