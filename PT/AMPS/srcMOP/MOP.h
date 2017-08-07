35bdc0b6d8257f62b2c070477b8c51a91b572cc7
vtenishe <>
2017-08-07 18:02:16 +0000

    1. updated difinitions of the Saturn's magnetosphere. 2. added definitions of the function set for sampling, interpolation and output of the model data into AMPS' output file (MOP::Sampling::Ouput)
    Former-commit-id: 54f430e59905f8aafc2bb3a02f76b56ab1e4b4ac

--- PT/AMPS/srcMOP/MOP.h
+++ PT/AMPS/srcMOP/MOP.h
@@ -31,6 +31,7 @@ using namespace std;
 #endif
 
 #include "MOP.dfn"
+#include "KMAGInterface.h"
 
 
 namespace MOP {
@@ -38,7 +39,27 @@ namespace MOP {
   //Saturnian system
   namespace SaturninanSystem {
 
+
+
+
+    //parameters that characterize the planet
     namespace Saturn {
+      void GetMagneticFieldDipole(double *B,double *x);
+    }
+
+    //parameters that characterize the magnetosphere
+    namespace Magnetosphere {
+      //get magnetic files in the Saturn's magnetosphere
+      void GetMagneticField(double *B,double *x);
+      void GetElectricField(double *E,double*x);
+
+      //get the corrotation speed of the magnetosphere
+      double GetCorotationSpeed(double *x);
+
+      //get the ionization lifetime of the neutral species
+      namespace Lifetime {
+        double Get(int spec,double *x);
+      }
     }
 
     //Enceladus is the main source of water in the Jovian system
@@ -72,12 +93,26 @@ namespace MOP {
     }
   }
 
+  //sampling and output of the model data
+  namespace Sampling {
+    namespace Output {
+     //function that are used for sample data output
+    //print out of the output file
+    void PrintVariableList(FILE* fout,int DataSetNumber);
+    void Interpolate(PIC::Mesh::cDataCenterNode** InterpolationList,double *InterpolationCoeficients,int nInterpolationCoeficients,PIC::Mesh::cDataCenterNode *CenterNode);
+    void PrintData(FILE* fout,int DataSetNumber,CMPI_channel *pipe,int CenterNodeThread,PIC::Mesh::cDataCenterNode *CenterNode);
+    }
+  }
+
   //interface for MOP
   double SourceRate(int spec);
   long int InjectParticles();
 
   //the requested local mesh resolution accounted for the topology of the planet system
   double GetLocalMeshResolution(double *x);
+
+  //init the model
+  void Init();
 }
 
 
