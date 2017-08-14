8c2b7471b51daa0abec13fddcf4894b95db4da43
adamtm <>
2017-08-14 15:42:43 +0000

    including code in oh.input.pl to read in the values from the VelocityDistributionFunction command in oh.input
    Former-commit-id: 0c2d1e6506bfff74863a9524f093799273b947d0

--- PT/AMPS/input/oh.input.pl
+++ PT/AMPS/input/oh.input.pl
@@ -6,6 +6,7 @@ use strict;
 use warnings;
 use POSIX qw(strftime);
 use List::Util qw(first);
+use Scalar::Util qw/looks_like_number/;
 
 use ampsConfigLib;
 
@@ -117,6 +118,108 @@ while ($line=<InputFile>) {
     
     ($s0,$s1,$s2)=split(' ',$InputLine,3);
     ampsConfigLib::ChangeValueOfVariable("double OH::UserGlobalTimeStep",$s1,"main/OH.cpp");   
+  }
+
+  elsif ($InputLine eq "VELOCITYDISTRIBUTIONSAMPLING"){
+      my $ReadSampleLocations=0;
+      my @SampleLocationTable;
+      my $t;
+      
+      $InputComment=~s/[(),]/ /g;
+      
+      while (defined $InputComment) {
+	  ($InputLine,$InputComment)=split(' ',$InputComment,2);
+	  
+	  if ($ReadSampleLocations == 1) { #reading of the sample location coordinates is in progress
+	      if (looks_like_number($InputLine)) {
+		  push(@SampleLocationTable,$InputLine);
+	      } 
+	      else {
+		  $ReadSampleLocations=0;
+	      }         
+	  }
+	  
+	  if ($ReadSampleLocations == 0) { #'$InputLine' is not a number
+	      if ($InputLine eq "ON") {
+              ampsConfigLib::ChangeValueOfVariable("const bool OH::Sampling::DistributionFunctionSample::Use","true","main/OH_sample_distribution_function.cpp");
+
+          }
+          elsif ($InputLine eq "OFF") {
+              ampsConfigLib::ChangeValueOfVariable("const bool OH::Sampling::DistributionFunctionSample::Use","false","main/OH_sample_distribution_function.cpp");
+          }
+          elsif ($InputLine eq "VMIN") {
+            ($InputLine,$InputComment)=split(' ',$InputComment,2);
+            
+            if (defined $InputLine) {
+              ampsConfigLib::ChangeValueOfVariable("double OH::Sampling::DistributionFunctionSample::vMin",$InputLine,"main/OH_sample_distribution_function.cpp");
+            }
+            else {
+              warn ("Cannot recognize the option (line=$InputLine, nline=$InputFileLineNumber)");
+              die "#1 Cannot recognize line $InputFileLineNumber ($line) in $InputFileName.Assembled\n";
+            }
+          }
+          elsif ($InputLine eq "VMAX") {
+            ($InputLine,$InputComment)=split(' ',$InputComment,2);
+            
+            if (defined $InputLine) {
+              ampsConfigLib::ChangeValueOfVariable("double OH::Sampling::DistributionFunctionSample::vMax",$InputLine,"main/OH_sample_distribution_function.cpp");
+            }
+            else {
+              warn ("Cannot recognize the option (line=$InputLine, nline=$InputFileLineNumber)");
+              die "#2 Cannot recognize line $InputFileLineNumber ($line) in $InputFileName.Assembled\n";
+            }
+          }
+          elsif ($InputLine eq "NSAMPLEINTERVALS") {
+            ($InputLine,$InputComment)=split(' ',$InputComment,2);
+            
+            if (defined $InputLine) {
+              ampsConfigLib::ChangeValueOfVariable("long int OH::Sampling::DistributionFunctionSample::nSampledFunctionPoints",$InputLine,"main/OH_sample_distribution_function.cpp");
+            }
+            else {
+              warn ("Cannot recognize the option (line=$InputLine, nline=$InputFileLineNumber)");
+              die "#3 Cannot recognize line $InputFileLineNumber ($line) in $InputFileName.Assembled\n";
+            }
+          }
+          elsif ($InputLine eq "X") {
+            #start reading of the sample location coordinates
+            $ReadSampleLocations=1;
+          }
+          else {
+            warn ("Cannot recognize the option (line=$InputLine, nline=$InputFileLineNumber)");
+            die "#4 $InputLine: Cannot recognize line $InputFileLineNumber ($line) in $InputFileName.Assembled\n";
+          }
+        }
+        
+      }
+      
+      #insert the number of the sample points into the code
+      my ($i,$j);
+      my @Table;
+      
+      $j=0;
+      
+      for ($i=0;$i<(scalar @SampleLocationTable);$i++) {
+        if ($j==0) {
+          $t="{";
+        }
+                
+        $t=$t."$SampleLocationTable[$i]";
+        $j++;
+        
+        if ($j==3) {          
+          $t=$t."}";
+          $j=0;
+         
+          push(@Table,"$t");
+        }
+        else {
+          $t=$t.",";
+        }
+      }
+      
+      ampsConfigLib::ChangeValueOfArray("double OH::Sampling::DistributionFunctionSample::SamplingLocations\\[\\]\\[3\\]",\@Table,"main/OH_sample_distribution_function.cpp");
+      ampsConfigLib::ChangeValueOfVariable("int OH::Sampling::DistributionFunctionSample::nSampleLocations",(scalar @Table),"main/OH_sample_distribution_function.cpp");      
+
   }
 
   elsif ($InputLine eq "DOMAINXMAX") {
