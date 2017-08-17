81a11d31c1d20d9c3699011991cdc6c07bc46963
adamtm <>
2017-08-17 16:05:13 +0000

    adding class, cSampled2DFunction, to OH.h which is used to calculate the 2D distribution function at a particular point for a particular origin tag of a particular species
    Former-commit-id: 6d1111bb2ae872a128383c114a7331491a937e5c

--- PT/AMPS/srcOH/OH.h
+++ PT/AMPS/srcOH/OH.h
@@ -101,11 +101,77 @@ namespace OH {
       extern int Sample_Velocity_Offset,Sample_Speed_Offset;
       extern int Sample_V2_Offset,SampleDataLength;
       
+      //-------------------------------------------------------
+      class cSampled2DFunction{
+      private:
+	double **Buffer;
+	double *Dir1, *Dir2;
+	double VMin1, VMin2;
+	double dV1, dV2;
+	int N1, N2;
+	char name[_MAX_STRING_LENGTH_PIC_];
+	bool DoPrint;
+      public:
+	inline void Init(int N1In, int N2In, double dV1In, double dV2In,
+			 double VMin1In, double VMin2In, 
+			 double* Dir1In, double* Dir2In, 
+			 char* nameIn, bool DoPrintIn){
+	  DoPrint = DoPrintIn;
+	  sprintf(name, "%s", nameIn);
+	  N1 = N1In; N2 = N2In; dV1 = dV1In; dV2 = dV2In;
+	  VMin1 = VMin1In; VMin2 = VMin2In;
+	  memcpy(Dir1, Dir1In, 3*sizeof(double));
+	  memcpy(Dir2, Dir2In, 3*sizeof(double));
+	  Buffer   = new double* [N1-1];
+	  Buffer[0]= new double  [(N1-1)*(N2-1)];
+	  for(int i=1; i<N2-1; i++)
+	    Buffer[i] = Buffer[i-1] + (N2-1);
+	  
+	  Flush();
+	}
+	//--------------
+	inline void Flush(){
+	  for(int i1=0; i1<N1-1; i1++)
+	    for(int i2=0; i2<N2-1; i2++)
+	      Buffer[i1][i2] = 0.0;
+	}
+	//--------------
+	inline void Sample(double* V, double Weight){
+	  int i1,i2;
+	  double V1=0.0, V2=0.0;
+	  for(int idim=0; idim<3; idim++){
+	    V1 += Dir1[idim] * V[idim];
+	    V2 += Dir2[idim] * V[idim];
+	  }
+	  i1 = (int)( (V1 - VMin1) / dV1 );
+	  i2 = (int)( (V2 - VMin2) / dV2 );
+	  if( i1 >= 0  && i1 < N1-1 && i2 >= 0 && i2 < N2-1)
+	    Buffer[i1][i2] += Weight;
+	}
+	//--------------
+	void Print(int DataOutputFileNumber);
+	//--------------
+	cSampled2DFunction(){
+	  DoPrint = false;
+	  N1=0;N2=0; VMin1=0.0; VMin2 = 0.0; dV1 = 1.0; dV2 = 1.0;
+	  Dir1 = new double [3];
+	  Dir2 = new double [3];
+	  for(int idim=0;idim<3;idim++){Dir1[idim]=0.0;Dir2[idim]=0.0;}
+	}
+      };
+      extern cSampled2DFunction **Sampled2DFunction;
+      extern int nSampledPlanes;
+
       void Init();
       void flushSamplingBuffers();
       long int GetSampleDataOffset(int spec,int OriginID,int SampleVariableOffset);    
-      void SampleDistributionFnction();
+      void SampleDistributionFunction();
       void printDistributionFunction(int DataOutputFileNumber);
+      
+      // 2d distribution function administrative functions
+      void Sample2dDistributionFunction();
+      void print2dDistributionFunction(int DataOutputFileNumber);
+
     }  
 
   }
