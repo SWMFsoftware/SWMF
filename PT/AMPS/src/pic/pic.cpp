7363c02dca1fe9965f145f084c00ff36d00f7c4e
vtenishe <>
2017-08-18 20:13:31 +0000

    the variable list in #pragma clause has been corrected
    Former-commit-id: 951bf29334d1ba1985157e407d203392634b6899

--- PT/AMPS/src/pic/pic.cpp
+++ PT/AMPS/src/pic/pic.cpp
@@ -714,7 +714,8 @@ void PIC::Sampling::Sampling() {
 #if _COMPILATION_MODE_ == _COMPILATION_MODE__HYBRID_
 #pragma omp parallel for schedule(dynamic,1) reduction(+:nTotalSampledParticles) default(none) \
   private (s,i,j,k,idim,LocalCellNumber,ptr,ptrNext,ParticleData, ParticleDataNext,cell,block,SamplingData,v,LocalParticleWeight,Speed2,v2,node, \
-    tempSamplingBuffer,tempParticleData,FirstCellParticleTable)  \
+    tempSamplingBuffer,tempParticleData,FirstCellParticleTable, \
+    lParallelTemepratureSampleDirection,l0TangentialTemepratureSampleDirection,l1TangentialTemepratureSampleDirection)  \
   shared (localSimulatedSpeciesParticleNumber,PIC::Mesh::mesh, \
      DomainBlockDecomposition::nLocalBlocks,DomainBlockDecomposition::BlockTable, \
      PIC::IDF::_VIBRATIONAL_ENERGY_SAMPLE_DATA_OFFSET_, PIC::IDF::_ROTATIONAL_ENERGY_SAMPLE_DATA_OFFSET_,PIC::IDF::_TOTAL_SAMPLE_PARTICLE_WEIGHT_SAMPLE_DATA_OFFSET_,  \
