d2695b1a4233106116acb0d57c5f9db32c65334e
shyinsi <>
2017-09-07 18:12:47 +0000

    remove line for the load_trajectory_off case. Assign values for the variable TESTMPIRUN1
    Former-commit-id: 5979dbd06c3469846bf684e17601108250043cbb

--- PT/AMPS/utility/TestScripts/Valeriy/AllGNU.sh
+++ PT/AMPS/utility/TestScripts/Valeriy/AllGNU.sh
@@ -9,16 +9,10 @@ cd $WorkDir/Tmp_AMPS_test/GNU/AMPS
 ./Config.pl -link-option=-lmpi_cxx
 make test_compile >>& test_amps.log
 
-#making of test_CG-PostProcess--Read-Trajectories--off is not standard - used a custom build
-cd $WorkDir/Tmp_AMPS_test/GNU/AMPS/srcCG-PostProcess; ./Config.pl -extraflag=-fopenmp 
-cd $WorkDir/Tmp_AMPS_test/GNU/AMPS/utility/PostProcess; ./Config.pl -extraflag=-fopenmp
-
 cd $WorkDir/Tmp_AMPS_test/GNU/AMPS/
-make EXTRALINKEROPTIONS="./utility/PostProcess/pproc.a -fopenmp" test_CG-PostProcess--Read-Trajectories--off_compile -j
-make test_CG-PostProcess--Read-Trajectories--off_rundir
 
 echo " done."
 
 echo -n "Executing tests GNU....."
-make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 4" test_run >>& test_amps.log
+make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 4" TESTMPIRUN1="export DYLD_LIBRARY_PATH=/Users/ccmc/boost/lib:/opt/intel/compilers_and_libraries/mac/lib;mpirun -np 1" test_run >>& test_amps.log
 echo " done."
