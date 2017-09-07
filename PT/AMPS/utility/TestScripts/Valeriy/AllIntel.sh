399521dd6853b0148969aa9944fd696f6a118c78
shyinsi <>
2017-09-07 18:15:02 +0000

    remove lines for the load_trajectory_off case. Set value to the variable TESTMPIRUN1, which is used for post-process test.
    Former-commit-id: 36024b1063386d4934cecb76321c41fcde1efe1f

--- PT/AMPS/utility/TestScripts/Valeriy/AllIntel.sh
+++ PT/AMPS/utility/TestScripts/Valeriy/AllIntel.sh
@@ -8,17 +8,8 @@ echo -n "Compiling Intel....."
 cd $WorkDir/Tmp_AMPS_test/Intel/AMPS; 
 make test_compile >>& test_amps.log
 
-#making of test_CG-PostProcess--Read-Trajectories--off is not standard - used a custom build
-cd $WorkDir/Tmp_AMPS_test/Intel/AMPS/srcCG-PostProcess; ./Config.pl -extraflag=-fopenmp
-cd $WorkDir/Tmp_AMPS_test/Intel/AMPS/utility/PostProcess; ./Config.pl -extraflag=-fopenmp
-
-cd $WorkDir/Tmp_AMPS_test/Intel/AMPS/
-make EXTRALINKEROPTIONS="./utility/PostProcess/pproc.a -fopenmp" test_CG-PostProcess--Read-Trajectories--off_compile -j
-mpicxx -o amps srcTemp/main/main.a srcTemp/libAMPS.a -lstdc++ -lmpi_cxx    /Users/vtenishe/SPICE/Toolkit/cspice/lib/cspice.a ./utility/PostProcess/pproc.a /Users/vtenishe/Debugger/eclipse-workspace/MERCURYAMPS/AMPS/libiomp5.a
-make test_CG-PostProcess--Read-Trajectories--off_rundir
-
 echo " done."
 
 echo -n "Executing tests Intel....."
-make TESTMPIRUN4="mpirun -np 4" MPIRUN="mpirun -np 4" test_run >>& test_amps.log
+make TESTMPIRUN4="mpirun -np 4" MPIRUN="mpirun -np 4" TESTMPIRUN1="export DYLD_LIBRARY_PATH=/Users/ccmc/boost/lib:/opt/intel/compilers_and_libraries/mac/lib;mpirun -np 1" test_run >>& test_amps.log
 echo " done."
