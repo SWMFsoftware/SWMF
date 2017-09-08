d31a39cc064057290eee5cd6131d87d0a86216b0
shyinsi <>
2017-09-08 20:33:10 +0000

    The lines related to the PostProcess "trajectory-off" case are removed.
    Former-commit-id: 70314dc470665a3bbafd04c5c29c92a4955b9d5a

--- PT/AMPS/utility/TestScripts/Valeriy/AllPGI.sh
+++ PT/AMPS/utility/TestScripts/Valeriy/AllPGI.sh
@@ -8,14 +8,6 @@ echo -n "Compiling PGI....."
 cd $WorkDir/Tmp_AMPS_test/PGI/AMPS  
 make test_compile >>& test_amps.log
 
-#making of test_CG-PostProcess--Read-Trajectories--off is not standard - used a custom build
-cd $WorkDir/Tmp_AMPS_test/PGI/AMPS/srcCG-PostProcess; ./Config.pl -extraflag=-openmp
-cd $WorkDir/Tmp_AMPS_test/PGI/AMPS/utility/PostProcess; ./Config.pl -extraflag=-openmp
-
-cd $WorkDir/Tmp_AMPS_test/PGI/AMPS/
-make EXTRALINKEROPTIONS="./utility/PostProcess/pproc.a -openmp" test_CG-PostProcess--Read-Trajectories--off_compile -j
-mpicxx -o amps srcTemp/main/main.a srcTemp/libAMPS.a -lstdc++ /Users/vtenishe/SPICE/Toolkit/cspice/lib/cspice.a ./utility/PostProcess/pproc.a
-make test_CG-PostProcess--Read-Trajectories--off_rundir
 
 echo " done."
 
