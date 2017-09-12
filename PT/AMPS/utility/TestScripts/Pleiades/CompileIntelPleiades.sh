9a700ef17b28b6c896a4e263d86a8ac277393355
shyinsi <>
2017-09-12 20:54:31 +0000

    with boost loaded
    Former-commit-id: 0c806b8987b5632f9a02ea926a21e372e769d923

--- PT/AMPS/utility/TestScripts/Pleiades/CompileIntelPleiades.sh
+++ PT/AMPS/utility/TestScripts/Pleiades/CompileIntelPleiades.sh
@@ -8,6 +8,7 @@ set WorkDir = /nobackup/`whoami`
 module purge;
 module load comp-intel/2016.2.181;           
 module load mpi-sgi/mpt;
+module load boost/1.62
 
 echo -n "Compiling Intel....."                 
 cd $WorkDir/Tmp_AMPS_test/Intel/AMPS           
