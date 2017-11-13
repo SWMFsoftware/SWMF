c7e964160f4bd1c61772a27c9cd9a1b78be9c203
gtoth <>
2017-11-13 22:50:49 +0000

    Moved the Fortran matvec into ../test so it can be different for different applications.
    Former-commit-id: e4cc609aec59540e1f06fcec3f11a978d4c7abff

--- share/Library/src/linear_solver_wrapper.f90
+++ share/Library/src/linear_solver_wrapper.f90
@@ -5,30 +5,6 @@
 ! Wrapper to call ModLinearSolver methods from C/C++ code
 
 !============================================================================
-subroutine linear_solver_matvec(x_I, y_I, n)
-
-  ! Fortran subroutine calling C matvec routine
-
-  use  iso_c_binding
-  implicit none
-  interface
-     subroutine linear_solver_matvec_c(x_I, y_I, n)  bind(C) 
-       use iso_c_binding
-       integer(c_int ), VALUE:: n
-       real(c_double)        :: x_I(n)
-       real(c_double)        :: y_I(n)
-     end subroutine linear_solver_matvec_c
-  end interface
-
-  integer(c_int), intent(in) :: n
-  real(c_double), intent(in) :: x_I(n) 
-  real(c_double), intent(out):: y_I(n)
-  !--------------------------------------------------------------------------
-
-  call linear_solver_matvec_c(x_I, y_I, n)
-
-end subroutine linear_solver_matvec
-!============================================================================
 subroutine linear_solver_gmres(Rhs_I, x_I, lInit, n, nKrylov, &
      Tolerance, nIter, iError, lTest) bind(C)
 
