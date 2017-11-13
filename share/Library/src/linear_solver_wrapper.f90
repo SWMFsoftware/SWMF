81ebd9a7ed36356c01dbea01c92d631e17f00ec4
shyinsi <>
2017-11-13 20:14:37 +0000

    Added wrapper of ModLinearSolver for C/C++ code
    Former-commit-id: 3ea89db97fce1e4635082872a331ed34e3bcb72f

---
+++ share/Library/src/linear_solver_wrapper.f90
@@ -0,0 +1,72 @@
+!  Copyright (C) 2002 Regents of the University of Michigan, 
+!  portions used with permission 
+!  For more information, see http://csem.engin.umich.edu/tools/swmf
+!
+! Wrapper to call ModLinearSolver methods from C/C++ code
+
+!============================================================================
+subroutine linear_solver_matvec(x_I, y_I, n)
+
+  ! Fortran subroutine calling C matvec routine
+
+  use  iso_c_binding
+  implicit none
+  interface
+     subroutine linear_solver_MatVec_C(x_I, y_I, n)  bind(C) 
+       use iso_c_binding
+       integer(c_int ), VALUE:: n
+       real(c_double)        :: x_I(n)
+       real(c_double)        :: y_I(n)
+     end subroutine linear_solver_MatVec_C
+  end interface
+
+  integer(c_int),  intent(in)   :: n
+  real (c_double), intent(in)   :: x_I(n) 
+  real (c_double), intent(inout):: y_I(n)
+  !--------------------------------------------------------------------------
+
+  call linear_solver_matvec_c(x_I, y_I, n)
+
+end subroutine linear_solver_matvec
+!============================================================================
+subroutine linear_solver_gmres(Rhs_I, x_I, lInit, n, nKrylov, &
+     Tolerance, nIter, iError, lTest) bind(C)
+
+  use iso_c_binding
+  use ModLinearSolver, ONLY: gmres     
+
+  implicit none     
+ 
+  ! subroutine for matrix vector multiplication 
+  interface
+     subroutine linear_solver_MatVec(x_I, y_I, n) 
+       implicit none
+       ! Calculate y = M.x where M is the matrix
+       integer, intent(in) :: n
+       real, intent(in) ::  x_I(n)
+       real, intent(out) :: y_I(n)
+     end subroutine linear_solver_Matvec
+  end interface
+
+  integer, intent(in) :: n         !  number of unknowns.
+  integer, intent(in) :: nKrylov   ! size of krylov subspace
+  real,    intent(in) :: Rhs_I(n)    ! right hand side vector
+  real, intent(inout) :: x_I(n)    ! initial guess / solution vector
+  integer, intent(in) :: lInit
+  real, intent(inout) :: Tolerance       ! required / achieved residual
+
+  integer, intent(inout) :: nIter    ! maximum/actual number of iterations
+
+  integer, intent(out)   :: iError    ! gives reason for returning:
+  !     abs(info)=  0 - solution found satisfying given tolerance.
+  !                 2 - no convergence within maximum number of iterations.
+  !                 3 - initial guess satisfies the stopping criterion.
+  !    sign(info)=  + - residual decreased
+  !                 - - residual did not reduce
+  integer, intent(in) :: lTest
+
+  !----------------------------------------------------------------------
+  call  gmres(linear_solver_Matvec,Rhs_I, x_I, lInit==1, n, nKrylov,&
+       Tolerance,'rel',nIter,iError,lTest==1)
+
+end subroutine Linear_solver_gmres
