8ce7b5845f0b42d61ed3d429d7d64df519455410
gtoth <>
2017-11-14 23:36:57 +0000

    Put F90 matvec here with a switch that is set in the call linear_solver_gmres
    Former-commit-id: 67751ecd032f46d6be7afea8f07878f058ebb890

--- share/Library/src/linear_solver_wrapper.f90
+++ share/Library/src/linear_solver_wrapper.f90
@@ -4,17 +4,80 @@
 !
 ! Wrapper to call ModLinearSolver methods from C/C++ code
 
+
 !============================================================================
-subroutine linear_solver_gmres(Rhs_I, x_I, lInit, n, nKrylov, &
+subroutine linear_solver_matvec(x_I, y_I, n)
+
+  ! Fortran subroutine calling C matvec routine
+
+  use ModLinearSolver, ONLY: iMatvecC
+  use iso_c_binding
+
+  implicit none
+
+  interface
+     subroutine linear_solver_matvec_c(x_I, y_I, n)  bind(C) 
+       use iso_c_binding
+       integer(c_int ), VALUE:: n
+       real(c_double)        :: x_I(n)
+       real(c_double)        :: y_I(n)
+     end subroutine linear_solver_matvec_c
+  end interface
+
+  interface
+     subroutine linear_solver_matvec_c2(x_I, y_I, n)  bind(C) 
+       use iso_c_binding
+       integer(c_int ), VALUE:: n
+       real(c_double)        :: x_I(n)
+       real(c_double)        :: y_I(n)
+     end subroutine linear_solver_matvec_c2
+  end interface
+
+  integer(c_int), intent(in) :: n
+  real(c_double), intent(in) :: x_I(n) 
+  real(c_double), intent(out):: y_I(n)
+  !--------------------------------------------------------------------------
+
+  select case(iMatVecC)
+  case(1)
+     !call linear_solver_matvec_c(x_I, y_I, n)  ! MATVEC1
+  case(2)
+     !call linear_solver_matvec_c2(x_I, y_I, n) ! MATVEC2
+  end select
+
+end subroutine linear_solver_matvec
+
+!============================================================================
+subroutine linear_solver_gmres(iMatvec, &
+     Rhs_I, x_I, lInit, n, nKrylov, &
      Tolerance, nIter, iError, lTest) bind(C)
 
   ! GMRES subroutine that can be called from C
 
   use iso_c_binding
-  use ModLinearSolver, ONLY: gmres     
+  use ModLinearSolver, ONLY: gmres, iMatvecC
 
   implicit none     
- 
+
+  integer, intent(in):: iMatvec   ! index of C Matvec function.
+  integer, intent(in):: n         ! number of unknowns.
+  integer, intent(in):: nKrylov   ! size of krylov subspace
+  real,    intent(in):: Rhs_I(n)  ! right hand side vector
+  real, intent(inout):: x_I(n)    ! initial guess / solution vector
+  integer, intent(in):: lInit     ! true if initial guess is not zero
+  real, intent(inout):: Tolerance ! required / achieved residual
+
+  integer, intent(inout):: nIter  ! maximum/actual number of iterations
+
+  integer, intent(out)  :: iError ! gives reason for returning:
+  !     abs(info)=  0 - solution found satisfying given tolerance.
+  !                 2 - no convergence within maximum number of iterations.
+  !                 3 - initial guess satisfies the stopping criterion.
+  !    sign(info)=  + - residual decreased
+  !                 - - residual did not reduce
+
+  integer, intent(in)   :: lTest ! true if test info should be written
+
   ! subroutine for matrix vector multiplication 
   interface
      subroutine linear_solver_matvec(x_I, y_I, n) 
@@ -26,24 +89,9 @@ subroutine linear_solver_gmres(Rhs_I, x_I, lInit, n, nKrylov, &
      end subroutine linear_solver_matvec
   end interface
 
-  integer, intent(in) :: n         !  number of unknowns.
-  integer, intent(in) :: nKrylov   ! size of krylov subspace
-  real,    intent(in) :: Rhs_I(n)    ! right hand side vector
-  real, intent(inout) :: x_I(n)    ! initial guess / solution vector
-  integer, intent(in) :: lInit
-  real, intent(inout) :: Tolerance       ! required / achieved residual
-
-  integer, intent(inout) :: nIter    ! maximum/actual number of iterations
-
-  integer, intent(out)   :: iError    ! gives reason for returning:
-  !     abs(info)=  0 - solution found satisfying given tolerance.
-  !                 2 - no convergence within maximum number of iterations.
-  !                 3 - initial guess satisfies the stopping criterion.
-  !    sign(info)=  + - residual decreased
-  !                 - - residual did not reduce
-  integer, intent(in) :: lTest
-
   !----------------------------------------------------------------------
+  iMatvecC = iMatvec
+
   call gmres(linear_solver_matvec, Rhs_I, x_I, lInit==1, n, nKrylov,&
        Tolerance,'rel',nIter,iError,lTest==1)
 
