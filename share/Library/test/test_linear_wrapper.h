352a312664afbc274ec08ce98852ecfe5ef76e16
shyinsi <>
2017-11-20 19:31:55 +0000

    Now the header is in ../src/linear_solver_wrapper_c.h with some modifications.
    Former-commit-id: 53fa5a654c115c3b6f27d476634b6f8adb67b2ce

--- share/Library/test/test_linear_wrapper.h
+++
@@ -1,7 +0,0 @@
-extern "C"{
-  void linear_solver_matvec_c(double * VecIn, double * VecOut, int n );
-  void linear_solver_gmres(int * iMatvec, double * Rhs_I, double * x_I, 
-			    int * lInit, int * n, int * nKrylov,
-			   double * Tol, int * nIter, int * iError, int * lTest, MPI_Fint * iComm);
-
-}
