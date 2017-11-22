6663ba1b99fcb8c508bd3a3d7afc93607ab4447b
shyinsi <>
2017-11-22 20:55:19 +0000

    Now the test solves a 1-D laplacian problem, with fix boundary condition at two ends.
    Former-commit-id: 7f46695d6e937c06f43aae45a20f77bf6c2d22e6

--- share/Library/test/test_linear_wrapper.cpp
+++ share/Library/test/test_linear_wrapper.cpp
@@ -5,17 +5,17 @@
 # include "linear_solver_wrapper_c.h"
 # include <stdio.h>
 
-int rank, size;
+int rank, nProc;
 
 void pass_cell_info(double * Vec, int n, double  *head, double *end){
   double  buffer_forward, buffer_backward;
   MPI_Status  status;
   
-  if (rank != size-1){
+  if (rank != nProc-1){
     MPI_Recv(&buffer_forward,1,MPI_DOUBLE,rank+1,1,MPI_COMM_WORLD,&status);
     (*end)=buffer_forward;
   }else{
-    (*end) = 0.0;
+    (*end) = n-1;
   }
 
   if (rank != 0) {
@@ -26,16 +26,16 @@ void pass_cell_info(double * Vec, int n, double  *head, double *end){
   if (rank != 0){
     MPI_Recv(&buffer_backward,1,MPI_DOUBLE,rank-1,0,MPI_COMM_WORLD,&status);
     (*head)=buffer_backward;
-    // printf("buffer_backward:%f,head:%f\n",buffer_backward,*head);
   }else{
     (*head) = 0.0;
   }
   
-  if (rank != size-1){
+  if (rank != nProc-1){
     buffer_backward = Vec[n-1];
     MPI_Send(&buffer_backward,1,MPI_DOUBLE,rank+1,0,MPI_COMM_WORLD);
   }
 }
+
 
 void linear_solver_matvec_c_default(double* VecIn, double * VecOut, int n){
   
@@ -43,25 +43,50 @@ void linear_solver_matvec_c_default(double* VecIn, double * VecOut, int n){
   double  end;
 
   pass_cell_info(VecIn,n,&head,&end);//pass ghost cell info
-  
-  double alpha=0.1;
 
-  // printf("rank:%d,head:%f,end:%f\n",rank,head,end);
   for (int i=1; i<n-1; i++){ 
-    VecOut[i] = -alpha*(VecIn[i-1]+VecIn[i+1])+(1+2.*alpha)*VecIn[i];
+    VecOut[i] = VecIn[i-1]+VecIn[i+1]-2.*VecIn[i];
   }
 
   if(rank != 0){
-    VecOut[0] = -alpha*((head)+VecIn[1])+(1+2.*alpha)*VecIn[0];
+    VecOut[0] = head+VecIn[1]-2*VecIn[0];
   }else{
-    VecOut[0] = 0.0; //set bc
+    VecOut[0] = VecIn[0]; //set bc
   }
 
-  if(rank != size-1){
-    VecOut[n-1] = -alpha*(VecIn[n-2]+(end))+(1+2.*alpha)*VecIn[n-1];
+  if(rank != nProc-1){
+    VecOut[n-1] = VecIn[n-2]+end-2.*VecIn[n-1];
   }else{
-    VecOut[n-1] = 0.0; //set bc
+    VecOut[n-1] = VecIn[n-1]; //set bc
   }
+
+}
+
+double ** GetPrecondMatrix(int nVectorLen, int nDim){
+  double ** matrix = new double * [2*nDim+1];
+  matrix[0] = new double [(2*nDim+1)*nVectorLen];
+  for (int i=1;i<2*nDim+1;i++) matrix[i]=matrix[i-1]+nVectorLen;
+
+  for (int i=0;i<nVectorLen;i++){
+    // set the diagonal vector   
+    matrix[0][i] = -2;
+    // set the sub diagonal vector 
+    matrix[1][i] = (i!=0)?1:0;   
+    // set the super diagonal vector 
+    matrix[2][i] = (i!=nVectorLen-1)?1:0;
+    
+  }
+  //first type of boundary condition at two ends
+  //work for one block per proc case
+  if (rank==0) {
+    matrix[0][0] =1.; 
+    matrix[2][0]=0.0;
+  }
+  if (rank==nProc-1) {
+    matrix[0][nVectorLen-1]=1.;
+    matrix[1][nVectorLen-1]=0.0;
+  }
+  return matrix;
 
 }
 
@@ -78,63 +103,55 @@ int main(){
   MPI_Init(NULL,NULL);
  
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_size(MPI_COMM_WORLD, &nProc);
 
-  int n=101; // number of true cells
-  int nLen =n/size; //length of first size-1 procs
+  int n=200; // number of true cells
+  int nLen =n/nProc; //length of first nProc-1 procs
   int nCellProc=nLen;//number of cell for this proc
-  if (rank==size-1){
-    nCellProc =(n%nLen==0)?nLen:n-(size-1)*nLen;//nCellProc not includes ghost cells  
+  if (rank==nProc-1){
+    nCellProc =(n%nLen==0)?nLen:n-(nProc-1)*nLen;//nCellProc not includes ghost cells  
   }
  
   //alpha = 0.1; // the coefficient (dt)/(dx)^2, defined in MatVec_C.cpp
   //dt =1e-5;
   double  dx=1.0/((double)n-1);
   double nTotalStep=2;
-
-  int lInit=1;
-  int nKrylov=200;
-  double Tol=1e-7;
+  double Tol=1e-5;
   int nIter=200;
   int iError;
-  int lTest=0;
+  int lTest = (rank == 0);
 
   // set the init condition as a sine function
   // the Sol_I contains the first initial guess the same as Rhs_I
   double Rhs_I[nCellProc];
   double Sol_I[nCellProc];
-  double Pi = 3.14159265;
   
   for (int i=0; i<nCellProc; i++) {
-    Sol_I[i] = 100.*sin(Pi*(i+rank*nLen)/(n-1));
+    Rhs_I[i] = 0;
   }
+
+  //set boundary condition 
+  if (rank==nProc-1) Rhs_I[nCellProc-1] = n-1; 
 
   // Convert C MPI communicator to Fortran integer
   MPI_Fint iComm = MPI_Comm_c2f(MPI_COMM_WORLD);
   MPI_Status  status;
   int nTotalLenProc;// cells including ghost cells
-
-  for (int iStep=1; iStep<=nTotalStep; iStep++){   
-    if (rank==0)      Sol_I[0]           = 0.0;
-    if (rank==size-1) Sol_I[nCellProc-1] = 0.0;
     
-    for (int i=0; i<nCellProc; i++) Rhs_I[i] = Sol_I[i];
-    /* test input
-    for (int iProc=0;iProc<size;iProc++){
-      if (rank==iProc) {
-	printf("rank:%d,nCellProc:%d\n", rank,nCellProc);
-	for (int i=0; i<nCellProc; i++) {
-	  printf("rank:%d input i:%d, %f\n",rank,i,Rhs_I[i]);
-	}
-      }
-    }
-    */ 
-    linear_solver_gmres( Rhs_I, Sol_I, &lInit, &nCellProc, &nKrylov, &Tol, &nIter, &iError, &lTest, &iComm);
-  }
-  
-  // the analytic solution is 100*exp(-Pi^2*t)sin(Pi*x)
-  // dt=1e-5; the peak at t=1e-2 is 90.6
-  
+  double ** precond_matrix_II;
+  // parameter to choose preconditioner types
+  //0:No precondition; 1: BILU; 2:DILU;
+    //[-1,0): MBILU; 
+  double PrecondParam=2;
+  int nVar=1, nDim=1, nI=nCellProc, nJ=1, nK=1, nBlock=1;
+
+  // get precond matrix for 1D case 
+  precond_matrix_II = GetPrecondMatrix(nVar*nVar*nI*nJ*nK,nDim); 
+  linear_solver_wrapper("GMRES", &Tol,&nIter, &nVar, &nDim,
+			&nI, &nJ, &nK, &nBlock, &iComm, Rhs_I,
+			Sol_I, &PrecondParam, precond_matrix_II[0], 
+			&lTest);
+      
   int tag=0, nLength;
   double buffer[nCellProc], output[n];
 
@@ -154,19 +171,22 @@ int main(){
     }
 
     // recieve from others
-    for (int i=1; i<size; i++) {
+    for (int i=1; i<nProc; i++) {
       // number of elements to recieve
       nLength = nLen;
 
       // Last processor can have different number of elements
-      if (i == size-1) nLength = (n%nLen==0)?nLen:n-nLen*(size-1);
+      if (i == nProc-1) nLength = (n%nLen==0)?nLen:n-nLen*(nProc-1);
 
       MPI_Recv(buffer, nLength, MPI_DOUBLE, i,  tag, MPI_COMM_WORLD,  &status);
 
       // print result out
       for (int j=0; j<nLength; j++) output[j+i*nLen] = buffer[j];
     }
-    for (int i=0; i<n; i++) printf("%f\n",output[i]);
+    for (int i=0; i<n; i++) {
+      printf("%f\n",output[i]);
+      if (abs(output[i]-i)>1e-3) printf("ERROR!\n");
+    }
   }
 
   MPI_Finalize();
