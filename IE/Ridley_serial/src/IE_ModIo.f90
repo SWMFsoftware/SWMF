ba160367758149480329643bff0f00a57eea7a2b
gtoth <>
2017-11-28 22:27:12 +0000

    Added DoRestart logical and removed some unused variables.
    Former-commit-id: a59d43da16835af82a2a1dfc4c7d06dc141d2f22

--- IE/Ridley_serial/src/IE_ModIo.f90
+++ IE/Ridley_serial/src/IE_ModIo.f90
@@ -5,7 +5,7 @@ module IE_ModIo
 
   ! Unit number for temporary files
   use ModIoUnit, ONLY: iUnit => UNITTMP_, STDOUT_
-  use ModUtilities, ONLY: open_file, close_file
+  use ModUtilities, ONLY: open_file, close_file, check_dir, make_dir
 
   implicit none
   save
@@ -20,17 +20,19 @@ module IE_ModIo
   ! Actual number of plot files
   integer :: nFile=0
 
-  character (LEN=3)   :: plot_form(maxfile)
-  character (LEN=100) :: plot_vars(maxfile)
-  character (LEN=80)  :: filename
-  character (len=100) :: NameIonoDir='ionosphere/'
-  character (len=100) :: NameRestartInDir  = 'IE/restartIN/'
-  character (len=100) :: NameRestartOutDir = 'IE/restartOUT/'
-  character (len=100) :: NameFile
+  character(LEN=3)   :: plot_form(MaxFile)
+  character(LEN=100) :: plot_vars(MaxFile)
+  character(len=100) :: NameIonoDir='ionosphere/'
 
-  integer             :: dn_output(MaxFile)=-1, dn_magoutput = -1
-  real                :: dt_output(MaxFile)=-1.0, dt_magoutput = -1.0
-  integer             :: t_output_last(MaxFile)=-1, t_magoutput_last = -1
+  logical            :: DoRestart = .false.
+  character(len=100) :: NameRestartInDir  = 'IE/restartIN/'
+  character(len=100) :: NameRestartOutDir = 'IE/restartOUT/'
+
+  character(len=100) :: NameFile
+
+  integer            :: dn_output(MaxFile)=-1
+  real               :: dt_output(MaxFile)=-1.0
+  integer            :: t_output_last(MaxFile)=-1
 
   ! Plot file name string logicals.
   logical :: IsPlotName_e = .false.
@@ -46,6 +48,5 @@ contains
     if(iUnitOut==STDOUT_)write(*,'(a)',ADVANCE='NO')trim(StringPrefix)
 
   end subroutine write_prefix
-
 
 end module IE_ModIo
