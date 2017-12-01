4d9e2ccc8387bcb0e14d59641543a874fae483ca
vtenishe <>
2017-12-01 23:38:25 +0000

    modified linear_solver_wrapper(): in the declaration of the function 'char*' is replaced by 'const char*'
    Former-commit-id: 5754313a5b730ea7215bd96036d03697bc09b2a9

--- share/Library/src/linear_solver_wrapper_c.h
+++ share/Library/src/linear_solver_wrapper_c.h
@@ -1,7 +1,7 @@
 // function interface for the fortran code ModLinearSolver
 extern "C"{
   void linear_wrapper_matvec_c(double * VecIn, double * VecOut, int n );
-  void linear_solver_wrapper(char * solverType,double * tolerance, int * nIteration, 
+  void linear_solver_wrapper(const char * solverType,double * tolerance, int * nIteration, 
 			     int* nVar,int* nDim,int* nI,int* nJ,int* nK,int* nBlock,
 			     int* iComm, double* Rhs_I, double* x_I, 
 			     double* PrecondParam,double *precond_matrix, int* lTest);
