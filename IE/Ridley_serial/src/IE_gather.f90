0d2516864b82e278de0a4250ad86f616c4d30790
gtoth <>
2017-12-02 20:36:44 +0000

    Using MPI_reduce_real_array to simplify routine
    Former-commit-id: c324b0cd3dbf00c6decc177d9d4d303afdb8d27d

--- IE/Ridley_serial/src/IE_gather.f90
+++ IE/Ridley_serial/src/IE_gather.f90
@@ -1,4 +1,5 @@
-!  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
+!  Copyright (C) 2002 Regents of the University of Michigan, 
+!  portions used with permission 
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
 
 subroutine IE_gather
@@ -9,75 +10,49 @@ subroutine IE_gather
 
   implicit none
 
-  real :: localVar(2*IONO_nTheta-1, IONO_nPsi)
-  integer :: iError, iSize
+  integer :: iError, n
   !--------------------------------------------------------------------------
-  IONO_phi = -1.0e32
-  IONO_IonNumFlux = -1.0e32
-  IONO_Joule = -1.0e32
-  IONO_Jr = -1.0e32
-  IONO_Ave_E = -1.0e32
-  IONO_Eflux = -1.0e32
-  IONO_SigmaP = -1.0e32
-  IONO_SigmaH = -1.0e32
+  IONO_phi        = -Huge(1.0)
+  IONO_IonNumFlux = -Huge(1.0)
+  IONO_Joule      = -Huge(1.0)
+  IONO_Jr         = -Huge(1.0)
+  IONO_Ave_E      = -Huge(1.0)
+  IONO_Eflux      = -Huge(1.0)
+  IONO_SigmaP     = -Huge(1.0)
+  IONO_SigmaH     = -Huge(1.0)
 
   if (iProc == 0) then
-     IONO_phi(1:IONO_nTheta,:) = IONO_NORTH_Phi
+     IONO_phi(1:IONO_nTheta,:)        = IONO_NORTH_Phi
      IONO_IonNumFlux(1:IONO_nTheta,:) = IONO_NORTH_IonNumFlux
-     IONO_Joule(1:IONO_nTheta,:) = IONO_NORTH_Joule
-     IONO_Jr(1:IONO_nTheta,:) = IONO_NORTH_Jr
-     IONO_Ave_E(1:IONO_nTheta,:) = IONO_NORTH_Ave_E
-     IONO_Eflux(1:IONO_nTheta,:) = IONO_NORTH_EFlux
-     IONO_SigmaP(1:IONO_nTheta,:) = IONO_NORTH_SigmaP
-     IONO_SigmaH(1:IONO_nTheta,:) = IONO_NORTH_SigmaH
+     IONO_Joule(1:IONO_nTheta,:)      = IONO_NORTH_Joule
+     IONO_Jr(1:IONO_nTheta,:)         = IONO_NORTH_Jr
+     IONO_Ave_E(1:IONO_nTheta,:)      = IONO_NORTH_Ave_E
+     IONO_Eflux(1:IONO_nTheta,:)      = IONO_NORTH_EFlux
+     IONO_SigmaP(1:IONO_nTheta,:)     = IONO_NORTH_SigmaP
+     IONO_SigmaH(1:IONO_nTheta,:)     = IONO_NORTH_SigmaH
   endif
 
   if (iProc == nProc-1) then
-     IONO_phi(IONO_nTheta:2*IONO_nTheta-1,:) = IONO_SOUTH_Phi
+     IONO_phi(IONO_nTheta:2*IONO_nTheta-1,:)        = IONO_SOUTH_Phi
      IONO_IonNumFlux(IONO_nTheta:2*IONO_nTheta-1,:) = IONO_SOUTH_IonNumFlux
-     IONO_Joule(IONO_nTheta:2*IONO_nTheta-1,:) = IONO_SOUTH_Joule
-     IONO_Jr(IONO_nTheta:2*IONO_nTheta-1,:) = IONO_SOUTH_Jr
-     IONO_Ave_E(IONO_nTheta:2*IONO_nTheta-1,:) = IONO_SOUTH_Ave_E
-     IONO_Eflux(IONO_nTheta:2*IONO_nTheta-1,:) = IONO_SOUTH_EFlux
-     IONO_SigmaP(IONO_nTheta:2*IONO_nTheta-1,:) = IONO_SOUTH_SigmaP
-     IONO_SigmaH(IONO_nTheta:2*IONO_nTheta-1,:) = IONO_SOUTH_SigmaH
+     IONO_Joule(IONO_nTheta:2*IONO_nTheta-1,:)      = IONO_SOUTH_Joule
+     IONO_Jr(IONO_nTheta:2*IONO_nTheta-1,:)         = IONO_SOUTH_Jr
+     IONO_Ave_E(IONO_nTheta:2*IONO_nTheta-1,:)      = IONO_SOUTH_Ave_E
+     IONO_Eflux(IONO_nTheta:2*IONO_nTheta-1,:)      = IONO_SOUTH_EFlux
+     IONO_SigmaP(IONO_nTheta:2*IONO_nTheta-1,:)     = IONO_SOUTH_SigmaP
+     IONO_SigmaH(IONO_nTheta:2*IONO_nTheta-1,:)     = IONO_SOUTH_SigmaH
   endif
 
   if (nProc > 1) then
-
-     iSize = (2*IONO_nTheta-1) * IONO_nPsi
-
-     localVar = IONO_Phi
-     call MPI_REDUCE(localVar, IONO_Phi, iSize, MPI_REAL, MPI_MAX, &
-          0, iComm, iError)
-
-     localVar = IONO_IonNumFlux
-     call MPI_REDUCE(localVar, IONO_IonNumFlux, iSize, MPI_REAL, MPI_MAX, &
-          0, iComm, iError)
-
-     localVar = IONO_Joule
-     call MPI_REDUCE(localVar, IONO_Joule, iSize, MPI_REAL, MPI_MAX, &
-          0, iComm, iError)
-     
-     localVar = IONO_Jr
-     call MPI_REDUCE(localVar, IONO_Jr, iSize, MPI_REAL, MPI_MAX, &
-          0, iComm, iError)
-
-     localVar = IONO_Ave_E
-     call MPI_REDUCE(localVar, IONO_Ave_E, iSize, MPI_REAL, MPI_MAX, &
-          0, iComm, iError)
-
-     localVar = IONO_Eflux
-     call MPI_REDUCE(localVar, IONO_Eflux, iSize, MPI_REAL, MPI_MAX, &
-          0, iComm, iError)     
-
-     localVar = IONO_SigmaP
-     call MPI_REDUCE(localVar, IONO_SigmaP, iSize, MPI_REAL, MPI_MAX, &
-          0, iComm, iError)     
-
-     localVar = IONO_SigmaH
-     call MPI_REDUCE(localVar, IONO_SigmaH, iSize, MPI_REAL, MPI_MAX, &
-          0, iComm, iError)     
+     n = size(IONO_Phi)
+     call MPI_reduce_real_array(IONO_Phi,        n, MPI_MAX, 0, iComm, iError)
+     call MPI_reduce_real_array(IONO_IonNumFlux, n, MPI_MAX, 0, iComm, iError)
+     call MPI_reduce_real_array(IONO_Joule,      n, MPI_MAX, 0, iComm, iError)
+     call MPI_reduce_real_array(IONO_Jr,         n, MPI_MAX, 0, iComm, iError)
+     call MPI_reduce_real_array(IONO_Ave_E,      n, MPI_MAX, 0, iComm, iError)
+     call MPI_reduce_real_array(IONO_Eflux,      n, MPI_MAX, 0, iComm, iError)
+     call MPI_reduce_real_array(IONO_SigmaP,     n, MPI_MAX, 0, iComm, iError)
+     call MPI_reduce_real_array(IONO_SigmaH,     n, MPI_MAX, 0, iComm, iError)
 
   endif
 
