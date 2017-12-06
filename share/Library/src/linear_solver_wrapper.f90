4bdf0181c105a88f8b9999634f2edb0c3a5f9263
shyinsi <>
2017-12-06 19:52:49 +0000

    Moved the subroutine linear_solver_matvec and some shared variables to the module. moved get_precond_matrix before looping through blocks to calculate the RHS with preconditioner multiplied.
    Former-commit-id: 937e28459f3c3a70435513e658dbaa3e1677da99

--- share/Library/src/linear_solver_wrapper.f90
+++ share/Library/src/linear_solver_wrapper.f90
@@ -4,46 +4,112 @@
 !
 
 !============================================================================
+module ModLinearSolverWrapper
+
+  use ModLinearSolver, only: LinearSolverParamType
+  implicit none
+  type(LinearSolverParamType), public :: Param
+  integer, public :: iBlock, nVarIjk
+  integer, public :: nDim       ! Number of spatial dimensions
+  integer, public :: nVar       ! Number of impl. variables/cell
+  integer, public :: nI, nJ, nK ! Number of cells in a block
+  integer, public :: nBlock     ! Number of impl. blocks on current proc
+  real, pointer,   public :: PrecondMatrix_II(:,:)
+
+  contains
+    subroutine linear_solver_matvec(x_I, y_I, n)
+
+      ! Fortran subroutine calling C matvec routine
+      use iso_c_binding
+      use ModLinearSolver, ONLY: multiply_left_precond
+
+
+      implicit none
+
+      interface
+         subroutine linear_wrapper_matvec_c(x_I, y_I, n)  bind(C) 
+           integer, VALUE:: n
+           real          :: x_I(n)
+           real          :: y_I(n)
+         end subroutine linear_wrapper_matvec_c
+      end interface
+
+      integer, intent(in) :: n
+      real,    intent(in) :: x_I(n) 
+      real,    intent(out):: y_I(n)
+
+      integer :: iStart
+      !-----------------------------------------------------------------------
+
+      call linear_wrapper_matvec_c(x_I, y_I, n)
+
+      if(Param%DoPrecond) then
+         do iBlock = 1, nBlock
+
+            ! Starting index in the linear arrays
+            iStart = nVarIjk*(iBlock-1)+1
+            call multiply_left_precond(&
+                 Param%TypePrecond, Param%TypePrecondSide, &
+                 nVar, nDim, nI, nJ, nK, PrecondMatrix_II, &
+                 y_I(iStart))
+         end do
+      end if
+
+    end subroutine linear_solver_matvec
+    !=========================================================================
+
+  end module ModLinearSolverWrapper
+!============================================================================
 subroutine linear_solver_wrapper(TypeSolver, Tolerance, nIteration, &
-     nVar, nDim, nI, nJ, nK, nBlock, iComm, Rhs_I, x_I, &
-     PrecondParam, PrecondMatrix_II, lTest) bind(C)
+     nVarIn, nDimIn, nIIn, nJIn, nKIn, nBlockIn, iComm, Rhs_I, x_I, &
+     PrecondParam, PrecondMatrixIn_II, lTest) bind(C)
 
   ! Wrapper to call ModLinearSolver methods from C/C++ code
 
-  use iso_c_binding   
-  use ModLinearSolver, only: get_precond_matrix, multiply_left_precond,&
+  use iso_c_binding
+  use ModLinearSolverWrapper
+  use ModLinearSolver, ONLY: get_precond_matrix, multiply_left_precond,&
        multiply_initial_guess, bicgstab,gmres,cg,LinearSolverParamType
   use ModUtilities, ONLY: char_array_to_string, CON_stop
+ 
   implicit none
 
   character(c_char), intent(in) :: TypeSolver(*) 
   real,    intent(in):: Tolerance  ! tolerance for the solver
   integer, intent(in):: nIteration ! max iteration number
-  integer, intent(in):: nVar       ! Number of impl. variables/cell
-  integer, intent(in):: nDim       ! Number of spatial dimensions
-  integer, intent(in):: nI, nJ, nK ! Number of cells in a block
-  integer, intent(in):: nBlock     ! Number of impl. blocks on current proc
+  integer, intent(in):: nVarIn       ! Number of impl. variables/cell
+  integer, intent(in):: nDimIn       ! Number of spatial dimensions
+  integer, intent(in):: nIIn, nJIn, nKIn ! Number of cells in a block
+  integer, intent(in):: nBlockIn     ! Number of impl. blocks on current proc
   integer, intent(in):: iComm      ! MPI communicator for processors
   real,    intent(in):: PrecondParam ! Parameter for the preconditioner
   integer, intent(in):: lTest      ! if lTest==1 DoTest=.true.
-  real,    intent(inout):: PrecondMatrix_II(nVar*nVar*nI*nJ*nK,2*nDim+1)
+  real, target, intent(inout):: PrecondMatrixIn_II(nVarIn*nVarIn*nIIn*nJIn*nKIn,2*nDimIn+1)
   !precond_matrix contains diagonal and super/sub diagonal elements from
   !the matrix A, which is in the equation Ax = b 
 
-  real, intent(inout):: Rhs_I(nVar*nI*nJ*nK*nBlock) ! RHS vector
-  real, intent(inout):: x_I(nVar*nI*nJ*nK*nBlock)   ! Initial guess/solution
+  real, intent(inout):: Rhs_I(nVarIn*nIIn*nJIn*nKIn*nBlockIn) ! RHS vector
+  real, intent(inout):: x_I(nVarIn*nIIn*nJIn*nKIn*nBlockIn)   ! Initial guess/solution
 
   logical :: DoTest     ! show Krylov iterations and convergence
 
   ! Local variables
-  integer:: n, iBlock, i, j, k, iVar
-  integer:: nVarIjk, nImpl
-
-  type(LinearSolverParamType) :: Param
+  integer:: n, i, j, k, iVar
+  integer:: nImpl
 
   logical:: DoDebug = .false.
   character(len=*), parameter:: NameSub = 'linear_solver_wrapper'
-  !---------------------------------------------------------------------
+
+  !---------------------------------------------------------------------------
+  
+  nBlock = nBlockIn
+  nVar   = nVarIn
+  nDim   = nDimIn
+  nI     = nIIn
+  nJ     = nJIn
+  nK     = nKIn
+  PrecondMatrix_II => PrecondMatrixIn_II
+
   ! PrecondParam:      The parameter for Gustafsson modification:
   !
   !           +2 DILU  prec: LU for diagonal, keep off-diagonal blocks
@@ -95,11 +161,11 @@ subroutine linear_solver_wrapper(TypeSolver, Tolerance, nIteration, &
   ! Get preconditioning matrix if required. 
   ! Precondition RHS and initial guess (for symmetric prec only)
   if(Param%DoPrecond)then
-     do iBlock = 1, nBlock
-        ! Preconditioning  matrix
-        call get_precond_matrix(                         &
-             Param%PrecondParam, nVar, nDim, nI, nJ, nK, PrecondMatrix_II)
+     ! Preconditioning  matrix
+     call get_precond_matrix(                         &
+          Param%PrecondParam, nVar, nDim, nI, nJ, nK, PrecondMatrix_II)
 
+     do iBlock = 1, nBlock
         ! Starting index in the linear arrays
         n = nVarIjk*(iBlock-1)+1
 
@@ -148,46 +214,6 @@ subroutine linear_solver_wrapper(TypeSolver, Tolerance, nIteration, &
 
   ! Converging without any iteration is not a real error, so set iError=0
   if(Param%iError==3) Param%iError=0
-
-contains
-
-  subroutine linear_solver_matvec(x_I, y_I, n)
-
-    ! Fortran subroutine calling C matvec routine
-    use iso_c_binding
-
-    implicit none
-
-    interface
-       subroutine linear_wrapper_matvec_c(x_I, y_I, n)  bind(C) 
-         integer, VALUE:: n
-         real          :: x_I(n)
-         real          :: y_I(n)
-       end subroutine linear_wrapper_matvec_c
-    end interface
-
-    integer, intent(in) :: n
-    real,    intent(in) :: x_I(n) 
-    real,    intent(out):: y_I(n)
-
-    integer :: iStart
-    !--------------------------------------------------------------------------
-
-    call linear_wrapper_matvec_c(x_I, y_I, n)
-
-    if(Param%DoPrecond) then
-       do iBlock = 1, nBlock
-
-          ! Starting index in the linear arrays
-          iStart = nVarIjk*(iBlock-1)+1
-          call multiply_left_precond(&
-               Param%TypePrecond, Param%TypePrecondSide, &
-               nVar, nDim, nI, nJ, nK, PrecondMatrix_II, &
-               y_I(iStart))
-       end do
-    end if
-
-  end subroutine linear_solver_matvec
 
 end subroutine linear_solver_wrapper
 !=========================================================================
