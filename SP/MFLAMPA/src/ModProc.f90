a1800a86b3e64920249b3a497dc5b024bab201b9
igorsok <>
2017-12-17 22:35:58 +0000

    Add a separate module for iProc, nProc, iComm
    Former-commit-id: bf04b308ff8ac518b6256bab0d1bfa7fd0530fea

---
+++ SP/MFLAMPA/src/ModProc.f90
@@ -0,0 +1,15 @@
+!  Copyright (C) 2002 Regents of the University of Michigan, 
+!  portions used with permission 
+!  For more information, see http://csem.engin.umich.edu/tools/swmf
+!=============================================================!
+module SP_ModProc
+  implicit none
+  SAVE
+  !\
+  ! MPI information
+  !----------------------------------------------------------------------------
+  integer:: iComm = -1
+  integer:: iProc = -1
+  integer:: nProc = -1
+  !/
+end module SP_ModProc
