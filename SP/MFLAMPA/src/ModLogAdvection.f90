0e038e4c6f54d09f5997c14821335d936bb405a3
igorsok <>
2017-12-18 06:50:45 +0000

    Improved identation and comments.
    Former-commit-id: 59bf1d37a3c5d5a4c4b342c6080196f975a34e37

--- SP/MFLAMPA/src/ModLogAdvection.f90
+++ SP/MFLAMPA/src/ModLogAdvection.f90
@@ -2,108 +2,92 @@
 !  portions used with permission 
 !  For more information, see http://csem.engin.umich.edu/tools/swmf
 module SP_ModLogAdvection
+  !DESCRIPTION:
+  !==============================================================
+  ! In space physics applications one often needs to solve the
+  ! "log-advection" equations of the kind of:
+  !         f_t+A*f_lnp=0             (NC)
+  ! or
+  !         f_t+A*(pf)_p=0 .          (C)
+  ! The CONSERVATION LAW Eq.(C) can be also written as
+  !         f_t+A*f_lnp+ A*f=0,
+  ! which is the total of the NONCONSERVATIVE Eq.(NC) plus Af
+  ! Both Eqs.(NC,C) propagate the initial values of f along the lines
+  !         p=p_0*exp(At),
+  ! which results in a uniform expansion (A>0) or shrinking (A<0) of
+  ! the distribution, keeping its shape unchanged. For Eq.(C) the solution
+  ! also scales accordingly, keeping unchanged the integral \int{f dp}
+  ! 
+  ! EXAMPLES: Non-conservative formulation: first-order Fermi acceleration,
+  !           Alfven-wave turbulence evolution in the solar wind
+  !           Conservative formulation: Kolmogorov-type cascade, 
+  !           wave-particle interactions
+  !
+  ! In all these applications, A does not depend on the phase coordinate, p 
   implicit none
   !Revision history
   !Prototype: Sokolov&Roussev, FLAMPA code, 2004
-  !Present version: Sokolov& Roussev, Jan,2008
-
-  !DESCRIPTION
-  !===========================================================================!
-  ! In space physics applications one often needs to solve the "log-advection"!
-  ! equations of the kind of:                                                 !
-  !         f_t+A*f_lnp=0             (NC)                                    !
-  ! or                                                                        !
-  !         f_t+A*(pf)_p=0 .          (C)                                     !
-  ! The latter equation which is a CONSERVATION LAW can be also written as    !
-  !         f_t+A*f_lnp+ A*f=0,                                               !
-  ! which is the total of the NONCONSERVATIVE equation plus Af                !
-  ! Both equations propagate the initial values of f along the lines          !
-  !         p=p_0*exp(At),                                                    !
-  ! which results in a uniform expansion (A>0) or shrinking (A<0) of the      !
-  ! distribution, keeping its shape unchanged. For the conservative equation  !
-  ! the solution also scales accordingly, keeping unchanged the integral      !
-  !\int{f dp}                                                                 !
-  !                                                                           !
-  ! EXAMPLES: Non-conservative formulation: first-order Fermi acceleration,   !
-  !           Alfven-wave turbulence evolution in the solar wind              !
-  !           Conservative formulation: Kolmogorov-type cascade, wave-particle!
-  !           interactions                                                    !
-  ! In all these applications, A does not depend on the phase coordinate, p   !
-  !                                                                           !
-  !===========================================================================!
-
+  !Version: Sokolov& Roussev, Jan,2008, SP/FLAMPA/src/ModLogAdvection.f90
 contains
-  !===========advance_log_advection==========================================!
-  !DESCRIPTION: the procedure integrates the log-advection equation, in the  !
-  !conservative or non-conservative formulation, at a logarithmic grid, using!
-  !a one-state second order scheme                                           !
-  !==========================================================================!
-  subroutine advance_log_advection(&
-       CFLIn,         & 
-       nP,            &
-       nGCLeft,       &
-       nGCRight,      &
-       FInOut_I,      &
-       IsConservative,&
-       DeltaLnP)      
+  !===========advance_log_advection=======================================
+  !DESCRIPTION: the procedure integrates the log-advection equation, in
+  !the conservative or non-conservative formulation, at a logarithmic grid, 
+  !using a single-stage second order scheme
+  subroutine advance_log_advection(CFLIn, nP, nGCLeft, nGCRight,        & 
+       FInOut_I, IsConservative, DeltaLnP)      
 
-    !-------------------------------------------------------------------------!
-    real,intent(in):: CFLIn           !Time step * acceleration rate/(Dlnp).  !
-    integer,intent(in):: nP           !Number of meshes along lnp-coordinate. !
-    integer,intent(in):: nGCLeft      !The solution in the ghost cells is not !
-    integer,intent(in):: nGCRight     !advanced in time but used as the       !
-    !boundary condition, if any              !
-    real,dimension(1-nGCLeft:nP+nGCRight),intent(inout):: &
-         FInOut_I                     !In: sol. to be advanced; Out: advanced !
-    !sol. index 0 is to set boundary         !
-    !condition at the injection energy.      !
-    logical,intent(in)::IsConservative!Solve (C) if .true. (NC) otherwise     !
-    real,optional,intent(in)::DeltaLnP!Used only for NonConservative=.true.   !
-    !-------------------------------------------------------------------------!
-    integer:: iP,iStep,nStep
-    real,dimension(1-max(nGCLeft,2):nP+max(nGCRight,2))::F_I
-    real,dimension(0:nP+1):: FSemiintUp_I,FSemiintDown_I
-    real::CFL,HalfADtIfNeeded
-    !-------------------------NonConservative---------------------------------!
-    ! This is a one-stage second-order scheme for the advection equation:     !
-    !             f_t+A f_{ln p}=0,         .                                 !
-    !                                                                         !
-    ! Herewith CFL=A*Delta t/Delta (ln p):                                    !
-    ! f^(n+1)_i-CFL*(f^n_(i-1/2)-f^n_(i+1/2)=f^n_i, where                     !
-    ! For CFL>0:                                                              !
-    ! f^n_(i-1/2)=f^n_(i-1)+cHalf*(cOne-CFL)*df_lim^n_(i-1)                   !
-    ! For CFL<0:                                                              !
-    ! f^n_(i-1/2)=f^n_(i  )-cHalf*(cOne+CFL)*df_lim^n_(i  )                   !
-    !-------------------------------------------------------------------------!
-    !-------------------------   Conservative---------------------------------!
-    ! This is a one-stage second-order scheme for the advection equation:     !
-    !             f_t+A (f p}_p=0,         .                                  !
-    ! We now need to know both                                                !
-    ! A*Delta t=CFLIn*Delta (LnP)                                             !
-    ! and                                                                     !
-    ! CFL=A*Delta t/(2 tanh(Delta (ln p)/2)                                   !
-    ! f^(n+1)_i=CFL*(f^n_(i-1/2)-f^n_(i+1/2))- (1/2)(f^n_(i-1/2)+f^n_(i+1/2))*!
-    !                                   *A*Delta t+f^n_i,                     !
-    !where                                                                    !
-    ! For CFL>0:                                                              !
-    !f^n_(i-1/2)=f^n_(i-1)*(1-(1/2)*A*Delta t)+cHalf*(cOne-CFL)*df_lim^n_(i-1)!
-    ! For CFL<0:                                                              !
-    !f^n_(i-1/2)=f^n_(i  )*(1-(1/2)*A*Delta t)-cHalf*(cOne+CFL)*df_lim^n_(i  )!
-    !-------------------------------------------------------------------------!
-
-
+    real,   intent(in):: CFLIn        !Time step * acceleration rate/(Dlnp)
+    integer,intent(in):: nP           !Number of meshes along lnp-coordinate
+    integer,intent(in):: nGCLeft      !The solution in the ghost cells is not
+    integer,intent(in):: nGCRight     !advanced in time but used as the BCs
+    real,intent(inout):: FInOut_I(1-nGCLeft:nP+nGCRight)  !Solution
+    !sol. index 0 is to set boundary condition at the injection energy
+    logical,intent(in):: IsConservative  !Solve (C) if .true. (NC) otherwise
+    real,optional,intent(in)::DeltaLnP   !Used only for NonConservative=.true.!
+    !Loop variables
+    integer :: iP, iStep 
+    !Subcycling to achive the condition CFL<1, if nStep>1
+    integer :: nStep
+    !Extended version of the sulution array to implement BCc
+    real    :: F_I(1 - max(nGCLeft,2):nP+max(nGCRight,2))
+    real    :: FSemiintUp_I(0:nP+1), FSemiintDown_I(0:nP+1)
+    real    :: CFL, HalfADtIfNeeded
+    !-------------------------NonConservative---------------------------
+    ! This is a one-stage second-order scheme for the advection equation: 
+    !             f_t+A f_{ln p}=0, 
+    ! 
+    ! Herewith CFL = A*Delta t/Delta(ln p):
+    ! f^(n+1)_i-CFL*(f^n_(i-1/2)-f^n_(i+1/2)=f^n_i, where
+    ! For CFL>0:
+    ! f^n_(i-1/2)=f^n_(i-1)+cHalf*(cOne-CFL)*df_lim^n_(i-1)
+    ! For CFL<0:
+    ! f^n_(i-1/2)=f^n_(i  )-cHalf*(cOne+CFL)*df_lim^n_(i  )
+    !-------------------------   Conservative---------------------------
+    ! This is a one-stage second-order scheme for the advection equation:
+    !             f_t+A (f p}_p=0, 
+    ! We now need to use both 
+    ! A*Delta t = CFLIn*Delta (LnP) 
+    ! and
+    ! CFL = A*Delta t/(2 tanh(Delta (ln p)/2)
+    ! f^(n+1)_i = CFL*(f^n_(i-1/2)-f^n_(i+1/2))- 
+    !        (1/2)(f^n_(i-1/2)+f^n_(i+1/2))A*Delta t + f^n_i,  
+    !where 
+    ! For CFL>0:
+    !f^n_(i-1/2)=f^n_(i-1)*(1-(1/2)*A*Delta t)+cHalf*(cOne-CFL)*df_lim^n_(i-1)
+    ! For CFL<0:
+    !f^n_(i-1/2)=f^n_(i  )*(1-(1/2)*A*Delta t)-cHalf*(cOne+CFL)*df_lim^n_(i  )
 
     if(IsConservative)then
-       HalfADtIfNeeded=0.50*CFLIn*DeltaLnP
-       CFL=HalfADtIfneeded/tanh(0.50 * DeltaLnP)
-       HalfADtIfNeeded=-HalfADtIfNeeded
+       HalfADtIfNeeded = 0.50*CFLIn*DeltaLnP
+       CFL = HalfADtIfneeded/tanh(0.50 * DeltaLnP)
+       HalfADtIfNeeded = -HalfADtIfNeeded
     else
-       HalfADtIfNeeded=0.0; CFL=CFLIn
+       HalfADtIfNeeded = 0.0; CFL = CFLIn
     end if
 
-    nStep=1+int(CFL); CFL=CFL/real(nStep)
+    nStep = 1 + int(CFL); CFL = CFL/real(nStep)
     HalfADtIfNeeded=HalfADtIfNeeded/real(nStep)
-    F_I(1-nGCLeft:nP+nGCRight)=FInOut_I(1-nGCLeft:nP+nGCRight)
+    F_I(1-nGCLeft:nP+nGCRight) = FInOut_I(1 - nGCLeft:nP+nGCRight)
 
     !Check for positivity
     if(any(F_I(1-nGCLeft:nP+nGCRight)<=0.0))then
