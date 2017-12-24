27d4a624185fd362b29992f6a91938f436e4d5d6
igorsok <>
2017-12-24 08:22:00 +0000

    Add functions for tramsforming between momentum and energy for fixed choice of partcles ("protons").
    Former-commit-id: 545319466bf4ddd87637a43f9369db276592096d

--- SP/MFLAMPA/src/ModUnit.f90
+++ SP/MFLAMPA/src/ModUnit.f90
@@ -4,11 +4,19 @@
 !=============================================================!
 module SP_ModUnit
   use SP_ModGrid, ONLY: nVar, LagrID_, FluxMax_
+  use ModConst,   ONLY: &
+       gen_kin_energy_to_momentum=>kinetic_energy_to_momentum, &
+       gen_momentum_to_kin_energy=>momentum_to_kinetic_energy, &
+       gen_momentum_to_energy    =>momentum_to_energy             
   implicit none
+  SAVE
+  private !Except
+  public :: kinetic_energy_to_momentum, momentum_to_energy, &
+       momentum_to_kinetic_energy, NameEUnit
   !\
   ! unit of SEP energy is also applicable for ion temperature
   character(len=*), parameter :: NameEUnit = 'kev'
-  real                        :: UnitEnergy
+  real, public                :: UnitEnergy
   ! simulated particles, used for converting momentum to energy
   ! is back. For different sorts of ions the sqrt(A) factor needs
   ! to be used in these relations.
@@ -45,4 +53,25 @@ module SP_ModUnit
        'p.f.u.', &
        'p.f.u.', &
        '??????'  /)
+contains
+  real function kinetic_energy_to_momentum(Energy)
+    real, intent(in) :: Energy
+    !---------------
+    kinetic_energy_to_momentum = &
+         gen_kin_energy_to_momentum(Energy, NameParticle)
+  end function kinetic_energy_to_momentum
+  !======================================
+  real function momentum_to_energy(Momentum)
+    real, intent(in) :: Momentum
+    !---------------
+    momentum_to_energy = &
+         gen_momentum_to_energy(Momentum, NameParticle)
+  end function momentum_to_energy
+  !======================================
+  real function momentum_to_kinetic_energy(Momentum)
+    real, intent(in) :: Momentum
+    !---------------
+    momentum_to_kinetic_energy = &
+         gen_momentum_to_kin_energy(Momentum, NameParticle)
+  end function momentum_to_kinetic_energy
 end module SP_ModUnit
