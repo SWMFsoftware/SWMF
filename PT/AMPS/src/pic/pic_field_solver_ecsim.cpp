03ff39874919f4657a50d367c8b9d9c577e174e7
vtenishe <>
2018-01-18 19:57:40 +0000

    functionality of the field solver ECSIM
    Former-commit-id: e35935dc522f9b0e10eaa14e1e42e95b06e3a7dd

---
+++ PT/AMPS/src/pic/pic_field_solver_ecsim.cpp
@@ -0,0 +1,104 @@
+//$Id$
+//the field solver routines
+
+/*
+ * pic_field_solver_ecsim.cpp
+ *
+ *  Created on: Jan 18, 2018
+ *      Author: vtenishe
+ */
+
+#include "pic.h"
+
+PIC::FieldSolver::Electromagnetic::ECSIM::fSetIC PIC::FieldSolver::Electromagnetic::ECSIM::SetIC=PIC::FieldSolver::Electromagnetic::ECSIM::SetIC_default;
+
+//The global initialization procedure
+void PIC::FieldSolver::Init() {
+  switch (_PIC_FILED_SOLVER_MODE_) {
+  case _PIC_FIELD_SOLVER_MODE__ELECTROMAGNETIC__ECSIM_:
+    PIC::FieldSolver::Electromagnetic::ECSIM::Init();
+    break;
+  default:
+    exit(__LINE__,__FILE__,"Error: The field solver Init() has been called with an unknown _PIC_FILED_SOLVER_MODE_ value");
+  }
+}
+
+//Field Solver of IPIC3D
+void PIC::FieldSolver::Electromagnetic::ECSIM::Init() {
+  //init the electric and magnetic field offsets
+  //Magnetic field is in the center nodes
+  //Electric field is in the corner nodes
+
+  if (PIC::CPLR::DATAFILE::Offset::MagneticField.active==true) {
+    exit(__LINE__,__FILE__,"Error: reinitialization of the magnetic field offset");
+  }
+  else {
+    PIC::CPLR::DATAFILE::Offset::MagneticField.active=true;
+    PIC::CPLR::DATAFILE::Offset::MagneticField.RelativeOffset=PIC::Mesh::cDataCenterNode::totalAssociatedDataLength;
+    PIC::Mesh::cDataCenterNode::totalAssociatedDataLength+=PIC::CPLR::DATAFILE::Offset::MagneticField.nVars*sizeof(double);
+  }
+
+  if (PIC::CPLR::DATAFILE::Offset::ElectricField.active==true) {
+    exit(__LINE__,__FILE__,"Error: reinitialization of the electric field offset");
+  }
+  else {
+    PIC::CPLR::DATAFILE::Offset::ElectricField.active=true;
+    PIC::CPLR::DATAFILE::Offset::ElectricField.RelativeOffset=PIC::Mesh::cDataCornerNode::totalAssociatedDataLength;
+    PIC::Mesh::cDataCornerNode::totalAssociatedDataLength+=PIC::CPLR::DATAFILE::Offset::ElectricField.nVars*sizeof(double);
+  }
+}
+
+void PIC::FieldSolver::Electromagnetic::ECSIM::Init_IC() {
+  //set the initial conditions
+  SetIC();
+}
+
+//set default initial conditions
+void  PIC::FieldSolver::Electromagnetic::ECSIM::SetIC_default() {
+  int i,j,k,iNode,idim;
+  cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *node;
+  PIC::Mesh::cDataCenterNode *CenterNode;
+  PIC::Mesh::cDataCornerNode *CornerNode;
+  PIC::Mesh::cDataBlockAMR *block;
+  double *E,*B;
+
+  if (PIC::CPLR::DATAFILE::Offset::ElectricField.active==false) exit(__LINE__,__FILE__,"Error: the electric field offset is not active");
+  if (PIC::CPLR::DATAFILE::Offset::MagneticField.active==false) exit(__LINE__,__FILE__,"Error: the magnetic field offset is not active");
+
+  //loop through all blocks
+  for (int iNode=0;iNode<DomainBlockDecomposition::nLocalBlocks;iNode++) {
+    node=DomainBlockDecomposition::BlockTable[iNode];
+    block=node->block;
+
+    if (block!=NULL) {
+      //set the electric field (corner nodes)
+      for (i=0;i<_BLOCK_CELLS_X_+1;i++) for (j=0;j<_BLOCK_CELLS_Y_+1;j++) for (k=0;k<_BLOCK_CELLS_Z_+1;k++) {
+        CornerNode=block->GetCornerNode(PIC::Mesh::cDataBlockAMR::getCornerNodeLocalNumber(i,j,k));
+
+        if (CornerNode!=NULL) {
+          E=(double*)(CornerNode->GetAssociatedDataBufferPointer()+PIC::CPLR::DATAFILE::Offset::ElectricField.RelativeOffset);
+          for (idim=0;idim<3;idim++) E[idim]=0.0;
+        }
+      }
+
+      //set the magnetic field (center nodes)
+      for (i=0;i<_BLOCK_CELLS_X_;i++) for (j=0;j<_BLOCK_CELLS_Y_;j++) for (k=0;k<_BLOCK_CELLS_Z_;k++) {
+        CenterNode=block->GetCenterNode(PIC::Mesh::cDataBlockAMR::getCenterNodeLocalNumber(i,j,k));
+
+        if (CenterNode!=NULL) {
+          B=(double*)(CenterNode->GetAssociatedDataBufferPointer()+PIC::CPLR::DATAFILE::Offset::MagneticField.RelativeOffset);
+          for (idim=0;idim<3;idim++) B[idim]=0.0;
+        }
+      }
+    }
+  }
+
+  //update the 'ghost' cells and 'ghost' blocks
+  switch (_PIC_BC__PERIODIC_MODE_) {
+  case _PIC_BC__PERIODIC_MODE_ON_:
+    PIC::BC::ExternalBoundary::Periodic::UpdateData();
+    break;
+  default:
+    PIC::Mesh::mesh.ParallelBlockDataExchange();
+  }
+}
