8f59be34f7dfa0c591be28a16f16d733c4574a4a
vtenishe <>
2018-01-27 01:57:46 +0000

    declarations needed for the namespace 'Exosphere' have been added
    Former-commit-id: e1a73418f6fca108941df64bfc9bfae5e245f5ba

--- PT/AMPS/test/srcPeriodicBCTest/main_lib.cpp
+++ PT/AMPS/test/srcPeriodicBCTest/main_lib.cpp
@@ -26,4 +26,17 @@
 #include "meshAMRcutcell.h"
 #include "cCutBlockSet.h"
 
+#include "pic.h"
+#include "Exosphere.h"
+
+double Exosphere::OrbitalMotion::GetTAA(SpiceDouble et) {return 0.0;}
+int Exosphere::ColumnIntegral::GetVariableList(char *vlist) {return 0;}
+void Exosphere::ColumnIntegral::ProcessColumnIntegrationVector(double *res,int resLength) {}
+double Exosphere::GetSurfaceTemeprature(double cosSubsolarAngle,double *x_LOCAL_SO_OBJECT) {return 0.0;}
+char Exosphere::SO_FRAME[_MAX_STRING_LENGTH_PIC_]="GALL_EPHIOD";
+char Exosphere::ObjectName[_MAX_STRING_LENGTH_PIC_]="Europa";
+void Exosphere::ColumnIntegral::CoulumnDensityIntegrant(double *res,int resLength,double* x,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node) {}
+double Exosphere::SurfaceInteraction::StickingProbability(int spec,double& ReemissionParticleFraction,double Temp) {return 0.0;}
+
+const double DomainLength[3]={1.0E8,1.0E8,1.0E8},DomainCenterOffset[3]={0.0,0.0,0.0};
 
