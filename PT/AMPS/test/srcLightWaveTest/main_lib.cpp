8d625d9da032412f38b6eacae38ab66f682c00aa
vtenishe <>
2018-02-09 21:45:17 +0000

    a missing declaration needded for namespace Exosphere has been added
    Former-commit-id: d120e4eb7fd2d719dbc30e484928caa35a830ba2

--- PT/AMPS/test/srcLightWaveTest/main_lib.cpp
+++ PT/AMPS/test/srcLightWaveTest/main_lib.cpp
@@ -34,6 +34,7 @@ int Exosphere::ColumnIntegral::GetVariableList(char *vlist) {return 0;}
 void Exosphere::ColumnIntegral::ProcessColumnIntegrationVector(double *res,int resLength) {}
 double Exosphere::GetSurfaceTemeprature(double cosSubsolarAngle,double *x_LOCAL_SO_OBJECT) {return 0.0;}
 char Exosphere::SO_FRAME[_MAX_STRING_LENGTH_PIC_]="GALL_EPHIOD";
+char Exosphere::IAU_FRAME[_MAX_STRING_LENGTH_PIC_]="GALL_EPHIOD";
 char Exosphere::ObjectName[_MAX_STRING_LENGTH_PIC_]="Europa";
 void Exosphere::ColumnIntegral::CoulumnDensityIntegrant(double *res,int resLength,double* x,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node) {}
 double Exosphere::SurfaceInteraction::StickingProbability(int spec,double& ReemissionParticleFraction,double Temp) {return 0.0;}
