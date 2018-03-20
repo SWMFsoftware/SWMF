a5293d7f72bb4d00c35ac72756a3d54d027aa414
gtoth <>
2018-03-20 15:06:35 +0000

    Minor improvements. No need to write out a "scalar parameter".
    Former-commit-id: c9652a1416daae7ea3eb4ca33ede74b7e02b8a53

--- IM/CIMI/src/plot_fieldline.f90
+++ IM/CIMI/src/plot_fieldline.f90
@@ -1,30 +1,29 @@
-subroutine IM_plot_fieldline(nAlt,iLat,iLon,tSimulation,Length_I,RadialDist_I,Bfield_I)
+subroutine IM_plot_fieldline(nAlt, iLat, iLon, tSimulation, &
+     Length_I,RadialDist_I,Bfield_I)
 
   use ModIoUnit, ONLY: UnitTmp_
-  use ModNumConst, ONLY:cDegToRad
+
   implicit none
   
   integer, intent(in) :: nAlt,iLat,iLon
-  real   , intent(in) :: tSimulation,Length_I(nAlt),RadialDist_I(nAlt),Bfield_I(nAlt)
+  real   , intent(in) :: tSimulation
+  real   , intent(in) :: Length_I(nAlt),RadialDist_I(nAlt),Bfield_I(nAlt)
+
   integer :: i,iTimeOut
-  real :: Gamma = 5.0/3.0 
-  Character(len=100) :: NameFieldLine
-!-----------------------------------------------------------------------------
+  character(len=100) :: NameFieldLine
+  !---------------------------------------------------------------------------
 
   iTimeOut=int(tSimulation)
   write(NameFieldLine,"(a,i2.2,i2.2,a,i8.8,a)") &
        'IM/FieldLine',iLat,iLon,'_',iTimeOut,'.idl'
   open(UnitTmp_,FILE=NameFieldLine)
-
   write (UnitTmp_,"(a79)") 'RB_line'
-  write (UnitTmp_,"(i8,1pe13.5,3i3)") 1,0.0,1,1,2
-  write (UnitTmp_,"(3i4)") nAlt
-  write (UnitTmp_,"(100(1pe13.5))") Gamma
-  write (UnitTmp_,"(a79)")&
-       'Length Radius Bfield g'
-  do i=1,nAlt  
-     WRITE (UnitTmp_,"(100es18.10)") &
-          Length_I(i),RadialDist_I(i),Bfield_I(i)
+  write (UnitTmp_,"(i8,es13.5,3i3)") iTimeOut, tSimulation, 1, 0, 2
+  write (UnitTmp_,"(i4)") nAlt
+  write (UnitTmp_,"(a79)") 'Length Radius Bfield'
+  do i = 1, nAlt  
+     write(UnitTmp_,"(3es18.10)") &
+          Length_I(i), RadialDist_I(i), Bfield_I(i)
   enddo
   close(UnitTmp_)
 
