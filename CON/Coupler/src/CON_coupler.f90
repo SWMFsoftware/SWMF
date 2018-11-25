3af4f2b6b4193dbdeca350a1d8edfc380865e0e9
Gabor Toth <gtoth@umich.edu>
2018-11-24 22:00:35 -0500

    Fixed MaxCouple (found by Yuxi)
    Former-commit-id: 14006880d575fc08eb7b40f81a0f2056a55466bf

--- CON/Coupler/src/CON_coupler.f90
+++ CON/Coupler/src/CON_coupler.f90
@@ -57,7 +57,7 @@ module CON_coupler
 
   public :: MaxCouple           ! Maximum number of couplings
 
-  integer, parameter :: MaxCouple = 30
+  integer, parameter :: MaxCouple = 31
 
   public :: nCouple             ! Actual number of couplings
 
