51ebee5dac12e70e6cc3a28324f112ed1395b2ee
Gabor Toth <gtoth1@pfe24.nas.nasa.gov>
2020-04-10 11:40:08 -0700

    Let's have something here so it is not absolutely necessary to create this file. On Pleiades it can be slow.
    Former-commit-id: 5cb94eee658847ed39fdb76a08917512d212c93e

---
+++ CON/Control/src/show_git_info.h
@@ -0,0 +1,37 @@
+
+  write(*,'(100(a/))') &
+       'Main dir=/nobackupp8/gtoth1/Tmp/SWMF', &
+       'Git repo=git@gitlab.umich.edu:swmf_software/SWMF original 2020-04-06 7bc3490   ', &
+       '  BATL                              original 2020-04-09 aa22982   ', &
+       '  BATL/share                        original 2020-04-09 8f74b29   ', &
+       '  BATL/share/Python                 original 2020-04-08 6c9c58a   ', &
+       '  BATL/src                          original 2020-04-05 0e42f9d   ', &
+       '  BATL/util                         original 2020-04-07 ae3b13f   ', &
+       '  CZ/FSAM                           original 2020-03-28 d37cc4b   ', &
+       '  EE/BATSRUS/dataCRASH              original 2018-05-22 de91868   ', &
+       '  GM/BATSRUS                        original 2020-04-09 cfead39   ', &
+       '  GM/BATSRUS/dataCRASH              original 2018-05-22 de91868   ', &
+       '  GM/BATSRUS/srcBATL                original 2020-04-05 0e42f9d   ', &
+       '  IE/Ridley_serial                  original 2020-01-22 41ea7b6   ', &
+       '  IH/BATSRUS/dataCRASH              original 2018-05-22 de91868   ', &
+       '  IM/CIMI                           original 2020-01-22 24a8165   ', &
+       '  IM/HEIDI                          original 2020-01-22 e7e5138   ', &
+       '  IM/RCM2                           original 2020-03-28 2f4f199   ', &
+       '  OH/BATSRUS/dataCRASH              original 2018-05-22 de91868   ', &
+       '  PC/ALTOR                          original 2020-04-01 69aa5da   ', &
+       '  PC/ALTOR/srcBATL_orig             original 2020-04-05 0e42f9d   ', &
+       '  PC/AMPS                           original 2020-04-09 fa1a5a6   ', &
+       '  PC/FLEKS                          original 2020-03-28 ef13004   ', &
+       '  PC/IPIC3D2                        original 2020-03-29 0aeb13b   ', &
+       '  PS/DGCPM                          original 2020-01-22 811a0a0   ', &
+       '  PT/AMPS                           original 2020-04-09 fa1a5a6   ', &
+       '  PW/PWOM                           original 2020-03-28 8816523   ', &
+       '  RB/RBE                            original 2020-03-28 4f4f6cc   ', &
+       '  SC/BATSRUS/dataCRASH              original 2018-05-22 de91868   ', &
+       '  SP/MFLAMPA                        original 2020-03-28 c9c6f12   ', &
+       '  UA/GITM2                          original 2020-04-04 737ad00   ', &
+       '  share                             original 2020-04-09 8f74b29   ', &
+       '  share/Python                      original 2020-04-08 6c9c58a   ', &
+       '  util                              original 2020-04-07 ae3b13f   ', &
+       '  util/AMREX                        original 2020-01-23 cad368fa5 ', &
+       '  util/HYPRE                        original 2018-06-19 9f1e885   '
