618db2aaca976cc897edc8b7bdb8846e4da42f29
Igor V. Sokolov <igorsok@umich.edu>
2020-07-21 04:14:56 -0400

    Remove acidentally added files
    Former-commit-id: 70aa83f24c40a2a118ca9e694e8defc794ed5c4b

--- a.f90
+++
@@ -1,9 +0,0 @@
-program test_array
-  implicit none
-  integer, parameter :: iM_II(5,5)=reshape([ 3, 7, 5,21, 8,&
-                                            16, 8,17,53, 7,&
-                                            14, 6,35,18, 1,&
-                                            13,19, 4,22,11,&
-                                             9,21, 1,23,12],[5,5])
-  write(*,'(4i5)')iM_II(2:5,[1,2,3,5])
-end program test_array
