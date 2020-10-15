aea0a58f64532dcd6ab0367c8d2d2e45aba5b19f
Toth <gtoth@mesh.engin.umich.edu>
2020-10-15 16:37:23 -0400

    Replaced with separate repository. Not part of open source.

--- SP/FLAMPA/src/stdout.h
+++
@@ -1,6 +0,0 @@
-!  Copyright (C) 2002 Regents of the University of Michigan, portions used with permission 
-!  For more information, see http://csem.engin.umich.edu/tools/swmf
-       common/SP_stdout/iStdOut, DoWriteAll,prefix
-       integer iStdOut
-       character*4 prefix
-       logical DoWriteAll
