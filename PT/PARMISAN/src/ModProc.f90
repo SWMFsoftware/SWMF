9d532e5806afce4ebc4fb3588a64ebbd696987f0
Igor V. Sokolov <igorsok@umich.edu>
2024-04-20 18:08:37 -0400

    Particle AR+MI Solver on Advected Nodes

---
+++ PT/PARMISAN/src/ModProc.f90
@@ -0,0 +1,14 @@
+!  Copyright (C) 2002 Regents of the University of Michigan,
+!  portions used with permission
+!  For more information, see http://csem.engin.umich.edu/tools/swmf
+module PT_ModProc
+  implicit none
+  SAVE
+
+  ! MPI information
+  !----------------------------------------------------------------------------
+  integer:: iComm = -1
+  integer:: iProc = -1
+  integer:: nProc = -1
+end module PT_ModProc
+!==============================================================================
